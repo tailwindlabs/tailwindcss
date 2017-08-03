49fdc9d41cc788ea75104a5563464130b0fa1891
Jonathan Reinink <jonathan@reinink.ca>
2017-08-03 15:53:23 -0400

    Add paper airplane logo.

--- docs/source/_layouts/master.blade.php
+++ docs/source/_layouts/master.blade.php
@@ -9,9 +9,16 @@
         <script type="text/javascript" src="/js/prism.js" defer=""></script>
     </head>
     <body class="full-height flex-col">
-        <header class="p-6 bg-primary">
-            <a class="text-light text-lg" href="/">Tailwind CSS</a>
-            <div class="text-primary-light text-xs">A utility-first CSS framework for rapid UI development</div>
+        <header class="px-6 py-5 bg-primary">
+            <div class="flex-y-center">
+                <svg class="mr-2" style="height: 34px; width: auto;" xmlns="http://www.w3.org/2000/svg" viewBox="173.345 95.819 442.976 281.173">
+                    <path fill="#ffffff" d="M173.345 188.574l100.745 78.263-.216 110.155 80.33-53.23-28.388-26.996 111.5 80.226L616.32 95.82l-442.975 92.754zM576.58 122.36L331.837 278.67l-39.912 67.82.622-86.155 284.03-137.974z"/>
+                </svg>
+                <div>
+                    <a class="text-light text-lg" href="/">Tailwind CSS</a>
+                    <div class="text-primary-light text-xs">A utility-first CSS framework for rapid UI development</div>
+                </div>
+            </div>
         </header>
         <div class="flex flex-fill">
             <nav class="px-6 pb-6 bg-light-softer text-light text-sm leading-loose" style="flex: 0 0 12rem;">
