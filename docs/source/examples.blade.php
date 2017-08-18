e20faf8f1c5f5c7f396fcd2ab1faea0f03dcea47
Adam Wathan <adam.wathan@gmail.com>
2017-08-18 15:56:57 -0400

    Use flex for horizontal card content spacing

--- docs/source/examples.blade.php
+++ docs/source/examples.blade.php
@@ -93,8 +93,8 @@
         <div class="rounded-l w-128 text-center overflow-hidden">
             <img class="h-64" src="/img/card-left.jpg" alt="">
         </div>
-        <div class="border-t border-r border-b border-dark-softer rounded-r p-4">
-            <div class="mb-12">
+        <div class="border-t border-r border-b border-dark-softer rounded-r p-4 flex-col flex-spaced">
+            <div>
                 <p class="text-sm text-dark-softer flex-y-center">
                     <svg class="text-dark-softest w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z"/></svg>
                     Members only
