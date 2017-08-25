ddaaa2a7f89501aee518d6773b9422e63e3f9143
Adam Wathan <adam.wathan@gmail.com>
2017-08-24 21:25:19 -0400

    Add support for object based custom media queries

--- src/tailwind.js
+++ src/tailwind.js
@@ -27,9 +27,23 @@ function findMixin(css, mixin) {
 }
 
 function addCustomMediaQueries(css, breakpoints) {
+  function buildMediaQuery(breakpoint) {
+    if (_.isString(breakpoint)) {
+      breakpoint = { min: breakpoint }
+    }
+    return _(breakpoint).toPairs().map(([feature, value]) => {
+      feature = _.get({
+        min: 'min-width',
+        max: 'max-width',
+      }, feature, feature)
+
+      return `(${feature}: ${value})`
+    }).join(' and ')
+  }
+
   Object.keys(breakpoints).forEach(breakpoint => {
     const variableName = `--breakpoint-${breakpoint}`
-    const mediaQuery = `(min-width: ${breakpoints[breakpoint]})`
+    const mediaQuery = buildMediaQuery(breakpoints[breakpoint])
     const rule = postcss.atRule({
       name: 'custom-media',
       params: `${variableName} ${mediaQuery}`
