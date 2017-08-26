b33c76efe46ef63c7253dc7ce3fcfe98ce6467e2
Adam Wathan <adam.wathan@gmail.com>
2017-08-26 11:16:50 -0400

    Switch to two spaces in build.js; tweak formatting

--- build.js
+++ build.js
@@ -2,21 +2,21 @@ console.info('Building Tailwind!')
 
 const fs = require('fs')
 const postcss = require('postcss')
-const tailwind = require('./src/tailwind')
 const defaultConfig = require('./src/default-config')
 
 fs.readFile('./src/tailwind.css', (err, css) => {
-    postcss([tailwind(defaultConfig)])
-        .process(css, {
-            from: './src/tailwind.css',
-            to: './dist/tailwind.css',
-            map: {inline: false},
-        })
-        .then(result => {
-            fs.writeFileSync('dist/tailwind.css', result.css)
-            if (result.map)
-                fs.writeFileSync('dist/tailwind.css.map', result.map)
-        })
+  postcss([
+    require('./src/tailwind')(defaultConfig),
+  ]).process(css, {
+    from: './src/tailwind.css',
+    to: './dist/tailwind.css',
+    map: {inline: false},
+  }).then(result => {
+    fs.writeFileSync('dist/tailwind.css', result.css)
+    if (result.map) {
+      fs.writeFileSync('dist/tailwind.css.map', result.map)
+    }
+  })
 })
 
 console.log('Finished Building Tailwind!')
