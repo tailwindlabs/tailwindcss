2759872ce05575f6d5bc45a5df9f071d8763c089
Adam Wathan <adam.wathan@gmail.com>
2017-08-26 23:34:04 -0400

    Convert all property values to strings
    Allows using unquoted numeric values. Tricky to test because only
    breaks when trying to run the whole thing through cssnext in the full
    chain. Will figure it out later :)

--- src/util/defineClass.js
+++ src/util/defineClass.js
@@ -5,7 +5,7 @@ export default function(className, properties) {
   const decls = _.map(properties, (value, property) => {
     return postcss.decl({
       prop: _.kebabCase(property),
-      value: value,
+      value: `${value}`,
     })
   })
 
