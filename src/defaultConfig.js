148dac12ebfde846b29c9cc0d7e674959bc7a9b6
Adam Wathan <adam.wathan@gmail.com>
2017-08-27 17:58:28 -0400

    Add PostCSS spacing utilities

--- src/defaultConfig.js
+++ src/defaultConfig.js
@@ -294,6 +294,21 @@ export default {
       'screen': '100vh'
     }
   },
+  spacing: {
+    common: {
+      'px': '1px',
+      '0': '0',
+      '1': '0.25rem',
+      '2': '0.5rem',
+      '3': '0.75rem',
+      '4': '1rem',
+      '6': '1.5rem',
+      '8': '2rem',
+    },
+    padding: {},
+    margin: {},
+    pull: {},
+  },
   constrain: {
     'xs': '20rem',
     'sm': '30rem',
