7f506ff0418a19aa0b790638d847918623662708
Adam Wathan <adam.wathan@gmail.com>
2017-08-26 23:37:13 -0400

    Refactor shadow generator to map object directly instead of converting to pairs

--- src/generators/shadows.js
+++ src/generators/shadows.js
@@ -2,12 +2,9 @@ import _ from 'lodash'
 import defineClass from '../util/defineClass'
 
 export default function({shadows}) {
-  return _(shadows)
-    .toPairs()
-    .map(([className, shadow]) => {
-      return defineClass(`shadow-${className}`, {
-        boxShadow: shadow,
-      })
+  return _.map(shadows, (shadow, className) => {
+    return defineClass(`shadow-${className}`, {
+      boxShadow: shadow,
     })
-    .value()
+  })
 }
