8163761b7df60219bb45ce4f70eab15dccd680d9
Adam Wathan <adam.wathan@gmail.com>
2017-08-27 20:59:20 -0400

    Make background colors hoverable by default

--- src/generators/backgroundColors.js
+++ src/generators/backgroundColors.js
@@ -1,11 +1,12 @@
 import _ from 'lodash'
 import defineClass from '../util/defineClass'
 import normalizeColorList from '../util/normalizeColorList'
+import hoverable from '../util/hoverable'
 
-export default function ({ colors, backgroundColors }) {
-  return _.map(normalizeColorList(backgroundColors, colors), (color, className) => {
+export default function ({ colors, backgrounds }) {
+  return hoverable(_.map(normalizeColorList(backgrounds.colors, colors), (color, className) => {
     return defineClass(`bg-${className}`, {
       'background-color': color,
     })
-  })
+  }))
 }
