b936532414e1cd138c86f18080b7b1874b1384c1
Adam Wathan <adam.wathan@gmail.com>
2017-08-27 21:01:08 -0400

    Make border colors hoverable by default

--- src/generators/borderColors.js
+++ src/generators/borderColors.js
@@ -1,13 +1,14 @@
 import _ from 'lodash'
 import defineClass from '../util/defineClass'
 import normalizeColorList from '../util/normalizeColorList'
+import hoverable from '../util/hoverable'
 
 export default function ({ colors, borders }) {
   const borderColors = normalizeColorList(borders.colors, colors)
 
-  return _.map(borderColors, (color, className) => {
+  return hoverable(_.map(borderColors, (color, className) => {
     return defineClass(`border-${className}`, {
       'border-color': color,
     })
-  })
+  }))
 }
