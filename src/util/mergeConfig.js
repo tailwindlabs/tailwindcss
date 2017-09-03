50ad9dba2b6efa695e0aaca392251c95df50021d
Adam Wathan <adam.wathan@gmail.com>
2017-09-03 09:28:16 -0400

    Refactor config merging to be less absolute trash
    Needs better tests still.

--- src/util/mergeConfig.js
+++ src/util/mergeConfig.js
@@ -1,119 +1,67 @@
 import _ from 'lodash'
 
-function normalizeColorList(colors) {
-  if (_.isArray(colors)) {
-    colors = _(colors).flatMap(color => {
-      if (_.isString(color)) {
-        return [[color, color]]
-      }
-      return _.toPairs(color)
-    }).fromPairs().value()
-  }
-
-  return _.toPairs(colors)
+const configTemplate = {
+  breakpoints: null,
+  colors: null,
+  text: {
+    fonts: null,
+    sizes: null,
+    weights: null,
+    leading: null,
+    tracking: null,
+    colors: null
+  },
+  backgrounds: {
+    colors: null,
+  },
+  borders: {
+    defaults: null,
+    widths: null,
+    rounded: {
+      default: null,
+      modifiers: null
+    },
+    colors: null
+  },
+  sizing: {
+    common: null,
+    width: null,
+    height: null
+  },
+  spacing: {
+    common: null,
+    padding: null,
+    margin: null,
+    pull: null
+  },
+  constrain: null,
+  shadows: null,
+  zIndex: null
 }
 
-function normalizeConfig(config) {
-  return {
-    breakpoints: _.toPairs(_.get(config, 'breakpoints', {})),
-    colors: _.toPairs(_.get(config, 'colors', {})),
-    text: {
-      fonts: _.toPairs(_.get(config, 'text.fonts', {})),
-      sizes: _.toPairs(_.get(config, 'text.sizes', {})),
-      weights: _.toPairs(_.get(config, 'text.weights', {})),
-      leading: _.toPairs(_.get(config, 'text.leading', {})),
-      tracking: _.toPairs(_.get(config, 'text.tracking', {})),
-      colors: normalizeColorList(_.get(config, 'text.colors', {})),
-    },
-    backgrounds: {
-      colors: normalizeColorList(_.get(config, 'backgrounds.colors', {})),
-    },
-    borders: {
-      defaults: _.get(config, 'borders.defaults', {}),
-      widths: _.toPairs(_.get(config, 'borders.widths', {})),
-      rounded: {
-        default: _.get(config, 'borders.rounded.default'),
-        modifiers: _.toPairs(_.get(config, 'borders.rounded.modifiers', {})),
-      },
-      colors: normalizeColorList(_.get(config, 'borders.colors', {})),
-    },
-    sizing: {
-      common: _.toPairs(_.get(config, 'sizing.common', {})),
-      width: _.toPairs(_.get(config, 'sizing.width', {})),
-      height: _.toPairs(_.get(config, 'sizing.height', {})),
-    },
-    spacing: {
-      common: _.toPairs(_.get(config, 'spacing.common', {})),
-      padding: _.toPairs(_.get(config, 'spacing.padding', {})),
-      margin: _.toPairs(_.get(config, 'spacing.margin', {})),
-      pull: _.toPairs(_.get(config, 'spacing.pull', {})),
-    },
-    constrain: _.toPairs(_.get(config, 'constrain', {})),
-    shadows: _.toPairs(_.get(config, 'shadows', {})),
-    zIndex: _.toPairs(_.get(config, 'zIndex', {})),
-    extend: _.get(config, 'extend', {})
-  }
+function replaceDefaults(template, defaults, replacements) {
+  return Object.keys(template).reduce((merged, key) => {
+    const value = template[key]
+
+    if (_.isPlainObject(value)) {
+      merged[key] = replaceDefaults(value, _.get(defaults, key), _.get(replacements, key))
+    } else {
+      merged[key] = _.get(replacements, key, _.get(defaults, key))
+    }
+
+    return merged
+  }, {})
 }
 
-function fixPairs(config) {
-  return {
-    breakpoints: _.fromPairs(_.get(config, 'breakpoints', {})),
-    colors: _.fromPairs(_.get(config, 'colors', {})),
-    text: {
-      fonts: _.fromPairs(_.get(config, 'text.fonts', {})),
-      sizes: _.fromPairs(_.get(config, 'text.sizes', {})),
-      weights: _.fromPairs(_.get(config, 'text.weights', {})),
-      leading: _.fromPairs(_.get(config, 'text.leading', {})),
-      tracking: _.fromPairs(_.get(config, 'text.tracking', {})),
-      colors: normalizeColorList(_.get(config, 'text.colors', {})),
-    },
-    backgrounds: {
-      colors: normalizeColorList(_.get(config, 'backgrounds.colors', {})),
-    },
-    borders: {
-      defaults: _.get(config, 'borders.defaults', {}),
-      widths: _.fromPairs(_.get(config, 'borders.widths', {})),
-      rounded: {
-        default: _.get(config, 'borders.rounded.default'),
-        modifiers: _.fromPairs(_.get(config, 'borders.rounded.modifiers', {})),
-      },
-      colors: normalizeColorList(_.get(config, 'borders.colors', {})),
-    },
-    sizing: {
-      common: _.fromPairs(_.get(config, 'sizing.common', {})),
-      width: _.fromPairs(_.get(config, 'sizing.width', {})),
-      height: _.fromPairs(_.get(config, 'sizing.height', {})),
-    },
-    spacing: {
-      common: _.fromPairs(_.get(config, 'spacing.common', {})),
-      padding: _.fromPairs(_.get(config, 'spacing.padding', {})),
-      margin: _.fromPairs(_.get(config, 'spacing.margin', {})),
-      pull: _.fromPairs(_.get(config, 'spacing.pull', {})),
-    },
-    constrain: _.fromPairs(_.get(config, 'constrain', {})),
-    shadows: _.fromPairs(_.get(config, 'shadows', {})),
-    zIndex: _.fromPairs(_.get(config, 'zIndex', {})),
-  }
+function appendConfig(base, appends) {
+  return _.mergeWith({}, base, appends, (baseValue, appendsValue) => {
+    if (_.isArray(baseValue)) {
+      return baseValue.concat(appendsValue);
+    }
+  })
 }
-
 
 export default function mergeConfig(base, other) {
-  const replaced = _.defaultsDeep(
-    {},
-    normalizeConfig(other),
-    normalizeConfig(base),
-  )
-
-  const extended = _.mergeWith(
-    {},
-    replaced,
-    normalizeConfig(_.get(replaced, 'extend', {})),
-    (objValue, srcValue) => {
-      if (_.isArray(objValue)) {
-        return objValue.concat(srcValue);
-      }
-    }
-  )
-
-  return fixPairs(extended)
+  const replaced = replaceDefaults(configTemplate, base, other)
+  return appendConfig(replaced, _.get(other, 'extend', {}))
 }
