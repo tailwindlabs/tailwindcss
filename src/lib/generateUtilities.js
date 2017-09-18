4ca2ec574e887c8d2ec1664d7f799f89ee80e43e
Adam Wathan <adam.wathan@gmail.com>
2017-09-18 18:20:34 -0400

    Switch from '@tailwind utilities' to single '@tailwind-utilities' rule

--- src/lib/generateUtilities.js
+++ src/lib/generateUtilities.js
@@ -32,42 +32,40 @@ import zIndex from '../generators/zIndex'
 
 export default function(options) {
   return function(css) {
-    css.walkAtRules('tailwind', atRule => {
-      if (atRule.params === 'utilities') {
-        const utilities = _.flatten([
-          textSizes(options),
-          textWeights(options),
-          textFonts(options),
-          textColors(options),
-          textLeading(options),
-          textTracking(options),
-          textAlign(options),
-          textWrap(options),
-          textStyle(options),
-          verticalAlign(options),
-          backgroundColors(options),
-          backgroundSize(options),
-          borderWidths(options),
-          borderColors(options),
-          borderStyles(options),
-          rounded(options),
-          display(options),
-          position(options),
-          overflow(options),
-          sizing(options),
-          spacing(options),
-          constrain(options),
-          shadows(options),
-          flex(options),
-          zIndex(options),
-          opacity(options),
-          userSelect(options),
-          resize(options),
-          cursor(options),
-        ])
-        atRule.before(responsive(utilities))
-        atRule.remove()
-      }
+    css.walkAtRules('tailwind-utilities', atRule => {
+      const utilities = _.flatten([
+        textSizes(options),
+        textWeights(options),
+        textFonts(options),
+        textColors(options),
+        textLeading(options),
+        textTracking(options),
+        textAlign(options),
+        textWrap(options),
+        textStyle(options),
+        verticalAlign(options),
+        backgroundColors(options),
+        backgroundSize(options),
+        borderWidths(options),
+        borderColors(options),
+        borderStyles(options),
+        rounded(options),
+        display(options),
+        position(options),
+        overflow(options),
+        sizing(options),
+        spacing(options),
+        constrain(options),
+        shadows(options),
+        flex(options),
+        zIndex(options),
+        opacity(options),
+        userSelect(options),
+        resize(options),
+        cursor(options),
+      ])
+      atRule.before(responsive(utilities))
+      atRule.remove()
     })
   }
 }
