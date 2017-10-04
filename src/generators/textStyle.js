8b3cbbd7a7206172fd77fcb98099eb202379ac12
Jonathan Reinink <jonathan@reinink.ca>
2017-10-03 20:26:49 -0400

    Add text smooth utility.

--- src/generators/textStyle.js
+++ src/generators/textStyle.js
@@ -8,6 +8,10 @@ export default function() {
     'text-capitalize': { 'text-transform': 'capitalize' },
     'text-underline': { 'text-decoration': 'underline' },
     'text-strike': { 'text-decoration': 'line-through' },
+    'text-smooth': {
+      '-webkit-font-smoothing': 'antialiased',
+      '-moz-osx-font-smoothing': 'grayscale',
+    },
     'text-normal': {
       'text-decoration': 'none',
       'text-transform': 'none',
