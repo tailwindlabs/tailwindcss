f3fd0acdfd07d93250d5ef189f20c8350e962e05
Jonathan Reinink <jonathan@reinink.ca>
2017-10-18 17:06:56 -0400

    Rename text wrap utilities

--- src/generators/textWrap.js
+++ src/generators/textWrap.js
@@ -2,13 +2,14 @@ import defineClasses from '../util/defineClasses'
 
 export default function() {
   return defineClasses({
-    'wrap-normal': { 'white-space': 'normal' },
-    'wrap-none': { 'white-space': 'nowrap' },
-    'wrap-pre': { 'white-space': 'pre' },
-    'wrap-pre-line': { 'white-space': 'pre-line' },
-    'wrap-pre-wrap': { 'white-space': 'pre-wrap' },
-    'wrap-force': { 'word-wrap': 'break-word' },
-    'wrap-truncate': {
+    'wrap': { 'white-space': 'normal' },
+    'no-wrap': { 'white-space': 'nowrap' },
+    'pre': { 'white-space': 'pre' },
+    'pre-line': { 'white-space': 'pre-line' },
+    'pre-wrap': { 'white-space': 'pre-wrap' },
+    'break-words': { 'word-wrap': 'break-word' },
+    'break-normal': { 'word-wrap': 'normal' },
+    'truncate': {
       'overflow': 'hidden',
       'text-overflow': 'ellipsis',
       'white-space': 'nowrap',
