8e1fcd72622b7db7f73f14031a56384de941bfcc
Adam Wathan <adam.wathan@gmail.com>
2017-10-19 12:00:14 -0400

    Generate minified file on build
    Useful for checking gzip size and making sure we are still reasonable.
    Currently at 19.6kb 🤙🏻

--- src/build.js
+++ src/build.js
@@ -2,6 +2,7 @@ import fs from 'fs'
 import postcss from 'postcss'
 import tailwind from '..'
 import defaultConfig from './defaultConfig'
+import CleanCSS from 'clean-css'
 
 console.info('Building Tailwind!')
 
@@ -10,11 +11,18 @@ fs.readFile('./css/tailwind.css', (err, css) => {
     .process(css, {
       from: './css/tailwind.css',
       to: './dist/tailwind.css',
-      map: {inline: false},
+      map: { inline: false },
     })
     .then(result => {
       fs.writeFileSync('./dist/tailwind.css', result.css)
-      if (result.map) fs.writeFileSync('./dist/tailwind.css.map', result.map)
+      if (result.map) {
+        fs.writeFileSync('./dist/tailwind.css.map', result.map)
+      }
+      return result
+    })
+    .then(result => {
+      const minified = new CleanCSS().minify(result.css)
+      fs.writeFileSync('./dist/tailwind.min.css', minified.styles)
     })
     .catch(error => console.log(error))
 })
