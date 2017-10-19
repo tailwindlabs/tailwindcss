323855eb83b3546a9eca6c3cd13d98430f8508d8
Adam Wathan <adam.wathan@gmail.com>
2017-10-19 12:20:38 -0400

    Use grim uncreative white-space utility names
    Because CSS is terrible and there's no way to pretty this up no matter
    how hard I try.

--- src/generators/textWrap.js
+++ src/generators/textWrap.js
@@ -2,13 +2,15 @@ import defineClasses from '../util/defineClasses'
 
 export default function() {
   return defineClasses({
-    'wrap': { 'white-space': 'normal' },
-    'no-wrap': { 'white-space': 'nowrap' },
-    'pre': { 'white-space': 'pre' },
-    'pre-line': { 'white-space': 'pre-line' },
-    'pre-wrap': { 'white-space': 'pre-wrap' },
+    'white-space-normal': { 'white-space': 'normal' },
+    'white-space-nowrap': { 'white-space': 'nowrap' },
+    'white-space-pre': { 'white-space': 'pre' },
+    'white-space-pre-line': { 'white-space': 'pre-line' },
+    'white-space-pre-wrap': { 'white-space': 'pre-wrap' },
+
     'break-words': { 'word-wrap': 'break-word' },
     'break-normal': { 'word-wrap': 'normal' },
+
     'truncate': {
       'overflow': 'hidden',
       'text-overflow': 'ellipsis',
