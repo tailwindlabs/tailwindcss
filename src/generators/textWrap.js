ee804cb9dcf5aef94cf278bdf5301829f6fe5d32
Adam Wathan <adam.wathan@gmail.com>
2017-10-19 12:31:11 -0400

    Use English for whitespace utilities

--- src/generators/textWrap.js
+++ src/generators/textWrap.js
@@ -2,11 +2,11 @@ import defineClasses from '../util/defineClasses'
 
 export default function() {
   return defineClasses({
-    'white-space-normal': { 'white-space': 'normal' },
-    'white-space-nowrap': { 'white-space': 'nowrap' },
-    'white-space-pre': { 'white-space': 'pre' },
-    'white-space-pre-line': { 'white-space': 'pre-line' },
-    'white-space-pre-wrap': { 'white-space': 'pre-wrap' },
+    'whitespace-normal': { 'white-space': 'normal' },
+    'whitespace-no-wrap': { 'white-space': 'nowrap' },
+    'whitespace-pre': { 'white-space': 'pre' },
+    'whitespace-pre-line': { 'white-space': 'pre-line' },
+    'whitespace-pre-wrap': { 'white-space': 'pre-wrap' },
 
     'break-words': { 'word-wrap': 'break-word' },
     'break-normal': { 'word-wrap': 'normal' },
