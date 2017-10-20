0bbe669cd62169ebd3dd3d31ebfc8ea151329b74
Adam Wathan <adam.wathan@gmail.com>
2017-10-20 12:10:56 -0400

    Remove md text size alias (🖕🏻) and align pixel comments

--- src/defaultConfig.js
+++ src/defaultConfig.js
@@ -85,15 +85,14 @@ module.exports = {
       'mono': '"SFMono-Regular", Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
     },
     sizes: {
-      'base': '1rem', // 16px
-      'xs': '.75rem', // 12px
-      'sm': '.875rem', // 14px
-      'md': '1rem', // 16px
-      'lg': '1.125rem', // 18px
-      'xl': '1.25rem', // 20px
-      '2xl': '1.75rem', // 28px
-      '3xl': '2.375rem', // 38px
-      '4xl': '2.875rem', // 46px
+      'xs': '.75rem',     // 12px
+      'sm': '.875rem',    // 14px
+      'base': '1rem',     // 16px
+      'lg': '1.125rem',   // 18px
+      'xl': '1.25rem',    // 20px
+      '2xl': '1.75rem',   // 28px
+      '3xl': '2.375rem',  // 38px
+      '4xl': '2.875rem',  // 46px
     },
     weights: {
       'hairline': 100,
