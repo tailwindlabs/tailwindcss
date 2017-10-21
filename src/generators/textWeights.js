eba5970810de2c32eda74cdfa9c7b79da3c0b496
Adam Wathan <adam.wathan@gmail.com>
2017-10-21 10:52:39 -0400

    Add hover variants for font weight utilities

--- src/generators/textWeights.js
+++ src/generators/textWeights.js
@@ -1,10 +1,11 @@
 import _ from 'lodash'
 import defineClass from '../util/defineClass'
+import hoverable from '../util/hoverable'
 
 export default function ({ text }) {
-  return _.map(text.weights, (weight, modifier) => {
+  return hoverable(_.map(text.weights, (weight, modifier) => {
     return defineClass(`font-${modifier}`, {
       'font-weight': `${weight}`,
     })
-  })
+  }))
 }
