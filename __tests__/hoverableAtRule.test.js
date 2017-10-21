c989134554728bbc543f1e3ef670ab957a8d09b1
Adam Wathan <adam.wathan@gmail.com>
2017-10-21 11:26:30 -0400

    Add basic test for hoverable at-rule

---
+++ __tests__/hoverableAtRule.test.js
@@ -0,0 +1,25 @@
+import postcss from 'postcss'
+import plugin from '../src/lib/substituteHoverableAtRules'
+
+function run(input, opts = {}) {
+  return postcss([plugin(opts)]).process(input)
+}
+
+test("it adds a hoverable variant to each nested class definition", () => {
+  const input = `
+    @hoverable {
+      .banana { color: yellow; }
+      .chocolate { color: brown; }
+    }
+  `
+
+  const output = `
+      .banana, .hover\\:banana:hover { color: yellow; }
+      .chocolate, .hover\\:chocolate:hover { color: brown; }
+  `
+
+  return run(input, {}).then(result => {
+    expect(result.css).toEqual(output)
+    expect(result.warnings().length).toBe(0)
+  })
+})
