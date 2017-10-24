4478b04090427ecb9d031e4b4e2e17ccd467d6ef
Adam Wathan <adam.wathan@gmail.com>
2017-10-24 08:53:47 -0400

    Don't do any merging of config values at all
    Leave this up to the user; their config file is a JS file and we give
    them access to the default config, so they can do as much merging of
    defaults as they like.

--- src/build.js
+++ src/build.js
@@ -1,13 +1,12 @@
 import fs from 'fs'
 import postcss from 'postcss'
 import tailwind from '..'
-import defaultConfig from '../defaultConfig'
 import CleanCSS from 'clean-css'
 
 console.info('Building Tailwind!')
 
 fs.readFile('./css/tailwind.css', (err, css) => {
-  postcss([tailwind(defaultConfig)])
+  postcss([tailwind()])
     .process(css, {
       from: './css/tailwind.css',
       to: './dist/tailwind.css',
