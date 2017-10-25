bc8ea314e6070e23fa6afcee8b4cdb3597e302a6
Adam Wathan <adam.wathan@gmail.com>
2017-10-25 16:05:32 -0400

    Use new icons

--- docs/source/_layouts/master.blade.php
+++ docs/source/_layouts/master.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" class="bg-white">
+<html lang="en" class="bg-white antialiased">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
