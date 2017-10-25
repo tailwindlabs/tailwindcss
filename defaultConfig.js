cfbe2660d438236268a10b3b85ed9fa91d7fc6e0
Jonathan Reinink <jonathan@reinink.ca>
2017-10-25 17:36:50 -0400

    Fix alignment of text sizes

--- defaultConfig.js
+++ defaultConfig.js
@@ -92,8 +92,8 @@ module.exports = {
       'xl': '1.25rem',    // 20px
       '2xl': '1.5rem',    // 24px
       '3xl': '1.875rem',  // 30px
-      '4xl': '2.25rem',  // 36px
-      '5xl': '3rem',  // 48px
+      '4xl': '2.25rem',   // 36px
+      '5xl': '3rem',      // 48px
     },
     weights: {
       'hairline': 100,
