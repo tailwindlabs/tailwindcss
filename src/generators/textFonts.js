ea4b64837e92c755aca12dd924f8a1a12091fada
Jonathan Reinink <jonathan@reinink.ca>
2017-10-25 18:18:22 -0400

    Allow font families to be defined as arrays

--- src/generators/textFonts.js
+++ src/generators/textFonts.js
@@ -3,6 +3,10 @@ import defineClass from '../util/defineClass'
 
 export default function ({ fonts }) {
   return _.map(fonts, (families, font) => {
+    if (_.isArray(families)) {
+      families = families.join(', ')
+    }
+
     return defineClass(`font-${font}`, {
       'font-family': `${families}`,
     })
