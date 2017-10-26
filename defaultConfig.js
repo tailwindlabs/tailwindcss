e1140cd6d054be9facd8b9d81544fe4ed934c3ff
Jonathan Reinink <jonathan@reinink.ca>
2017-10-26 11:41:32 -0400

    Tweak wording of color docs in config

--- defaultConfig.js
+++ defaultConfig.js
@@ -29,17 +29,17 @@ var defaultConfig = require('tailwindcss').defaultConfig()
 
 /*
 |-------------------------------------------------------------------------------
-| Colors                             https://tailwindcss.com/docs/color-palette
+| Colors                                     https://tailwindcss.com/docs/color
 |-------------------------------------------------------------------------------
 |
-| Here you can specify the colors used in your project. As you'll see
-| below these colors are applied to the text, background and border
-| utilities.
+| Here you can specify the colors used in your project. To get you started,
+| we've provided a generous palette of great looking colors that are perfect
+| for prototyping, but don't hesitate to change them for your project. You
+| own these colors, nothing will break if you change everything about them.
 |
-| We've provided a bunch of colors to get you started, but please modify
-| them for your project. Also, how you name your colors is entirely up
-| to you. You can use color names (as we've done below), or follow a
-| numeric scale (like 100, 200, 300), or something else entirely.
+| We've used literal color names ("red", "blue", etc.) for the default
+| palette, but if you'd rather use functional names like "primary" and
+| "secondary", or even a numeric scale like "100" and "200", go for it.
 |
 */
 
