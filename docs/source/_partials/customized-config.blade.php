4160b13d74e580199c2bfdaabd35617d2d9e73f1
Adam Wathan <adam.wathan@gmail.com>
2017-10-27 19:00:34 -0400

    Fix incorrect border color in custom config component

--- docs/source/_partials/customized-config.blade.php
+++ docs/source/_partials/customized-config.blade.php
@@ -1,6 +1,6 @@
 <div class="border-2 border-smoke rounded mb-8">
     <div class="md:flex">
-        <div class="flex-1 bg-smoke-lighter border-b-2 md:border-b-0 md:border-r-2 border-smoke">
+        <div class="flex-1 bg-smoke-lighter border-b-2 border-smoke md:border-b-0 md:border-r-2 md:border-smoke">
             <div class="px-4 py-3 bg-white font-semibold border-b-2 border-smoke-light text-smoke-darker">Default values</div>
             <pre class="bg-smoke markdown language-js"><code>{{ $default }}</code></pre>
         </div>
