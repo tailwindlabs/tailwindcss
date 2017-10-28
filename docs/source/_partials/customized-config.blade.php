74126db79b2d19a211213086b37ffb4146ace3c0
Jonathan Reinink <jonathan@reinink.ca>
2017-10-28 08:35:16 -0400

    Fix responsiveness of the customized config component

--- docs/source/_partials/customized-config.blade.php
+++ docs/source/_partials/customized-config.blade.php
@@ -1,10 +1,10 @@
 <div class="border-2 border-smoke rounded mb-8">
     <div class="md:flex">
-        <div class="w-1/2 bg-smoke-lighter border-b-2 border-smoke md:border-b-0 md:border-r-2 md:border-smoke">
+        <div class="bg-smoke-lighter border-b-2 border-smoke md:w-1/2 md:border-b-0 md:border-r-2 md:border-smoke">
             <div class="px-4 py-3 bg-white font-semibold border-b-2 border-smoke-light text-smoke-darker">Default values</div>
             <pre class="bg-smoke markdown language-js"><code>{{ $default }}</code></pre>
         </div>
-        <div class="w-1/2 bg-smoke-lighter">
+        <div class="bg-smoke-lighter md:w-1/2">
             <div class="px-4 py-3 bg-white font-semibold border-b-2 border-smoke-light text-smoke-darker">Customized example</div>
             <pre class="bg-smoke markdown language-js"><code>{{ $customized }}</code></pre>
         </div>
