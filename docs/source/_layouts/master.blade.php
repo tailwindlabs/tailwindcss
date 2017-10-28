f2e948e477d34c2f950eae771d0c8cd7b7f1b9d0
Adam Wathan <adam.wathan@gmail.com>
2017-10-28 12:52:42 -0400

    Add favicon to docs

--- docs/source/_layouts/master.blade.php
+++ docs/source/_layouts/master.blade.php
@@ -4,6 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/manifest.json">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00b4b6">
+        <meta name="theme-color" content="#ffffff">
         @if (isset($title))
         <title>{{ $title . ' - Tailwind CSS' }}</title>
         @else
