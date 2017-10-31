1c3f93d48941e12d95d0659b2f7e1d5f6629e510
Jonathan Reinink <jonathan@reinink.ca>
2017-10-31 19:03:56 -0400

    Disable search (until it's ready)

--- docs/source/_layouts/documentation.blade.php
+++ docs/source/_layouts/documentation.blade.php
@@ -11,7 +11,8 @@
                 </a>
             </div>
             <div class="relative">
-                <input class="rounded bg-white border border-smoke py-2 pr-4 pl-10 block w-full" type="text" placeholder="Search">
+                <div class="absolute h-full ml-10 pin-l text-slate-lighter text-sm leading-none flex items-center">Search coming soon</div>
+                <input class="rounded bg-white border border-smoke py-2 pr-4 pl-10 block w-full" type="text" {{-- placeholder="Search" --}} disabled>
                 <div class="pointer-events-none absolute pin-y pin-l pl-3 flex items-center">
                     <svg class="pointer-events-none text-slate h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/></svg>
                 </div>
