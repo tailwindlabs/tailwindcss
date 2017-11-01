34c22c62190309cea3da552a0c9aaef21715b246
Jordan Pittman <jordan@cryptica.me>
2017-11-01 00:04:40 -0400

    Fix typo
    border color -> border width

--- defaultConfig.js
+++ defaultConfig.js
@@ -363,7 +363,7 @@ module.exports = {
   |
   | Here is where you define your border widths. Take note that border
   | widths require a special "default" value set as well. This is the
-  | width that will be used when you do not specify a border color.
+  | width that will be used when you do not specify a border width.
   |
   | Class name: .border{-side?}{-width?}
   |
