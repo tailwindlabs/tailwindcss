82f41ffe1d1bb8794062fb32af74ce0b5d1b3812
Adam Wathan <adam.wathan@gmail.com>
2017-11-02 13:26:27 -0400

    Add separate preflight and utilities dist files
    Just so folks can pull these in independently via CDN.

--- src/build.js
+++ src/build.js
@@ -3,27 +3,44 @@ import postcss from 'postcss'
 import tailwind from '..'
 import CleanCSS from 'clean-css'
 
+function buildDistFile(filename) {
+  return new Promise((resolve, reject) => {
+    console.log(`Processing ./css/${filename}.css...`)
+
+    fs.readFile(`./css/${filename}.css`, (err, css) => {
+      return postcss([tailwind()])
+        .process(css, {
+          from: `./css/${filename}.css`,
+          to: `./dist/${filename}.css`,
+          map: { inline: false },
+        })
+        .then(result => {
+          fs.writeFileSync(`./dist/${filename}.css`, result.css)
+          if (result.map) {
+            fs.writeFileSync(`./dist/${filename}.css.map`, result.map)
+          }
+          return result
+        })
+        .then(result => {
+          const minified = new CleanCSS().minify(result.css)
+          fs.writeFileSync(`./dist/${filename}.min.css`, minified.styles)
+        })
+        .then(resolve)
+        .catch(error => {
+          console.log(error)
+          reject()
+        })
+    })
+  })
+}
+
 console.info('Building Tailwind!')
 
-fs.readFile('./css/tailwind.css', (err, css) => {
-  postcss([tailwind()])
-    .process(css, {
-      from: './css/tailwind.css',
-      to: './dist/tailwind.css',
-      map: { inline: false },
-    })
-    .then(result => {
-      fs.writeFileSync('./dist/tailwind.css', result.css)
-      if (result.map) {
-        fs.writeFileSync('./dist/tailwind.css.map', result.map)
-      }
-      return result
-    })
-    .then(result => {
-      const minified = new CleanCSS().minify(result.css)
-      fs.writeFileSync('./dist/tailwind.min.css', minified.styles)
-    })
-    .catch(error => console.log(error))
+Promise.all([
+  buildDistFile('preflight'),
+  buildDistFile('utilities'),
+  buildDistFile('tailwind'),
+]).then(() => {
+  console.log('Finished Building Tailwind!')
 })
 
-console.log('Finished Building Tailwind!')
