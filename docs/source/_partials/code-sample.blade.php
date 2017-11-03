fe22f647f2d1badb7294f121ebb4858e1ccb780b
Adam Wathan <adam.wathan@gmail.com>
2017-11-03 07:51:37 -0400

    Switch from smoke to grey in code sample partial

--- docs/source/_partials/code-sample.blade.php
+++ docs/source/_partials/code-sample.blade.php
@@ -1,8 +1,8 @@
-<div class="rounded overflow-hidden border-2 border-smoke mb-8 bg-white">
-  <div class="border-b-2 border-smoke p-4 {{ $class ?? '' }}">
+<div class="rounded overflow-hidden border-2 border-grey-light mb-8 bg-white">
+  <div class="border-b-2 border-grey-light p-4 {{ $class ?? '' }}">
     {{ $slot }}
   </div>
-  <div class="p-4 bg-smoke-lighter">
+  <div class="p-4 bg-grey-lightest">
     <pre class="language-{{ $lang ?? 'html' }}" style="margin: 0; padding: 0;"><code>{{ e($code ?? $slot) }}</code></pre>
   </div>
 </div>
