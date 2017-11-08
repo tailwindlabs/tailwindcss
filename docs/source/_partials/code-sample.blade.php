b8479d419609690226d00e4322020b95f0e074af
James Brooks <jbrooksuk@me.com>
2017-11-08 14:34:27 +0000

    Work on an interactive example editor

--- docs/source/_partials/code-sample.blade.php
+++ docs/source/_partials/code-sample.blade.php
@@ -1,3 +1,6 @@
+@if($interactive ?? false)
+<code-sample-editor :options="{ tabSize: 2 }" className="{{ $class ?? '' }}" code="{{ e($code ?? $slot) }}"></code-sample-editor>
+@else
 <div class="rounded overflow-hidden border-2 border-grey-light mb-8 bg-white">
   <div class="border-b-2 border-grey-light p-4 {{ $class ?? '' }}">
     {{ $slot }}
@@ -6,3 +9,4 @@
     <pre class="language-{{ $lang ?? 'html' }}" style="margin: 0; padding: 0;"><code>{{ e($code ?? $slot) }}</code></pre>
   </div>
 </div>
+@endif
