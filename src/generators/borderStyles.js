558239a3252ecf62adca81fe2d596ef20e8ca526
Danilo Alonso <damusix@gmail.com>
2017-11-09 17:45:41 -0500

    only border solid

--- src/generators/borderStyles.js
+++ src/generators/borderStyles.js
@@ -2,21 +2,6 @@ import defineClasses from '../util/defineClasses'
 
 export default function() {
   return defineClasses({
-    'border-double': {
-      'border-style': 'double',
-    },
-    'border-groove': {
-      'border-style': 'groove',
-    },
-    'border-ridge': {
-      'border-style': 'ridge',
-    },
-    'border-inset': {
-      'border-style': 'inset',
-    },
-    'border-outset': {
-      'border-style': 'outset',
-    },
     'border-solid': {
       'border-style': 'solid',
     },
