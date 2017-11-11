d1deaba4c586aa9af99c20de27612d7537f6aa65
Adam Wathan <adam.wathan@gmail.com>
2017-11-11 13:19:12 -0500

    Combine sides and sizes for rounded utilities, like we do with border width

--- src/generators/rounded.js
+++ src/generators/rounded.js
@@ -1,35 +1,32 @@
 import _ from 'lodash'
-import defineClass from '../util/defineClass'
 import defineClasses from '../util/defineClasses'
 
-function sideVariants() {
+function sizedBorder(radius, modifier) {
+  modifier = modifier === 'default' ? '' : `-${modifier}`
+
   return defineClasses({
-    'rounded-t': {
-      'border-bottom-right-radius': '0',
-      'border-bottom-left-radius': '0',
+    [`rounded${modifier}`]: {
+      'border-radius': `${radius}`,
     },
-    'rounded-r': {
-      'border-bottom-left-radius': '0',
-      'border-top-left-radius': '0',
+    [`rounded-t${modifier}`]: {
+      'border-top-right-radius': `${radius}`,
+      'border-top-left-radius': `${radius}`,
     },
-    'rounded-b': {
-      'border-top-left-radius': '0',
-      'border-top-right-radius': '0',
+    [`rounded-r${modifier}`]: {
+      'border-top-right-radius': `${radius}`,
+      'border-bottom-right-radius': `${radius}`,
     },
-    'rounded-l': {
-      'border-top-right-radius': '0',
-      'border-bottom-right-radius': '0',
+    [`rounded-b${modifier}`]: {
+      'border-bottom-right-radius': `${radius}`,
+      'border-bottom-left-radius': `${radius}`,
+    },
+    [`rounded-l${modifier}`]: {
+      'border-top-left-radius': `${radius}`,
+      'border-bottom-left-radius': `${radius}`,
     },
   })
 }
 
 module.exports = function({ borderRadius }) {
-  return _(borderRadius)
-    .map((radius, modifier) => {
-      return defineClass(modifier === 'default' ? 'rounded' : `rounded-${modifier}`, {
-        'border-radius': radius,
-      })
-    })
-    .concat(sideVariants())
-    .value()
+  return _.flatMap(borderRadius, sizedBorder)
 }
