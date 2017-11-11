b806cd1461dab356a66c138ba206ec02a327333d
Adam Wathan <adam.wathan@gmail.com>
2017-11-11 08:39:11 -0500

    Only load docsearch on docs pages

--- docs/source/_layouts/documentation.blade.php
+++ docs/source/_layouts/documentation.blade.php
@@ -115,3 +115,17 @@
   </defs>
 </svg>
 @endsection
+
+@push('scripts')
+@if ($page->production)
+  <!-- Algolia DocSearch  -->
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/docsearch.js/2/docsearch.min.js"></script>
+  <script type="text/javascript">
+    docsearch({
+      apiKey: '3df93446658cd9c4e314d4c02a052188',
+      indexName: 'tailwindcss',
+      inputSelector: '#docsearch',
+    });
+  </script>
+@endif
+@endpush
--- docs/source/_layouts/master.blade.php
+++ docs/source/_layouts/master.blade.php
@@ -25,16 +25,6 @@
 @yield('body')
 
 @if ($page->production)
-  <!-- Algolia DocSearch  -->
-  <script type="text/javascript" src="https://cdn.jsdelivr.net/docsearch.js/2/docsearch.min.js"></script>
-  <script type="text/javascript">
-    docsearch({
-      apiKey: '3df93446658cd9c4e314d4c02a052188',
-      indexName: 'tailwindcss',
-      inputSelector: '#docsearch',
-    });
-  </script>
-
   <!-- Google Analytics  -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109068504-1"></script>
   <script>
@@ -45,6 +35,8 @@
     gtag('config', 'UA-109068504-1');
   </script>
 @endif
+
+@stack('scripts')
 
 </body>
 </html>
