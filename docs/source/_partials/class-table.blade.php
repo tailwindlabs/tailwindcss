35c76715789fab21141c9e5e96b2b6502333b3fc
Adam Wathan <adam.wathan@gmail.com>
2017-11-15 17:08:44 -0500

    Add class-table partial to make class tables more maintainable

---
+++ docs/source/_partials/class-table.blade.php
@@ -0,0 +1,20 @@
+<div class="border-t border-grey-lighter">
+  <table class="w-full text-left table-collapse">
+    <thead>
+      <tr>
+        <th class="text-sm font-semibold text-grey-darker p-2 bg-grey-lightest">Class</th>
+        <th class="text-sm font-semibold text-grey-darker p-2 bg-grey-lightest">Properties</th>
+        <th class="text-sm font-semibold text-grey-darker p-2 bg-grey-lightest">Description</th>
+      </tr>
+    </thead>
+    <tbody class="align-baseline">
+      @foreach ($rows as $row)
+      <tr>
+        <td class="p-2 border-t {{ $loop->first ? 'border-smoke' : 'border-smoke-light' }} font-mono text-xs text-purple-dark whitespace-no-wrap">{{ $row[0] }}</td>
+        <td class="p-2 border-t {{ $loop->first ? 'border-smoke' : 'border-smoke-light' }} font-mono text-xs text-blue-dark whitespace-pre">{{ $row[1] }}</td>
+        <td class="p-2 border-t {{ $loop->first ? 'border-smoke' : 'border-smoke-light' }} text-sm text-grey-darker">{{ $row[2] }}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+</div>
