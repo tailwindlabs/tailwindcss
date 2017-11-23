e603a22f5ed6915fb520eaf2885ade25cdd66129
Adam Wathan <adam.wathan@gmail.com>
2017-11-23 15:36:01 -0500

    Invert conditional to guard clause

--- src/lib/substituteTailwindPreflightAtRule.js
+++ src/lib/substituteTailwindPreflightAtRule.js
@@ -4,12 +4,12 @@ import postcss from 'postcss'
 export default function() {
   return function(css) {
     css.walkAtRules('tailwind', atRule => {
-      if (atRule.params === 'preflight') {
-        atRule.before(
-          postcss.parse(fs.readFileSync(`${__dirname}/../../css/preflight.css`, 'utf8'))
-        )
-        atRule.remove()
+      if (atRule.params !== 'preflight') {
+        return
       }
+
+      atRule.before(postcss.parse(fs.readFileSync(`${__dirname}/../../css/preflight.css`, 'utf8')))
+      atRule.remove()
     })
   }
 }
