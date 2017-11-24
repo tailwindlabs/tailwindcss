fc19bc2e89ca995ee1ea5de63d9114de397a7f6f
Adam Wathan <adam.wathan@gmail.com>
2017-11-24 12:34:21 -0500

    Add test to make sure custom config is actually used

---
+++ __tests__/customConfig.test.js
@@ -0,0 +1,28 @@
+import path from 'path'
+import postcss from 'postcss'
+import tailwind from '../src/index'
+
+test('it uses the values from the custom config file', () => {
+  return postcss([tailwind(path.resolve(`${__dirname}/fixtures/customConfig.js`))])
+    .process(`
+      @responsive {
+        .foo {
+          color: blue;
+        }
+      }
+    `)
+    .then(result => {
+      const expected = `
+        .foo {
+          color: blue;
+        }
+        @media (min-width: 400px) {
+          .mobile\\:foo {
+            color: blue;
+          }
+        }
+      `
+
+      expect(result.css).toMatchCss(expected)
+    })
+})
