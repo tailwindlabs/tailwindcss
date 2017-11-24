37f5a0a0d478a4f6851c17a19e5a73e545abf61d
Adam Wathan <adam.wathan@gmail.com>
2017-11-24 09:56:36 -0500

    Sort pin utilities from general to specific

--- src/generators/position.js
+++ src/generators/position.js
@@ -12,17 +12,17 @@ export default function() {
       bottom: 'auto',
       left: 'auto',
     },
-    'pin-t': { top: 0 },
-    'pin-r': { right: 0 },
-    'pin-b': { bottom: 0 },
-    'pin-l': { left: 0 },
-    'pin-y': { top: 0, bottom: 0 },
-    'pin-x': { right: 0, left: 0 },
     pin: {
       top: 0,
       right: 0,
       bottom: 0,
       left: 0,
     },
+    'pin-y': { top: 0, bottom: 0 },
+    'pin-x': { right: 0, left: 0 },
+    'pin-t': { top: 0 },
+    'pin-r': { right: 0 },
+    'pin-b': { bottom: 0 },
+    'pin-l': { left: 0 },
   })
 }
