81b632f5d08cfc5daaf94b1279ff5ea6852e916a
Sorin Ruse <sorin64.ruse@gmail.com>
2017-11-25 02:55:13 +0200

    Updated background-attachment utility

--- src/generators/backgroundAttachment.js
+++ src/generators/backgroundAttachment.js
@@ -5,8 +5,5 @@ export default function() {
     'bg-scroll': { 'background-attachment': 'scroll' },
     'bg-fixed': { 'background-attachment': 'fixed' },
     'bg-local': { 'background-attachment': 'local' },
-    'bg-inherit': { 'background-attachment': 'inherit' },
-    'bg-initial': { 'background-attachment': 'initial' },
-    'bg-unset': { 'background-attachment': 'unset' },
   })
 }
