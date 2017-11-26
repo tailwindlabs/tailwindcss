df8d80fbbf2667cf61b758f43be3d472f68e89ba
Adam Wathan <adam.wathan@gmail.com>
2017-11-26 14:53:30 -0500

    Defer unwrapping config until actually evaluating the config() function

--- src/lib/evaluateTailwindFunctions.js
+++ src/lib/evaluateTailwindFunctions.js
@@ -2,11 +2,10 @@ import _ from 'lodash'
 import functions from 'postcss-functions'
 
 export default function(config) {
-  const options = config()
-
   return functions({
     functions: {
       config: (path, defaultValue) => {
+        const options = config()
         return _.get(options, _.trim(path, `'"`), defaultValue)
       },
     },
