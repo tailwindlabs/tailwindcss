ec19740eb08df53a5748a190399ae36e32b37959
Adam Wathan <adam.wathan@gmail.com>
2017-11-28 16:02:32 -0500

    Add support for hardcore bloat mode with `modules: 'all'`
    Turns on every variant for every module :feelsgood:

--- src/util/mergeConfigWithDefaults.js
+++ src/util/mergeConfigWithDefaults.js
@@ -1,8 +1,16 @@
 import _ from 'lodash'
 
+function mergeModules(userModules, defaultModules) {
+  if (userModules === 'all') {
+    return _.mapValues(defaultModules, () => ['responsive', 'hover', 'focus', 'parent-hover'])
+  }
+
+  return _.defaults(userModules, defaultModules)
+}
+
 export default function(userConfig, defaultConfig) {
   _.defaults(userConfig, defaultConfig)
-  userConfig.modules = _.defaults(userConfig.modules, defaultConfig.modules)
+  userConfig.modules = mergeModules(userConfig.modules, defaultConfig.modules)
   userConfig.options = _.defaults(userConfig.options, defaultConfig.options)
   return userConfig
 }
