6179e6ac51166819a3e0374990d3c6bc51b82f97
Agustín Rodríguez <killgt@gmail.com>
2017-11-29 22:34:43 +0100

    utility to make both axes resizables

--- src/generators/resize.js
+++ src/generators/resize.js
@@ -5,5 +5,6 @@ export default function() {
     'resize-none': { resize: 'none' },
     'resize-y': { resize: 'vertical' },
     'resize-x': { resize: 'horizontal' },
+    resize: { resize: 'both' },
   })
 }
