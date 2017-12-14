38a391727e5f98f318fc66b3c97736a225f6fa01
Adam Wathan <adam.wathan@gmail.com>
2017-12-14 08:29:12 -0500

    Desaturate dark pinks slightly

--- defaultConfig.stub.js
+++ defaultConfig.stub.js
@@ -120,8 +120,8 @@ let colors = {
   'purple-lighter': '#d6bbfc',
   'purple-lightest': '#f3ebff',
 
-  'pink-darkest': '#45051e',
-  'pink-darker': '#72173a',
+  'pink-darkest': '#451225',
+  'pink-darker': '#6f213f',
   'pink-dark': '#eb5286',
   'pink': '#f66d9b',
   'pink-light': '#fa7ea8',
