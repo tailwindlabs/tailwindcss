5acfa18d92ebdc46b3a378aed4dc4baa0463d730
Nestor Vera <nestorvera@me.com>
2018-01-22 20:39:34 +0100

    Add bg-auto to the backgroundSize generator

--- src/generators/backgroundSize.js
+++ src/generators/backgroundSize.js
@@ -2,6 +2,9 @@ import defineClasses from '../util/defineClasses'
 
 export default function() {
   return defineClasses({
+    'bg-auto': {
+      'background-size': 'auto',
+    },
     'bg-cover': {
       'background-size': 'cover',
     },
