357c87376ee3c6c0f151f889f8e1c2da4a29629f
Emil Tholin <emtholin@gmail.com>
2018-01-28 21:18:52 +0100

    Add support for passing a function as the prefix

--- __tests__/applyClassPrefix.test.js
+++ __tests__/applyClassPrefix.test.js
@@ -16,3 +16,27 @@ test('it prefixes classes with the provided prefix', () => {
   expect(result.css).toEqual(expected)
   expect(result.warnings().length).toBe(0)
 })
+
+test('it handles a function as the prefix', () => {
+  const input = postcss.parse(`
+    .foo { color: red; }
+    .apple, .pear { color: green; }
+  `)
+
+  const expected = `
+    .tw-foo { color: red; }
+    .apple, .pear { color: green; }
+  `
+
+  const prefixFunc = selector => {
+    if (selector === '.foo') {
+      return 'tw-'
+    }
+
+    return ''
+  }
+
+  const result = applyClassPrefix(input, prefixFunc).toResult()
+  expect(result.css).toEqual(expected)
+  expect(result.warnings().length).toBe(0)
+})
