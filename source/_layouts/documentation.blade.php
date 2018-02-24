496070070d3eaf284f09de167fb59e9d5575127f
Adam Wathan <adam.wathan@gmail.com>
2018-02-23 19:39:26 -0500

    Get sticky stuff working again

--- source/_layouts/documentation.blade.php
+++ source/_layouts/documentation.blade.php
@@ -87,9 +87,8 @@
 
     {{-- Side nav --}}
     <div id="sidebar" class="hidden absolute z-90 top-16 bg-white w-full border-b -mb-16 lg:-mb-0 lg:static lg:bg-transparent lg:border-b-0 lg:pt-0 lg:w-1/4 lg:block lg:border-0 xl:w-1/5">
-      <div class="px-6 pt-6 overflow-y-scroll lg:block lg:fixed lg:w-64 lg:top-16 lg:py-12 lg:pl-6 lg:pr-8 lg:h-(screen-16) lg:max-h-(screen-16)">
-
-        <nav id="nav" class="text-base lg:text-sm">
+      <div class="lg:block lg:relative lg:sticky lg:top-16">
+        <nav id="nav" class="px-6 pt-6 overflow-y-scroll text-base lg:text-sm lg:py-12 lg:pl-6 lg:pr-8 lg:h-(screen-16) lg:max-h-(screen-16)">
           @foreach ($page->navigation as $sectionName => $sectionItems)
           <div class="mb-8">
             <p class="mb-3 lg:mb-2 text-grey uppercase tracking-wide font-bold text-sm lg:text-xs">{{ $sectionName }}</p>
