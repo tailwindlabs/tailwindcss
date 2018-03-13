de24a2a45e37eba22d349aaa41d98280f26efd96
Adam Wathan <adam.wathan@gmail.com>
2018-03-13 12:28:32 -0400

    Remove special scrollbar styling for IE/Edge

--- src/generators/overflow.js
+++ src/generators/overflow.js
@@ -6,22 +6,10 @@ export default function() {
     'overflow-hidden': { overflow: 'hidden' },
     'overflow-visible': { overflow: 'visible' },
     'overflow-scroll': { overflow: 'scroll' },
-    'overflow-x-auto': {
-      'overflow-x': 'auto',
-      '-ms-overflow-style': '-ms-autohiding-scrollbar',
-    },
-    'overflow-y-auto': {
-      'overflow-y': 'auto',
-      '-ms-overflow-style': '-ms-autohiding-scrollbar',
-    },
-    'overflow-x-scroll': {
-      'overflow-x': 'scroll',
-      '-ms-overflow-style': '-ms-scrollbar',
-    },
-    'overflow-y-scroll': {
-      'overflow-y': 'scroll',
-      '-ms-overflow-style': '-ms-scrollbar',
-    },
+    'overflow-x-auto': { 'overflow-x': 'auto' },
+    'overflow-y-auto': { 'overflow-y': 'auto' },
+    'overflow-x-scroll': { 'overflow-x': 'scroll' },
+    'overflow-y-scroll': { 'overflow-y': 'scroll' },
     'scrolling-touch': { '-webkit-overflow-scrolling': 'touch' },
     'scrolling-auto': { '-webkit-overflow-scrolling': 'auto' },
   })
