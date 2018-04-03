f4c9d45f8e1eb8ba29644f58529b0cb6c3359eba
Nestor Vera <nestorvera@me.com>
2018-04-03 11:38:51 +0200

    Add classes to set overflow visible on independent axis

--- src/generators/overflow.js
+++ src/generators/overflow.js
@@ -10,6 +10,8 @@ export default function() {
     'overflow-y-auto': { 'overflow-y': 'auto' },
     'overflow-x-hidden': { 'overflow-x': 'hidden' },
     'overflow-y-hidden': { 'overflow-y': 'hidden' },
+    'overflow-x-visible': { 'overflow-x': 'visible' },
+    'overflow-y-visible': { 'overflow-y': 'visible' },
     'overflow-x-scroll': { 'overflow-x': 'scroll' },
     'overflow-y-scroll': { 'overflow-y': 'scroll' },
     'scrolling-touch': { '-webkit-overflow-scrolling': 'touch' },
