f188bd3093ff0ded0409ef04ca284949f6f3b7de
Jonathan Reinink <jonathan@reinink.ca>
2018-06-15 09:04:04 -0400

    Change no-outline utility value form "none" to "0"

--- src/generators/outline.js
+++ src/generators/outline.js
@@ -2,6 +2,6 @@ import defineClasses from '../util/defineClasses'
 
 export default function() {
   return defineClasses({
-    'no-outline': { outline: 'none' },
+    'no-outline': { outline: '0' },
   })
 }
