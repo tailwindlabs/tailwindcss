8333d46caeb4a40b647b54777c3f1acb56cae9b8
Adam Wathan <adam.wathan@gmail.com>
2018-06-20 12:21:14 -0400

    Add variant prefix to last class in a selector, not the first

--- src/util/buildClassVariant.js
+++
@@ -1,5 +0,0 @@
-import escapeClassName from './escapeClassName'
-
-export default function buildClassVariant(className, variantName, separator) {
-  return `.${variantName}${escapeClassName(separator)}${className.slice(1)}`
-}
