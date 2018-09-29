d86efdb8f9a8ec6fb7221eadc2af80d887e4a081
SpiltCoffee <spiltcoffee5@gmail.com>
2018-09-29 13:18:54 +1000

    Changed Border Color and Border Style Generation to provide individual edge utilities (fixes #559)

--- src/generators/borderStyle.js
+++ src/generators/borderStyle.js
@@ -1,18 +1,37 @@
+import _ from 'lodash'
 import defineClasses from '../util/defineClasses'
 
-export default function() {
-  return defineClasses({
-    'border-solid': {
-      'border-style': 'solid',
-    },
-    'border-dashed': {
-      'border-style': 'dashed',
-    },
-    'border-dotted': {
-      'border-style': 'dotted',
-    },
-    'border-none': {
-      'border-style': 'none',
-    },
+function defineBorderStyleUtilities(borderStyles) {
+  const generators = [
+    style =>
+      defineClasses({
+        [`border-${style}`]: {
+          'border-style': `${style}`,
+        },
+      }),
+    style =>
+      defineClasses({
+        [`border-t-${style}`]: {
+          'border-top-style': `${style}`,
+        },
+        [`border-r-${style}`]: {
+          'border-right-style': `${style}`,
+        },
+        [`border-b-${style}`]: {
+          'border-bottom-style': `${style}`,
+        },
+        [`border-l-${style}`]: {
+          'border-left-style': `${style}`,
+        },
+      }),
+  ]
+
+  return _.flatMap(generators, generator => {
+    return _.flatMap(borderStyles, generator)
   })
+}
+
+export default function() {
+  const borderStyles = ['solid', 'dashed', 'dotted', 'none']
+  return defineBorderStyleUtilities(borderStyles)
 }
