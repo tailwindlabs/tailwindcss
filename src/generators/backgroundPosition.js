85ea38b2f54fe39f0517107cd6a9697146190e7c
Benoît Rouleau <benoit.rouleau@icloud.com>
2019-01-02 17:32:02 -0500

    Add ability to configure background position utilities

--- src/generators/backgroundPosition.js
+++ src/generators/backgroundPosition.js
@@ -1,15 +1,10 @@
-import defineClasses from '../util/defineClasses'
+import _ from 'lodash'
+import defineClass from '../util/defineClass'
 
-export default function() {
-  return defineClasses({
-    'bg-bottom': { 'background-position': 'bottom' },
-    'bg-center': { 'background-position': 'center' },
-    'bg-left': { 'background-position': 'left' },
-    'bg-left-bottom': { 'background-position': 'left bottom' },
-    'bg-left-top': { 'background-position': 'left top' },
-    'bg-right': { 'background-position': 'right' },
-    'bg-right-bottom': { 'background-position': 'right bottom' },
-    'bg-right-top': { 'background-position': 'right top' },
-    'bg-top': { 'background-position': 'top' },
+export default function({ backgroundPosition }) {
+  return _.map(backgroundPosition, (position, className) => {
+    return defineClass(`bg-${className}`, {
+      'background-position': position,
+    })
   })
 }
