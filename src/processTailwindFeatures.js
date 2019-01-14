060d23439de45065aec734b031e4be2445acbd40
Adam Wathan <adam.wathan@gmail.com>
2019-01-14 15:06:08 -0500

    Remove code obsoleted by upgrading PostCSS

--- src/processTailwindFeatures.js
+++ src/processTailwindFeatures.js
@@ -23,31 +23,6 @@ export default function(getConfig) {
       substituteResponsiveAtRules(config),
       substituteScreenAtRules(config),
       substituteClassApplyAtRules(config, processedPlugins.utilities),
-
-      // This quick plugin is necessary to avoid a serious performance
-      // hit due to nodes created by postcss-js having an empty `raws`
-      // value, and PostCSS not providing a default `raws.semicolon`
-      // value. This turns determining what value to use there into an
-      // O(n) operation instead of an O(1) operation.
-      //
-      // The latest version of PostCSS 7.x has this patched internally,
-      // but patching from userland until we upgrade from v6 to v7.
-      function(root) {
-        root.rawCache = {
-          colon: ': ',
-          indent: '  ',
-          beforeDecl: '\n',
-          beforeRule: '\n',
-          beforeOpen: ' ',
-          beforeClose: '\n',
-          beforeComment: '\n',
-          after: '\n',
-          emptyBody: '',
-          commentLeft: ' ',
-          commentRight: ' ',
-          semicolon: false,
-        }
-      },
     ]).process(css, { from: _.get(css, 'source.input.file') })
   }
 }
