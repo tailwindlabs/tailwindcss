b948c2549974f13cc7229f6977ca40fde610f5fb
Adam Wathan <adam.wathan@gmail.com>
2019-01-14 15:31:00 -0500

    Delete possibly last remnants of dead code

--- src/util/defineClasses.js
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash'
-import defineClass from './defineClass'
-
-export default function defineClasses(classes) {
-  return _.map(classes, (properties, className) => {
-    return defineClass(className, properties)
-  })
-}
