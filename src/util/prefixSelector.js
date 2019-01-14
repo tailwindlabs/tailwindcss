2b5358d92ace8dfdd9516f5392cc460ca32b59df
Adam Wathan <adam.wathan@gmail.com>
2019-01-14 14:17:02 -0500

    Update prefixSelector for latest selector parser

--- src/util/prefixSelector.js
+++ src/util/prefixSelector.js
@@ -1,11 +1,14 @@
 import parser from 'postcss-selector-parser'
+import get from 'lodash/get'
 
 export default function(prefix, selector) {
   const getPrefix = typeof prefix === 'function' ? prefix : () => prefix
 
   return parser(selectors => {
     selectors.walkClasses(classSelector => {
-      classSelector.value = `${getPrefix('.' + classSelector.value)}${classSelector.value}`
+      const baseClass = get(classSelector, 'raws.value', classSelector.value)
+
+      classSelector.setPropertyAndEscape('value', `${getPrefix('.' + baseClass)}${baseClass}`)
     })
   }).processSync(selector)
 }
