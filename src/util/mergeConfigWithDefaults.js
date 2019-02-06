703b2768b75b6e68d2e6550e2db43e9ca00c23e2
Kevin Lee Drum <kevinleedrum@gmail.com>
2019-02-06 14:19:07 -0500

    use object spread to eliminate need for lodash defaults

--- src/util/mergeConfigWithDefaults.js
+++ src/util/mergeConfigWithDefaults.js
@@ -1,12 +1,8 @@
-import _ from 'lodash'
-
 export default function(userConfig, defaultConfig) {
-  return _.defaults(
-    {
-      theme: _.defaults(userConfig.theme, defaultConfig.theme),
-      variants: _.defaults(userConfig.variants, defaultConfig.variants),
-    },
-    userConfig,
-    defaultConfig
-  )
+  return {
+    ...defaultConfig,
+    ...userConfig,
+    theme: { ...defaultConfig.theme, ...userConfig.theme },
+    variants: { ...defaultConfig.variants, ...userConfig.variants },
+  }
 }
