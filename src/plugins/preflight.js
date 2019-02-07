a9a0cf03e51401b99767958b9fd58855ff1d2e0f
Adam Wathan <adam.wathan@gmail.com>
2019-02-07 15:27:17 -0500

    Move preflight CSS into plugins directory

--- src/plugins/preflight.js
+++ src/plugins/preflight.js
@@ -3,9 +3,7 @@ import postcss from 'postcss'
 
 export default function() {
   return function({ addBase }) {
-    const preflightStyles = postcss.parse(
-      fs.readFileSync(`${__dirname}/../../css/preflight.css`, 'utf8')
-    )
+    const preflightStyles = postcss.parse(fs.readFileSync(`${__dirname}/css/preflight.css`, 'utf8'))
     addBase(preflightStyles.nodes)
   }
 }
