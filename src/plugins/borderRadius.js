645909cfdc3c6ce7c49984b10cf1c8fec861833c
Adam Wathan <adam.wathan@gmail.com>
2019-02-18 13:07:23 -0500

    Don't handle default border radius modifier specially

--- src/plugins/borderRadius.js
+++ src/plugins/borderRadius.js
@@ -4,38 +4,36 @@ export default function({ values, variants }) {
   return function({ addUtilities, e }) {
     const generators = [
       (value, modifier) => ({
-        [`.${e(`radius${modifier}`)}`]: { borderRadius: `${value}` },
+        [`.${e(`radius-${modifier}`)}`]: { borderRadius: `${value}` },
       }),
       (value, modifier) => ({
-        [`.${e(`radius-t${modifier}`)}`]: {
+        [`.${e(`radius-t-${modifier}`)}`]: {
           borderTopLeftRadius: `${value}`,
           borderTopRightRadius: `${value}`,
         },
-        [`.${e(`radius-r${modifier}`)}`]: {
+        [`.${e(`radius-r-${modifier}`)}`]: {
           borderTopRightRadius: `${value}`,
           borderBottomRightRadius: `${value}`,
         },
-        [`.${e(`radius-b${modifier}`)}`]: {
+        [`.${e(`radius-b-${modifier}`)}`]: {
           borderBottomRightRadius: `${value}`,
           borderBottomLeftRadius: `${value}`,
         },
-        [`.${e(`radius-l${modifier}`)}`]: {
+        [`.${e(`radius-l-${modifier}`)}`]: {
           borderTopLeftRadius: `${value}`,
           borderBottomLeftRadius: `${value}`,
         },
       }),
       (value, modifier) => ({
-        [`.${e(`radius-tl${modifier}`)}`]: { borderTopLeftRadius: `${value}` },
-        [`.${e(`radius-tr${modifier}`)}`]: { borderTopRightRadius: `${value}` },
-        [`.${e(`radius-br${modifier}`)}`]: { borderBottomRightRadius: `${value}` },
-        [`.${e(`radius-bl${modifier}`)}`]: { borderBottomLeftRadius: `${value}` },
+        [`.${e(`radius-tl-${modifier}`)}`]: { borderTopLeftRadius: `${value}` },
+        [`.${e(`radius-tr-${modifier}`)}`]: { borderTopRightRadius: `${value}` },
+        [`.${e(`radius-br-${modifier}`)}`]: { borderBottomRightRadius: `${value}` },
+        [`.${e(`radius-bl-${modifier}`)}`]: { borderBottomLeftRadius: `${value}` },
       }),
     ]
 
     const utilities = _.flatMap(generators, generator => {
-      return _.flatMap(values, (value, modifier) => {
-        return generator(value, modifier === 'default' ? '' : `-${modifier}`)
-      })
+      return _.flatMap(values, generator)
     })
 
     addUtilities(utilities, variants)
