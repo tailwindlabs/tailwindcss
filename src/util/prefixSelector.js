b8f107822942c3b3959e6ad6a920814197e22b03
Adam Wathan <adam.wathan@gmail.com>
2019-02-27 13:43:26 -0500

    Use postcss-selector-parser class escape handling

--- src/util/prefixSelector.js
+++ src/util/prefixSelector.js
@@ -1,14 +1,14 @@
 import parser from 'postcss-selector-parser'
-import get from 'lodash/get'
+import tap from 'lodash/tap'
 
 export default function(prefix, selector) {
   const getPrefix = typeof prefix === 'function' ? prefix : () => prefix
 
   return parser(selectors => {
     selectors.walkClasses(classSelector => {
-      const baseClass = get(classSelector, 'raws.value', classSelector.value)
-
-      classSelector.setPropertyAndEscape('value', `${getPrefix('.' + baseClass)}${baseClass}`)
+      tap(classSelector.value, baseClass => {
+        classSelector.value = `${getPrefix('.' + baseClass)}${baseClass}`
+      })
     })
   }).processSync(selector)
 }
