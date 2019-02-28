237baa3188d6e043ab62488681e3f73951f98e9a
Adam Wathan <adam.wathan@gmail.com>
2019-02-28 11:37:02 -0500

    Source backgroundPosition options from config

--- src/plugins/backgroundPosition.js
+++ src/plugins/backgroundPosition.js
@@ -1,9 +1,9 @@
 import _ from 'lodash'
 
-export default function({ values, variants }) {
-  return function({ addUtilities, e }) {
+export default function() {
+  return function({ addUtilities, e, config }) {
     const utilities = _.fromPairs(
-      _.map(values, (value, modifier) => {
+      _.map(config('theme.backgroundPosition'), (value, modifier) => {
         return [
           `.${e(`bg-${modifier}`)}`,
           {
@@ -13,6 +13,6 @@ export default function({ values, variants }) {
       })
     )
 
-    addUtilities(utilities, variants)
+    addUtilities(utilities, config('variants.backgroundPosition'))
   }
 }
