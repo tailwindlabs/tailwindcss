dc57496674396fde46332e82165623e2fbca5e41
Adam Wathan <adam.wathan@gmail.com>
2019-02-28 12:35:48 -0500

    Source textAlign options from config

--- src/plugins/textAlign.js
+++ src/plugins/textAlign.js
@@ -1,5 +1,5 @@
-export default function({ variants }) {
-  return function({ addUtilities }) {
+export default function() {
+  return function({ addUtilities, config }) {
     addUtilities(
       {
         '.text-left': { 'text-align': 'left' },
@@ -7,7 +7,7 @@ export default function({ variants }) {
         '.text-right': { 'text-align': 'right' },
         '.text-justify': { 'text-align': 'justify' },
       },
-      variants
+      config('variants.textAlign')
     )
   }
 }
