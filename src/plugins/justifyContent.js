ebcd4b0bfe125fa79f920b053b96fccedd20975e
Adam Wathan <adam.wathan@gmail.com>
2019-02-28 12:01:29 -0500

    Source justifyContent options from config

--- src/plugins/justifyContent.js
+++ src/plugins/justifyContent.js
@@ -1,5 +1,5 @@
-export default function({ variants }) {
-  return function({ addUtilities }) {
+export default function() {
+  return function({ addUtilities, config }) {
     addUtilities(
       {
         '.justify-start': {
@@ -18,7 +18,7 @@ export default function({ variants }) {
           'justify-content': 'space-around',
         },
       },
-      variants
+      config('variants.justifyContent')
     )
   }
 }
