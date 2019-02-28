7e848e71e4a3c172ad4033034766599a77ebfb7b
Adam Wathan <adam.wathan@gmail.com>
2019-02-28 12:37:47 -0500

    Source userSelect options from config

--- src/plugins/userSelect.js
+++ src/plugins/userSelect.js
@@ -1,11 +1,11 @@
-export default function({ variants }) {
-  return function({ addUtilities }) {
+export default function() {
+  return function({ addUtilities, config }) {
     addUtilities(
       {
         '.select-none': { 'user-select': 'none' },
         '.select-text': { 'user-select': 'text' },
       },
-      variants
+      config('variants.userSelect')
     )
   }
 }
