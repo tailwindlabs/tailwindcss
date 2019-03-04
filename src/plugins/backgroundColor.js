c3c9cdf5d68972a72ea27656ba46d322d5ee58fb
Adam Wathan <adam.wathan@gmail.com>
2019-03-04 13:05:18 -0500

    Support nested object for backgroundColor plugin

--- src/plugins/backgroundColor.js
+++ src/plugins/backgroundColor.js
@@ -1,9 +1,22 @@
 import _ from 'lodash'
 
+function buildColorPalette(colors) {
+  const result = _(colors)
+    .flatMap((color, name) => {
+      return _.isObject(color)
+        ? _.map(color, (value, key) => [`${name}-${key}`, value])
+        : [[name, color]]
+    })
+    .fromPairs()
+    .value()
+
+  return result
+}
+
 export default function() {
   return function({ addUtilities, e, config }) {
     const utilities = _.fromPairs(
-      _.map(config('theme.backgroundColor'), (value, modifier) => {
+      _.map(buildColorPalette(config('theme.backgroundColor')), (value, modifier) => {
         return [
           `.${e(`bg-${modifier}`)}`,
           {
