7ade6ea74406f2c5542de161cd5c4b5f17110111
Adam Wathan <adam.wathan@gmail.com>
2019-03-04 16:19:07 -0500

    Load tailwind.config.js automatically if present

--- __tests__/customConfig.test.js
+++ __tests__/customConfig.test.js
@@ -1,6 +1,19 @@
+import fs from 'fs'
 import path from 'path'
 import postcss from 'postcss'
 import tailwind from '../src/index'
+
+function inTempDirectory(callback) {
+  return new Promise(resolve => {
+    fs.mkdirSync(path.resolve('./__tmp'))
+    process.chdir(path.resolve('./__tmp'))
+    callback().then(() => {
+      process.chdir(path.resolve('../'))
+      fs.rmdirSync(path.resolve('./__tmp'))
+      resolve()
+    })
+  })
+}
 
 test('it uses the values from the custom config file', () => {
   return postcss([tailwind(path.resolve(`${__dirname}/fixtures/custom-config.js`))])
@@ -25,7 +38,6 @@ test('it uses the values from the custom config file', () => {
           }
         }
       `
-
       expect(result.css).toMatchCss(expected)
     })
 })
@@ -64,4 +76,44 @@ test('custom config can be passed as an object', () => {
 
       expect(result.css).toMatchCss(expected)
     })
+})
+
+test('tailwind.config.js is picked up by default', () => {
+  return inTempDirectory(() => {
+    fs.writeFileSync(
+      path.resolve('./tailwind.config.js'),
+      `module.exports = {
+        theme: {
+          screens: {
+            mobile: '400px',
+          },
+        },
+      }`
+    )
+
+    return postcss([tailwind])
+      .process(
+        `
+          @responsive {
+            .foo {
+              color: blue;
+            }
+          }
+        `,
+        { from: undefined }
+      )
+      .then(result => {
+        expect(result.css).toMatchCss(`
+          .foo {
+            color: blue;
+          }
+          @media (min-width: 400px) {
+            .mobile\\:foo {
+              color: blue;
+            }
+          }
+        `)
+        fs.unlinkSync(path.resolve('./tailwind.config.js'))
+      })
+  })
 })
