33bfb8793fc08d4b737fa1447da6f502d0887bea
Adam Wathan <adam.wathan@gmail.com>
2019-03-14 12:35:37 -0400

    Make inset utilities customizable

--- src/plugins/inset.js
+++ src/plugins/inset.js
@@ -1,27 +1,32 @@
+import _ from 'lodash'
+
 export default function() {
-  return function({ addUtilities, config }) {
-    addUtilities(
-      {
-        '.inset-auto': {
-          top: 'auto',
-          right: 'auto',
-          bottom: 'auto',
-          left: 'auto',
+  return function({ addUtilities, e, config }) {
+    const generators = [
+      (size, modifier) => ({
+        [`.${e(`inset-${modifier}`)}`]: {
+          top: `${size}`,
+          right: `${size}`,
+          bottom: `${size}`,
+          left: `${size}`,
         },
-        '.inset-0': {
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-        },
-        '.inset-y-0': { top: 0, bottom: 0 },
-        '.inset-x-0': { right: 0, left: 0 },
-        '.top-0': { top: 0 },
-        '.right-0': { right: 0 },
-        '.bottom-0': { bottom: 0 },
-        '.left-0': { left: 0 },
-      },
-      config('variants.inset')
-    )
+      }),
+      (size, modifier) => ({
+        [`.${e(`inset-y-${modifier}`)}`]: { top: `${size}`, bottom: `${size}` },
+        [`.${e(`inset-x-${modifier}`)}`]: { right: `${size}`, left: `${size}` },
+      }),
+      (size, modifier) => ({
+        [`.${e(`top-${modifier}`)}`]: { top: `${size}` },
+        [`.${e(`right-${modifier}`)}`]: { right: `${size}` },
+        [`.${e(`bottom-${modifier}`)}`]: { bottom: `${size}` },
+        [`.${e(`left-${modifier}`)}`]: { left: `${size}` },
+      }),
+    ]
+
+    const utilities = _.flatMap(generators, generator => {
+      return _.flatMap(config('theme.inset'), generator)
+    })
+
+    addUtilities(utilities, config('variants.inset'))
   }
 }
