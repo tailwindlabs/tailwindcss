1940e31b124b517f8d2c2c4b8a8d0099ff626d02
Adam Wathan <adam.wathan@gmail.com>
2019-03-14 12:23:51 -0400

    Rename pin utilities to top/right/bottom/left/inset

--- src/plugins/inset.js
+++ src/plugins/inset.js
@@ -2,24 +2,24 @@ export default function() {
   return function({ addUtilities, config }) {
     addUtilities(
       {
-        '.pin-none': {
+        '.inset-auto': {
           top: 'auto',
           right: 'auto',
           bottom: 'auto',
           left: 'auto',
         },
-        '.pin': {
+        '.inset-0': {
           top: 0,
           right: 0,
           bottom: 0,
           left: 0,
         },
-        '.pin-y': { top: 0, bottom: 0 },
-        '.pin-x': { right: 0, left: 0 },
-        '.pin-t': { top: 0 },
-        '.pin-r': { right: 0 },
-        '.pin-b': { bottom: 0 },
-        '.pin-l': { left: 0 },
+        '.inset-y-0': { top: 0, bottom: 0 },
+        '.inset-x-0': { right: 0, left: 0 },
+        '.top-0': { top: 0 },
+        '.right-0': { right: 0 },
+        '.bottom-0': { bottom: 0 },
+        '.left-0': { left: 0 },
       },
       config('variants.inset')
     )
