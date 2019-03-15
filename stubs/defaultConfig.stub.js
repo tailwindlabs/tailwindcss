71b11a667ac341b696b37f08522f91ca8a2884a0
mattstypa <matt@mattstypa.com>
2019-03-15 09:05:06 -0500

    Updating the stub

--- stubs/defaultConfig.stub.js
+++ stubs/defaultConfig.stub.js
@@ -436,6 +436,7 @@ module.exports = {
     verticalAlign: ['responsive'],
     visibility: ['responsive'],
     whitespace: ['responsive'],
+    wordBreak: ['responsive'],
     width: ['responsive'],
     zIndex: ['responsive'],
   },
