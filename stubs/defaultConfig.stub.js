426a549e500c7f5a611576a9bd10e24aef64e347
Adam Wathan <adam.wathan@gmail.com>
2019-03-15 11:13:46 -0400

    Update shadow-outline to be based on new blue

--- stubs/defaultConfig.stub.js
+++ stubs/defaultConfig.stub.js
@@ -325,7 +325,7 @@ module.exports = {
       xl: '0 20px 25px -5px rgba(0, 0, 0, .1), 0 10px 10px -5px rgba(0, 0, 0, .04)',
       '2xl': '0 25px 50px -12px rgba(0, 0, 0, .25)',
       inner: 'inset 0 2px 4px 0 rgba(0,0,0,0.06)',
-      outline: '0 0 0 3px rgba(52,144,220,0.5)',
+      outline: '0 0 0 3px rgba(66,153,225,0.5)',
       none: 'none',
     },
     zIndex: {
