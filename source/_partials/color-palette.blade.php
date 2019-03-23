8a83a7bf8573111c21f3ade0c424f56e3b62882e
Adam Wathan <adam.wathan@gmail.com>
2019-03-23 13:40:47 -0400

    WIP

--- source/_partials/color-palette.blade.php
+++ source/_partials/color-palette.blade.php
@@ -1,10 +1,15 @@
-<div class="relative my-8 overflow-hidden rounded shadow-md">
-  <div class="relative shadow bg-{{ $color }}-500 text-{{ $color }}-{{ $breakpoint < 500 ? '100' : '900' }} px-3 py-4 md:py-1 text-sm font-semibold uppercase">{{ $colorName }}</div>
-  <div class="md:flex md:flex-row-reverse">
+<div class="relative my-8">
+  <h3 class="markdown">{{ $colorName }}</h3>
+  {{-- <div class="relative text-xl text-gray-900 font-medium">{{ $colorName }}</div> --}}
+  <div class="md:-mx-2 -mt-5 md:flex md:flex-wrap">
     @foreach (range(100, 900, 100) as $variant)
-    <div class="text-{{ $color }}-{{ $variant > $breakpoint ? '100' : '900' }} bg-{{ $color }}-{{ $variant }} px-2 py-4 text-xs flex-1 leading-tight">
-      <div class="font-bold">{{ $variant }}</div>
-      <div class="mt-1 font-normal opacity-75">{{ strtoupper($page->config['theme']['colors'][$color][$variant]) }}</div>
+    <div class="flex items-center md:w-1/3 md:px-2 mt-5">
+      {{-- <div class="rounded-lg shadow-inner text-{{ $color }}-{{ $variant > $breakpoint ? '100' : '900' }} bg-{{ $color }}-{{ $variant }} px-2 py-4 text-xs leading-tight"> --}}
+      <div class="h-12 w-12 rounded-lg shadow-inner bg-{{ $color }}-{{ $variant }}"></div>
+      <div class="ml-2 text-gray-800 text-xs leading-none pl-1">
+        <div class="font-semibold">{{ $variant }}</div>
+        <div class="mt-1 font-normal opacity-75">{{ strtoupper($page->config['theme']['colors'][$color][$variant]) }}</div>
+      </div>
     </div>
     @endforeach
   </div>
