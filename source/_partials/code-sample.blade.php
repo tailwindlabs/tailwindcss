8818f7510e52739c29ca19f6a8ff61afd66436f2
Adam Wathan <adam.wathan@gmail.com>
2019-03-24 08:07:53 -0700

    Hide scrollbars on code samples on macOS (don't @ me)

--- source/_partials/code-sample.blade.php
+++ source/_partials/code-sample.blade.php
@@ -2,7 +2,7 @@
   <div class="bg-white rounded-t-lg border border-gray-400 p-4 {{ $class ?? '' }}">
     {{ $slot }}
   </div>
-  <div class="rounded-b-lg p-4 bg-gray-800">
-    <pre class="language-{{ $lang ?? 'html' }}" style="margin: 0; padding: 0;"><code>{{ e($code ?? $slot) }}</code></pre>
+  <div class="scollbar-none rounded-b-lg p-4 bg-gray-800">
+    <pre class="scrollbar-none language-{{ $lang ?? 'html' }}" style="margin: 0; padding: 0;"><code>{{ e($code ?? $slot) }}</code></pre>
   </div>
 </div>
