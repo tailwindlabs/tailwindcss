12afd6e8a1575a6752f9dc436ebae10553365757
David Hemphill <davidlee.hemphill@gmail.com>
2019-04-02 15:55:19 -0500

    fix stroke utility generation

--- src/plugins/stroke.js
+++ src/plugins/stroke.js
@@ -1,9 +1,10 @@
 import _ from 'lodash'
+import flattenColorPalette from '../util/flattenColorPalette'
 
 export default function() {
   return function({ addUtilities, e, config }) {
     const utilities = _.fromPairs(
-      _.map(config('theme.stroke'), (value, modifier) => {
+      _.map(flattenColorPalette(config('theme.stroke')), (value, modifier) => {
         return [
           `.${e(`stroke-${modifier}`)}`,
           {
