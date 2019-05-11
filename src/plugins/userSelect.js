0fac54f8f11ddeb97ace42cd018d8bec603246b3
Adam Wathan <adam.wathan@gmail.com>
2019-05-11 07:06:32 -0400

    Remove select-contain due to limited browser support

--- src/plugins/userSelect.js
+++ src/plugins/userSelect.js
@@ -5,7 +5,6 @@ export default function() {
         '.select-none': { 'user-select': 'none' },
         '.select-text': { 'user-select': 'text' },
         '.select-all': { 'user-select': 'all' },
-        '.select-contain': { 'user-select': 'contain' },
         '.select-auto': { 'user-select': 'auto' },
       },
       variants('userSelect')
