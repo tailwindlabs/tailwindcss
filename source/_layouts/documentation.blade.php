9a43f493cd59392f2063823537fde6dc0c2a0e04
Adam Wathan <adam.wathan@gmail.com>
2019-05-17 15:05:04 -0400

    Actually fix mobile menu

--- source/_layouts/documentation.blade.php
+++ source/_layouts/documentation.blade.php
@@ -121,7 +121,7 @@
   <div class="lg:flex -mx-6">
 
     {{-- Side nav --}}
-    <div id="sidebar" class="hidden fixed inset-0 pt-16 h-full bg-white z-90 w-full border-b -mb-16 lg:-mb-0 lg:static lg:overflow-y-visible lg:border-b-0 lg:pt-0 lg:w-1/4 lg:block lg:border-0 xl:w-1/5">
+    <div id="sidebar" class="hidden fixed inset-0 pt-16 h-full bg-white z-90 w-full border-b -mb-16 lg:-mb-0 lg:static lg:h-auto lg:overflow-y-visible lg:border-b-0 lg:pt-0 lg:w-1/4 lg:block lg:border-0 xl:w-1/5">
 
       <div id="navWrapper" class="h-full overflow-y-auto scrolling-touch lg:h-auto lg:block lg:relative lg:sticky lg:top-16 bg-white lg:bg-transparent">
         <div id="navGradient" class="hidden"></div>
