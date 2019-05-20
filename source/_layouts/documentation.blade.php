27126f80bdfef5516c28630e345e159d800ddc72
Adam Wathan <adam.wathan@gmail.com>
2019-05-20 09:45:03 -0400

    Add version switcher to homepage

--- source/_layouts/documentation.blade.php
+++ source/_layouts/documentation.blade.php
@@ -126,6 +126,15 @@
       <div id="navWrapper" class="h-full overflow-y-auto scrolling-touch lg:h-auto lg:block lg:relative lg:sticky lg:top-16 bg-white lg:bg-transparent">
         <div id="navGradient" class="hidden"></div>
         <nav id="nav" class="px-6 pt-6 overflow-y-auto text-base lg:text-sm lg:py-12 lg:pl-6 lg:pr-8 sticky?lg:h-(screen-16)">
+          <div class="relative -mx-2 w-24 mb-8 lg:hidden">
+            <select data-version-switcher class="appearance-none block bg-white pl-2 pr-8 py-1 text-gray-500 font-medium text-base focus:outline-none focus:text-gray-800">
+              <option value="v1">v1.0.1</option>
+              <option value="v0">v0.7.4</option>
+            </select>
+            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500">
+              <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+            </div>
+          </div>
           <div class="mb-10">
             <a href="/docs/installation" class="flex items-center px-2 -mx-2 py-1 hover:text-gray-900 font-medium {{ $page->active('/docs') ? 'text-gray-900' : 'text-gray-600' }}">
               <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -155,15 +164,6 @@
               </svg>
               <span class="ml-3">Community</span>
             </a>
-          </div>
-          <div class="relative -mx-2 w-24 mb-8 lg:hidden">
-            <select data-version-switcher class="appearance-none block bg-white pl-2 pr-8 py-1 text-gray-500 font-medium text-base focus:outline-none focus:text-gray-800">
-              <option value="v1">v1.0.1</option>
-              <option value="v0">v0.7.4</option>
-            </select>
-            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500">
-              <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-            </div>
           </div>
           @foreach ($page->navigation[$page->activeCategory()] as $sectionName => $sectionItems)
           <div class="mb-8">
--- source/_layouts/homepage.blade.php
+++ source/_layouts/homepage.blade.php
@@ -90,12 +90,21 @@
                 </button type="button">
 
                 {{-- Links section --}}
-                <div class="hidden lg:flex lg:w-1/4 px-6">
+                <div class="hidden lg:flex lg:items-center lg:justify-between xl:w-1/4 px-6">
+                  <div class="relative mr-4">
+                    <select data-version-switcher class="appearance-none block bg-transparent pl-2 pr-8 py-1 text-gray-500 font-medium text-base focus:outline-none focus:text-gray-800">
+                      <option value="v1">v{{ $page->version }}</option>
+                      <option value="v0">v0.7.4</option>
+                    </select>
+                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500">
+                      <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                    </div>
+                  </div>
                   <div class="flex justify-start items-center text-gray-500">
-                    <a class="block flex items-center hover:text-gray-700 mr-6" href="https://github.com/tailwindcss/tailwindcss">
+                    <a class="block flex items-center hover:text-gray-700 mr-5" href="https://github.com/tailwindcss/tailwindcss">
                       <svg class="fill-current w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>GitHub</title><path d="M10 0a10 10 0 0 0-3.16 19.49c.5.1.68-.22.68-.48l-.01-1.7c-2.78.6-3.37-1.34-3.37-1.34-.46-1.16-1.11-1.47-1.11-1.47-.9-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.9 1.52 2.34 1.08 2.91.83.1-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.94 0-1.1.39-1.99 1.03-2.69a3.6 3.6 0 0 1 .1-2.64s.84-.27 2.75 1.02a9.58 9.58 0 0 1 5 0c1.91-1.3 2.75-1.02 2.75-1.02.55 1.37.2 2.4.1 2.64.64.7 1.03 1.6 1.03 2.69 0 3.84-2.34 4.68-4.57 4.93.36.31.68.92.68 1.85l-.01 2.75c0 .26.18.58.69.48A10 10 0 0 0 10 0"/></svg>
                     </a>
-                    <a class="block flex items-center hover:text-gray-700 mr-6" href="https://twitter.com/tailwindcss">
+                    <a class="block flex items-center hover:text-gray-700 mr-5" href="https://twitter.com/tailwindcss">
                       <svg class="fill-current w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Twitter</title><path d="M6.29 18.25c7.55 0 11.67-6.25 11.67-11.67v-.53c.8-.59 1.49-1.3 2.04-2.13-.75.33-1.54.55-2.36.65a4.12 4.12 0 0 0 1.8-2.27c-.8.48-1.68.81-2.6 1a4.1 4.1 0 0 0-7 3.74 11.65 11.65 0 0 1-8.45-4.3 4.1 4.1 0 0 0 1.27 5.49C2.01 8.2 1.37 8.03.8 7.7v.05a4.1 4.1 0 0 0 3.3 4.03 4.1 4.1 0 0 1-1.86.07 4.1 4.1 0 0 0 3.83 2.85A8.23 8.23 0 0 1 0 16.4a11.62 11.62 0 0 0 6.29 1.84"/></svg>
                     </a>
                     <a class="block flex items-center hover:text-gray-700" href="/discord">
@@ -104,6 +113,7 @@
                   </div>
                 </div>
               </div>
+
 
             </div>
           </div>
@@ -143,6 +153,15 @@
       <div id="navWrapper" class="h-full overflow-y-auto scrolling-touch lg:h-auto lg:block lg:relative lg:sticky lg:top-0 bg-gray-100 lg:bg-transparent overflow-hidden">
         <div id="navGradient" class="hidden lg:block h-16 pointer-events-none absolute inset-x-0 z-10" style="background-image: linear-gradient(rgba(255,255,255,1), rgba(255,255,255,0));"></div>
         <nav id="nav" class="px-6 pt-6 overflow-y-auto text-base lg:text-sm lg:py-12 lg:pl-6 lg:pr-8 sticky?lg:h-screen">
+          <div class="relative -mx-2 w-24 mb-8 lg:hidden">
+            <select data-version-switcher class="appearance-none block bg-transparent pl-2 pr-8 py-1 text-gray-500 font-medium text-base focus:outline-none focus:text-gray-800">
+              <option value="v1">v1.0.1</option>
+              <option value="v0">v0.7.4</option>
+            </select>
+            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500">
+              <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+            </div>
+          </div>
           <div class="mb-10">
             <a href="/docs/installation" class="flex items-center px-2 -mx-2 py-1 hover:text-gray-900 font-medium text-gray-900">
               <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -172,15 +191,6 @@
               </svg>
               <span class="ml-3">Community</span>
             </a>
-          </div>
-          <div class="relative -mx-2 w-24 mb-8 lg:hidden">
-            <select data-version-switcher class="appearance-none block bg-transparent pl-2 pr-8 py-1 text-gray-500 font-medium text-base focus:outline-none focus:text-gray-800">
-              <option value="v1">v1.0.1</option>
-              <option value="v0">v0.7.4</option>
-            </select>
-            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500">
-              <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-            </div>
           </div>
           @foreach ($page->navigation['Documentation'] as $sectionName => $sectionItems)
           <div class="mb-8">
