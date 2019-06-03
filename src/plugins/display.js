d91e97678b60ef092b12320ceeb15c801f584a5f
Cosmin Popovici <hellocosmin@gmail.com>
2019-06-03 19:21:31 +0300

    Add more table display utilities.

--- src/plugins/display.js
+++ src/plugins/display.js
@@ -20,11 +20,29 @@ export default function() {
         '.table': {
           display: 'table',
         },
-        '.table-row': {
-          display: 'table-row',
+        '.table-caption': {
+          display: 'table-caption',
         },
         '.table-cell': {
           display: 'table-cell',
+        },
+        '.table-column': {
+          display: 'table-column',
+        },
+        '.table-column-group': {
+          display: 'table-column-group',
+        },
+        '.table-footer-group': {
+          display: 'table-footer-group',
+        },
+        '.table-header-group': {
+          display: 'table-header-group',
+        },
+        '.table-row-group': {
+          display: 'table-row-group',
+        },
+        '.table-row': {
+          display: 'table-row',
         },
         '.hidden': {
           display: 'none',
