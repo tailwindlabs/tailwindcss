b8c8fad7b064fe8fbf88e9228652c72465463773
Adam Wathan <adam.wathan@gmail.com>
2019-06-11 12:09:52 -0400

    Tweak text

--- source/course.blade.php
+++ source/course.blade.php
@@ -106,7 +106,7 @@
       </li>
       <li class="mt-3 flex">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-6 w-6 mt-px mr-3 flex-shrink-0"><circle cx="12" cy="12" r="10" class="text-green-200 fill-current"></circle><path d="M10 14.59l6.3-6.3a1 1 0 0 1 1.4 1.42l-7 7a1 1 0 0 1-1.4 0l-3-3a1 1 0 0 1 1.4-1.42l2.3 2.3z" class="text-green-600 fill-current"></path></svg>
-        <span class="-mt-px">Flexible positioning techniques building components that are adaptive instead of static and rigid</span>
+        <span class="-mt-px">Flexible positioning techniques for building components that are adaptive instead of static and rigid</span>
       </li>
       <li class="mt-3 flex">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-6 w-6 mt-px mr-3 flex-shrink-0"><circle cx="12" cy="12" r="10" class="text-green-200 fill-current"></circle><path d="M10 14.59l6.3-6.3a1 1 0 0 1 1.4 1.42l-7 7a1 1 0 0 1-1.4 0l-3-3a1 1 0 0 1 1.4-1.42l2.3 2.3z" class="text-green-600 fill-current"></path></svg>
