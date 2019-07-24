0824bba0113de976c51fc1fd2910fd9abc52fbfc
Nikita Dyomin <mykyta.domin@massive.se>
2019-07-24 17:44:20 +0200

    added ability of theme function to return default value of config object instead of [object Object]

--- src/lib/evaluateTailwindFunctions.js
+++ src/lib/evaluateTailwindFunctions.js
@@ -6,6 +6,9 @@ export default function(config) {
     functions: {
       theme: (path, ...defaultValue) => {
         return _.thru(_.get(config.theme, _.trim(path, `'"`), defaultValue), value => {
+          if (value.hasOwnProperty('default')) {
+            return value.default
+          }
           return Array.isArray(value) ? value.join(', ') : value
         })
       },
