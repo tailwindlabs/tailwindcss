c77d9bf56a888c25bc6b968fe21560d985e8c1d9
proFox <profox.rus@gmail.com>
2019-08-07 16:55:47 +0300

    Fix

--- src/plugins/placeholderPosition.js
+++ src/plugins/placeholderPosition.js
@@ -1,10 +1,9 @@
 import _ from 'lodash'
-import flattenColorPalette from '../util/flattenColorPalette'
 
 export default function() {
   return function({ addUtilities, e, theme, variants }) {
     const utilities = _.fromPairs(
-      _.map(flattenColorPalette(theme('placeholderPosition')), (value, modifier) => {
+      _.map(theme('placeholderPosition'), (value, modifier) => {
         return [
           `.${e(`placeholder-${modifier}`)}::placeholder`,
           {
