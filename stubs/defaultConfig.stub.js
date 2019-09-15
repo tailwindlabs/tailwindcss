95d4a3a25fa7ecb16ea03833cf9d0038febc4173
Matt Schlobohm <mattschlobohm@users.noreply.github.com>
2019-09-15 16:07:30 -0500

    change 0% to 0 on flex["1"] in default config

--- stubs/defaultConfig.stub.js
+++ stubs/defaultConfig.stub.js
@@ -206,7 +206,7 @@ module.exports = {
       current: 'currentColor',
     },
     flex: {
-      '1': '1 1 0%',
+      '1': '1 1 0',
       auto: '1 1 auto',
       initial: '0 1 auto',
       none: 'none',
