b630d965f6fb2d470005ad49a66b2e53c3e268d7
Adam Wathan <adam.wathan@gmail.com>
2019-12-24 06:54:35 -0500

    Support scaling x and y dimensions independently

--- src/plugins/scale.js
+++ src/plugins/scale.js
@@ -1,5 +1,9 @@
 import createUtilityPlugin from '../util/createUtilityPlugin'
 
 export default function() {
-  return createUtilityPlugin('scale', [['scale', ['--transform-scale']]])
+  return createUtilityPlugin('scale', [
+    ['scale', ['--transform-scale-x', '--transform-scale-y']],
+    ['scale-x', ['--transform-scale-x']],
+    ['scale-y', ['--transform-scale-y']],
+  ])
 }
