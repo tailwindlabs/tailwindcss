375a87aa0d7f7f5e1b5bf8e2ba6d18f11cc5cfbf
Adam Wathan <adam.wathan@gmail.com>
2019-12-24 06:25:28 -0500

    Add script for rebuilding fixtures

---
+++ scripts/rebuildFixtures.js
@@ -0,0 +1,48 @@
+import fs from 'fs'
+import postcss from 'postcss'
+import tailwind from '..'
+import CleanCSS from 'clean-css'
+
+function build({ from, to, config }) {
+  return new Promise((resolve, reject) => {
+    console.log(`Processing ./${from}...`)
+
+    fs.readFile(`./${from}`, (err, css) => {
+      if (err) throw err
+
+      return postcss([tailwind(config)])
+        .process(css, {
+          from: undefined,
+        })
+        .then(result => {
+          fs.writeFileSync(`./${to}`, result.css)
+          return result
+        })
+        .then(resolve)
+        .catch(error => {
+          console.log(error)
+          reject()
+        })
+    })
+  })
+}
+
+console.info('\nRebuilding fixtures...\n')
+
+Promise.all([
+  build({
+    from: '__tests__/fixtures/tailwind-input.css',
+    to: '__tests__/fixtures/tailwind-output.css',
+    config: {},
+  }),
+  build({
+    from: '__tests__/fixtures/tailwind-input.css',
+    to: '__tests__/fixtures/tailwind-output-important.css',
+    config: { important: true },
+  }),
+]).then(() => {
+  console.log('\nFinished rebuilding fixtures.')
+  console.log(
+    '\nPlease triple check that the fixture output matches what you expect before committing this change.'
+  )
+})
