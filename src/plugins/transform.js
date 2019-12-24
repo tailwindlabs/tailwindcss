dee24466b2bdf74fba544e9165f16ca6db116d3a
Adam Wathan <adam.wathan@gmail.com>
2019-12-24 06:54:35 -0500

    Support scaling x and y dimensions independently

--- src/plugins/transform.js
+++ src/plugins/transform.js
@@ -3,12 +3,14 @@ export default function() {
     addUtilities(
       {
         '.transform': {
-          '--transform-scale': '1',
+          '--transform-scale-x': '1',
+          '--transform-scale-y': '1',
           '--transform-rotate': '0',
           '--transform-translate-x': '0',
           '--transform-translate-y': '0',
           transform: [
-            'scale(var(--transform-scale))',
+            'scaleX(var(--transform-scale-x))',
+            'scaleY(var(--transform-scale-y))',
             'rotate(var(--transform-rotate))',
             'translateX(var(--transform-translate-x))',
             'translateY(var(--transform-translate-y))',
