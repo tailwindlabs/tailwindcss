d5199e40756691b149c6a1b998bf62aedd80f30d
Adam Wathan <adam.wathan@gmail.com>
2020-01-08 10:09:00 -0500

    Rename transition-duration utilities to `duration-{ms}`

--- src/plugins/transitionDuration.js
+++ src/plugins/transitionDuration.js
@@ -1,5 +1,5 @@
 import createUtilityPlugin from '../util/createUtilityPlugin'
 
 export default function() {
-  return createUtilityPlugin('transitionDuration', [['transition', ['transitionDuration']]])
+  return createUtilityPlugin('transitionDuration', [['duration', ['transitionDuration']]])
 }
