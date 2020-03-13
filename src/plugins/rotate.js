6a797cab324413fbbcda6b1f760fabee37c0d139
Daniël Koek <contact@danielkoek.net>
2020-03-13 14:10:42 +0100

    Extending rotate utlity and fixing some linter issues when running with vscode

--- src/plugins/rotate.js
+++ src/plugins/rotate.js
@@ -1,5 +1,9 @@
 import createUtilityPlugin from '../util/createUtilityPlugin'
 
 export default function() {
-  return createUtilityPlugin('rotate', [['rotate', ['--transform-rotate']]])
+  return createUtilityPlugin('rotate', [
+    ['rotate', ['--transform-rotate-x', '--transform-rotate-y']],
+    ['rotate-x', ['--transform-rotate-x']],
+    ['rotate-y', ['--transform-rotate-y']],
+  ])
 }
