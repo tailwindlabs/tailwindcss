c9f4701332feaeeb22bdd460b3d0a0d1d54fa835
Daniël Koek <contact@danielkoek.net>
2020-03-13 14:26:56 +0100

    Update translate.js

--- src/plugins/translate.js
+++ src/plugins/translate.js
@@ -2,6 +2,7 @@ import createUtilityPlugin from '../util/createUtilityPlugin'
 
 export default function() {
   return createUtilityPlugin('translate', [
+    ['translate', ['--transform-translate']],
     ['translate-x', ['--transform-translate-x']],
     ['translate-y', ['--transform-translate-y']],
   ])
