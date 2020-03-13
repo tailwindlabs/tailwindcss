5231bbed51e7ebe591382f11171416d8e4f4b3c2
Daniël Koek <contact@danielkoek.net>
2020-03-13 14:37:09 +0100

    Small changes again, now so that everything is using the correct vars

--- src/plugins/skew.js
+++ src/plugins/skew.js
@@ -2,6 +2,7 @@ import createUtilityPlugin from '../util/createUtilityPlugin'
 
 export default function() {
   return createUtilityPlugin('skew', [
+    ['skew', ['--transform-skew-x', '--transform-skew-y']],
     ['skew-x', ['--transform-skew-x']],
     ['skew-y', ['--transform-skew-y']],
   ])
--- src/plugins/translate.js
+++ src/plugins/translate.js
@@ -2,7 +2,7 @@ import createUtilityPlugin from '../util/createUtilityPlugin'
 
 export default function() {
   return createUtilityPlugin('translate', [
-    ['translate', ['--transform-translate']],
+    ['translate', ['--transform-translate-x', '--transform-translate-y']],
     ['translate-x', ['--transform-translate-x']],
     ['translate-y', ['--transform-translate-y']],
   ])
