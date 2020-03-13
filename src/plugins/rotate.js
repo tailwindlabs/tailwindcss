a4061efb33db95880a70b01a9aa8643d3355212b
Daniël Koek <contact@danielkoek.net>
2020-03-13 14:16:08 +0100

    No need to do both x and y if you can just do the root one

--- src/plugins/rotate.js
+++ src/plugins/rotate.js
@@ -2,7 +2,7 @@ import createUtilityPlugin from '../util/createUtilityPlugin'
 
 export default function() {
   return createUtilityPlugin('rotate', [
-    ['rotate', ['--transform-rotate-x', '--transform-rotate-y']],
+    ['rotate', ['--transform-rotate']],
     ['rotate-x', ['--transform-rotate-x']],
     ['rotate-y', ['--transform-rotate-y']],
   ])
--- src/plugins/scale.js
+++ src/plugins/scale.js
@@ -2,7 +2,7 @@ import createUtilityPlugin from '../util/createUtilityPlugin'
 
 export default function() {
   return createUtilityPlugin('scale', [
-    ['scale', ['--transform-scale-x', '--transform-scale-y']],
+    ['scale', ['--transform-scale']],
     ['scale-x', ['--transform-scale-x']],
     ['scale-y', ['--transform-scale-y']],
   ])
