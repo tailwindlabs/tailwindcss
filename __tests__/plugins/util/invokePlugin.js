921e2ff2025e7f385cd0c24c1009bfc06b4ad8ef
Adam Wathan <adam.wathan@gmail.com>
2020-04-16 09:25:21 -0400

    Add helper for testing utility plugins

---
+++ __tests__/plugins/util/invokePlugin.js
@@ -0,0 +1,29 @@
+import _ from 'lodash'
+import escapeClassName from '../../../src/util/escapeClassName'
+
+export default function(plugin, config) {
+  const addedUtilities = []
+
+  const getConfigValue = (path, defaultValue) => _.get(config, path, defaultValue)
+  const pluginApi = {
+    config: getConfigValue,
+    e: escapeClassName,
+    theme: (path, defaultValue) => getConfigValue(`theme.${path}`, defaultValue),
+    variants: (path, defaultValue) => {
+      if (_.isArray(config.variants)) {
+        return config.variants
+      }
+
+      return getConfigValue(`variants.${path}`, defaultValue)
+    },
+    addUtilities(utilities, variants) {
+      addedUtilities.push([utilities, variants])
+    },
+  }
+
+  plugin(pluginApi)
+
+  return {
+    utilities: addedUtilities,
+  }
+}
