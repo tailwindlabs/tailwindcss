a75b6901aa4c8f245b62b959ef5f3fcdf47ba07e
Adam Wathan <adam.wathan@gmail.com>
2020-04-20 13:34:00 -0400

    Support reverse order for space and divide utilities

--- src/plugins/divideWidth.js
+++ src/plugins/divideWidth.js
@@ -3,20 +3,34 @@ import _ from 'lodash'
 export default function() {
   return function({ addUtilities, e, theme, variants }) {
     const generators = [
-      (value, modifier) => ({
+      (size, modifier) => ({
         [`.${e(`divide-y${modifier}`)} > :not(template) ~ :not(template)`]: {
-          'border-top-width': `${value}`,
+          '--divide-y-reverse': '0',
+          'border-top': `calc(${size} * calc(1 - var(--divide-y-reverse)))`,
+          'border-bottom': `calc(${size} * var(--divide-y-reverse))`,
         },
         [`.${e(`divide-x${modifier}`)} > :not(template) ~ :not(template)`]: {
-          'border-left-width': `${value}`,
+          '--divide-x-reverse': '0',
+          'border-right': `calc(${size} * var(--divide-y-reverse))`,
+          'border-left': `calc(${size} * calc(1 - var(--divide-y-reverse)))`,
         },
       }),
     ]
 
     const utilities = _.flatMap(generators, generator => {
-      return _.flatMap(theme('divideWidth'), (value, modifier) => {
-        return generator(value, modifier === 'default' ? '' : `-${modifier}`)
-      })
+      return [
+        ..._.flatMap(theme('divideWidth'), (value, modifier) => {
+          return generator(value, modifier === 'default' ? '' : `-${modifier}`)
+        }),
+        {
+          '.divide-y-reverse > :not(template) ~ :not(template)': {
+            '--divide-y-reverse': '1',
+          },
+          '.divide-x-reverse > :not(template) ~ :not(template)': {
+            '--divide-x-reverse': '1',
+          },
+        },
+      ]
     })
 
     addUtilities(utilities, variants('divideWidth'))
