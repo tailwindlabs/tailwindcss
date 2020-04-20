a14ea02ff0d04d386488be4fd3bafb71e3182743
Adam Wathan <adam.wathan@gmail.com>
2020-04-20 14:18:04 -0400

    Use border-width for divide, not border

--- __tests__/plugins/divideWidth.test.js
+++ __tests__/plugins/divideWidth.test.js
@@ -22,33 +22,33 @@ test('generating divide width utilities', () => {
       {
         '.divide-y > :not(template) ~ :not(template)': {
           '--divide-y-reverse': '0',
-          'border-top': 'calc(1px * calc(1 - var(--divide-y-reverse)))',
-          'border-bottom': 'calc(1px * var(--divide-y-reverse))',
+          'border-top-width': 'calc(1px * calc(1 - var(--divide-y-reverse)))',
+          'border-bottom-width': 'calc(1px * var(--divide-y-reverse))',
         },
         '.divide-x > :not(template) ~ :not(template)': {
           '--divide-x-reverse': '0',
-          'border-right': 'calc(1px * var(--divide-y-reverse))',
-          'border-left': 'calc(1px * calc(1 - var(--divide-y-reverse)))',
+          'border-right-width': 'calc(1px * var(--divide-y-reverse))',
+          'border-left-width': 'calc(1px * calc(1 - var(--divide-y-reverse)))',
         },
         '.divide-y-2 > :not(template) ~ :not(template)': {
           '--divide-y-reverse': '0',
-          'border-top': 'calc(2px * calc(1 - var(--divide-y-reverse)))',
-          'border-bottom': 'calc(2px * var(--divide-y-reverse))',
+          'border-top-width': 'calc(2px * calc(1 - var(--divide-y-reverse)))',
+          'border-bottom-width': 'calc(2px * var(--divide-y-reverse))',
         },
         '.divide-x-2 > :not(template) ~ :not(template)': {
           '--divide-x-reverse': '0',
-          'border-right': 'calc(2px * var(--divide-y-reverse))',
-          'border-left': 'calc(2px * calc(1 - var(--divide-y-reverse)))',
+          'border-right-width': 'calc(2px * var(--divide-y-reverse))',
+          'border-left-width': 'calc(2px * calc(1 - var(--divide-y-reverse)))',
         },
         '.divide-y-4 > :not(template) ~ :not(template)': {
           '--divide-y-reverse': '0',
-          'border-top': 'calc(4px * calc(1 - var(--divide-y-reverse)))',
-          'border-bottom': 'calc(4px * var(--divide-y-reverse))',
+          'border-top-width': 'calc(4px * calc(1 - var(--divide-y-reverse)))',
+          'border-bottom-width': 'calc(4px * var(--divide-y-reverse))',
         },
         '.divide-x-4 > :not(template) ~ :not(template)': {
           '--divide-x-reverse': '0',
-          'border-right': 'calc(4px * var(--divide-y-reverse))',
-          'border-left': 'calc(4px * calc(1 - var(--divide-y-reverse)))',
+          'border-right-width': 'calc(4px * var(--divide-y-reverse))',
+          'border-left-width': 'calc(4px * calc(1 - var(--divide-y-reverse)))',
         },
         '.divide-y-reverse > :not(template) ~ :not(template)': {
           '--divide-y-reverse': '1',
