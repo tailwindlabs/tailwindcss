9c5e5bd0af6674196f746eed2a523741f7bfbb21
Ahmad Abu-Aysheh <ahmadabuaysheh@gmail.com>
2020-05-22 05:04:26 +0300

    Change default purge config value to be a new object instead of array

--- stubs/defaultConfig.stub.js
+++ stubs/defaultConfig.stub.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: {},
   target: 'relaxed',
   prefix: '',
   important: false,
--- stubs/simpleConfig.stub.js
+++ stubs/simpleConfig.stub.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: {},
   theme: {
     extend: {},
   },
