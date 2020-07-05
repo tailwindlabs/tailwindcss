1ea125c98b2aad544658807d4924338d876daa2b
Jason Kurian <JaKXz@users.noreply.github.com>
2020-07-05 14:58:26 -0400

    use percentage value for rounded-full

--- stubs/defaultConfig.stub.js
+++ stubs/defaultConfig.stub.js
@@ -179,7 +179,7 @@ module.exports = {
       default: '0.25rem',
       md: '0.375rem',
       lg: '0.5rem',
-      full: '9999px',
+      full: '50%',
     },
     borderWidth: {
       default: '1px',
