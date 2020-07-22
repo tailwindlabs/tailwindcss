09f1c2f971c7b8db3ae60b9ecadc304fc1e11898
Brad Cornes <bradlc41@gmail.com>
2020-07-22 17:18:14 +0100

    Update syntax highlighting

--- remark/withSyntaxHighlighting.js
+++ remark/withSyntaxHighlighting.js
@@ -1,14 +1,17 @@
 const visit = require('unist-util-visit')
 const Prism = require('prismjs')
+const loadLanguages = require('prismjs/components/')
+loadLanguages(['diff'])
+require('./prism-diff-highlight')(Prism)
 
 module.exports.withSyntaxHighlighting = () => {
   return (tree) => {
     visit(tree, 'code', (node) => {
-      if (['html', 'css', 'javascript', 'js'].includes(node.lang)) {
+      if (node.lang !== null) {
         node.type = 'html'
         node.value = [
-          `<pre class="language-${node.value}">`,
-          `<code class="language-${node.value}">`,
+          `<pre class="language-${node.lang}">`,
+          `<code class="language-${node.lang}">`,
           Prism.highlight(node.value, Prism.languages[node.lang], node.lang),
           '</code>',
           '</pre>',
