6c9bf986f08af9a3dc9bba61eabe5f74b49e608d
Brad Cornes <bradlc41@gmail.com>
2020-07-23 11:40:40 +0100

    Add custom document with lang attribute

---
+++ src/pages/_document.js
@@ -0,0 +1,20 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class Document extends NextDocument {
+  static async getInitialProps(ctx) {
+    const initialProps = await NextDocument.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
