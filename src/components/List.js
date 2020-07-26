b5c945d7cc98c4bbbac186b0b26547a036419161
Brad Cornes <bradlc41@gmail.com>
2020-07-26 16:24:24 +0100

    class -> className

--- src/components/List.js
+++ src/components/List.js
@@ -9,11 +9,11 @@ export function List({ children }) {
 export function ListItemGood({ children }) {
   return (
     <li className="flex items-center">
-      <svg viewBox="0 0 24 24" class="h-6 w-6 mr-2 flex-shrink-0">
-        <circle cx="12" cy="12" r="10" class="text-green-200 fill-current" />
+      <svg viewBox="0 0 24 24" className="h-6 w-6 mr-2 flex-shrink-0">
+        <circle cx="12" cy="12" r="10" className="text-green-200 fill-current" />
         <path
           d="M10 14.59l6.3-6.3a1 1 0 0 1 1.4 1.42l-7 7a1 1 0 0 1-1.4 0l-3-3a1 1 0 0 1 1.4-1.42l2.3 2.3z"
-          class="text-green-600 fill-current"
+          className="text-green-600 fill-current"
         />
       </svg>
       <span>{children}</span>
