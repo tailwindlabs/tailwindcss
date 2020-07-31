6b0b54ab83912d07faf9cb0f505fa3c3f61b8720
Brad Cornes <bradlc41@gmail.com>
2020-07-31 16:55:24 +0100

    Allow hidden headings

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,7 +1,7 @@
 import { useEffect, useContext, useRef } from 'react'
 import { DocumentContext } from '@/layouts/ContentsLayout'
 
-export function Heading({ level, id, children, number, badge }) {
+export function Heading({ level, id, children, number, badge, hidden = false }) {
   let Component = `h${level}`
   const { updateHeading } = useContext(DocumentContext)
 
@@ -18,20 +18,22 @@ export function Heading({ level, id, children, number, badge }) {
   }, [id, updateHeading])
 
   return (
-    <Component className="group flex" id={id} ref={ref}>
-      {/* eslint-disable-next-line */}
-      <a
-        href={`#${id}`}
-        className="absolute text-gray-500 no-underline after:hash opacity-0 group-hover:opacity-100"
-        style={{ marginLeft: '-1em', paddingRight: '0.5em' }}
-        aria-label="Anchor"
-      />
+    <Component className={`group flex ${hidden ? '-mb-6' : ''}`} id={id} ref={ref}>
+      {!hidden && (
+        // eslint-disable-next-line
+        <a
+          href={`#${id}`}
+          className="absolute text-gray-500 no-underline after:hash opacity-0 group-hover:opacity-100"
+          style={{ marginLeft: '-1em', paddingRight: '0.5em' }}
+          aria-label="Anchor"
+        />
+      )}
       {number && (
         <span className="bg-gray-200 h-6 inline-flex items-center justify-center rounded-full text-gray-700 text-lg w-6 mr-3 flex-none">
           {number}
         </span>
       )}
-      {children}
+      {hidden ? <span className="sr-only">{children}</span> : children}
       {badge && (
         <span className="ml-3 inline-flex items-center px-3 py-1 rounded-full text-sm font-medium leading-4 bg-green-150 text-green-900">
           {badge}
