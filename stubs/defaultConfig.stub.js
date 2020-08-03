5f545eaf8cc80547a474fa6a75fb71d8af293fe4
Martijn Cuppens <martijn.cuppens@gmail.com>
2020-08-03 20:11:15 +0200

    Remove redundant animation states

--- stubs/defaultConfig.stub.js
+++ stubs/defaultConfig.stub.js
@@ -631,15 +631,12 @@ module.exports = {
     },
     keyframes: {
       spin: {
-        from: { transform: 'rotate(0deg)' },
         to: { transform: 'rotate(360deg)' },
       },
       ping: {
-        '0%': { transform: 'scale(1)', opacity: '1' },
         '75%, 100%': { transform: 'scale(2)', opacity: '0' },
       },
       pulse: {
-        '0%, 100%': { opacity: '1' },
         '50%': { opacity: '.5' },
       },
       bounce: {
