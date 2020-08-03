c65fbf677769ec64da40dfaae3f1cb038b6cf4ab
Martijn Cuppens <martijn.cuppens@gmail.com>
2020-08-03 20:26:24 +0200

    Simplify `translateY(0)` to `none`

--- stubs/defaultConfig.stub.js
+++ stubs/defaultConfig.stub.js
@@ -645,7 +645,7 @@ module.exports = {
           animationTimingFunction: 'cubic-bezier(0.8,0,1,1)',
         },
         '50%': {
-          transform: 'translateY(0)',
+          transform: 'none',
           animationTimingFunction: 'cubic-bezier(0,0,0.2,1)',
         },
       },
