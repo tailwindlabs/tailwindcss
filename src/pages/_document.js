9932b158be44c403b0254457d06c5a602e4f61ff
Brad Cornes <bradlc41@gmail.com>
2020-08-07 11:16:28 +0100

    Add favicons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,14 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en" className="bg-white antialiased">
-        <Head />
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00b4b6" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <body>
           <Main />
           <NextScript />
