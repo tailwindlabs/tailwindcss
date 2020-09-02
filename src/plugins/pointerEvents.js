e97172f363148cce8939c0c3f970a8430c83df0c
Ranieri Althoff <1993083+ranisalt@users.noreply.github.com>
2020-09-02 19:59:13 +0000

    Add pointer-events: initial

--- src/plugins/pointerEvents.js
+++ src/plugins/pointerEvents.js
@@ -4,6 +4,7 @@ export default function() {
       {
         '.pointer-events-none': { 'pointer-events': 'none' },
         '.pointer-events-auto': { 'pointer-events': 'auto' },
+        '.pointer-events-initial': { 'pointer-events': 'initial' },
       },
       variants('pointerEvents')
     )
