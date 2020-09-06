987982ee75f9ebfa73ca556738ae3b85d9c045c0
Federico Ciardi <fed.ciardi@gmail.com>
2020-09-06 14:42:55 +0200

    feat: add default 'transition-duration' to transitionProperty utility

--- src/plugins/transitionProperty.js
+++ src/plugins/transitionProperty.js
@@ -1,5 +1,19 @@
-import createUtilityPlugin from '../util/createUtilityPlugin'
+import _ from 'lodash'
 
 export default function() {
-  return createUtilityPlugin('transitionProperty', [['transition', ['transitionProperty']]])
+  return function({ addUtilities, e, theme, variants }) {
+    const utilities = _.fromPairs(
+      _.map(theme('transitionProperty'), (value, modifier) => {
+        return [
+          `.${e(`transition-${modifier}`)}`,
+          {
+            transitionProperty: value,
+            transitionDuration: '250ms',
+          },
+        ]
+      })
+    )
+
+    addUtilities(utilities, variants('transitionProperty'))
+  }
 }
