b84b714be17054015660bf3e8173c7ecc83d6edc
Adam Wathan <adam.wathan@gmail.com>
2020-10-08 15:24:55 -0400

    Make outline configurable, add white/black dotted outlines

--- src/plugins/outline.js
+++ src/plugins/outline.js
@@ -1,10 +1,21 @@
+import _ from 'lodash'
+
 export default function() {
-  return function({ addUtilities, variants }) {
-    addUtilities(
-      {
-        '.outline-none': { outline: '2px solid transparent', outlineOffset: '1px' },
-      },
-      variants('outline')
+  return function({ addUtilities, e, theme, variants }) {
+    const utilities = _.fromPairs(
+      _.map(theme('outline'), (value, modifier) => {
+        const [outline, outlineOffset = '0'] = Array.isArray(value) ? value : [value]
+
+        return [
+          `.${e(`outline-${modifier}`)}`,
+          {
+            outline,
+            outlineOffset,
+          },
+        ]
+      })
     )
+
+    addUtilities(utilities, variants('outline'))
   }
 }
