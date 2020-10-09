91afd6759a5f7b63d9a66b52d96788167719f509
Adam Wathan <adam.wathan@gmail.com>
2020-10-09 08:16:44 -0400

    Add `xl`, `2xl`, and `3xl` border radius values

--- stubs/defaultConfig.stub.js
+++ stubs/defaultConfig.stub.js
@@ -192,6 +192,9 @@ module.exports = {
       default: '0.25rem',
       md: '0.375rem',
       lg: '0.5rem',
+      xl: '0.75rem',
+      '2xl': '1rem',
+      '3xl': '1.5rem',
       full: '9999px',
     },
     borderWidth: {
