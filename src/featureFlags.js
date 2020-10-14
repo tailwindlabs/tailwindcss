ae8b634f332a4290436ba13a8eac6f0a42853d14
Adam Wathan <adam.wathan@gmail.com>
2020-10-14 16:54:13 -0400

    Move truncate to textOverflow (#2562)
    * Move truncate to textOverflow
    
    * Update changelog

--- src/featureFlags.js
+++ src/featureFlags.js
@@ -8,6 +8,7 @@ const featureFlags = {
     'purgeLayersByDefault',
     'defaultLineHeights',
     'standardFontWeights',
+    'moveTruncateToTextOverflow',
   ],
   experimental: [
     'uniformColorPalette',
--- src/plugins/textOverflow.js
+++ src/plugins/textOverflow.js
@@ -1,7 +1,18 @@
+import { flagEnabled } from '../featureFlags'
+
 export default function() {
-  return function({ addUtilities, variants }) {
+  return function({ addUtilities, variants, config }) {
     addUtilities(
       {
+        ...(flagEnabled(config(), 'moveTruncateToTextOverflow')
+          ? {
+              '.truncate': {
+                overflow: 'hidden',
+                'text-overflow': 'ellipsis',
+                'white-space': 'nowrap',
+              },
+            }
+          : {}),
         '.overflow-ellipsis': { 'text-overflow': 'ellipsis' },
         '.overflow-clip': { 'text-overflow': 'clip' },
       },
