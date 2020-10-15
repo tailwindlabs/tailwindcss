f28dd9772af2a08daf3b2c3daa4739c4bb625f07
Adam Wathan <adam.wathan@gmail.com>
2020-10-15 15:27:10 -0400

    Switch from normalize.css to modern-normalize

--- src/plugins/preflight.js
+++ src/plugins/preflight.js
@@ -3,7 +3,9 @@ import postcss from 'postcss'
 
 export default function() {
   return function({ addBase }) {
-    const normalizeStyles = postcss.parse(fs.readFileSync(require.resolve('normalize.css'), 'utf8'))
+    const normalizeStyles = postcss.parse(
+      fs.readFileSync(require.resolve('modern-normalize'), 'utf8')
+    )
     const preflightStyles = postcss.parse(fs.readFileSync(`${__dirname}/css/preflight.css`, 'utf8'))
     addBase([...normalizeStyles.nodes, ...preflightStyles.nodes])
   }
