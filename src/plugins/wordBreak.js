fca7af76d56f279df028fb2f1049a2255e7fdbd0
Adam Wathan <adam.wathan@gmail.com>
2020-10-16 15:42:24 -0400

    Remove word-wrap fallback for IE11

--- src/plugins/wordBreak.js
+++ src/plugins/wordBreak.js
@@ -5,14 +5,10 @@ export default function () {
     addUtilities(
       {
         '.break-normal': {
-          // For IE 11, remove 'word-wrap' when we have a 'modern' mode
-          'word-wrap': 'normal',
           'overflow-wrap': 'normal',
           'word-break': 'normal',
         },
         '.break-words': {
-          // For IE 11, remove 'word-wrap' when we have a 'modern' mode
-          'word-wrap': 'break-word',
           'overflow-wrap': 'break-word',
         },
         '.break-all': { 'word-break': 'break-all' },
