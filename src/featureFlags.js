69271559560321b52b93d93c958c141e8cdefacb
Adam Wathan <adam.wathan@gmail.com>
2020-10-16 14:47:22 -0400

    Don't warn about future flags

--- src/featureFlags.js
+++ src/featureFlags.js
@@ -42,14 +42,6 @@ function experimentalFlagsEnabled(config) {
   )
 }
 
-function futureFlagsAvailable(config) {
-  if (config.future === 'all') {
-    return []
-  }
-
-  return featureFlags.future.filter(flag => !_.has(config, ['future', flag]))
-}
-
 export function issueFlagNotices(config) {
   if (process.env.JEST_WORKER_ID !== undefined) {
     return
@@ -63,18 +55,6 @@ export function issueFlagNotices(config) {
     log.warn([
       `You have enabled experimental features: ${changes}`,
       'Experimental features are not covered by semver, may introduce breaking changes, and can change at any time.',
-    ])
-  }
-
-  if (futureFlagsAvailable(config).length > 0) {
-    const changes = futureFlagsAvailable(config)
-      .map(s => chalk.magenta(s))
-      .join(', ')
-
-    log.risk([
-      `There are upcoming breaking changes: ${changes}`,
-      'We highly recommend opting-in to these changes now to simplify upgrading Tailwind in the future.',
-      'https://tailwindcss.com/docs/upcoming-changes',
     ])
   }
 }
