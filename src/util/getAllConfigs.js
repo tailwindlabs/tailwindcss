051c54d526e814b155893b3342613ff394f722bc
Adam Wathan <adam.wathan@gmail.com>
2020-10-17 20:45:10 -0400

    Add line-heights to font-size utilities by default

--- src/util/getAllConfigs.js
+++ src/util/getAllConfigs.js
@@ -2,7 +2,6 @@ import defaultConfig from '../../stubs/defaultConfig.stub.js'
 import { flagEnabled } from '../featureFlags'
 import uniformColorPalette from '../flagged/uniformColorPalette.js'
 import extendedSpacingScale from '../flagged/extendedSpacingScale.js'
-import defaultLineHeights from '../flagged/defaultLineHeights.js'
 import extendedFontSizeScale from '../flagged/extendedFontSizeScale.js'
 import darkModeVariant from '../flagged/darkModeVariant.js'
 import standardFontWeights from '../flagged/standardFontWeights'
@@ -17,7 +16,6 @@ export default function getAllConfigs(config, defaultPresets = [defaultConfig]) 
   const features = {
     uniformColorPalette,
     extendedSpacingScale,
-    defaultLineHeights,
     extendedFontSizeScale,
     standardFontWeights,
     darkModeVariant,
