4959a99f7729d7f20da06c56d0c45cbdf1342d3c
Adam Wathan <adam.wathan@gmail.com>
2020-10-17 20:37:33 -0400

    Remove deprecated gap utilities

--- src/featureFlags.js
+++ src/featureFlags.js
@@ -4,7 +4,6 @@ import log from './util/log'
 
 const featureFlags = {
   future: [
-    'removeDeprecatedGapUtilities',
     'purgeLayersByDefault',
     'defaultLineHeights',
     'standardFontWeights',
