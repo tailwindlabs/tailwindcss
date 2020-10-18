dfacab452b992197bba00a62d1cca6edba0e089b
20lives <lior@dotcore.co.il>
2020-10-18 15:55:37 +0300

    Simplifying animation utilities generation

--- src/plugins/animation.js
+++ src/plugins/animation.js
@@ -4,23 +4,13 @@ import nameClass from '../util/nameClass'
 export default function () {
   return function ({ addUtilities, theme, variants }) {
     const keyframesConfig = theme('keyframes')
-    const keyframesStyles = _.fromPairs(
-      _.toPairs(keyframesConfig).map(([name, keyframes]) => {
-        return [`@keyframes ${name}`, keyframes]
-      })
-    )
+    const keyframesStyles = _.mapKeys(keyframesConfig, (_keyframes, name) => `@keyframes ${name}`)
     addUtilities(keyframesStyles, { respectImportant: false })
 
     const animationConfig = theme('animation')
-    const utilities = _.fromPairs(
-      _.toPairs(animationConfig).map(([suffix, animation]) => {
-        return [
-          nameClass('animate', suffix),
-          {
-            animation,
-          },
-        ]
-      })
+    const utilities = _.mapValues(
+      _.mapKeys(animationConfig, (_animation, suffix) => nameClass('animate', suffix)),
+      (animation) => ({ animation })
     )
     addUtilities(utilities, variants('animation'))
   }
