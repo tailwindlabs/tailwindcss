ec1eec37158a32371cb8eed8ec43675188bbcd99
Robin Malfait <malfait.robin@gmail.com>
2020-10-21 21:19:09 +0200

    only prefix animation names that are defined

--- __tests__/plugins/animation.test.js
+++ __tests__/plugins/animation.test.js
@@ -80,13 +80,52 @@ test('defining animation and keyframes with prefix', () => {
         }
       }
     }
-    
+
     @layer utilities {
       @variants {
         .tw-animate-none { animation: none; }
         .tw-animate-spin { animation: tw-spin 1s linear infinite; }
         .tw-animate-ping { animation: tw-ping 1s cubic-bezier(0, 0, 0.2, 1) infinite; }
       }
-    }  
+    }
+  `)
+})
+
+test('defining animation and keyframes with prefix (skip undefined animations)', () => {
+  const config = {
+    prefix: 'tw-',
+    theme: {
+      animation: {
+        none: 'none',
+        spin: 'spin 1s linear infinite',
+        ping: 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
+      },
+      keyframes: {
+        spin: { to: { transform: 'rotate(360deg)' } },
+      },
+    },
+    variants: {
+      animation: [],
+    },
+  }
+
+  const { utilities } = processPlugins([plugin()], config)
+
+  expect(css(utilities)).toMatchCss(`
+    @layer utilities {
+      @variants {
+        @keyframes tw-spin {
+          to { transform: rotate(360deg); }
+        }
+      }
+    }
+
+    @layer utilities {
+      @variants {
+        .tw-animate-none { animation: none; }
+        .tw-animate-spin { animation: tw-spin 1s linear infinite; }
+        .tw-animate-ping { animation: ping 1s cubic-bezier(0, 0, 0.2, 1) infinite; }
+      }
+    }
   `)
 })
