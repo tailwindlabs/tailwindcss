37addbe0e0cef9664f8cec043e2bc0dc200ee6f7
Adam Wathan <adam.wathan@gmail.com>
2020-10-23 14:51:58 -0400

    Rename whitespace-no-wrap to whitespace-nowrap

--- src/plugins/whitespace.js
+++ src/plugins/whitespace.js
@@ -3,7 +3,7 @@ export default function () {
     addUtilities(
       {
         '.whitespace-normal': { 'white-space': 'normal' },
-        '.whitespace-no-wrap': { 'white-space': 'nowrap' },
+        '.whitespace-nowrap': { 'white-space': 'nowrap' },
         '.whitespace-pre': { 'white-space': 'pre' },
         '.whitespace-pre-line': { 'white-space': 'pre-line' },
         '.whitespace-pre-wrap': { 'white-space': 'pre-wrap' },
