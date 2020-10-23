b27608557aa36994c7e1bdeeabfb100380638eda
Adam Wathan <adam.wathan@gmail.com>
2020-10-23 13:23:21 -0400

    Include default config by default in presets (#2660)

--- src/util/getAllConfigs.js
+++ src/util/getAllConfigs.js
@@ -2,9 +2,9 @@ import defaultConfig from '../../stubs/defaultConfig.stub.js'
 import { flagEnabled } from '../featureFlags'
 import { flatMap, get } from 'lodash'
 
-export default function getAllConfigs(config, defaultPresets = [defaultConfig]) {
-  const configs = flatMap([...get(config, 'presets', defaultPresets)].reverse(), (preset) => {
-    return getAllConfigs(preset, [])
+export default function getAllConfigs(config) {
+  const configs = flatMap([...get(config, 'presets', [defaultConfig])].reverse(), (preset) => {
+    return getAllConfigs(preset)
   })
 
   const features = {
