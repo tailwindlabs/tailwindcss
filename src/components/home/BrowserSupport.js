b59438d2c8a8cc0e6e4566220423b437698c4cf1
Brad Cornes <bradlc41@gmail.com>
2020-10-23 16:44:32 +0100

    "browser support" demo responsive (inc. animation rework)

--- src/components/home/BrowserSupport.js
+++ src/components/home/BrowserSupport.js
@@ -5,11 +5,52 @@ import { CodeWindow } from '@/components/CodeWindow'
 import { gradients } from '@/utils/gradients'
 import { ReactComponent as Icon } from '@/img/icons/home/browser-support.svg'
 import { useState } from 'react'
-import { AnimatePresence, AnimateSharedLayout, motion, useInvertedScale } from 'framer-motion'
+import { AnimateSharedLayout, motion, useInvertedScale } from 'framer-motion'
+import { usePrevious } from '@/hooks/usePrevious'
 
 export function BrowserSupport() {
   const [feature, setFeature] = useState('grid')
   const [selectedGradient, setSelectedGradient] = useState(gradients.purple)
+  const prevFeature = usePrevious(feature)
+
+  const augment = (style, x, y) => {
+    let augmented = {}
+    for (let k in style) {
+      if (k === 'x') augmented[k] = [style[k] + 50 * x, style[k]]
+      else if (k === 'y') augmented[k] = [style[k] + 50 * y, style[k]]
+      else augmented[k] = [style[k], style[k]]
+    }
+    return augmented
+  }
+
+  const animate = (transforms, grid, x, y) => {
+    if (feature === 'transforms') {
+      return {
+        animate: prevFeature === 'gradients' ? augment(transforms, x, y) : transforms,
+        transition:
+          prevFeature === 'gradients' ? { delay: 0.3, duration: 0.25, ease: [0, 0, 0.2, 1] } : {},
+      }
+    }
+    if (feature === 'grid') {
+      return {
+        animate: prevFeature === 'gradients' ? augment(grid, x, y) : grid,
+        transition:
+          prevFeature === 'gradients' ? { delay: 0.3, duration: 0.25, ease: [0, 0, 0.2, 1] } : {},
+      }
+    }
+    if (feature === 'gradients' && prevFeature === 'grid') {
+      return {
+        animate: { ...grid, x: grid.x + 50 * x, y: grid.y + 50 * y },
+        transition: { duration: 0.25, ease: [0, 0, 0.2, 1] },
+      }
+    }
+    if (feature === 'gradients' && prevFeature === 'transforms') {
+      return {
+        animate: { ...transforms, x: transforms.x + 50 * x, y: transforms.y + 50 * y },
+        transition: { duration: 0.25, ease: [0, 0, 0.2, 1] },
+      }
+    }
+  }
 
   return (
     <section id="browser-support">
@@ -59,142 +100,120 @@ export function BrowserSupport() {
           </div>
         }
         left={
-          <div className="flex lg:mr-6 text-4xl font-black text-purple-300" style={{ height: 336 }}>
-            <AnimatePresence initial={false} exitBeforeEnter>
-              {feature === 'grid' || feature === 'transforms' ? (
-                <div key="blocks" className="w-full flex-none grid grid-cols-3 grid-rows-2 gap-4">
-                  <motion.div
-                    initial={
-                      feature === 'transforms'
-                        ? { opacity: 0, scaleX: 1.156, scaleY: 1.156, rotate: -7, x: -50, y: -50 }
-                        : { opacity: 0, x: -50, y: -50 }
-                    }
-                    animate={
-                      feature === 'transforms'
-                        ? { opacity: 1, scaleX: 1.156, scaleY: 1.156, rotate: -7, x: 0, y: 0 }
-                        : { opacity: 1, scaleX: 1, scaleY: 1, rotate: 0, x: 0, y: 0 }
-                    }
-                    exit={{
-                      opacity: 0,
-                      // transition: { duration: 0.3 },
-                      // ...(feature === 'transforms' ? {} : { x: -50, y: -50 }),
-                    }}
-                    className="bg-white rounded-xl shadow-lg flex items-center justify-center"
+          <div className="flex pl-4 pb-6 sm:pl-0 lg:pb-0 lg:mr-6 text-4xl leading-10 font-black text-purple-300">
+            <motion.div
+              key="blocks"
+              initial={false}
+              animate={{ opacity: feature === 'grid' || feature === 'transforms' ? 1 : 0 }}
+              transition={{
+                duration: 0.3,
+                delay: feature === 'grid' || feature === 'transforms' ? 0.3 : 0,
+              }}
+              className={`w-full flex-none grid grid-cols-3 grid-rows-2 gap-4 ${
+                feature === 'gradients' ? 'pointer-events-none' : ''
+              }`}
+            >
+              <motion.div
+                className="relative"
+                initial={false}
+                {...animate(
+                  { scaleX: 1.156, scaleY: 1.156, rotate: -7, x: 0, y: 0 },
+                  { scaleX: 1, scaleY: 1, rotate: 0, x: 0, y: 0 },
+                  -1,
+                  -1
+                )}
+              >
+                <div className="pt-full bg-white rounded-xl shadow-lg flex items-center justify-center" />
+                <Number>1</Number>
+              </motion.div>
+              <motion.div
+                className="relative col-start-3 col-end-4 row-start-2 row-end-3"
+                initial={false}
+                {...animate(
+                  {
+                    scaleX: 0.8375,
+                    scaleY: 0.8375,
+                    rotate: 8.6,
+                    y: 58,
+                    x: 7,
+                  },
+                  { scaleX: 1, scaleY: 1, rotate: 0, y: 0, x: 0 },
+                  1,
+                  1
+                )}
+              >
+                <div className="pt-full bg-white rounded-xl shadow-lg flex items-center justify-center" />
+                <Number>2</Number>
+              </motion.div>
+              <motion.div
+                className="relative origin-right"
+                initial={false}
+                {...animate({ scaleX: 1.475, x: 0, y: 44 }, { scaleX: 1, x: 0, y: 0 }, -1, 1)}
+              >
+                <div className="pt-full bg-white rounded-xl shadow-lg flex items-center justify-center" />
+                <Number>3</Number>
+              </motion.div>
+              <motion.div
+                className="relative"
+                initial={false}
+                {...animate({ x: 0, y: 105 }, { x: 0, y: 0 }, 0, 1)}
+              >
+                <div className="pt-full bg-white rounded-xl shadow-lg flex items-center justify-center" />
+                <Number>4</Number>
+              </motion.div>
+              <motion.div
+                className="relative bg-white rounded-xl shadow-lg flex items-center justify-center col-start-2 col-end-4 row-start-1 row-end-2"
+                initial={false}
+                {...animate({ opacity: 1, x: 87, y: 15 }, { opacity: 1, x: 0, y: 0 }, 1, -1)}
+              >
+                <Number>5</Number>
+              </motion.div>
+            </motion.div>
+
+            <AnimateSharedLayout>
+              <motion.ul
+                key="gradients"
+                className={`self-center bg-white shadow-lg rounded-3xl w-full flex-none -ml-full p-2 grid grid-cols-6 ${
+                  feature === 'gradients' ? '' : 'pointer-events-none'
+                }`}
+                initial={false}
+                animate={{ opacity: feature === 'gradients' ? 1 : 0 }}
+                transition={{ duration: 0.3, delay: feature === 'gradients' ? 0.3 : 0 }}
+              >
+                {[
+                  gradients.purple,
+                  gradients.blue,
+                  gradients.lightblue,
+                  gradients.teal,
+                  gradients.amber,
+                  gradients.pink,
+                ].map((gradient, i) => (
+                  <motion.li
+                    key={gradient}
+                    initial={false}
+                    animate={feature === 'gradients' ? { opacity: [0, 1] } : { opacity: 0 }}
+                    transition={{ delay: feature === 'gradients' ? 0.3 + 0.06 * i : 0 }}
                   >
-                    <InverseScale>1</InverseScale>
-                  </motion.div>
-                  <motion.div
-                    initial={{
-                      opacity: 0,
-                      ...(feature === 'transforms'
-                        ? { scaleX: 0.8375, scaleY: 0.8375, rotate: 8.6, y: 58 + 50, x: 7 + 50 }
-                        : { scaleX: 1, scaleY: 1, rotate: 0, y: 50, x: 50 }),
-                    }}
-                    animate={
-                      feature === 'transforms'
-                        ? { opacity: 1, scaleX: 0.8375, scaleY: 0.8375, rotate: 8.6, y: 58, x: 7 }
-                        : { opacity: 1, scaleX: 1, scaleY: 1, rotate: 0, y: 0, x: 0 }
-                    }
-                    exit={{ opacity: 0 }}
-                    className="bg-white rounded-xl shadow-lg col-span-2 flex items-center justify-center col-start-3 col-end-4 row-start-2 row-end-3"
-                  >
-                    <InverseScale>2</InverseScale>
-                  </motion.div>
-                  <motion.div
-                    initial={{
-                      opacity: 0,
-                      ...(feature === 'transforms'
-                        ? { scaleX: 1.475, x: -50, y: 44 + 50 }
-                        : { scaleX: 1, x: -50, y: 50 }),
-                    }}
-                    animate={
-                      feature === 'transforms'
-                        ? { opacity: 1, scaleX: 1.475, x: 0, y: 44 }
-                        : { opacity: 1, scaleX: 1, x: 0, y: 0 }
-                    }
-                    exit={{ opacity: 0 }}
-                    className="bg-white rounded-xl shadow-lg flex items-center justify-center origin-right"
-                  >
-                    <InverseScale>3</InverseScale>
-                  </motion.div>
-                  <motion.div
-                    initial={{
-                      opacity: 0,
-                      ...(feature === 'transforms' ? { x: 0, y: 105 + 50 } : { x: 0, y: 50 }),
-                    }}
-                    animate={
-                      feature === 'transforms'
-                        ? { opacity: 1, x: 0, y: 105 }
-                        : { opacity: 1, x: 0, y: 0 }
-                    }
-                    exit={{ opacity: 0 }}
-                    className="bg-white rounded-xl shadow-lg flex items-center justify-center"
-                  >
-                    <InverseScale>4</InverseScale>
-                  </motion.div>
-                  <motion.div
-                    initial={{
-                      opacity: 0,
-                      ...(feature === 'transforms'
-                        ? { x: 87 + 50, y: 15 - 50 }
-                        : { x: 50, y: -50 }),
-                    }}
-                    animate={
-                      feature === 'transforms'
-                        ? { opacity: 1, x: 87, y: 15 }
-                        : { opacity: 1, x: 0, y: 0 }
-                    }
-                    exit={{ opacity: 0 }}
-                    className="bg-white rounded-xl shadow-lg flex items-center justify-center col-start-2 col-end-4 row-start-1 row-end-2"
-                  >
-                    <InverseScale>5</InverseScale>
-                  </motion.div>
-                </div>
-              ) : (
-                <AnimateSharedLayout>
-                  <motion.ul
-                    key="gradients"
-                    className="self-center bg-white shadow-lg rounded-3xl w-full p-2 grid grid-cols-6"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                  >
-                    {[
-                      gradients.purple,
-                      gradients.blue,
-                      gradients.lightblue,
-                      gradients.teal,
-                      gradients.amber,
-                      gradients.pink,
-                    ].map((gradient, i) => (
-                      <motion.li
-                        key={gradient}
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ delay: 0.06 * i }}
-                      >
-                        <button
-                          type="button"
-                          className="relative flex w-full pt-full rounded-full focus:outline-none"
-                          onClick={() => setSelectedGradient(gradient)}
-                        >
-                          <span className="sr-only"></span>
-                          {selectedGradient === gradient && (
-                            <motion.div
-                              layoutId="highlight"
-                              className="absolute inset-0 rounded-full bg-gray-200"
-                            />
-                          )}
-                          <div
-                            className={`absolute z-10 inset-2 rounded-full bg-gradient-to-br ${gradient[0]}`}
-                          />
-                        </button>
-                      </motion.li>
-                    ))}
-                  </motion.ul>
-                </AnimateSharedLayout>
-              )}
-            </AnimatePresence>
+                    <button
+                      type="button"
+                      className="relative flex w-full pt-full rounded-full focus:outline-none"
+                      onClick={() => setSelectedGradient(gradient)}
+                    >
+                      <span className="sr-only"></span>
+                      {selectedGradient === gradient && (
+                        <motion.div
+                          layoutId="highlight"
+                          className="absolute inset-0 rounded-full bg-gray-200"
+                        />
+                      )}
+                      <div
+                        className={`absolute z-10 inset-1 sm:inset-2 lg:inset-1 xl:inset-2 rounded-full bg-gradient-to-br ${gradient[0]}`}
+                      />
+                    </button>
+                  </motion.li>
+                ))}
+              </motion.ul>
+            </AnimateSharedLayout>
           </div>
         }
         right={
@@ -207,7 +226,14 @@ export function BrowserSupport() {
   )
 }
 
-function InverseScale({ children }) {
+function Number({ children }) {
   const { scaleX, scaleY } = useInvertedScale()
-  return <motion.div style={{ scaleX, scaleY }}>{children}</motion.div>
+  return (
+    <motion.div
+      className="absolute top-1/2 left-0 w-full -mt-5 text-center"
+      style={{ scaleX, scaleY }}
+    >
+      {children}
+    </motion.div>
+  )
 }
