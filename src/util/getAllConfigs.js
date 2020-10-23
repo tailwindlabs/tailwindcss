c42304579d37ab99578b98ee8ae8f3e0b7ca2ba7
Adam Wathan <adam.wathan@gmail.com>
2020-10-23 13:54:18 -0400

    Include default config by default in presets

--- src/util/getAllConfigs.js
+++ src/util/getAllConfigs.js
@@ -9,9 +9,9 @@ import standardFontWeights from '../flagged/standardFontWeights'
 import additionalBreakpoint from '../flagged/additionalBreakpoint'
 import { flatMap, get } from 'lodash'
 
-export default function getAllConfigs(config, defaultPresets = [defaultConfig]) {
-  const configs = flatMap([...get(config, 'presets', defaultPresets)].reverse(), preset => {
-    return getAllConfigs(preset, [])
+export default function getAllConfigs(config) {
+  const configs = flatMap([...get(config, 'presets', [defaultConfig])].reverse(), preset => {
+    return getAllConfigs(preset)
   })
 
   const features = {
