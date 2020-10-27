5fbaa549fff4a6c76b13b8eb46c299a4e5a99a8d
20lives <lior@dotcore.co.il>
2020-10-27 17:50:03 +0200

    Refactor getAllConfigs util function

--- src/util/getAllConfigs.js
+++ src/util/getAllConfigs.js
@@ -1,21 +1,19 @@
 import defaultConfig from '../../stubs/defaultConfig.stub.js'
 import { flagEnabled } from '../featureFlags'
-import { flatMap, get, isFunction } from 'lodash'
 
 export default function getAllConfigs(config) {
-  const configs = flatMap([...get(config, 'presets', [defaultConfig])].reverse(), (preset) => {
-    return getAllConfigs(isFunction(preset) ? preset() : preset)
-  })
+  const configs = (config?.presets ?? [defaultConfig])
+    .slice()
+    .reverse()
+    .flatMap((preset) => getAllConfigs(preset instanceof Function ? preset() : preset))
 
   const features = {
     // Add experimental configs here...
   }
 
-  Object.keys(features).forEach((feature) => {
-    if (flagEnabled(config, feature)) {
-      configs.unshift(features[feature])
-    }
-  })
+  const experimentals = Object.keys(features)
+    .filter((feature) => flagEnabled(config, feature))
+    .map((feature) => features[feature])
 
-  return [config, ...configs]
+  return [config, ...experimentals, ...configs]
 }
