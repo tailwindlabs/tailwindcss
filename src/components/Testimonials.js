f3b0178874457b8c0755422803180ae1c5874280
Brad Cornes <bradlc41@gmail.com>
2020-10-29 17:54:16 +0000

    update testimonials section

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,74 +4,73 @@ import { gradients } from '@/utils/gradients'
 import { useInView } from 'react-intersection-observer'
 
 const colors = {
-  teal: [gradients.teal[0], 'text-teal-100', 'text-green-200'],
-  pink: [gradients.pink[0], 'text-rose-100', 'text-rose-200'],
-  violet: [gradients.violet[0], 'text-purple-100', 'text-purple-200'],
+  lightblue: [gradients.lightblue[0], 'text-cyan-100', 'text-cyan-100'],
+  purple: [gradients.purple[0], 'text-fuchsia-100', 'text-fuchsia-100'],
+  orange: [gradients.orange[0], 'text-orange-100', 'text-orange-100'],
+  teal: [gradients.teal[0], 'text-green-100', 'text-green-100'],
+  violet: [gradients.violet[0], 'text-purple-100', 'text-purple-100'],
   amber: [gradients.amber[0], 'text-orange-100', 'text-orange-100'],
+  pink: [gradients.pink[0], 'text-rose-100', 'text-rose-100'],
+  blue: [gradients.blue[0], 'text-lightBlue-100', 'text-lightBlue-100'],
 }
 
-const rotation = [
-  'transform -rotate-2',
-  'transform rotate-1',
-  '',
-  'transform rotate-2',
-  'transform -rotate-1',
-  'transform rotate-1',
-]
+const rotation = [-2, 1, -1, 2, -1, 1]
 
 const testimonials = [
   {
-    content: 'Tailwind is decent.',
-    color: 'teal',
+    content: 'I feel like an idiot for not using Tailwind CSS until now.',
+    tweetUrl: 'https://twitter.com/ryanflorence/status/1187951799442886656',
     author: {
-      name: 'Brad Cornes',
-      role: 'Webmaster',
-      avatar: 'https://unsplash.it/96/96?random&amp;i=1',
+      name: 'Ryan Florence',
+      role: 'Remix & React Training',
+      avatar: 'https://pbs.twimg.com/profile_images/1257111841508974592/2_rEXazl_400x400.jpg',
     },
   },
   {
-    content: 'Tailwind is decent.',
-    color: 'pink',
+    content:
+      'If I had to recommend a way of getting into programming today, it would be HTML + CSS with Tailwind CSS.',
+    tweetUrl: 'https://twitter.com/rauchg/status/1225611926320738304',
     author: {
-      name: 'Brad Cornes',
-      role: 'Webmaster',
-      avatar: 'https://unsplash.it/96/96?random&amp;i=2',
+      name: 'Guillermo Rauch',
+      role: 'Vercel',
+      avatar: 'https://pbs.twimg.com/profile_images/1029230542716264448/LgLb--Of_400x400.jpg',
     },
   },
   {
-    content: 'Tailwind is decent.',
-    color: 'violet',
+    content:
+      "I used Tailwind to redesign feltpresence.com the other day. It's fantastic! I only gave myself a few hours to hack it together and it made a huge difference.",
     author: {
-      name: 'Brad Cornes',
-      role: 'Webmaster',
-      avatar: 'https://unsplash.it/96/96?random&amp;i=3',
+      name: 'Ryan Singer',
+      role: 'Basecamp',
+      avatar: 'https://pbs.twimg.com/profile_images/1281306762075725824/O0WYHw22_400x400.jpg',
     },
   },
   {
-    content: 'Tailwind is decent.',
-    color: 'amber',
+    content: 'Ok Tailwind CSS just clicked for me and now I feel like a fucking idiot.',
+    tweetUrl: 'https://twitter.com/ken_wheeler/status/1225373231139475458',
     author: {
-      name: 'Brad Cornes',
-      role: 'Webmaster',
-      avatar: 'https://unsplash.it/96/96?random&amp;i=4',
+      name: 'Ken Wheeler',
+      role: '',
+      avatar: 'https://pbs.twimg.com/profile_images/1302002290967097346/qAqo16iu_400x400.jpg',
     },
   },
   {
-    content: 'Tailwind is decent.',
-    color: 'teal',
+    content:
+      "I didn't think I was going to like @tailwindcss... spent a day using it for a POC, love it! I wish this had been around when we started our company design system, seriously considering a complete rebuild",
+    tweetUrl: 'https://twitter.com/JonBloomer/status/1300923818622377984',
     author: {
-      name: 'Brad Cornes',
-      role: 'Webmaster',
-      avatar: 'https://unsplash.it/96/96?random&amp;i=5',
+      name: 'Jon Bloomer',
+      role: '',
+      avatar: 'https://pbs.twimg.com/profile_images/1298408572166930432/QJddzp2w_400x400.jpg',
     },
   },
   {
-    content: 'Tailwind is decent.',
-    color: 'teal',
+    content: '@tailwindcss looked unpleasant at first, but now I’m hooked on it.',
+    tweetUrl: 'https://twitter.com/droidgilliland/status/1222733372855848961',
     author: {
-      name: 'Brad Cornes',
-      role: 'Webmaster',
-      avatar: 'https://unsplash.it/96/96?random&amp;i=6',
+      name: 'Andrew Gilliland',
+      role: '',
+      avatar: 'https://pbs.twimg.com/profile_images/1288291827687346183/3557xYWU_400x400.jpg',
     },
   },
 ]
@@ -82,20 +81,24 @@ function Testimonial({ testimonial, base, index, total }) {
     [0, (100 / total) * (index + 1), (100 / total) * (index + 1), 100],
     ['0%', `${(index + 1) * -100}%`, `${total * 100 - (index + 1) * 100}%`, '0%']
   )
+  const [straight, setStraight] = useState(false)
+
+  const color = colors[Object.keys(colors)[index % Object.keys(colors).length]]
 
   return (
-    <motion.li className="px-3 md:px-4 flex-none" style={{ x }}>
-      <blockquote
-        className={`shadow-lg rounded-xl flex-none w-80 md:w-xl ${
-          rotation[index % rotation.length]
-        }`}
+    <motion.li
+      className="px-3 md:px-4 flex-none"
+      onMouseEnter={() => setStraight(true)}
+      onMouseLeave={() => setStraight(false)}
+      style={{ x }}
+    >
+      <motion.blockquote
+        className="shadow-lg rounded-xl flex-none w-80 md:w-xl"
+        initial={false}
+        animate={straight ? { rotate: 0 } : { rotate: rotation[index % rotation.length] }}
       >
         <div className="rounded-t-xl bg-white px-6 py-8 md:p-10 text-lg md:text-xl leading-8 font-semibold text-black">
-          <svg
-            width="45"
-            height="36"
-            className={`mb-5 fill-current ${colors[testimonial.color][1]}`}
-          >
+          <svg width="45" height="36" className={`mb-5 fill-current ${color[1]}`}>
             <path d="M13.415.001C6.07 5.185.887 13.681.887 23.041c0 7.632 4.608 12.096 9.936 12.096 5.04 0 8.784-4.032 8.784-8.784 0-4.752-3.312-8.208-7.632-8.208-.864 0-2.016.144-2.304.288.72-4.896 5.328-10.656 9.936-13.536L13.415.001zm24.768 0c-7.2 5.184-12.384 13.68-12.384 23.04 0 7.632 4.608 12.096 9.936 12.096 4.896 0 8.784-4.032 8.784-8.784 0-4.752-3.456-8.208-7.776-8.208-.864 0-1.872.144-2.16.288.72-4.896 5.184-10.656 9.792-13.536L38.183.001z" />
           </svg>
           {typeof testimonial.content === 'string' ? (
@@ -105,9 +108,7 @@ function Testimonial({ testimonial, base, index, total }) {
           )}
         </div>
         <footer
-          className={`flex items-center space-x-4 p-6 md:px-10 md:py-6 bg-gradient-to-br rounded-b-xl leading-6 font-semibold text-white ${
-            colors[testimonial.color][0]
-          }`}
+          className={`flex items-center space-x-4 p-6 md:px-10 md:py-6 bg-gradient-to-br rounded-b-xl leading-6 font-semibold text-white ${color[0]}`}
         >
           <div className="flex-none w-14 h-14 bg-white rounded-full flex items-center justify-center">
             <img src={testimonial.author.avatar} alt="" className="w-12 h-12 rounded-full" />
@@ -116,22 +117,22 @@ function Testimonial({ testimonial, base, index, total }) {
             <span>
               {testimonial.author.name}
               <br />
-              <span className={colors[testimonial.color][2]}>{testimonial.author.role}</span>
+              <span className={color[2]}>{testimonial.author.role}</span>
             </span>
             {testimonial.tweetUrl && (
-              <a href={testimonial.tweetUrl}>
+              <a
+                href={testimonial.tweetUrl}
+                className="opacity-50 hover:opacity-75 transition-opacity duration-200"
+              >
                 <span className="sr-only">View tweet by {testimonial.author.name}</span>
                 <svg width="33" height="32" fill="currentColor">
-                  <path
-                    d="M32.411 6.584c-1.113.493-2.309.826-3.566.977a6.228 6.228 0 002.73-3.437 12.4 12.4 0 01-3.944 1.506 6.212 6.212 0 00-10.744 4.253c0 .486.056.958.16 1.414a17.638 17.638 0 01-12.802-6.49 6.208 6.208 0 00-.84 3.122 6.212 6.212 0 002.762 5.17 6.197 6.197 0 01-2.813-.777v.08c0 3.01 2.14 5.52 4.983 6.091a6.258 6.258 0 01-2.806.107 6.215 6.215 0 005.803 4.312 12.464 12.464 0 01-7.715 2.66c-.501 0-.996-.03-1.482-.087a17.566 17.566 0 009.52 2.79c11.426 0 17.673-9.463 17.673-17.671 0-.267-.007-.536-.019-.803a12.627 12.627 0 003.098-3.213l.002-.004z"
-                    fillOpacity="0.5"
-                  />
+                  <path d="M32.411 6.584c-1.113.493-2.309.826-3.566.977a6.228 6.228 0 002.73-3.437 12.4 12.4 0 01-3.944 1.506 6.212 6.212 0 00-10.744 4.253c0 .486.056.958.16 1.414a17.638 17.638 0 01-12.802-6.49 6.208 6.208 0 00-.84 3.122 6.212 6.212 0 002.762 5.17 6.197 6.197 0 01-2.813-.777v.08c0 3.01 2.14 5.52 4.983 6.091a6.258 6.258 0 01-2.806.107 6.215 6.215 0 005.803 4.312 12.464 12.464 0 01-7.715 2.66c-.501 0-.996-.03-1.482-.087a17.566 17.566 0 009.52 2.79c11.426 0 17.673-9.463 17.673-17.671 0-.267-.007-.536-.019-.803a12.627 12.627 0 003.098-3.213l.002-.004z" />
                 </svg>
               </a>
             )}
           </cite>
         </footer>
-      </blockquote>
+      </motion.blockquote>
     </motion.li>
   )
 }
@@ -166,7 +167,7 @@ export function Testimonials() {
         style={{ height: 607, maxHeight: '50vh' }}
       />
       <div className="flex overflow-hidden -my-8">
-        <ul className="flex w-full py-8">
+        <ul className="flex items-center w-full py-8">
           {testimonials.map((testimonial, i) => (
             <Testimonial
               key={i}
