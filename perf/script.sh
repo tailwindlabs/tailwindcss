b0d56d888260ffd1c88a1ac1b0230665c503dcc3
Robin Malfait <malfait.robin@gmail.com>
2020-10-30 17:45:18 +0100

    rename babelify to build

--- perf/script.sh
+++ perf/script.sh
@@ -2,7 +2,7 @@
 rm isolate-*.log
 
 # Ensure we use the latest build version
-npm run babelify
+npm run build
 
 # Run Tailwind on the big fixture file & profile it
 node --prof lib/cli.js build ./perf/fixture.css -c ./perf/tailwind.config.js -o ./perf/output.css
