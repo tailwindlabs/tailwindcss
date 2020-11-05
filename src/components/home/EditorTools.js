ea908f6cb7d6370f06dbc8ef7f0909733d3b5b6c
Brad Cornes <bradlc41@gmail.com>
2020-11-05 17:32:28 +0000

    add squiggly example to "editor tools" demo

--- src/components/home/EditorTools.js
+++ src/components/home/EditorTools.js
@@ -239,7 +239,15 @@ function EditorToolsToken(props) {
   if (token[0] === 'attr-value' && token[1].includes('__CLASS__')) {
     return (
       <span className="text-code-attr-value">
-        <span className="text-code-punctuation">="</span>flex
+        <span className="text-code-punctuation">="</span>
+        <span
+          className="inline-flex bg-repeat-x bg-left-bottom"
+          style={{
+            backgroundImage: `url("data:image/svg+xml,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%206%203'%20enable-background%3D'new%200%200%206%203'%20height%3D'3'%20width%3D'6'%3E%3Cg%20fill%3D'%23fbbf24'%3E%3Cpolygon%20points%3D'5.5%2C0%202.5%2C3%201.1%2C3%204.1%2C0'%2F%3E%3Cpolygon%20points%3D'4%2C0%206%2C2%206%2C0.6%205.4%2C0'%2F%3E%3Cpolygon%20points%3D'0%2C2%201%2C3%202.4%2C3%200%2C0.6'%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E")`,
+          }}
+        >
+          flex
+        </span>
         {stage >= 0 &&
           ' bg-t'.split('').map((char, i) => (
             <motion.span
