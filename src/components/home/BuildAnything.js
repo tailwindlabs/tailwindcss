3e30a7511820f4c727c6af96f31ee4126acaa774
Brad Cornes <bradlc41@gmail.com>
2020-11-06 11:10:47 +0000

    update "build anything" code examples

--- src/components/home/BuildAnything.js
+++ src/components/home/BuildAnything.js
@@ -1,38 +1,225 @@
-import { useEffect, useRef, useState } from 'react'
+import { Fragment, useEffect, useRef, useState } from 'react'
 import { IconContainer, Caption, BigText, Paragraph, Link, Widont } from '@/components/home/common'
 import { GradientLockup } from '@/components/GradientLockup'
 import { Tabs } from '@/components/Tabs'
-import { CodeWindow } from '@/components/CodeWindow'
+import { CodeWindow, getClassNameForToken } from '@/components/CodeWindow'
 import { gradients } from '@/utils/gradients'
 import { ReactComponent as Icon } from '@/img/icons/home/build-anything.svg'
 import { HtmlZenGarden } from '@/components/HtmlZenGarden'
 import tokenize from '../../macros/tokenize.macro'
-import { Token } from '../Code'
-import { motion } from 'framer-motion'
-import { shuffle } from '@/utils/shuffle'
-import { randomIntFromInterval } from '@/utils/randomIntFromInterval'
+import clsx from 'clsx'
 
-const { tokens } = tokenize.html(`<div class="__CLASS__">
-  <div class="__CLASS__">
-    <img src="avatar.jpg" class="__CLASS__">
+const { lines } = tokenize.html(`<div class="_">
+  <div class="_">
+    <img src="https://unsplash.it/200/200" alt="" class="_" />
   </div>
-  <div class="__CLASS__">
-    <p class="__CLASS__">"If I had to recommend a way of
-      getting into programming today, it would be HTML + CSS
-      with @tailwindcss."
-    </p>
-    <div class="__CLASS__">
-      <div>
-        <h2 class="__CLASS__">Guillermo Rauch</h2>
-        <small class="__CLASS__">CEO Vercel</small>
+  <div class="_">
+    <div class="_">
+      <h1 class="_">
+        __content__
+      </h1>
+      <div class="_">
+        __content__
       </div>
-      <a href="https://twitter.com/rauchg" class="__CLASS__">View Tweet</a>
+      <div class="_">
+        In stock
+      </div>
+    </div>
+    <div class="_">
+      <div class="_">
+        <div class="_">XS</div>
+        <div class="_">S</div>
+        <div class="_">M</div>
+        <div class="_">L</div>
+        <div class="_">XL</div>
+      </div>
+      <div class="_">Size Guide</div>
+    </div>
+    <div class="_">
+      <div class="_">
+        <div class="_">Buy now</div>
+        <div class="_">Add to bag</div>
+      </div>
+      <div class="_">
+        <svg width="20" height="20" fill="currentColor">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+        </svg>
+      </div>
+    </div>
+    <div class="_">
+      Free shipping on all continental US orders.
     </div>
   </div>
-</div>`)
+</div>
+`)
+
+const code = {
+  simple: `<div class="flex">
+  <div class="flex-none w-48 relative">
+    <img src="https://unsplash.it/200/200" alt="" class="absolute inset-0 w-full h-full object-cover" />
+  </div>
+  <div class="flex-auto p-6">
+    <div class="flex flex-wrap">
+      <h1 class="flex-auto text-xl leading-7 font-semibold">Classic Utility Jacket</h1>
+      <div class="text-xl leading-7 font-semibold text-gray-500">$110.00</div>
+      <div class="w-full flex-none text-sm leading-5 font-medium text-gray-500 mt-2">In stock</div>
+    </div>
+    <div class="flex items-baseline mt-4 mb-6">
+      <div class="space-x-2 flex">
+        <div class="w-9 h-9 flex items-center justify-center bg-gray-100 rounded-lg">XS</div>
+        <div class="w-9 h-9 flex items-center justify-center">S</div>
+        <div class="w-9 h-9 flex items-center justify-center">M</div>
+        <div class="w-9 h-9 flex items-center justify-center">L</div>
+        <div class="w-9 h-9 flex items-center justify-center">XL</div>
+      </div>
+      <div class="ml-auto text-sm leading-5 text-gray-500 underline">Size Guide</div>
+    </div>
+    <div class="flex space-x-3 mb-4 text-sm leading-5 font-medium">
+      <div class="flex-auto flex space-x-3">
+        <div class="w-1/2 flex items-center justify-center rounded-md bg-black text-white">Buy now</div>
+        <div class="w-1/2 flex items-center justify-center rounded-md border border-gray-300">Add to bag</div>
+      </div>
+      <div class="flex-none flex items-center justify-center w-9 h-9 rounded-md text-gray-400 border border-gray-300">
+        <svg width="20" height="20" fill="currentColor">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+        </svg>
+      </div>
+    </div>
+    <div class="text-sm leading-5 text-gray-500">Free shipping on all continental US orders.</div>
+  </div>
+</div>`,
+  playful: `<div class="flex p-6">
+  <div class="flex-none w-44 relative">
+    <img src="https://unsplash.it/200/200" alt="" class="absolute inset-0 w-full h-full object-cover rounded-lg" />
+  </div>
+  <div class="flex-auto pl-6">
+    <div class="flex flex-wrap items-baseline">
+      <h1 class="w-full flex-none font-semibold mb-2.5">Kids Jumper</h1>
+      <div class="text-4xl leading-7 font-bold text-purple-600">$39.00</div>
+      <div class="text-sm leading-5 font-medium text-gray-400 ml-3">In stock</div>
+    </div>
+    <div class="flex items-baseline my-8">
+      <div class="space-x-2 flex text-sm leading-5 font-medium">
+        <div class="w-9 h-9 flex items-center justify-center rounded-full bg-purple-700 text-white">XS</div>
+        <div class="w-9 h-9 flex items-center justify-center rounded-full border-2 border-gray-200">S</div>
+        <div class="w-9 h-9 flex items-center justify-center rounded-full border-2 border-gray-200">M</div>
+        <div class="w-9 h-9 flex items-center justify-center rounded-full border-2 border-gray-200">L</div>
+        <div class="w-9 h-9 flex items-center justify-center rounded-full border-2 border-gray-200">XL</div>
+      </div>
+      <div class="ml-3 text-sm leading-5 text-gray-500 underline">Size Guide</div>
+    </div>
+    <div class="flex space-x-3 mb-4 text-sm leading-5 font-semibold">
+      <div class="flex-auto flex space-x-3">
+        <div class="w-1/2 flex items-center justify-center rounded-full bg-purple-700 text-white">Buy now</div>
+        <div class="w-1/2 flex items-center justify-center rounded-full bg-purple-50 text-purple-700">Add to bag</div>
+      </div>
+      <div class="flex-none flex items-center justify-center w-9 h-9 rounded-full bg-purple-50 text-purple-700">
+        <svg width="20" height="20" fill="currentColor">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+        </svg>
+      </div>
+    </div>
+    <div class="text-sm leading-5 text-gray-500">Free shipping on all continental US orders.</div>
+  </div>
+</div>`,
+  elegant: `<div class="flex p-1">
+  <div class="flex-none w-44 relative">
+    <img src="https://unsplash.it/200/200" alt="" class="absolute inset-0 w-full h-full object-cover" />
+  </div>
+  <div class="flex-auto py-7 px-8">
+    <div class="flex flex-wrap items-baseline">
+      <h1 class="w-full flex-none text-3xl leading-9 text-black mb-1.5">Fancy Suit Jacket</h1>
+      <div class="text-lg leading-6 text-black">$600.00</div>
+      <div class="text-sm leading-5 text-gray-500 ml-3">In stock</div>
+    </div>
+    <div class="flex items-baseline mt-9 py-4 border-t border-gray-100">
+      <div class="space-x-2 flex text-sm leading-5 font-light text-black">
+        <div class="w-9 h-9 flex items-center justify-center rounded-full bg-black text-white">XS</div>
+        <div class="w-9 h-9 flex items-center justify-center rounded-full">S</div>
+        <div class="w-9 h-9 flex items-center justify-center rounded-full">M</div>
+        <div class="w-9 h-9 flex items-center justify-center rounded-full">L</div>
+        <div class="w-9 h-9 flex items-center justify-center rounded-full">XL</div>
+      </div>
+      <div class="ml-auto text-sm leading-5 font-light text-gray-500">Size Guide</div>
+    </div>
+    <div class="flex space-x-3 mb-3 text-sm leading-5 font-semibold uppercase">
+      <div class="flex-auto flex space-x-3">
+        <div class="w-1/2 flex items-center justify-center bg-black text-white">Buy now</div>
+        <div class="w-1/2 flex items-center justify-center border border-gray-200">Add to bag</div>
+      </div>
+      <div class="flex-none flex items-center justify-center w-12 h-12 text-gray-900 border border-gray-200">
+        <svg width="20" height="20" fill="currentColor">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+        </svg>
+      </div>
+    </div>
+    <div class="text-sm leading-5 text-gray-500">Free shipping on all continental US orders.</div>
+  </div>
+</div>`,
+  brutalist: `<div class="flex p-6 font-mono">
+  <div class="flex-none w-40 relative">
+    <img src="https://unsplash.it/200/200" alt="" class="absolute inset-0 w-full h-full object-cover border border-black shadow-offset-lime" />
+  </div>
+  <div class="flex-auto pl-6">
+    <div class="flex flex-wrap items-baseline pl-52 -mt-6 -mr-6 -ml-52 py-6 pr-6 bg-black text-white">
+      <h1 class="w-full flex-none text-2xl leading-7 mb-2 font-bold">Retro Shoe</h1>
+      <div class="text-2xl leading-7 font-bold">$110.00</div>
+      <div class="text-sm leading-5 font-medium ml-3">In stock</div>
+    </div>
+    <div class="flex items-baseline py-8">
+      <div class="space-x-3.5 flex text-center text-sm leading-none font-bold text-gray-500">
+        <div class="w-6 text-black shadow-underline">XS</div>
+        <div class="w-6">S</div>
+        <div class="w-6">M</div>
+        <div class="w-6">L</div>
+        <div class="w-6">XL</div>
+      </div>
+      <div class="ml-auto text-xs underline">Size Guide</div>
+    </div>
+    <div class="flex space-x-3 text-sm leading-5 font-bold uppercase mb-4">
+      <div class="flex-auto flex space-x-3">
+        <div class="w-1/2 flex items-center justify-center bg-lime-300 text-black border border-black shadow-offset-black">Buy now</div>
+        <div class="w-1/2 flex items-center justify-center border border-black shadow-offset-black">Add to bag</div>
+      </div>
+      <div class="flex-none flex items-center justify-center w-9 h-9 border border-black">
+        <svg width="20" height="20" fill="currentColor">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+        </svg>
+      </div>
+    </div>
+    <div class="text-xs leading-5 text-gray-500">Free shipping on all continental US orders.</div>
+  </div>
+</div>`,
+}
+
+function extractClasses(code) {
+  return code.match(/class="[^"]+"/g).map((attr) => attr.substring(7, attr.length - 1))
+}
+
+const classes = {
+  simple: extractClasses(code.simple),
+  playful: extractClasses(code.playful),
+  elegant: extractClasses(code.elegant),
+  brutalist: extractClasses(code.brutalist),
+}
+
+const content = {
+  simple: ['Classic Utility Jacket', '$110.00'],
+  playful: ['Kids Jumper', '$39.00'],
+  elegant: ['Fancy Suit Jacket', '$600.00'],
+  brutalist: ['Retro Shoe', '$89.00'],
+}
 
 export function BuildAnything() {
   const [theme, setTheme] = useState('simple')
+  let classIndex = 0
+  let contentIndex = 0
+
+  const initial = useRef(true)
+
+  useEffect(() => {
+    initial.current = false
+  }, [])
 
   return (
     <section id="build-anything">
@@ -77,52 +264,59 @@ export function BuildAnything() {
         left={<HtmlZenGarden theme={theme} />}
         right={
           <CodeWindow className="bg-pink-600">
-            <CodeWindow.Code
-              tokens={tokens}
-              tokenComponent={BuildAnythingToken}
-              tokenProps={{ theme }}
-            />
+            <CodeWindow.Code2 lines={lines.length}>
+              {lines.map((tokens, lineIndex) => (
+                <Fragment key={lineIndex}>
+                  {tokens.map((token, tokenIndex) => {
+                    if (token.content === '_') {
+                      let cls = classes[theme][classIndex++]
+                      return (
+                        <span
+                          key={cls}
+                          className={clsx('code-highlight', getClassNameForToken(token), {
+                            'animate-flash-code': !initial.current,
+                          })}
+                        >
+                          {cls}
+                        </span>
+                      )
+                    }
+
+                    if (token.content.includes('__content__')) {
+                      let text = content[theme][contentIndex++]
+                      return (
+                        <Fragment key={text}>
+                          {token.content.split(/(__content__)/).map((part, i) =>
+                            i === 1 ? (
+                              <span
+                                key={i}
+                                className={clsx('code-highlight', getClassNameForToken(token), {
+                                  'animate-flash-code': !initial.current,
+                                })}
+                              >
+                                {text}
+                              </span>
+                            ) : (
+                              part
+                            )
+                          )}
+                        </Fragment>
+                      )
+                    }
+
+                    return (
+                      <span key={tokenIndex} className={getClassNameForToken(token)}>
+                        {token.content}
+                      </span>
+                    )
+                  })}
+                  {'\n'}
+                </Fragment>
+              ))}
+            </CodeWindow.Code2>
           </CodeWindow>
         }
       />
     </section>
   )
-}
-
-const classes = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare nisi at nisi ultricies egestas. Duis consequat sem non nulla placerat finibus. Sed leo turpis, imperdiet nec erat et, mollis venenatis lectus'
-  .toLowerCase()
-  .split(/[^a-z]+/i)
-
-function BuildAnythingToken(props) {
-  const { token, theme } = props
-  const initial = useRef(true)
-
-  useEffect(() => {
-    initial.current = false
-  }, [])
-
-  if (token[0] === 'attr-value' && token[1].includes('__CLASS__')) {
-    const noOfClasses = randomIntFromInterval(2, 5)
-    return (
-      <span className="text-code-attr-value">
-        <span className="text-code-punctuation">="</span>
-        <motion.span
-          className={`text-code-attr-value ${initial.current ? '' : 'animate-flash-code'}`}
-          key={theme}
-          style={{
-            borderRadius: 3,
-            padding: '1px 3px',
-            margin: '0 -3px',
-          }}
-        >
-          {shuffle([...classes])
-            .filter((_, i) => i <= noOfClasses)
-            .join(' ')}
-        </motion.span>
-        "
-      </span>
-    )
-  }
-
-  return <Token {...props} />
 }
