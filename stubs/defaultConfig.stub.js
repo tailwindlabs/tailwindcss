369cfae2905a577033529c46a5e8ca58c69f5623
Adam Wathan <adam.wathan@gmail.com>
2020-11-06 22:36:18 -0500

    Add "shadow-solid" utility

--- stubs/defaultConfig.stub.js
+++ stubs/defaultConfig.stub.js
@@ -134,6 +134,7 @@ module.exports = {
       '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
       inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
       outline: '0 0 0 3px rgba(147, 197, 253, 0.5)',
+      solid: '0 0 0 2px currentColor',
       none: 'none',
     },
     container: {},
