cd0d80d57c6f4a74e84ba35c9dc509217234a84c
Brad Cornes <bradlc41@gmail.com>
2020-11-09 14:04:25 +0000

    remove unused code

--- src/components/home/Performance.js
+++ src/components/home/Performance.js
@@ -31,7 +31,7 @@ function Counter({ from, to, round = 0, progress }) {
   return <span ref={ref}>{formatNumber(value.get())}</span>
 }
 
-const { lines, code } = tokenize.html(
+const { lines } = tokenize.html(
   `<div class="fixed inset-0 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end">
   <div class="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto">
     <div class="rounded-lg shadow-xs overflow-hidden">
@@ -62,8 +62,7 @@ const { lines, code } = tokenize.html(
     </div>
   </div>
 </div>
-`,
-  true
+`
 )
 
 addClassTokens2(lines)
