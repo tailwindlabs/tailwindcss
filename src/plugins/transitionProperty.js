85fd86fc2d06de4b8706c349dcf2dfd2c09c86d5
Adam Wathan <adam.wathan@gmail.com>
2020-11-10 22:13:33 -0500

    Add support for default duration and timing function to transition property utilities

--- src/plugins/transitionProperty.js
+++ src/plugins/transitionProperty.js
@@ -1,5 +1,28 @@
-import createUtilityPlugin from '../util/createUtilityPlugin'
+import _ from 'lodash'
+import nameClass from '../util/nameClass'
 
 export default function () {
-  return createUtilityPlugin('transitionProperty', [['transition', ['transitionProperty']]])
+  return function ({ addUtilities, theme, variants }) {
+    const defaultTimingFunction = theme('transitionTimingFunction.DEFAULT')
+    const defaultDuration = theme('transitionDuration.DEFAULT')
+
+    const utilities = _.fromPairs(
+      _.map(theme('transitionProperty'), (value, modifier) => {
+        return [
+          nameClass('transition', modifier),
+          {
+            'transition-property': value,
+            ...(value === 'none'
+              ? {}
+              : {
+                  'transition-timing-function': defaultTimingFunction,
+                  'transition-duration': defaultDuration,
+                }),
+          },
+        ]
+      })
+    )
+
+    addUtilities(utilities, variants('transitionProperty'))
+  }
 }
