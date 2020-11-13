1c42818c9d8e60526cd390d24a2d255ff8c8ab42
Simon Vrachliotis <simon@simonswiss.com>
2020-11-13 14:24:35 +1100

    overflow examples started

--- tailwind.config.js
+++ tailwind.config.js
@@ -106,7 +106,7 @@ module.exports = {
         source: ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
         'ubuntu-mono': ['Ubuntu Mono', ...defaultTheme.fontFamily.mono],
         system: defaultTheme.fontFamily.sans,
-        line: 'Flow',
+        flow: 'Flow',
       },
       spacing: {
         18: '4.5rem',
