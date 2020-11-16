80f44a25878f71db13214909f2e3f579de59ac3a
Robin Malfait <malfait.robin@gmail.com>
2020-11-16 19:45:52 +0100

    make sure postcss is included

--- src/index.postcss7.js
+++ src/index.postcss7.js
@@ -2,6 +2,7 @@ import path from 'path'
 import fs from 'fs'
 
 import _ from 'lodash'
+import postcss from 'postcss'
 
 import getModuleDependencies from './lib/getModuleDependencies'
 import registerConfigAsDependency from './lib/registerConfigAsDependency'
