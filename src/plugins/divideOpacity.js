3768c73aa2335b1931b317fd6adde1af8d64915d
20lives <lior@dotcore.co.il>
2020-11-17 11:53:08 +0200

    Refactor divideOpacity utilities generation

--- src/plugins/divideOpacity.js
+++ src/plugins/divideOpacity.js
@@ -1,19 +1,13 @@
-import _ from 'lodash'
+import mapObject from '../util/mapObject'
 import nameClass from '../util/nameClass'
 
-export default function () {
-  return function ({ addUtilities, theme, variants }) {
-    const utilities = _.fromPairs(
-      _.map(theme('divideOpacity'), (value, modifier) => {
-        return [
-          `${nameClass('divide-opacity', modifier)} > :not([hidden]) ~ :not([hidden])`,
-          {
-            '--tw-divide-opacity': value,
-          },
-        ]
-      })
-    )
+export default () => ({ addUtilities, theme, variants }) => {
+  const utilities = mapObject(theme('divideOpacity'), ([modifier, value]) => [
+    `${nameClass('divide-opacity', modifier)} > :not([hidden]) ~ :not([hidden])`,
+    {
+      '--tw-divide-opacity': value,
+    },
+  ])
 
-    addUtilities(utilities, variants('divideOpacity'))
-  }
+  addUtilities(utilities, variants('divideOpacity'))
 }
