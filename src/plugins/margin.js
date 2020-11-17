a98b15cc3f982bf9a8614e1cbf7fd0f9990c8c85
20lives <lior@dotcore.co.il>
2020-11-17 19:29:49 +0200

    Refactor margin utilities generation

--- src/plugins/margin.js
+++ src/plugins/margin.js
@@ -1,34 +1,31 @@
-import _ from 'lodash'
 import nameClass from '../util/nameClass'
 
-export default function () {
-  return function ({ addUtilities, theme, variants }) {
-    const generators = [
-      (size, modifier) => ({
-        [nameClass('m', modifier)]: { margin: `${size}` },
-      }),
-      (size, modifier) => ({
-        [nameClass('my', modifier)]: {
-          'margin-top': `${size}`,
-          'margin-bottom': `${size}`,
-        },
-        [nameClass('mx', modifier)]: {
-          'margin-left': `${size}`,
-          'margin-right': `${size}`,
-        },
-      }),
-      (size, modifier) => ({
-        [nameClass('mt', modifier)]: { 'margin-top': `${size}` },
-        [nameClass('mr', modifier)]: { 'margin-right': `${size}` },
-        [nameClass('mb', modifier)]: { 'margin-bottom': `${size}` },
-        [nameClass('ml', modifier)]: { 'margin-left': `${size}` },
-      }),
-    ]
+export default () => ({ addUtilities, theme, variants }) => {
+  const generators = [
+    ([modifier, size]) => ({
+      [nameClass('m', modifier)]: { margin: size },
+    }),
+    ([modifier, size]) => ({
+      [nameClass('my', modifier)]: {
+        'margin-top': size,
+        'margin-bottom': size,
+      },
+      [nameClass('mx', modifier)]: {
+        'margin-left': size,
+        'margin-right': size,
+      },
+    }),
+    ([modifier, size]) => ({
+      [nameClass('mt', modifier)]: { 'margin-top': size },
+      [nameClass('mr', modifier)]: { 'margin-right': size },
+      [nameClass('mb', modifier)]: { 'margin-bottom': size },
+      [nameClass('ml', modifier)]: { 'margin-left': size },
+    }),
+  ]
 
-    const utilities = _.flatMap(generators, (generator) => {
-      return _.flatMap(theme('margin'), generator)
-    })
+  const utilities = generators.flatMap((generator) =>
+    Object.entries(theme('margin')).flatMap(generator)
+  )
 
-    addUtilities(utilities, variants('margin'))
-  }
+  addUtilities(utilities, variants('margin'))
 }
