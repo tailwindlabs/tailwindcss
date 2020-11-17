57648618f70b991f222cba2b40b1166098da53ba
20lives <lior@dotcore.co.il>
2020-11-17 11:49:34 +0200

    Refactor animation utilities generation

--- src/plugins/animation.js
+++ src/plugins/animation.js
@@ -1,27 +1,27 @@
-import _ from 'lodash'
 import nameClass from '../util/nameClass'
 import parseAnimationValue from '../util/parseAnimationValue'
+import mapObject from '../util/mapObject'
 
-export default function () {
-  return function ({ addUtilities, theme, variants, prefix }) {
-    const prefixName = (name) => prefix(`.${name}`).slice(1)
-    const keyframesConfig = theme('keyframes')
-    const keyframesStyles = _.mapKeys(
-      keyframesConfig,
-      (_keyframes, name) => `@keyframes ${prefixName(name)}`
-    )
+export default () => ({ addUtilities, theme, variants, prefix }) => {
+  const prefixName = (name) => prefix(`.${name}`).slice(1)
+  const keyframesConfig = theme('keyframes')
+  const keyframesStyles = mapObject(keyframesConfig, ([key, value]) => [
+    `@keyframes ${prefixName(key)}`,
+    value,
+  ])
 
-    addUtilities(keyframesStyles, { respectImportant: false })
+  addUtilities(keyframesStyles, { respectImportant: false })
 
-    const animationConfig = theme('animation')
-    const utilities = _.mapValues(
-      _.mapKeys(animationConfig, (_animation, suffix) => nameClass('animate', suffix)),
-      (animation) => {
-        const { name } = parseAnimationValue(animation)
-        if (name === undefined || keyframesConfig[name] === undefined) return { animation }
-        return { animation: animation.replace(name, prefixName(name)) }
-      }
-    )
-    addUtilities(utilities, variants('animation'))
-  }
+  const animationConfig = theme('animation')
+  const utilities = mapObject(animationConfig, ([suffix, animation]) => {
+    const { name } = parseAnimationValue(animation)
+    return [
+      nameClass('animate', suffix),
+      name === undefined || keyframesConfig[name] === undefined
+        ? { animation }
+        : { animation: animation.replace(name, prefixName(name)) },
+    ]
+  })
+
+  addUtilities(utilities, variants('animation'))
 }
