115b000d5b9652fbf80a8b876a25b0d286869f63
20lives <lior@dotcore.co.il>
2020-11-17 12:33:11 +0200

    Refactor borderColor utilities generation

--- src/plugins/borderColor.js
+++ src/plugins/borderColor.js
@@ -1,31 +1,25 @@
-import _ from 'lodash'
 import flattenColorPalette from '../util/flattenColorPalette'
+import _mapObject from '../util/mapObject'
 import nameClass from '../util/nameClass'
 import toColorValue from '../util/toColorValue'
 import withAlphaVariable from '../util/withAlphaVariable'
 
-export default function () {
-  return function ({ addUtilities, theme, variants, corePlugins }) {
-    const colors = flattenColorPalette(theme('borderColor'))
+export default () => ({ addUtilities, theme, variants, corePlugins }) => {
+  const { DEFAULT, ...colors } = flattenColorPalette(theme('borderColor'))
 
-    const getProperties = (value) => {
-      if (corePlugins('borderOpacity')) {
-        return withAlphaVariable({
+  const getProperties = (value) =>
+    corePlugins('borderOpacity')
+      ? withAlphaVariable({
           color: value,
           property: 'border-color',
           variable: '--tw-border-opacity',
         })
-      }
+      : { 'border-color': toColorValue(value) }
 
-      return { 'border-color': toColorValue(value) }
-    }
+  const utilities = _mapObject(colors, ([modifier, value]) => [
+    nameClass('border', modifier),
+    getProperties(value),
+  ])
 
-    const utilities = _.fromPairs(
-      _.map(_.omit(colors, 'DEFAULT'), (value, modifier) => {
-        return [nameClass('border', modifier), getProperties(value)]
-      })
-    )
-
-    addUtilities(utilities, variants('borderColor'))
-  }
+  addUtilities(utilities, variants('borderColor'))
 }
