2aaac1d2ae542cc8c5ff5a5371d48bea54f617ae
Brad Cornes <bradlc41@gmail.com>
2020-11-17 22:59:45 +0000

    add prism plugin to improve highlighting of tailwind stuff

--- remark/utils.js
+++ remark/utils.js
@@ -3,6 +3,85 @@ const loadLanguages = require('prismjs/components/')
 loadLanguages()
 require('./prism-diff-highlight')(Prism)
 
+const HTML_TAG = /<\/?(?!\d)[^\s>\/=$<%]+(?:\s(?:\s*[^\s>\/=]+(?:\s*=\s*(?:"[^"]*"|'[^']*'|[^\s'">=]+(?=[\s>]))|(?=[\s/>])))+)?\s*\/?>/gi
+const PSEUDO_CLASSES = [
+  'active',
+  'any-link',
+  'blank',
+  'checked',
+  'current',
+  'default',
+  'defined',
+  'dir',
+  'disabled',
+  'drop',
+  'empty',
+  'enabled',
+  'first',
+  'first-child',
+  'first-of-type',
+  'fullscreen',
+  'future',
+  'focus',
+  'focus-visible',
+  'focus-within',
+  'has',
+  'host',
+  'host',
+  'host-context',
+  'hover',
+  'indeterminate',
+  'in-range',
+  'invalid',
+  'is',
+  'lang',
+  'last-child',
+  'last-of-type',
+  'left',
+  'link',
+  'local-link',
+  'not',
+  'nth-child',
+  'nth-col',
+  'nth-last-child',
+  'nth-last-col',
+  'nth-last-of-type',
+  'nth-of-type',
+  'only-child',
+  'only-of-type',
+  'optional',
+  'out-of-range',
+  'past',
+  'picture-in-picture',
+  'placeholder-shown',
+  'read-only',
+  'read-write',
+  'required',
+  'right',
+  'root',
+  'scope',
+  'state',
+  'target',
+  'target-within',
+  'user-invalid',
+  'valid',
+  'visited',
+  'where',
+]
+
+Prism.hooks.add('wrap', (env) => {
+  if (env.type === 'atrule') {
+    const content = env.content.replace(HTML_TAG, '')
+    if (content.startsWith('@apply')) {
+      env.classes.push('atapply')
+    }
+  } else if (env.type === 'pseudo-class') {
+    if (!new RegExp(`^::?(${PSEUDO_CLASSES.join('|')})`).test(env.content)) {
+      env.classes = env.classes.filter((x) => x !== 'pseudo-class')
+    }
+  }
+})
+
 module.exports.addImport = function addImport(tree, mod, name) {
   tree.children.unshift({
     type: 'import',
