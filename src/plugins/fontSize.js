8024c820b37347b4540eb291317a0e224397f4f3
20lives <lior@dotcore.co.il>
2020-11-17 18:44:18 +0200

    Refactor fontSize utilities generation

--- src/plugins/fontSize.js
+++ src/plugins/fontSize.js
@@ -1,36 +1,25 @@
-import _ from 'lodash'
+import mapObject from '../util/mapObject'
 import nameClass from '../util/nameClass'
 
-export default function () {
-  return function ({ addUtilities, theme, variants }) {
-    const utilities = _.fromPairs(
-      _.map(theme('fontSize'), (value, modifier) => {
-        const [fontSize, options] = Array.isArray(value) ? value : [value]
-        const { lineHeight, letterSpacing } = _.isPlainObject(options)
-          ? options
-          : {
-              lineHeight: options,
-            }
+export default () => ({ addUtilities, theme, variants }) => {
+  const utilities = mapObject(theme('fontSize'), ([modifier, value]) => {
+    const [fontSize, options] = Array.isArray(value) ? value : [value]
+    const { lineHeight, letterSpacing } =
+      options instanceof Object ? options : { lineHeight: options }
 
-        return [
-          nameClass('text', modifier),
-          {
-            'font-size': fontSize,
-            ...(lineHeight === undefined
-              ? {}
-              : {
-                  'line-height': lineHeight,
-                }),
-            ...(letterSpacing === undefined
-              ? {}
-              : {
-                  'letter-spacing': letterSpacing,
-                }),
-          },
-        ]
-      })
-    )
+    return [
+      nameClass('text', modifier),
+      {
+        'font-size': fontSize,
+        ...(lineHeight !== undefined && {
+          'line-height': lineHeight,
+        }),
+        ...(letterSpacing !== undefined && {
+          'letter-spacing': letterSpacing,
+        }),
+      },
+    ]
+  })
 
-    addUtilities(utilities, variants('fontSize'))
-  }
+  addUtilities(utilities, variants('fontSize'))
 }
