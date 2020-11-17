30b47f710da23a8c29ca18ae65a7232d5fe46005
20lives <lior@dotcore.co.il>
2020-11-17 11:55:15 +0200

    Refactor textColor utilities generation

--- src/plugins/textColor.js
+++ src/plugins/textColor.js
@@ -1,31 +1,25 @@
-import _ from 'lodash'
+import mapObject from '../util/mapObject'
 import flattenColorPalette from '../util/flattenColorPalette'
 import nameClass from '../util/nameClass'
 import toColorValue from '../util/toColorValue'
 import withAlphaVariable from '../util/withAlphaVariable'
 
-export default function () {
-  return function ({ addUtilities, theme, variants, corePlugins }) {
-    const colors = flattenColorPalette(theme('textColor'))
+export default () => ({ addUtilities, theme, variants, corePlugins }) => {
+  const colors = flattenColorPalette(theme('textColor'))
 
-    const getProperties = (value) => {
-      if (corePlugins('textOpacity')) {
-        return withAlphaVariable({
+  const getProperties = (value) =>
+    corePlugins('textOpacity')
+      ? withAlphaVariable({
           color: value,
           property: 'color',
           variable: '--tw-text-opacity',
         })
-      }
+      : { color: toColorValue(value) }
 
-      return { color: toColorValue(value) }
-    }
+  const utilities = mapObject(colors, ([modifier, value]) => [
+    nameClass('text', modifier),
+    getProperties(value),
+  ])
 
-    const utilities = _.fromPairs(
-      _.map(colors, (value, modifier) => {
-        return [nameClass('text', modifier), getProperties(value)]
-      })
-    )
-
-    addUtilities(utilities, variants('textColor'))
-  }
+  addUtilities(utilities, variants('textColor'))
 }
