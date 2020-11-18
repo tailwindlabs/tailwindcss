9b992ec58630e25009edf89305aca924ef4da4ac
Brad Cornes <bradlc41@gmail.com>
2020-11-18 10:58:07 +0000

    update compatibility tip styles

--- src/components/Tip.js
+++ src/components/Tip.js
@@ -24,20 +24,16 @@ export function TipBad({ children }) {
 
 export function TipCompat({ children }) {
   return (
-    <div className="text-sm bg-blue-100 text-blue-700 font-semi-bold px-4 py-2 mb-4 rounded">
-      <div className="flex items-center">
-        <div className="mr-2">
-          <svg fill="currentColor" viewBox="0 0 20 20" className="block text-blue-400 h-5 w-5">
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M16.432 15C14.387 9.893 12 8.547 12 6V3h.5a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5H8v3c0 2.547-2.387 3.893-4.432 9-.651 1.625-2.323 4 6.432 4s7.083-2.375 6.432-4zm-1.617 1.751c-.702.21-2.099.449-4.815.449s-4.113-.239-4.815-.449c-.249-.074-.346-.363-.258-.628.22-.67.635-1.828 1.411-3.121 1.896-3.159 3.863.497 5.5.497s1.188-1.561 1.824-.497a15.353 15.353 0 0 1 1.411 3.121c.088.265-.009.553-.258.628z"
-            />
-          </svg>
-        </div>
-        <div>
-          <p className="font-semibold">{children}</p>
-        </div>
+    <div className="text-sm bg-light-blue-100 text-light-blue-800 font-medium px-4 py-3 mb-4 rounded-xl">
+      <div className="flex items-start space-x-3">
+        <svg width="20" height="20" className="text-light-blue-500" fill="currentColor">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M6.998 2a1 1 0 00-.707 1.707l.707.707v3.758a1 1 0 01-.293.707l-4 4C.815 14.769 2.154 18 4.826 18H15.17c2.672 0 4.01-3.231 2.12-5.121l-4-4a1 1 0 01-.292-.707V4.414l.707-.707A1 1 0 0012.998 2h-6zm2 6.172V4h2v4.172a3 3 0 00.879 2.12l1.027 1.028a4 4 0 00-2.171.102l-.47.156a4 4 0 01-2.53 0l-.563-.187a1.992 1.992 0 00-.114-.035l1.063-1.063a3 3 0 00.879-2.121z"
+          />
+        </svg>
+        <p>{children}</p>
       </div>
     </div>
   )
