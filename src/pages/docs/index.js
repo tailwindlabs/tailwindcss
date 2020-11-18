b61a6b60f7c7fdd26e8051757406cee49962213a
Adam Wathan <adam.wathan@gmail.com>
2020-11-18 12:06:17 -0500

    Content tweaks

--- src/pages/docs/index.js
+++ src/pages/docs/index.js
@@ -99,9 +99,9 @@ export default function DocsLandingPage() {
           <div className="w-full relative text-white overflow-hidden rounded-3xl flex shadow-lg">
             <div className={`w-full flex md:flex-col bg-gradient-to-br ${gradients.violet[0]}`}>
               <div className="sm:max-w-sm sm:flex-none md:w-auto md:flex-auto flex flex-col items-start relative z-10 p-6 xl:p-8">
-                <h2 className="text-xl font-semibold mb-2 text-shadow">Read the guides</h2>
+                <h2 className="text-xl font-semibold mb-2 text-shadow">Read the docs</h2>
                 <p className="font-medium text-violet-100 text-shadow mb-4">
-                  Learn how to get Tailwind set up with your favorite tools.
+                  Learn how to get Tailwind set up in your project.
                 </p>
                 <Link href="/docs/installation">
                   <a className="mt-auto bg-violet-800 bg-opacity-50 hover:bg-opacity-75 transition-colors duration-200 rounded-xl font-semibold py-2 px-4 inline-flex">
@@ -148,7 +148,9 @@ export default function DocsLandingPage() {
             <div className={`w-full flex md:flex-col bg-gradient-to-br ${gradients.amber[0]}`}>
               <div className="sm:max-w-sm sm:flex-none md:w-auto md:flex-auto flex flex-col items-start relative z-10 p-6 xl:p-8">
                 <h2 className="text-xl font-semibold mb-2 text-shadow">Watch the screencasts</h2>
-                <p className="font-medium text-amber-100 text-shadow mb-4">{/* TODO */}</p>
+                <p className="font-medium text-amber-100 text-shadow mb-4">
+                  Learn more about Tailwind directly from the team on our channel.
+                </p>
                 <Link href="https://www.youtube.com/tailwindlabs">
                   <a className="mt-auto bg-amber-900 bg-opacity-50 hover:bg-opacity-75 transition-colors duration-200 rounded-xl font-semibold py-2 px-4 inline-flex">
                     Start watching
