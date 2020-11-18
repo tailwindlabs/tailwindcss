2375a04b4ae6963952db0ec66dfc4606f6d797e7
Brad Cornes <bradlc41@gmail.com>
2020-11-18 13:36:59 +0000

    home page responsive spacing

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -32,10 +32,10 @@ const footerNav = {
 
 export function Footer() {
   return (
-    <footer className="bg-gray-50 pt-32 pb-20">
+    <footer className="bg-gray-50 pt-16 pb-12 sm:pt-20 md:pt-24 xl:pt-32 sm:pb-20">
       <div className="max-w-screen-lg xl:max-w-screen-xl mx-auto divide-y divide-gray-200 px-4 sm:px-6 md:px-8">
         <ul
-          className={`${styles.nav} text-sm font-medium pb-20 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10`}
+          className={`${styles.nav} text-sm font-medium pb-14 sm:pb-20 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10`}
         >
           {Object.keys(footerNav).map((section, i) => (
             <li key={section} className={clsx('space-y-5', footerNav[section].className)}>
@@ -56,7 +56,7 @@ export function Footer() {
             </li>
           ))}
         </ul>
-        <div className="pt-12">
+        <div className="pt-10 sm:pt-12">
           <Logo width="208" height="26" />
         </div>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -68,10 +68,10 @@ function NpmInstallButton() {
 
 export default function Home() {
   return (
-    <div className="space-y-44 overflow-hidden b">
+    <div className="space-y-20 sm:space-y-32 md:space-y-40 lg:space-y-44 overflow-hidden">
       <header className="relative z-10 max-w-screen-lg xl:max-w-screen-xl mx-auto">
-        <div className="px-4 sm:px-6 md:px-8 mb-20 xl:mb-8">
-          <div className="border-b border-gray-200 py-6 flex items-center justify-between mb-20 -mx-4 px-4 sm:mx-0 sm:px-0">
+        <div className="px-4 sm:px-6 md:px-8 mb-14 sm:mb-20 xl:mb-8">
+          <div className="border-b border-gray-200 py-6 flex items-center justify-between mb-16 sm:mb-20 -mx-4 px-4 sm:mx-0 sm:px-0">
             <Search />
             <a
               href="https://github.com/tailwindlabs/tailwindcss"
@@ -87,10 +87,10 @@ export default function Home() {
             </a>
           </div>
           <Logo className="w-auto h-7 sm:h-8" />
-          <h1 className="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-gray-900 mt-14 mb-10">
+          <h1 className="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-gray-900 mt-10 mb-8 sm:mt-14 sm:mb-10">
             Rapidly build modern websites without ever leaving your HTML.
           </h1>
-          <p className="max-w-screen-lg text-lg sm:text-2xl sm:leading-10 font-medium mb-11">
+          <p className="max-w-screen-lg text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-11">
             A utility-first CSS framework packed with classes like <InlineCode>flex</InlineCode>,{' '}
             <InlineCode>pt-4</InlineCode>, <InlineCode>text-center</InlineCode> and{' '}
             <InlineCode>rotate-90</InlineCode> that can be composed to build any design, directly in
@@ -124,7 +124,7 @@ export default function Home() {
         </div>
       </section>
       <Testimonials />
-      <div className="max-w-screen-lg xl:max-w-screen-xl mx-auto space-y-44">
+      <div className="max-w-screen-lg xl:max-w-screen-xl mx-auto space-y-20 sm:space-y-32 md:space-y-40 lg:space-y-44">
         <ConstraintBased />
         <BuildAnything />
         <Performance />
