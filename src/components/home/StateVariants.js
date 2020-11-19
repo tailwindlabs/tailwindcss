8de884972476140ceee3e88dbbab8e5080a4f815
Brad Cornes <bradlc41@gmail.com>
2020-11-19 16:23:28 +0000

    add missing "state variants" classes

--- src/components/home/StateVariants.js
+++ src/components/home/StateVariants.js
@@ -57,7 +57,7 @@ const {
     <svg width="20" height="20" fill="currentColor" class="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
       <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
     </svg>
-    <input class="(input-focus)focus:border-light-blue-300 (input-focus)focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10" type="text" aria-label="Filter projects" placeholder="Filter projects" />
+    <input class="(input-focus)focus:border-light-blue-500 (input-focus)focus:ring-1 (input-focus)focus:ring-light-blue-500 (input-focus)focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10" type="text" aria-label="Filter projects" placeholder="Filter projects" />
   </form>
   <ul class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-4">
     <li x-for="item in items">
