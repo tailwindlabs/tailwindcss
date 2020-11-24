16195395ea16a8f44b18d04c598da7c02901d78e
Brad Cornes <bradlc41@gmail.com>
2020-11-24 10:49:55 +0000

    fix class table sorting for complex selectors

--- src/utils/sortClasses.js
+++ src/utils/sortClasses.js
@@ -2,9 +2,15 @@ import groupBy from 'just-group-by'
 
 export function numbersFirst(classes) {
   return [...classes].sort((a, b) => {
-    let aNum = a.replace(/\\/g, '').match(/-([0-9.]+)$/)
+    let aNum = a
+      .split(/\s+/)[0]
+      .replace(/\\/g, '')
+      .match(/-([0-9.]+)$/)
     aNum = aNum === null ? NaN : parseFloat(aNum[1])
-    let bNum = b.replace(/\\/g, '').match(/-([0-9.]+)$/)
+    let bNum = b
+      .split(/\s+/)[0]
+      .replace(/\\/g, '')
+      .match(/-([0-9.]+)$/)
     bNum = bNum === null ? NaN : parseFloat(bNum[1])
     if (isNaN(aNum) && isNaN(bNum)) return 0
     if (isNaN(aNum)) return 1
@@ -14,5 +20,5 @@ export function numbersFirst(classes) {
 }
 
 export function numbersFirstByPrefix(classes) {
-  return Object.values(groupBy(classes, (c) => c.match(/^(-?.*?)[^-]+$/)[1])).flatMap(numbersFirst)
+  return Object.values(groupBy(classes, (c) => c.match(/^(.*?)[^-]+$/)[1])).flatMap(numbersFirst)
 }
