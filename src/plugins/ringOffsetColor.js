b3eb0f46e586724e031496534ae3060de768d916
20lives <lior@dotcore.co.il>
2020-11-25 13:59:54 +0200

    Refactor ringOffsetColor utilities generation

--- src/plugins/ringOffsetColor.js
+++ src/plugins/ringOffsetColor.js
@@ -1,21 +1,15 @@
-import _ from 'lodash'
+import mapObject from '../util/mapObject'
 import flattenColorPalette from '../util/flattenColorPalette'
 import nameClass from '../util/nameClass'
 import toColorValue from '../util/toColorValue'
 
-export default function () {
-  return function ({ addUtilities, theme, variants }) {
-    const colors = flattenColorPalette(theme('ringOffsetColor'))
-    const utilities = _.fromPairs(
-      _.map(_.omit(colors, 'DEFAULT'), (value, modifier) => {
-        return [
-          nameClass('ring-offset', modifier),
-          {
-            '--tw-ring-offset-color': toColorValue(value),
-          },
-        ]
-      })
-    )
-    addUtilities(utilities, variants('ringOffsetColor'))
-  }
+export default () => ({ addUtilities, theme, variants }) => {
+  const { DEFAULT, ...colors } = flattenColorPalette(theme('ringOffsetColor'))
+  const utilities = mapObject(colors, ([modifier, value]) => [
+    nameClass('ring-offset', modifier),
+    {
+      '--tw-ring-offset-color': toColorValue(value),
+    },
+  ])
+  addUtilities(utilities, variants('ringOffsetColor'))
 }
