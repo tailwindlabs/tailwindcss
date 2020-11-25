a5a3bf3e1d6b392b6e0b5563d67bbf4b19dd3d16
20lives <lior@dotcore.co.il>
2020-11-25 12:49:11 +0200

    Refactor fill utilities generation

--- src/plugins/fill.js
+++ src/plugins/fill.js
@@ -1,18 +1,15 @@
-import _ from 'lodash'
+import mapObject from '../util/mapObject'
 import flattenColorPalette from '../util/flattenColorPalette'
 import nameClass from '../util/nameClass'
 import toColorValue from '../util/toColorValue'
 
-export default function () {
-  return function ({ addUtilities, theme, variants }) {
-    const colors = flattenColorPalette(theme('fill'))
+export default () => ({ addUtilities, theme, variants }) => {
+  const colors = flattenColorPalette(theme('fill'))
 
-    const utilities = _.fromPairs(
-      _.map(colors, (value, modifier) => {
-        return [nameClass('fill', modifier), { fill: toColorValue(value) }]
-      })
-    )
+  const utilities = mapObject(colors, ([modifier, value]) => [
+    nameClass('fill', modifier),
+    { fill: toColorValue(value) },
+  ])
 
-    addUtilities(utilities, variants('fill'))
-  }
+  addUtilities(utilities, variants('fill'))
 }
