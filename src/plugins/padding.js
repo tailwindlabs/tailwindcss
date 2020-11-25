75919c59d4e9fa2c8967c5b7b5d2c10ecab9a7f5
20lives <lior@dotcore.co.il>
2020-11-25 13:25:43 +0200

    Refactor padding utilities generation

--- src/plugins/padding.js
+++ src/plugins/padding.js
@@ -1,28 +1,25 @@
-import _ from 'lodash'
 import nameClass from '../util/nameClass'
 
-export default function () {
-  return function ({ addUtilities, theme, variants }) {
-    const generators = [
-      (size, modifier) => ({
-        [nameClass('p', modifier)]: { padding: `${size}` },
-      }),
-      (size, modifier) => ({
-        [nameClass('py', modifier)]: { 'padding-top': `${size}`, 'padding-bottom': `${size}` },
-        [nameClass('px', modifier)]: { 'padding-left': `${size}`, 'padding-right': `${size}` },
-      }),
-      (size, modifier) => ({
-        [nameClass('pt', modifier)]: { 'padding-top': `${size}` },
-        [nameClass('pr', modifier)]: { 'padding-right': `${size}` },
-        [nameClass('pb', modifier)]: { 'padding-bottom': `${size}` },
-        [nameClass('pl', modifier)]: { 'padding-left': `${size}` },
-      }),
-    ]
+export default () => ({ addUtilities, theme, variants }) => {
+  const generators = [
+    ([modifier, size]) => ({
+      [nameClass('p', modifier)]: { padding: `${size}` },
+    }),
+    ([modifier, size]) => ({
+      [nameClass('py', modifier)]: { 'padding-top': `${size}`, 'padding-bottom': `${size}` },
+      [nameClass('px', modifier)]: { 'padding-left': `${size}`, 'padding-right': `${size}` },
+    }),
+    ([modifier, size]) => ({
+      [nameClass('pt', modifier)]: { 'padding-top': `${size}` },
+      [nameClass('pr', modifier)]: { 'padding-right': `${size}` },
+      [nameClass('pb', modifier)]: { 'padding-bottom': `${size}` },
+      [nameClass('pl', modifier)]: { 'padding-left': `${size}` },
+    }),
+  ]
 
-    const utilities = _.flatMap(generators, (generator) => {
-      return _.flatMap(theme('padding'), generator)
-    })
+  const utilities = generators.flatMap((generator) =>
+    Object.entries(theme('padding')).flatMap(generator)
+  )
 
-    addUtilities(utilities, variants('padding'))
-  }
+  addUtilities(utilities, variants('padding'))
 }
