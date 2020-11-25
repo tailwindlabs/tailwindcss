7ec232bcf5b92d52b4551afec745e5fbd38743e0
20lives <lior@dotcore.co.il>
2020-11-25 13:29:17 +0200

    Refactor stroke utilities generation

--- src/plugins/stroke.js
+++ src/plugins/stroke.js
@@ -1,18 +1,15 @@
-import _ from 'lodash'
+import mapObject from '../util/mapObject'
 import flattenColorPalette from '../util/flattenColorPalette'
 import nameClass from '../util/nameClass'
 import toColorValue from '../util/toColorValue'
 
-export default function () {
-  return function ({ addUtilities, theme, variants }) {
-    const colors = flattenColorPalette(theme('stroke'))
+export default () => ({ addUtilities, theme, variants }) => {
+  const colors = flattenColorPalette(theme('stroke'))
 
-    const utilities = _.fromPairs(
-      _.map(colors, (value, modifier) => {
-        return [nameClass('stroke', modifier), { stroke: toColorValue(value) }]
-      })
-    )
+  const utilities = mapObject(colors, ([modifier, value]) => [
+    nameClass('stroke', modifier),
+    { stroke: toColorValue(value) },
+  ])
 
-    addUtilities(utilities, variants('stroke'))
-  }
+  addUtilities(utilities, variants('stroke'))
 }
