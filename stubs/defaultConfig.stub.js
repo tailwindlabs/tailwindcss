712d3bd1816ef3823c398b3268aa18e13ce08af8
Brian Hyun <62228052+brianhyun@users.noreply.github.com>
2021-02-25 19:18:24 -0800

    Remove unnecessary extra blank line

--- stubs/defaultConfig.stub.js
+++ stubs/defaultConfig.stub.js
@@ -15,7 +15,6 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-
       black: colors.black,
       white: colors.white,
       gray: colors.coolGray,
