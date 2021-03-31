449d53f67d9d3538000799320c125619d823b5ec
Dr. Derek Austin <derekraustin@gmail.com>
2021-03-31 14:46:34 -0500

    Update prettier.config.js to remove "unknown option" editorconfig: true

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  editorconfig: true, // Read .editorconfig
-  // These settings are handled in .editorconfig:
-  // tabWidth: 2, // indent_size = 2
-  // useTabs: false, // indent_style = space
-  // endOfLine: 'lf', // end_of_line = lf
+  // These settings are duplicated in .editorconfig:
+  tabWidth: 2, // indent_size = 2
+  useTabs: false, // indent_style = space
+  endOfLine: 'lf', // end_of_line = lf
   semi: false, // default: true
   singleQuote: true, // default: false
   printWidth: 100, // default: 80
