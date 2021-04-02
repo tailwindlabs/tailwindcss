d81edb739ddda7a8a5eae9c52b4399cd490452d9
Adam Wathan <adam.wathan@gmail.com>
2021-04-02 12:27:15 -0400

    Add PostCSS 7 support (maybe)

--- jit/index.js
+++ jit/index.js
@@ -1,4 +1,5 @@
 const postcss = require('postcss')
+const dlv = require('dlv')
 
 const evaluateTailwindFunctions = require('../lib/lib/evaluateTailwindFunctions').default
 const substituteScreenAtRules = require('../lib/lib/substituteScreenAtRules').default
@@ -13,6 +14,50 @@ const collapseAdjacentRules = require('./lib/collapseAdjacentRules')
 const { env } = require('./lib/sharedState')
 
 module.exports = (configOrPath = {}) => {
+  return [
+    env.DEBUG &&
+      function (root) {
+        console.log('\n')
+        console.time('JIT TOTAL')
+        return root
+      },
+    function (root, result) {
+      function registerDependency(fileName, type = 'dependency') {
+        result.messages.push({
+          type,
+          plugin: 'tailwindcss-jit',
+          parent: result.opts.from,
+          file: fileName,
+        })
+      }
+
+      rewriteTailwindImports(root)
+
+      let context = setupContext(configOrPath)(result, root)
+
+      if (!env.TAILWIND_DISABLE_TOUCH) {
+        if (context.configPath !== null) {
+          registerDependency(context.configPath)
+        }
+      }
+
+      return postcss([
+        removeLayerAtRules(context),
+        expandTailwindAtRules(context, registerDependency),
+        expandApplyAtRules(context),
+        evaluateTailwindFunctions(context.tailwindConfig),
+        substituteScreenAtRules(context.tailwindConfig),
+        collapseAdjacentRules(context),
+      ]).process(root, { from: undefined })
+    },
+    env.DEBUG &&
+      function (root) {
+        console.timeEnd('JIT TOTAL')
+        console.log('\n')
+        return root
+      },
+  ].filter(Boolean)
+
   return {
     postcssPlugin: 'tailwindcss-jit',
     plugins: [
