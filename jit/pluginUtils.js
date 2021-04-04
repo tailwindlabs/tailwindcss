0848bdf21c5ecf9438a3876a1d124f893eb4cee1
Nurettin Kaya <sibbngheid@gmail.com>
2021-04-04 16:17:45 -0700

    Take non digit characters into account and fix transform

--- jit/pluginUtils.js
+++ jit/pluginUtils.js
@@ -112,7 +112,7 @@ function asValue(modifier, lookup = {}, { validate = () => true, transform = (v)
   }
 
   // add spaces around operators inside calc() that do not follow an operator or (
-  return transform(value).replace(/(?<=^calc\(.+?)(?!\b-\d)(?<![-+*/(])([-+*/])/g, ' $1 ')
+  return transform(value).replace(/(?<=calc\(.+?)(?!\b-\b)(?<![-+*/(])([-+*/])/g, ' $1 ')
 }
 
 function asUnit(modifier, units, lookup = {}) {
