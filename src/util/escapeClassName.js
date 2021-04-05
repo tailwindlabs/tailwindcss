db3f97a21bc814f0b5140180cfa42471352f9e14
Adam Wathan <adam.wathan@gmail.com>
2021-04-05 11:03:54 -0400

    Escape commas in native escapeClassName and nameClass functions

--- src/util/escapeClassName.js
+++ src/util/escapeClassName.js
@@ -1,8 +1,12 @@
 import parser from 'postcss-selector-parser'
 import get from 'lodash/get'
 
+function escapeCommas(className) {
+  return className.replace(/\\,/g, '\\2c ')
+}
+
 export default function escapeClassName(className) {
   const node = parser.className()
   node.value = className
-  return get(node, 'raws.value', node.value)
+  return escapeCommas(get(node, 'raws.value', node.value))
 }
