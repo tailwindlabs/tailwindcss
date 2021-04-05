ab2896be5956a5ac3c2a2704647ef4d13be3b387
Adam Wathan <adam.wathan@gmail.com>
2021-04-05 13:48:43 -0400

    Add version badges, update release notes link

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -5,10 +5,12 @@ export function PageHeader({ title, description, badge = {}, border = true }) {
 
   return (
     <div className={clsx('pb-10', { 'border-b border-gray-200 mb-10': border })}>
-      <div className="flex items-center">
-        <h1 className="text-3xl font-extrabold text-gray-900 tracking-tight">{title}</h1>
+      <div>
+        <h1 className="inline-block text-3xl font-extrabold text-gray-900 tracking-tight">
+          {title}
+        </h1>
         {badge.key && badge.value && (
-          <dl className="mt-0 mb-1 ml-3 flex-none inline-flex items-center px-3 py-1 rounded-full text-sm font-medium leading-4 bg-green-150 text-green-900 tracking-tight">
+          <dl className="ml-3 mt-1.5 align-top inline-flex items-center px-3 py-1 rounded-full text-sm font-medium leading-4 bg-cyan-100 text-cyan-900 tracking-tight">
             <dt className="sr-only">{badge.key}</dt>
             <dd>{badge.value}</dd>
           </dl>
