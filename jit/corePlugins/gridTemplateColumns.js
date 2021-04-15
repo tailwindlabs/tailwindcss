3af6aabcfe99359b5a8332cd6c28e71df3bb8e07
Nathaniel Pendy <76533062+nathanielop@users.noreply.github.com>
2021-04-15 16:09:37 -0500

    Update gridTemplateColumns.js
    Enable JIT styling for grid-cols

--- jit/corePlugins/gridTemplateColumns.js
+++ jit/corePlugins/gridTemplateColumns.js
@@ -1,15 +1,17 @@
-const { asList, nameClass } = require('../pluginUtils')
+const { createSimpleStaticUtilityPlugin } = require('../pluginUtils')
 
-module.exports = function ({ matchUtilities }) {
-  matchUtilities({
-    'grid-cols': (modifier, { theme }) => {
-      let value = asList(modifier, theme.gridTemplateColumns)
-
-      if (value === undefined) {
-        return []
-      }
-
-      return { [nameClass('grid-cols', modifier)]: { 'grid-template-columns': value } }
-    },
-  })
-}
+module.exports = createSimpleStaticUtilityPlugin({
+  '.grid-cols-1': { gridTemplateColumns: 'repeat(1, minmax(0, 1fr))' },
+  '.grid-cols-2': { gridTemplateColumns: 'repeat(2, minmax(0, 1fr))' },
+  '.grid-cols-3': { gridTemplateColumns: 'repeat(3, minmax(0, 1fr))' },
+  '.grid-cols-4': { gridTemplateColumns: 'repeat(4, minmax(0, 1fr))' },
+  '.grid-cols-5': { gridTemplateColumns: 'repeat(5, minmax(0, 1fr))' },
+  '.grid-cols-6': { gridTemplateColumns: 'repeat(6, minmax(0, 1fr))' },
+  '.grid-cols-7': { gridTemplateColumns: 'repeat(7, minmax(0, 1fr))' },
+  '.grid-cols-8': { gridTemplateColumns: 'repeat(8, minmax(0, 1fr))' },
+  '.grid-cols-9': { gridTemplateColumns: 'repeat(9, minmax(0, 1fr))' },
+  '.grid-cols-10': { gridTemplateColumns: 'repeat(10, minmax(0, 1fr))' },
+  '.grid-cols-11': { gridTemplateColumns: 'repeat(11, minmax(0, 1fr))' },
+  '.grid-cols-12': { gridTemplateColumns: 'repeat(12, minmax(0, 1fr))' },
+  '.grid-cols-none': { gridTemplateColumns: 'none' } 
+})
