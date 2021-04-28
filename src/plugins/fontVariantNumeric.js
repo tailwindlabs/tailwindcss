5dfd64832d9f0acba7d09d7f2ee5b10f58e350c4
Adam Wathan <adam.wathan@gmail.com>
2021-04-28 12:17:19 -0400

    Fix AOT generation bugs

--- src/plugins/fontVariantNumeric.js
+++ src/plugins/fontVariantNumeric.js
@@ -10,24 +10,6 @@ let fontVariantBaseStyles = {
   },
 }
 
-let fontVariantUtilities = {
-  'normal-nums': { '.normal-nums': { 'font-variant-numeric': 'normal' } },
-  ordinal: { '.ordinal': { '--tw-ordinal': 'ordinal' } },
-  'slashed-zero': { '.slashed-zero': { '--tw-slashed-zero': 'slashed-zero' } },
-  'lining-nums': { '.lining-nums': { '--tw-numeric-figure': 'lining-nums' } },
-  'oldstyle-nums': { '.oldstyle-nums': { '--tw-numeric-figure': 'oldstyle-nums' } },
-  'proportional-nums': {
-    '.proportional-nums': { '--tw-numeric-spacing': 'proportional-nums' },
-  },
-  'tabular-nums': { '.tabular-nums': { '--tw-numeric-spacing': 'tabular-nums' } },
-  'diagonal-fractions': {
-    '.diagonal-fractions': { '--tw-numeric-fraction': 'diagonal-fractions' },
-  },
-  'stacked-fractions': {
-    '.stacked-fractions': { '--tw-numeric-fraction': 'stacked-fractions' },
-  },
-}
-
 export default function () {
   return function ({ config, matchUtilities, addUtilities, variants }) {
     if (config('mode') === 'jit') {
@@ -43,12 +25,54 @@ export default function () {
         'stacked-fractions': fontVariantBaseStyles,
       })
 
-      matchUtilities(fontVariantUtilities)
+      matchUtilities({
+        'normal-nums': { '.normal-nums': { 'font-variant-numeric': 'normal' } },
+        ordinal: { '.ordinal': { '--tw-ordinal': 'ordinal' } },
+        'slashed-zero': { '.slashed-zero': { '--tw-slashed-zero': 'slashed-zero' } },
+        'lining-nums': { '.lining-nums': { '--tw-numeric-figure': 'lining-nums' } },
+        'oldstyle-nums': { '.oldstyle-nums': { '--tw-numeric-figure': 'oldstyle-nums' } },
+        'proportional-nums': {
+          '.proportional-nums': { '--tw-numeric-spacing': 'proportional-nums' },
+        },
+        'tabular-nums': { '.tabular-nums': { '--tw-numeric-spacing': 'tabular-nums' } },
+        'diagonal-fractions': {
+          '.diagonal-fractions': { '--tw-numeric-fraction': 'diagonal-fractions' },
+        },
+        'stacked-fractions': {
+          '.stacked-fractions': { '--tw-numeric-fraction': 'stacked-fractions' },
+        },
+      })
     } else {
       addUtilities(
         {
           ...fontVariantBaseStyles,
-          ...fontVariantUtilities,
+          '.normal-nums': {
+            'font-variant-numeric': 'normal',
+          },
+          '.ordinal': {
+            '--tw-ordinal': 'ordinal',
+          },
+          '.slashed-zero': {
+            '--tw-slashed-zero': 'slashed-zero',
+          },
+          '.lining-nums': {
+            '--tw-numeric-figure': 'lining-nums',
+          },
+          '.oldstyle-nums': {
+            '--tw-numeric-figure': 'oldstyle-nums',
+          },
+          '.proportional-nums': {
+            '--tw-numeric-spacing': 'proportional-nums',
+          },
+          '.tabular-nums': {
+            '--tw-numeric-spacing': 'tabular-nums',
+          },
+          '.diagonal-fractions': {
+            '--tw-numeric-fraction': 'diagonal-fractions',
+          },
+          '.stacked-fractions': {
+            '--tw-numeric-fraction': 'stacked-fractions',
+          },
         },
         variants('fontVariantNumeric')
       )
