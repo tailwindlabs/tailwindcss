79e1feba67d807360df42f1d1daf2a79348cfd79
Robin Malfait <malfait.robin@gmail.com>
2021-05-17 14:15:27 +0200

    add integration tests with `webpack 5`

---
+++ integrations/webpack-5/webpack.config.js
@@ -0,0 +1,16 @@
+let MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  output: {
+    clean: true,
+  },
+}
