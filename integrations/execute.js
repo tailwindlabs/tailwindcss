7565099c1f7dddf1b0423b6d69a41bbc8271d145
Robin Malfait <malfait.robin@gmail.com>
2021-05-18 17:21:35 +0200

    Integrations setup (#4354)
    * add integration test tools
    
    * setup jest in the integrations folder
    
    * add `test:integrations` script
    
    The default `npm test` script will ignore all the tests in the
    `integrations` folder.
    
    * add integration tests with `webpack 5`
    
    * add integration tests with `postcss-cli`
    
    * add `npm run install:integrations` script
    
    This script will run `npm install` in every integration, and in the
    integrations folder itself (to setup Jest for example).
    
    * add `toIncludeCss` custom matcher
    
    * increate Jest timeout for integration tests
    
    * add integration tests with `vite`
    
    * add integration tests with `webpack 4`
    
    * add isomorphic fetch
    
    * add the ability to wait for specific stdout/stderr output
    
    * write vite tests, assert using API calls
    
    We will wait for the correct stdout/stderr output, once we know that we
    can request the fresh css, we will fetch it and make assertions
    accordingly.
    
    Port is currently hardcoded, maybe we should use a packaage to ensure
    that we use a free port.
    
    * add integration tests with `rollup`
    
    * add integration tests with `parcel`
    
    * run all integration tests in band
    
    * add .gitignore in integrations folder

---
+++ integrations/execute.js
@@ -0,0 +1,99 @@
+let path = require('path')
+let { spawn } = require('child_process')
+let resolveToolRoot = require('./resolve-tool-root')
+
+let runningProcessess = []
+
+afterEach(() => {
+  runningProcessess.splice(0).forEach((runningProcess) => runningProcess.stop())
+})
+
+module.exports = function $(command, options = {}) {
+  let abortController = new AbortController()
+  let cwd = resolveToolRoot()
+
+  let args = command.split(' ')
+  command = args.shift()
+  command = path.resolve(cwd, 'node_modules', '.bin', command)
+
+  let stdoutMessages = []
+  let stderrMessages = []
+
+  let stdoutActors = []
+  let stderrActors = []
+
+  function notifyNext(actors, messages) {
+    if (actors.length <= 0) return
+    let [next] = actors
+
+    for (let [idx, message] of messages.entries()) {
+      if (next.predicate(message)) {
+        messages.splice(0, idx + 1)
+        let actorIdx = actors.indexOf(next)
+        actors.splice(actorIdx, 1)
+        next.resolve()
+        break
+      }
+    }
+  }
+
+  function notifyNextStdoutActor() {
+    return notifyNext(stdoutActors, stdoutMessages)
+  }
+
+  function notifyNextStderrActor() {
+    return notifyNext(stderrActors, stderrMessages)
+  }
+
+  let runningProcess = new Promise((resolve, reject) => {
+    let child = spawn(command, args, {
+      ...options,
+      env: {
+        ...process.env,
+        ...options.env,
+      },
+      signal: abortController.signal,
+      cwd,
+    })
+
+    let stdout = ''
+    let stderr = ''
+
+    child.stdout.on('data', (data) => {
+      stdoutMessages.push(data.toString())
+      notifyNextStdoutActor()
+      stdout += data
+    })
+
+    child.stderr.on('data', (data) => {
+      stderrMessages.push(data.toString())
+      notifyNextStderrActor()
+      stderr += data
+    })
+
+    child.on('close', (code, signal) => {
+      ;(signal === 'SIGTERM' ? resolve : code === 0 ? resolve : reject)({ code, stdout, stderr })
+    })
+  })
+
+  runningProcessess.push(runningProcess)
+
+  return Object.assign(runningProcess, {
+    stop() {
+      abortController.abort()
+      return runningProcess
+    },
+    onStdout(predicate) {
+      return new Promise((resolve) => {
+        stdoutActors.push({ predicate, resolve })
+        notifyNextStdoutActor()
+      })
+    },
+    onStderr(predicate) {
+      return new Promise((resolve) => {
+        stderrActors.push({ predicate, resolve })
+        notifyNextStderrActor()
+      })
+    },
+  })
+}
---
+++ integrations/vite/tests/integration.test.js
@@ -0,0 +1,264 @@
+require('isomorphic-fetch')
+
+let $ = require('../../execute')
+let { css, html, javascript } = require('../../syntax')
+
+let { readOutputFile, appendToInputFile, writeInputFile } = require('../../io')({
+  output: 'dist',
+  input: '.',
+})
+
+let PORT = 1337
+
+async function fetchCSS() {
+  let response = await fetch(`http://0.0.0.0:${PORT}/index.css`, {
+    headers: {
+      Accept: 'text/css',
+    },
+  })
+  return response.text()
+}
+
+describe('static build', () => {
+  it('should be possible to generate tailwind output', async () => {
+    await writeInputFile(
+      'index.html',
+      html`
+        <link rel="stylesheet" href="./index.css" />
+        <div class="font-bold"></div>
+      `
+    )
+
+    await $('vite build', {
+      env: { NODE_ENV: 'production' },
+    })
+
+    expect(await readOutputFile(/index.\w+\.css$/)).toIncludeCss(
+      css`
+        .font-bold {
+          font-weight: 700;
+        }
+      `
+    )
+  })
+})
+
+describe('watcher', () => {
+  test('classes are generated when the html file changes', async () => {
+    await writeInputFile(
+      'index.html',
+      html`
+        <link rel="stylesheet" href="./index.css" />
+        <div class="font-bold"></div>
+      `
+    )
+
+    let runningProcess = $(`vite --port ${PORT}`, {
+      env: { TAILWIND_MODE: 'watch' },
+    })
+    await runningProcess.onStdout((message) => message.includes('ready in'))
+
+    expect(await fetchCSS()).toIncludeCss(
+      css`
+        .font-bold {
+          font-weight: 700;
+        }
+      `
+    )
+
+    await appendToInputFile('index.html', html`<div class="font-normal"></div>`)
+    await runningProcess.onStdout((message) => message.includes('hmr update /index.css'))
+
+    expect(await fetchCSS()).toIncludeCss(
+      css`
+        .font-bold {
+          font-weight: 700;
+        }
+        .font-normal {
+          font-weight: 400;
+        }
+      `
+    )
+
+    await appendToInputFile('index.html', html`<div class="bg-red-500"></div>`)
+    await runningProcess.onStdout((message) => message.includes('hmr update /index.css'))
+
+    expect(await fetchCSS()).toIncludeCss(
+      css`
+        .bg-red-500 {
+          --tw-bg-opacity: 1;
+          background-color: rgba(239, 68, 68, var(--tw-bg-opacity));
+        }
+        .font-bold {
+          font-weight: 700;
+        }
+        .font-normal {
+          font-weight: 400;
+        }
+      `
+    )
+
+    return runningProcess.stop()
+  })
+
+  test('classes are generated when the tailwind.config.js file changes', async () => {
+    await writeInputFile(
+      'index.html',
+      html`
+        <link rel="stylesheet" href="./index.css" />
+        <div class="font-bold md:font-medium"></div>
+      `
+    )
+
+    let runningProcess = $(`vite --port ${PORT}`, {
+      env: { TAILWIND_MODE: 'watch' },
+    })
+    await runningProcess.onStdout((message) => message.includes('ready in'))
+
+    expect(await fetchCSS()).toIncludeCss(
+      css`
+        .font-bold {
+          font-weight: 700;
+        }
+        @media (min-width: 768px) {
+          .md\\:font-medium {
+            font-weight: 500;
+          }
+        }
+      `
+    )
+
+    await writeInputFile(
+      'tailwind.config.js',
+      javascript`
+        module.exports = {
+          purge: ['./index.html'],
+          mode: 'jit',
+          darkMode: false, // or 'media' or 'class'
+          theme: {
+            extend: {
+              screens: {
+                md: '800px'
+              },
+              fontWeight: {
+                bold: 'bold'
+              }
+            },
+          },
+          variants: {
+            extend: {},
+          },
+          corePlugins: {
+            preflight: false,
+          },
+          plugins: [],
+        }
+      `
+    )
+    await runningProcess.onStdout((message) => message.includes('hmr update /index.css'))
+
+    expect(await fetchCSS()).toIncludeCss(
+      css`
+        .font-bold {
+          font-weight: bold;
+        }
+        @media (min-width: 800px) {
+          .md\\:font-medium {
+            font-weight: 500;
+          }
+        }
+      `
+    )
+
+    return runningProcess.stop()
+  })
+
+  test('classes are generated when the index.css file changes', async () => {
+    await writeInputFile(
+      'index.html',
+      html`
+        <link rel="stylesheet" href="./index.css" />
+        <div class="font-bold btn"></div>
+      `
+    )
+
+    let runningProcess = $(`vite --port ${PORT}`, {
+      env: { TAILWIND_MODE: 'watch' },
+    })
+    await runningProcess.onStdout((message) => message.includes('ready in'))
+
+    expect(await fetchCSS()).toIncludeCss(
+      css`
+        .font-bold {
+          font-weight: 700;
+        }
+      `
+    )
+
+    await writeInputFile(
+      'index.css',
+      css`
+        @tailwind base;
+        @tailwind components;
+        @tailwind utilities;
+
+        @layer components {
+          .btn {
+            @apply px-2 py-1 rounded;
+          }
+        }
+      `
+    )
+    await runningProcess.onStdout((message) => message.includes('hmr update /index.css'))
+
+    expect(await fetchCSS()).toIncludeCss(
+      css`
+        .btn {
+          border-radius: 0.25rem;
+          padding-left: 0.5rem;
+          padding-right: 0.5rem;
+          padding-top: 0.25rem;
+          padding-bottom: 0.25rem;
+        }
+        .font-bold {
+          font-weight: 700;
+        }
+      `
+    )
+
+    await writeInputFile(
+      'index.css',
+      css`
+        @tailwind base;
+        @tailwind components;
+        @tailwind utilities;
+
+        @layer components {
+          .btn {
+            @apply px-2 py-1 rounded bg-red-500;
+          }
+        }
+      `
+    )
+    await runningProcess.onStdout((message) => message.includes('hmr update /index.css'))
+
+    expect(await fetchCSS()).toIncludeCss(
+      css`
+        .btn {
+          border-radius: 0.25rem;
+          --tw-bg-opacity: 1;
+          background-color: rgba(239, 68, 68, var(--tw-bg-opacity));
+          padding-left: 0.5rem;
+          padding-right: 0.5rem;
+          padding-top: 0.25rem;
+          padding-bottom: 0.25rem;
+        }
+        .font-bold {
+          font-weight: 700;
+        }
+      `
+    )
+
+    return runningProcess.stop()
+  })
+})
