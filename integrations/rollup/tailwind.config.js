a1e34dacefc937e64528bfa1da42562206f85dee
Robin Malfait <malfait.robin@gmail.com>
2021-05-18 15:52:25 +0200

    add integration tests with `rollup`

---
+++ integrations/rollup/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/index.html'],
+  mode: 'jit',
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [],
+}
