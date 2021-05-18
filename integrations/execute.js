24f0242b64ae4e4823d27371de54a1d92d0767b2
Robin Malfait <malfait.robin@gmail.com>
2021-05-18 14:46:51 +0200

    add the ability to wait for specific stdout/stderr output

--- integrations/execute.js
+++ integrations/execute.js
@@ -5,7 +5,7 @@ let resolveToolRoot = require('./resolve-tool-root')
 let runningProcessess = []
 
 afterEach(() => {
-  runningProcessess.splice(0).forEach((abort) => abort())
+  runningProcessess.splice(0).forEach((runningProcess) => runningProcess.stop())
 })
 
 module.exports = function $(command, options = {}) {
@@ -15,6 +15,35 @@ module.exports = function $(command, options = {}) {
   let args = command.split(' ')
   command = args.shift()
   command = path.resolve(cwd, 'node_modules', '.bin', command)
+
+  let stdoutMessages = []
+  let stderrMessages = []
+
+  let stdoutActors = []
+  let stderrActors = []
+
+  function notifyNext(actors, messages) {
+    if (actors.length <= 0) return
+    let [next] = actors
+
+    for (let [idx, message] of messages.entries()) {
+      if (next.predicate(message)) {
+        messages.splice(0, idx + 1)
+        let actorIdx = actors.indexOf(next)
+        actors.splice(actorIdx, 1)
+        next.resolve()
+        break
+      }
+    }
+  }
+
+  function notifyNextStdoutActor() {
+    return notifyNext(stdoutActors, stdoutMessages)
+  }
+
+  function notifyNextStderrActor() {
+    return notifyNext(stderrActors, stderrMessages)
+  }
 
   let runningProcess = new Promise((resolve, reject) => {
     let child = spawn(command, args, {
@@ -31,10 +60,14 @@ module.exports = function $(command, options = {}) {
     let stderr = ''
 
     child.stdout.on('data', (data) => {
+      stdoutMessages.push(data.toString())
+      notifyNextStdoutActor()
       stdout += data
     })
 
     child.stderr.on('data', (data) => {
+      stderrMessages.push(data.toString())
+      notifyNextStderrActor()
       stderr += data
     })
 
@@ -43,14 +76,24 @@ module.exports = function $(command, options = {}) {
     })
   })
 
-  runningProcessess.push(() => {
-    abortController.abort()
-  })
+  runningProcessess.push(runningProcess)
 
   return Object.assign(runningProcess, {
     stop() {
       abortController.abort()
       return runningProcess
+    },
+    onStdout(predicate) {
+      return new Promise((resolve) => {
+        stdoutActors.push({ predicate, resolve })
+        notifyNextStdoutActor()
+      })
+    },
+    onStderr(predicate) {
+      return new Promise((resolve) => {
+        stderrActors.push({ predicate, resolve })
+        notifyNextStderrActor()
+      })
     },
   })
 }
