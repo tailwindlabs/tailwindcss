021ff8b3db84609e69bc57e83a650a8df6fe1b5b
Adam Wathan <adam.wathan@gmail.com>
2021-05-28 13:46:45 -0400

    JIT: Add caret-color utilities
    Co-Authored-By: Jesse Katsumata <niconico.clarinet@gmail.com>

--- stubs/defaultConfig.stub.js
+++ stubs/defaultConfig.stub.js
@@ -165,6 +165,7 @@ module.exports = {
       inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
       none: 'none',
     },
+    caretColor: (theme) => theme('colors'),
     contrast: {
       0: '0',
       50: '.5',
