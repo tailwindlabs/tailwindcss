81816df3a17dfdc5bc8d162c084d8985effa4946
Adam Wathan <adam.wathan@gmail.com>
2021-06-14 16:03:08 -0400

    Support arbitrary values for object-position

--- src/plugins/objectPosition.js
+++ src/plugins/objectPosition.js
@@ -2,7 +2,5 @@ import createUtilityPlugin from '../util/createUtilityPlugin'
 import { asLookupValue } from '../util/pluginUtils'
 
 export default function () {
-  return createUtilityPlugin('objectPosition', [['object', ['object-position']]], {
-    resolveArbitraryValue: asLookupValue,
-  })
+  return createUtilityPlugin('objectPosition', [['object', ['object-position']]])
 }
