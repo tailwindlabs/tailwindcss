af4a77ae8fea0915ee5bd6c708e82d6a946ac5c3
Adam Wathan <adam.wathan@gmail.com>
2021-06-14 16:14:07 -0400

    Remove unused import

--- src/plugins/objectPosition.js
+++ src/plugins/objectPosition.js
@@ -1,5 +1,4 @@
 import createUtilityPlugin from '../util/createUtilityPlugin'
-import { asLookupValue } from '../util/pluginUtils'
 
 export default function () {
   return createUtilityPlugin('objectPosition', [['object', ['object-position']]])
