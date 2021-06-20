31ca2f4f97489990fb681482d4c609e95eeadcf7
Martijn Cuppens <martijn.cuppens@gmail.com>
2021-06-20 12:36:48 +0200

    Change text-lg line-height to 1.625rem

--- stubs/defaultConfig.stub.js
+++ stubs/defaultConfig.stub.js
@@ -271,7 +271,7 @@ module.exports = {
       xs: ['0.75rem', { lineHeight: '1rem' }],
       sm: ['0.875rem', { lineHeight: '1.25rem' }],
       base: ['1rem', { lineHeight: '1.5rem' }],
-      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      lg: ['1.125rem', { lineHeight: '1.625rem' }],
       xl: ['1.25rem', { lineHeight: '1.75rem' }],
       '2xl': ['1.5rem', { lineHeight: '2rem' }],
       '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
