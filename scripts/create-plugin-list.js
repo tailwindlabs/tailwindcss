b8fca6bb03a773aaf548b0551bf27f2da43af5d6
Sachin Raja <58836760+sachinraja@users.noreply.github.com>
2021-06-20 23:11:30 -0700

    Use `process.cwd()` instead of `__dirname` to resolve path
    Co-authored-by: Federico Ciardi <fed.ciardi@gmail.com>

--- scripts/create-plugin-list.js
+++ scripts/create-plugin-list.js
@@ -5,6 +5,6 @@ import path from 'path'
 const corePluginList = Object.keys(corePlugins)
 
 fs.writeFileSync(
-  path.join(__dirname, '..', 'src', 'corePluginList.js'),
+  path.join(process.cwd(), 'src', 'corePluginList.js'),
   `export default ${JSON.stringify(corePluginList)}`
 )
