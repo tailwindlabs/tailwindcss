cc4aaaaa49c7abb1cac4f9ef7ef31da5a3017cd5
Nestor Vera <nestorvera@me.com>
2021-07-17 21:19:14 +0200

    Add new utility for `align-self: baseline` (#5000)

--- src/plugins/alignSelf.js
+++ src/plugins/alignSelf.js
@@ -17,6 +17,9 @@ export default function () {
         '.self-stretch': {
           'align-self': 'stretch',
         },
+        '.self-baseline': {
+          'align-self': 'baseline',
+        },
       },
       variants('alignSelf')
     )
