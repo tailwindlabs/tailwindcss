fa3d454d067b90774c0154de3c4d90d0b85d305d
Robin Malfait <malfait.robin@gmail.com>
2021-08-26 11:52:47 +0200

    Ensure arbitrary values only support valid values (#5293)
    * ensure arbitrary values only support valid values
    
    * ensure we validate balancing [], () and {}

--- src/jit/lib/setupContextUtils.js
+++ src/jit/lib/setupContextUtils.js
@@ -125,6 +125,64 @@ function withIdentifiers(styles) {
   })
 }
 
+let matchingBrackets = new Map([
+  ['{', '}'],
+  ['[', ']'],
+  ['(', ')'],
+])
+let inverseMatchingBrackets = new Map(
+  Array.from(matchingBrackets.entries()).map(([k, v]) => [v, k])
+)
+
+let quotes = new Set(['"', "'", '`'])
+
+// Arbitrary values must contain balanced brackets (), [] and {}. Escaped
+// values don't count, and brackets inside quotes also don't count.
+//
+// E.g.: w-[this-is]w-[weird-and-invalid]
+// E.g.: w-[this-is\\]w-\\[weird-but-valid]
+// E.g.: content-['this-is-also-valid]-weirdly-enough']
+function isValidArbitraryValue(value) {
+  let stack = []
+  let inQuotes = false
+
+  for (let i = 0; i < value.length; i++) {
+    let char = value[i]
+
+    // Non-escaped quotes allow us to "allow" anything in between
+    if (quotes.has(char) && value[i - 1] !== '\\') {
+      inQuotes = !inQuotes
+    }
+
+    if (inQuotes) continue
+    if (value[i - 1] === '\\') continue // Escaped
+
+    if (matchingBrackets.has(char)) {
+      stack.push(char)
+    } else if (inverseMatchingBrackets.has(char)) {
+      let inverse = inverseMatchingBrackets.get(char)
+
+      // Nothing to pop from, therefore it is unbalanced
+      if (stack.length <= 0) {
+        return false
+      }
+
+      // Popped value must match the inverse value, otherwise it is unbalanced
+      if (stack.pop() !== inverse) {
+        return false
+      }
+    }
+  }
+
+  // If there is still something on the stack, it is also unbalanced
+  if (stack.length > 0) {
+    return false
+  }
+
+  // All good, totally balanced!
+  return true
+}
+
 function buildPluginApi(tailwindConfig, context, { variantList, variantMap, offsets }) {
   function getConfigValue(path, defaultValue) {
     return path ? dlv(tailwindConfig, path, defaultValue) : tailwindConfig
@@ -270,6 +328,10 @@ function buildPluginApi(tailwindConfig, context, { variantList, variantMap, offs
           let [value, coercedType] = coerceValue(type, modifier, options.values, tailwindConfig)
 
           if (!type.includes(coercedType) || value === undefined) {
+            return []
+          }
+
+          if (!isValidArbitraryValue(value)) {
             return []
           }
 
