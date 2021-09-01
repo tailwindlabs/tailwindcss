3088d596424e874858a9e3d44500d626c83bb0ad
Jonathan Reinink <jonathan@reinink.ca>
2021-09-01 14:48:48 -0400

    Inline modern-normalize, consolidate with preflight

--- src/plugins/preflight.js
+++ src/plugins/preflight.js
@@ -4,16 +4,12 @@ import packageJson from '../../package.json'
 
 export default function () {
   return function ({ addBase }) {
-    const normalizeStyles = postcss.parse(
-      fs.readFileSync(require.resolve('modern-normalize'), 'utf8')
-    )
     const preflightStyles = postcss.parse(fs.readFileSync(`${__dirname}/css/preflight.css`, 'utf8'))
 
     addBase([
       postcss.comment({
         text: `! tailwindcss v${packageJson.version} | MIT License | https://tailwindcss.com`,
       }),
-      ...normalizeStyles.nodes,
       ...preflightStyles.nodes,
     ])
   }
