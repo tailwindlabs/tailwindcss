8e5de0acefb36a3f3c160a22ad371813e78da099
Robin Malfait <malfait.robin@gmail.com>
2021-09-04 17:27:13 +0200

    implement custom cloneDeep to replace lodash's

--- defaultTheme.js
+++ defaultTheme.js
@@ -1,4 +1,15 @@
-const cloneDeep = require('lodash/cloneDeep')
-const defaultConfig = require('./stubs/defaultConfig.stub.js')
+let defaultConfig = require('./stubs/defaultConfig.stub.js')
 
 module.exports = cloneDeep(defaultConfig.theme)
+
+function cloneDeep(value) {
+  if (Array.isArray(value)) {
+    return value.map((child) => cloneDeep(child))
+  }
+
+  if (typeof value === 'object' && value !== null) {
+    return Object.fromEntries(Object.entries(value).map(([k, v]) => [k, cloneDeep(v)]))
+  }
+
+  return value
+}
