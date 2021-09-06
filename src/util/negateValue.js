8c73a762f3ed4538d61f1329e38bf52fde21073d
Robin Malfait <malfait.robin@gmail.com>
2021-09-06 14:28:00 +0200

    ensure we have the exact same behaviour

--- src/util/negateValue.js
+++ src/util/negateValue.js
@@ -1,4 +1,6 @@
 export default function (value) {
+  value = `${value}`
+
   // Flip sign of numbers
   if (
     /^[+-]?(\d+|\d*\.\d+)(e[+-]?\d+)?(%|cm|mm|Q|in|pc|pt|px|em|ex|ch|rem|lh|vw|vh|vmin|vmax)?$/.test(
