249f6ff5964fce2febd962fd369e1f4ff7121a1b
Robin Malfait <malfait.robin@gmail.com>
2021-09-09 08:39:14 +0200

    drop unused files (#5453)

--- src/util/prefixNegativeModifiers.js
+++
@@ -1,9 +0,0 @@
-export default function prefixNegativeModifiers(base, modifier) {
-  if (modifier === '-') {
-    return `-${base}`
-  } else if (modifier.startsWith('-')) {
-    return `-${base}-${modifier.slice(1)}`
-  } else {
-    return `${base}-${modifier}`
-  }
-}
