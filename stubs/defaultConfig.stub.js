b9964165f5e2aed582692da2795980068225680e
Mark Beech <mbeech@mark-beech.co.uk>
2021-09-23 11:35:40 +0100

    Add `listStylePosition` to `defaultConfig.stub.js`

--- stubs/defaultConfig.stub.js
+++ stubs/defaultConfig.stub.js
@@ -549,6 +549,10 @@ module.exports = {
       9: '2.25rem',
       10: '2.5rem',
     },
+    listStylePosition: {
+      inside: 'inside',
+      outside: 'outside',
+    },
     listStyleType: {
       none: 'none',
       disc: 'disc',
