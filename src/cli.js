4a69480fd8e0903d6e8738b604301fd3f44637fe
Robin Malfait <malfait.robin@gmail.com>
2021-09-30 13:54:30 +0200

    drop the dot from the extension
    Our transformers and extractors are implemented for `html` for example.
    However the `path.extname()` returns `.html`.
    
    This isn't an issue by default, but it could be for with custom
    extractors / transformers.

--- src/cli.js
+++ src/cli.js
@@ -467,7 +467,7 @@ async function build() {
     for (let file of files) {
       changedContent.push({
         content: fs.readFileSync(path.resolve(file), 'utf8'),
-        extension: path.extname(file),
+        extension: path.extname(file).slice(1),
       })
     }
 
@@ -729,7 +729,7 @@ async function build() {
         chain = chain.then(async () => {
           changedContent.push({
             content: fs.readFileSync(path.resolve(file), 'utf8'),
-            extension: path.extname(file),
+            extension: path.extname(file).slice(1),
           })
 
           await rebuild(config)
@@ -741,7 +741,7 @@ async function build() {
       chain = chain.then(async () => {
         changedContent.push({
           content: fs.readFileSync(path.resolve(file), 'utf8'),
-          extension: path.extname(file),
+          extension: path.extname(file).slice(1),
         })
 
         await rebuild(config)
