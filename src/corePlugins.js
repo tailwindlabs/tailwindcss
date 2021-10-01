c1713d7f8948cf6522dc060f2d0e68931508c0d1
Robin Malfait <malfait.robin@gmail.com>
2021-10-01 16:08:51 +0200

    generate keyframes as part of the animate plugin

--- src/corePlugins.js
+++ src/corePlugins.js
@@ -753,31 +753,28 @@ export default {
     let prefixName = (name) => prefix(`.${name}`).slice(1)
     let keyframes = Object.fromEntries(
       Object.entries(theme('keyframes') ?? {}).map(([key, value]) => {
-        return [key, [{ [`@keyframes ${prefixName(key)}`]: value }]]
+        return [key, { [`@keyframes ${prefixName(key)}`]: value }]
       })
     )
 
     matchUtilities(
       {
-        animate: (value, { includeRules }) => {
+        animate: (value) => {
           let animations = parseAnimationValue(value)
 
-          for (let { name } of animations) {
-            if (keyframes[name] !== undefined) {
-              includeRules(keyframes[name], { respectImportant: false })
-            }
-          }
-
-          return {
-            animation: animations
-              .map(({ name, value }) => {
-                if (name === undefined || keyframes[name] === undefined) {
-                  return value
-                }
-                return value.replace(name, prefixName(name))
-              })
-              .join(', '),
-          }
+          return [
+            ...animations.flatMap((animation) => keyframes[animation.name]),
+            {
+              animation: animations
+                .map(({ name, value }) => {
+                  if (name === undefined || keyframes[name] === undefined) {
+                    return value
+                  }
+                  return value.replace(name, prefixName(name))
+                })
+                .join(', '),
+            },
+          ]
         },
       },
       { values: theme('animation') }
