17211b73dbc40385ff38726710ff1faa5adf983b
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2021-10-20 20:31:19 +0200

    also set debug mode correctly in sharedState

--- src/lib/sharedState.js
+++ src/lib/sharedState.js
@@ -1,7 +1,7 @@
 export const env = {
   TAILWIND_MODE: process.env.TAILWIND_MODE,
   NODE_ENV: process.env.NODE_ENV,
-  DEBUG: process.env.DEBUG !== undefined,
+  DEBUG: process.env.DEBUG !== undefined && process.env.DEBUG !== '0',
   TAILWIND_DISABLE_TOUCH: process.env.TAILWIND_DISABLE_TOUCH !== undefined,
   TAILWIND_TOUCH_DIR: process.env.TAILWIND_TOUCH_DIR,
 }
