1bb2217e56f89c5fe7424ad5b592e4d45b4e0ada
Brad Cornes <bradlc41@gmail.com>
2021-12-15 13:57:24 +0000

    Fix dark mode border color

--- src/utils/mdxComponents.js
+++ src/utils/mdxComponents.js
@@ -5,5 +5,7 @@ export const mdxComponents = {
       <div className="absolute inset-0 rounded-lg ring-1 ring-inset ring-gray-900/10" />
     </div>
   ),
-  Footer: ({ children }) => <p className="border-t border-gray-200 mt-8 pt-6">{children}</p>,
+  Footer: ({ children }) => (
+    <p className="border-t border-gray-200 mt-8 pt-6 dark:border-gray-200/5">{children}</p>
+  ),
 }
