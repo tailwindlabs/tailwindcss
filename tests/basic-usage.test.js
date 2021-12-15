deb68d58166b48e107e998f47a49aa3ee206932a
Robin Malfait <malfait.robin@gmail.com>
2021-12-15 18:07:26 +0100

    Ensure all plugins are executed for a given candidate (#6540)
    * remove early return so that all plugins are handled
    
    We had an early return so that once a plugin was matched, that we could
    stop running the code through the other plugins. However, in this case
    we have an issue that user defined css is technically also a plugin.
    
    This means that:
      - `bg-green-light`
    Would check for:
      - `bg-green-light` (no hit, continue)
      - `bg-green` (Hit! Don't execute next plugins)
      - `bg` (This is the one that would have generated `bg-green-light`)
    
    We tested this change and it doesn't seem to have an impact functionally
    and also not really performance wise.
    
    * update changelog

--- tests/basic-usage.test.js
+++ tests/basic-usage.test.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-import { run, css } from './util/run'
+import { html, run, css } from './util/run'
 
 test('basic usage', () => {
   let config = {
@@ -20,5 +20,40 @@ test('basic usage', () => {
     let expected = fs.readFileSync(expectedPath, 'utf8')
 
     expect(result.css).toMatchFormattedCss(expected)
+  })
+})
+
+test('all plugins are executed that match a candidate', () => {
+  let config = {
+    content: [{ raw: html`<div class="bg-green-light bg-green"></div>` }],
+    theme: {
+      colors: {
+        green: {
+          light: 'green',
+        },
+      },
+    },
+    corePlugins: { preflight: false },
+  }
+
+  let input = css`
+    @tailwind utilities;
+
+    .bg-green {
+      /* Empty on purpose */
+    }
+  `
+
+  return run(input, config).then((result) => {
+    expect(result.css).toMatchFormattedCss(css`
+      .bg-green-light {
+        --tw-bg-opacity: 1;
+        background-color: rgb(0 128 0 / var(--tw-bg-opacity));
+      }
+
+      .bg-green {
+        /* Empty on purpose */
+      }
+    `)
   })
 })
