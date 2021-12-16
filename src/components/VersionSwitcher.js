ea81744ffe441d74c2e5152d43bc23c52abb75fd
Adam Wathan <adam.wathan@gmail.com>
2021-12-16 11:05:47 -0500

    Make version in header dynamic

--- src/components/VersionSwitcher.js
+++ src/components/VersionSwitcher.js
@@ -6,7 +6,7 @@ export function VersionSwitcher({ className }) {
   return (
     <Menu as="div" className="relative">
       <Menu.Button className="text-xs leading-5 font-semibold bg-gray-400/10 rounded-full py-1 px-3 flex items-center space-x-2 hover:bg-gray-400/20">
-        v3.0.0
+        v{pkg.version}
         <svg width="6" height="3" className="ml-2 overflow-visible" aria-hidden="true">
           <path
             d="M0 0L3 3L6 0"
@@ -20,7 +20,7 @@ export function VersionSwitcher({ className }) {
       <Menu.Items className="absolute top-full mt-1 py-2 w-40 rounded-lg bg-white shadow ring-1 ring-gray-900/5 text-sm leading-6 font-semibold text-gray-700">
         <Menu.Item disabled>
           <span className="flex items-center justify-between px-3 py-1 text-sky-500">
-            v3.0.0
+            v{pkg.version}
             <svg width="24" height="24" fill="none">
               <path
                 d="m7.75 12.75 2.25 2.5 6.25-6.5"
