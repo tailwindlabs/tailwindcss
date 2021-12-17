88b91ae46dfafdca6acd576bbd3cfea0913a759f
Brad Cornes <bradlc41@gmail.com>
2021-12-17 13:20:47 +0000

    Update dark mode examples

--- remark/withSyntaxHighlighting.js
+++ remark/withSyntaxHighlighting.js
@@ -8,16 +8,42 @@ module.exports.withSyntaxHighlighting = () => {
       if (node.type !== 'code') return node
       if (node.lang === null) return node
 
+      let re = /(<[^>]+)\s+dark-([a-z-]+)="([^"]+)"([^>]*>)/gi
+
+      let lightCode = node.value.replace(
+        re,
+        (_match, before, _key, _value, after) => `${before}${after}`
+      )
+      let darkCode = node.value.replace(re, (_match, before, key, value, after) =>
+        `${before}${after}`.replace(new RegExp(`(\\s${key})="[^"]+"`), `$1="${value}"`)
+      )
+
       node.type = 'html'
-      node.value = [
-        `<pre class="language-${node.lang}">`,
-        `<code class="language-${node.lang}">`,
-        highlightCode(node.value, node.lang),
-        '</code>',
-        '</pre>',
-      ]
-        .filter(Boolean)
-        .join('')
+
+      if (lightCode === darkCode) {
+        node.value = [
+          `<pre class="language-${node.lang}">`,
+          `<code class="language-${node.lang}">`,
+          highlightCode(lightCode, node.lang),
+          '</code>',
+          '</pre>',
+        ]
+          .filter(Boolean)
+          .join('')
+      } else {
+        node.value = [
+          `<pre class="language-${node.lang}">`,
+          `<code class="dark:hidden language-${node.lang}">`,
+          highlightCode(lightCode, node.lang),
+          '</code>',
+          `<code class="hidden dark:block language-${node.lang}">`,
+          highlightCode(darkCode, node.lang),
+          '</code>',
+          '</pre>',
+        ]
+          .filter(Boolean)
+          .join('')
+      }
 
       if (node.meta) {
         if (!componentName) {
