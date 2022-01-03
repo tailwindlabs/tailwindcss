e0357a8c0e6afef50ceec4146815287fcfe31666
Robin Malfait <malfait.robin@gmail.com>
2022-01-03 17:14:49 +0100

    remove watching context

--- src/lib/sharedState.js
+++ src/lib/sharedState.js
@@ -1,9 +1,6 @@
 export const env = {
-  TAILWIND_MODE: process.env.TAILWIND_MODE,
   NODE_ENV: process.env.NODE_ENV,
   DEBUG: resolveDebug(process.env.DEBUG),
-  TAILWIND_DISABLE_TOUCH: process.env.TAILWIND_DISABLE_TOUCH !== undefined,
-  TAILWIND_TOUCH_DIR: process.env.TAILWIND_TOUCH_DIR,
 }
 export const contextMap = new Map()
 export const configContextMap = new Map()
