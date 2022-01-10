6c877fcd8e7097fa97daa1f6a4987658a42e1dd8
Tiago Souza <33164463+etc-tiago@users.noreply.github.com>
2022-01-10 10:43:18 -0300

    refactor: change the year in the footer (#1120)
    * refactor: change year to new Date().getFullYear()
    
    * Tweak formatting
    
    Co-authored-by: Jonathan Reinink <jonathan@reinink.ca>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -50,7 +50,7 @@ export function Footer({ children, previous, next }) {
       )}
       <div className="pt-10 pb-28 border-t border-gray-200 sm:flex justify-between text-gray-500 dark:border-gray-200/5">
         <div className="mb-6 sm:mb-0 sm:flex">
-          <p>Copyright &copy; 2021 Tailwind Labs Inc.</p>
+          <p>Copyright &copy; {new Date().getFullYear()} Tailwind Labs Inc.</p>
           <p className="sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 dark:sm:border-gray-200/5">
             <Link href="/brand">
               <a className="hover:text-gray-900 dark:hover:text-gray-400">Trademark Policy</a>
