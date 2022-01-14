646088218af320414d038432ee96394335544f19
Jonathan Reinink <jonathan@reinink.ca>
2022-01-14 12:03:25 -0500

    Fix input hover style in variant section on homepage on iOS

--- src/components/home/StateVariants.js
+++ src/components/home/StateVariants.js
@@ -156,7 +156,7 @@ export function StateVariants() {
                     type="text"
                     aria-label="Filter projects"
                     placeholder="Filter projects..."
-                    className="w-full text-sm leading-6 bg-transparent text-slate-900 placeholder:text-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 dark:text-slate-100 dark:placeholder:text-slate-500 dark:ring-0 dark:focus:ring-2"
+                    className="appearance-none w-full text-sm leading-6 bg-transparent text-slate-900 placeholder:text-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 dark:text-slate-100 dark:placeholder:text-slate-500 dark:ring-0 dark:focus:ring-2"
                   />
                 </div>
               </header>
