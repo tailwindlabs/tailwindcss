dfa1bde136d8feb51ca67b17914ae3228eaa9192
Robin Malfait <malfait.robin@gmail.com>
2022-01-15 15:53:44 +0100

    re-add optional chaining
    This will allow us to be 100% sure that we can safely call
    hasOwnProperty in case we get some very strange objects.
    
    This will now also be compiled/transpiled by esbuild.

--- src/postcss-plugins/nesting/plugin.js
+++ src/postcss-plugins/nesting/plugin.js
@@ -16,7 +16,7 @@ export function nesting(opts = postcssNested) {
     let plugin = (() => {
       if (
         typeof opts === 'function' ||
-        (typeof opts === 'object' && opts.hasOwnProperty('postcssPlugin'))
+        (typeof opts === 'object' && opts?.hasOwnProperty?.('postcssPlugin'))
       ) {
         return opts
       }
