3e03d2198200c953ab3e3bc914308fb2b0d8b639
Jordan Pittman <jordan@cryptica.me>
2022-02-01 16:56:03 -0500

    Add failing tests for negative utility detection
    We're not generating them properly in all cases, when using at-apply we sometimes crash, and safelisting doesn't currently work as expected.

--- tests/prefix.test.js
+++ tests/prefix.test.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-import { run, css } from './util/run'
+import { run, html, css } from './util/run'
 
 test('prefix', () => {
   let config = {
@@ -72,4 +72,289 @@ test('prefix', () => {
 
     expect(result.css).toMatchFormattedCss(expected)
   })
+})
+
+it('negative values: marker before prefix', async () => {
+  let config = {
+    prefix: 'tw-',
+    content: [{ raw: html`<div class="-tw-top-1"></div>` }],
+    corePlugins: { preflight: false },
+  }
+
+  let input = css`
+    @tailwind utilities;
+  `
+
+  await run(input, config)
+
+  const result = await run(input, config)
+
+  expect(result.css).toMatchFormattedCss(css`
+    .-tw-top-1 {
+      top: -0.25rem;
+    }
+  `)
+})
+
+it('negative values: marker after prefix', async () => {
+  let config = {
+    prefix: 'tw-',
+    content: [{ raw: html`<div class="tw--top-1"></div>` }],
+    corePlugins: { preflight: false },
+  }
+
+  let input = css`
+    @tailwind utilities;
+  `
+
+  await run(input, config)
+
+  const result = await run(input, config)
+
+  expect(result.css).toMatchFormattedCss(css`
+    .tw--top-1 {
+      top: -0.25rem;
+    }
+  `)
+})
+
+it('negative values: marker before prefix and arbitrary value', async () => {
+  let config = {
+    prefix: 'tw-',
+    content: [{ raw: html`<div class="-tw-top-[1px]"></div>` }],
+    corePlugins: { preflight: false },
+  }
+
+  let input = css`
+    @tailwind utilities;
+  `
+
+  await run(input, config)
+
+  const result = await run(input, config)
+
+  expect(result.css).toMatchFormattedCss(css`
+    .-tw-top-\[1px\] {
+      top: -1px;
+    }
+  `)
+})
+
+it('negative values: marker after prefix and arbitrary value', async () => {
+  let config = {
+    prefix: 'tw-',
+    content: [{ raw: html`<div class="tw--top-[1px]"></div>` }],
+    corePlugins: { preflight: false },
+  }
+
+  let input = css`
+    @tailwind utilities;
+  `
+
+  await run(input, config)
+
+  const result = await run(input, config)
+
+  expect(result.css).toMatchFormattedCss(css`
+    .tw--top-\[1px\] {
+      top: -1px;
+    }
+  `)
+})
+
+it('negative values: no marker and arbitrary value', async () => {
+  let config = {
+    prefix: 'tw-',
+    content: [{ raw: html`<div class="tw-top-[-1px]"></div>` }],
+    corePlugins: { preflight: false },
+  }
+
+  let input = css`
+    @tailwind utilities;
+  `
+
+  await run(input, config)
+
+  const result = await run(input, config)
+
+  expect(result.css).toMatchFormattedCss(css`
+    .tw-top-\[-1px\] {
+      top: -1px;
+    }
+  `)
+})
+
+it('negative values: variant versions', async () => {
+  let config = {
+    prefix: 'tw-',
+    content: [
+      {
+        raw: html`
+          <div class="hover:-tw-top-1 hover:tw--top-1"></div>
+          <div class="hover:-tw-top-[1px] hover:tw--top-[1px]"></div>
+          <div class="hover:tw-top-[-1px]"></div>
+
+          <!-- this one should not generate anything -->
+          <div class="-hover:tw-top-1"></div>
+        `,
+      },
+    ],
+    corePlugins: { preflight: false },
+  }
+
+  let input = css`
+    @tailwind utilities;
+  `
+
+  await run(input, config)
+
+  const result = await run(input, config)
+
+  expect(result.css).toMatchFormattedCss(css`
+    .hover\:-tw-top-1:hover {
+      top: -0.25rem;
+    }
+    .hover\:tw--top-1:hover {
+      top: -0.25rem;
+    }
+    .hover\:-tw-top-\[1px\]:hover {
+      top: -1px;
+    }
+    .hover\:tw--top-\[1px\]:hover {
+      top: -1px;
+    }
+    .hover\:tw-top-\[-1px\]:hover {
+      top: -1px;
+    }
+  `)
+})
+
+it('negative values: prefix and apply', async () => {
+  let config = {
+    prefix: 'tw-',
+    content: [{ raw: html`` }],
+    corePlugins: { preflight: false },
+  }
+
+  let input = css`
+    @tailwind utilities;
+
+    .a {
+      @apply hover:tw--top-1;
+    }
+    .b {
+      @apply hover:-tw-top-1;
+    }
+    .c {
+      @apply hover:-tw-top-[1px];
+    }
+    .d {
+      @apply hover:tw--top-[1px];
+    }
+    .e {
+      @apply hover:tw-top-[-1px];
+    }
+  `
+
+  await run(input, config)
+
+  const result = await run(input, config)
+
+  expect(result.css).toMatchFormattedCss(css`
+    .a:hover {
+      top: -0.25rem;
+    }
+    .b:hover {
+      top: -0.25rem;
+    }
+    .c:hover {
+      top: -1px;
+    }
+    .d:hover {
+      top: -1px;
+    }
+    .e:hover {
+      top: -1px;
+    }
+  `)
+})
+
+it('negative values: prefix in the safelist', async () => {
+  let config = {
+    prefix: 'tw-',
+    safelist: [{ pattern: /-tw-top-1/g }, { pattern: /tw--top-1/g }],
+    theme: {
+      inset: {
+        1: '0.25rem',
+      },
+    },
+    content: [{ raw: html`` }],
+    corePlugins: { preflight: false },
+  }
+
+  let input = css`
+    @tailwind utilities;
+  `
+
+  await run(input, config)
+
+  const result = await run(input, config)
+
+  expect(result.css).toMatchFormattedCss(css`
+    .-tw-top-1 {
+      top: -0.25rem;
+    }
+    .tw--top-1 {
+      top: -0.25rem;
+    }
+  `)
+})
+
+it('prefix with negative values and variants in the safelist', async () => {
+  let config = {
+    prefix: 'tw-',
+    safelist: [
+      { pattern: /-tw-top-1/, variants: ['hover', 'sm:hover'] },
+      { pattern: /tw--top-1/, variants: ['hover', 'sm:hover'] },
+    ],
+    theme: {
+      inset: {
+        1: '0.25rem',
+      },
+    },
+    content: [{ raw: html`` }],
+    corePlugins: { preflight: false },
+  }
+
+  let input = css`
+    @tailwind utilities;
+  `
+
+  await run(input, config)
+
+  const result = await run(input, config)
+
+  expect(result.css).toMatchFormattedCss(css`
+    .-tw-top-1 {
+      top: -0.25rem;
+    }
+    .tw--top-1 {
+      top: -0.25rem;
+    }
+    .hover\:-tw-top-1:hover {
+      top: -0.25rem;
+    }
+
+    .hover\:tw--top-1:hover {
+      top: -0.25rem;
+    }
+    @media (min-width: 640px) {
+      .sm\:hover\:-tw-top-1:hover {
+        top: -0.25rem;
+      }
+      .sm\:hover\:tw--top-1:hover {
+        top: -0.25rem;
+      }
+    }
+  `)
 })
