4138aafef136d74143d82ab40ebc86eeb73c8962
Jordan Pittman <jordan@cryptica.me>
2022-02-03 13:41:55 -0500

    Nuxt - Add directives to CSS

--- examples/nuxt/nuxt.config.js
+++ examples/nuxt/nuxt.config.js
@@ -18,6 +18,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/main.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
