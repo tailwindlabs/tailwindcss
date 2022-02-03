1388f705d53560344798181d9fae5e56b7828d6f
Jordan Pittman <jordan@cryptica.me>
2022-02-03 14:14:57 -0500

    CRA - Start using Tailwind

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,24 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
