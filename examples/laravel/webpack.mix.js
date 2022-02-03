a204fd674e0bf0690aceaba9ec63c3338a013ab3
Jordan Pittman <jordan@cryptica.me>
2022-02-03 11:17:43 -0500

    Laravel - Install Tailwind CSS

--- examples/laravel/webpack.mix.js
+++ examples/laravel/webpack.mix.js
@@ -13,5 +13,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require("tailwindcss"),
     ]);
