edcbe8c0ec97a344f1be14d34123a12d67a3fef9
Jordan Pittman <jordan@cryptica.me>
2022-02-03 11:40:51 -0500

    Nuxt - Configure template paths

--- examples/nuxt/tailwind.config.js
+++ examples/nuxt/tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  content: [],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}"
+  ],
   theme: {
     extend: {},
   },
