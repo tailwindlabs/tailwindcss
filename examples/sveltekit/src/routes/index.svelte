13b4a1c4ec897c27bf87fdd77843cec1dcf667dc
Jordan Pittman <jordan@cryptica.me>
2022-02-03 14:19:34 -0500

    Sveltekit - Start using Tailwind

--- examples/sveltekit/src/routes/index.svelte
+++ examples/sveltekit/src/routes/index.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
