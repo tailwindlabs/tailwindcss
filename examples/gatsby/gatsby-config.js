bc3e0dc8627018ce0a00d9262e161dcd310ae54a
Jordan Pittman <jordan@cryptica.me>
2022-02-03 13:48:56 -0500

    Gatsby - Install Tailwind CSS

--- examples/gatsby/gatsby-config.js
+++ examples/gatsby/gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    'gatsby-plugin-postcss',
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
