f762ed67e63cdbc12707aee913b472e103ccfe0f
Jordan Pittman <jordan@cryptica.me>
2022-02-07 10:10:35 -0500

    Refactor test a bit

--- tests/getClassList.test.js
+++ tests/getClassList.test.js
@@ -5,22 +5,24 @@ it('should generate every possible class, without variants', () => {
   let config = {}
 
   let context = createContext(resolveConfig(config))
-  expect(context.getClassList()).toBeInstanceOf(Array)
+  let classes = context.getClassList()
+  expect(classes).toBeInstanceOf(Array)
 
   // Verify we have a `container` for the 'components' section.
-  expect(context.getClassList()).toContain('container')
+  expect(classes).toContain('container')
 
   // Verify we handle the DEFAULT case correctly
-  expect(context.getClassList()).toContain('border')
+  expect(classes).toContain('border')
 
   // Verify we handle negative values correctly
-  expect(context.getClassList()).toContain('-inset-1/4')
-  expect(context.getClassList()).toContain('-m-0')
-  expect(context.getClassList()).not.toContain('-uppercase')
-  expect(context.getClassList()).not.toContain('-opacity-50')
-  expect(
-    createContext(
-      resolveConfig({ theme: { extend: { margin: { DEFAULT: '5px' } } } })
-    ).getClassList()
-  ).not.toContain('-m-DEFAULT')
+  expect(classes).toContain('-inset-1/4')
+  expect(classes).toContain('-m-0')
+  expect(classes).not.toContain('-uppercase')
+  expect(classes).not.toContain('-opacity-50')
+
+  config = { theme: { extend: { margin: { DEFAULT: '5px' } } } }
+  context = createContext(resolveConfig(config))
+  classes = context.getClassList()
+
+  expect(classes).not.toContain('-m-DEFAULT')
 })
