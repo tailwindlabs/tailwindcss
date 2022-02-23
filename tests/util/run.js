c00028c34531e7f32f6872e886fe455240b09ab1
Jordan Pittman <jordan@cryptica.me>
2022-02-23 11:20:51 -0500

    Verify that source maps are correctly generated

--- tests/util/run.js
+++ tests/util/run.js
@@ -5,10 +5,29 @@ import tailwind from '../../src'
 export * from './strings'
 export * from './defaults'
 
+let map = JSON.stringify({
+  version: 3,
+  file: null,
+  sources: [],
+  names: [],
+  mappings: '',
+})
+
 export function run(input, config, plugin = tailwind) {
   let { currentTestName } = expect.getState()
 
   return postcss(plugin(config)).process(input, {
     from: `${path.resolve(__filename)}?test=${currentTestName}`,
+  })
+}
+
+export function runWithSourceMaps(input, config, plugin = tailwind) {
+  let { currentTestName } = expect.getState()
+
+  return postcss(plugin(config)).process(input, {
+    from: `${path.resolve(__filename)}?test=${currentTestName}`,
+    map: {
+      prev: map,
+    },
   })
 }
