fa8463eea23b63bef67e1c09f37abefbd7140f4a
Jonathan Reinink <jonathan@reinink.ca>
2022-02-24 14:37:31 -0500

    Improve combobox demo

--- src/pages/blog/headless-ui-v1-5/Demo.js
+++ src/pages/blog/headless-ui-v1-5/Demo.js
@@ -26,31 +26,28 @@ export default function Demo() {
         )
 
   return (
-    <Example className="h-96 flex justify-center pt-12">
+    <Example className="h-96 flex items-start justify-center pt-12">
       <Combobox value={selected} onChange={setSelected} __demoMode>
         <div className="relative w-full max-w-xs">
-          <div className="relative w-full text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-within:ring-2 focus-within:ring-indigo-500 sm:text-sm overflow-hidden">
-            <Combobox.Input
-              className="w-full bg-transparent border-none focus:ring-0 py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:outline-none"
-              displayValue={(person) => person.name}
-              onChange={(event) => setQuery(event.target.value)}
-            />
-            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5 text-gray-400"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </Combobox.Button>
-          </div>
+          <Combobox.Input
+            className="w-full rounded-lg bg-white py-2 pl-3 pr-10 shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm"
+            displayValue={(person) => person.name}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+          <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+            <svg
+              className="w-5 h-5 text-gray-400"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Combobox.Button>
           <Transition
             as={Fragment}
             leave="transition ease-in duration-100"
@@ -58,9 +55,9 @@ export default function Demo() {
             leaveTo="opacity-0"
             afterLeave={() => setQuery('')}
           >
-            <Combobox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
               {filteredPeople.length === 0 && query !== '' ? (
-                <div className="cursor-default select-none relative py-2 px-4 text-gray-700">
+                <div className="cursor-default select-none relative py-2 px-3 text-gray-500">
                   Nothing found.
                 </div>
               ) : (
@@ -68,27 +65,24 @@ export default function Demo() {
                   <Combobox.Option
                     key={person.id}
                     className={({ active }) =>
-                      `cursor-default select-none relative py-2 pl-10 pr-4 ${
-                        active ? 'text-white bg-indigo-600' : 'text-gray-900'
+                      `relative cursor-default select-none py-2 pl-3 pr-9 ${
+                        active ? 'bg-indigo-600 text-white' : 'text-gray-900'
                       }`
                     }
                     value={person}
                   >
                     {({ selected, active }) => (
                       <>
-                        <span
-                          className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}
-                        >
+                        <span className={`block truncate ${selected ? 'font-semibold' : ''}`}>
                           {person.name}
                         </span>
                         {selected ? (
                           <span
-                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            className={`absolute inset-y-0 right-0 flex items-center pr-4 ${
                               active ? 'text-white' : 'text-indigo-600'
                             }`}
                           >
                             <svg
-                              xmlns="http://www.w3.org/2000/svg"
                               className="h-5 w-5"
                               viewBox="0 0 20 20"
                               fill="currentColor"
