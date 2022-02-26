26bd819949b25e16a3ddee4fe45df9af306c07de
Alaa Badran <me@alaabadran.com>
2022-02-26 03:43:58 +0300

    Add text-start and text-end (#6656)
    These are useful for RTL languages

--- src/corePlugins.js
+++ src/corePlugins.js
@@ -1522,6 +1522,8 @@ export let corePlugins = {
       '.text-center': { 'text-align': 'center' },
       '.text-right': { 'text-align': 'right' },
       '.text-justify': { 'text-align': 'justify' },
+      '.text-start': { 'text-align': 'start' },
+      '.text-end': { 'text-align': 'end' },
     })
   },
 
