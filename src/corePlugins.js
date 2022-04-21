48d1a870afc95f31d70e04e2ebcc52cc9cddf231
Nestor Vera <nestorvera@me.com>
2022-04-21 14:35:02 +0200

    Add missing `.grid-flow-dense` utility

--- src/corePlugins.js
+++ src/corePlugins.js
@@ -884,6 +884,7 @@ export let corePlugins = {
     addUtilities({
       '.grid-flow-row': { gridAutoFlow: 'row' },
       '.grid-flow-col': { gridAutoFlow: 'column' },
+      '.grid-flow-dense': { gridAutoFlow: 'dense' },
       '.grid-flow-row-dense': { gridAutoFlow: 'row dense' },
       '.grid-flow-col-dense': { gridAutoFlow: 'column dense' },
     })
