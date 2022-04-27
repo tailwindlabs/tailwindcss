fec65329b7e78d1513b9e645698d3af4058514f7
Brad Cornes <hello@bradley.dev>
2022-04-27 14:45:02 +0100

    Update blog index desktop layout

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,9 +1,13 @@
+import { Button } from '@/components/Button'
 import { NewsletterForm } from '@/components/NewsletterForm'
-import PostItem from '@/components/PostItem'
 import { Widont } from '@/components/Widont'
 import { getAllPostPreviews } from '@/utils/getAllPosts'
+import Link from 'next/link'
+import clsx from 'clsx'
+import tinytime from 'tinytime'
 
 let posts = getAllPostPreviews()
+let postDateTemplate = tinytime('{MMMM} {DD}, {YYYY}')
 
 export default function Blog() {
   return (
@@ -20,19 +24,87 @@ export default function Blog() {
           <NewsletterForm action="https://app.convertkit.com/forms/3181837/subscriptions" />
         </section>
       </header>
-      <div className="space-y-16">
-        {posts.map(({ slug, module: { default: Component, meta } }, index) => (
-          <PostItem
-            key={index}
-            title={meta.title}
-            category={meta.category}
-            date={meta.date}
-            slug={slug}
-            wide
-          >
-            <Component />
-          </PostItem>
-        ))}
+      <div className="relative max-w-3xl ml-auto pb-12">
+        <div className="absolute top-3 bottom-0 right-full mr-[3.25rem] w-px bg-slate-200 dark:bg-slate-800" />
+        <div className="space-y-16">
+          {posts.map(({ slug, module: { default: Component, meta } }) => (
+            <article key={slug} className="relative">
+              <svg
+                viewBox="0 0 9 9"
+                className={clsx(
+                  'absolute right-full mr-12 w-[calc(0.5rem+1px)] h-[calc(0.5rem+1px)] overflow-visible',
+                  meta.isUpdate
+                    ? 'top-7 text-slate-300 dark:text-slate-600'
+                    : 'top-2.5 text-sky-400'
+                )}
+              >
+                {!meta.isUpdate && (
+                  <circle
+                    cx="4.5"
+                    cy="4.5"
+                    r="9.5"
+                    className="fill-white dark:fill-slate-900"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    strokeOpacity={0.5}
+                  />
+                )}
+                <circle
+                  cx="4.5"
+                  cy="4.5"
+                  r="4.5"
+                  stroke="currentColor"
+                  className={meta.isUpdate ? 'fill-white dark:fill-slate-900' : 'fill-current'}
+                  strokeWidth={2}
+                />
+              </svg>
+              {meta.isUpdate ? (
+                <Link href={`/blog/${slug}`}>
+                  <a className="relative block leading-8 -mx-6 -my-4 px-6 py-4 rounded-2xl hover:bg-slate-50/70 dark:hover:bg-slate-800/50">
+                    <div className="line-clamp-2">
+                      <h3 className="inline font-semibold text-slate-900 tracking-tight dark:text-slate-200">
+                        {meta.title}
+                      </h3>
+                      <p className="inline text-slate-600 dark:text-slate-400">
+                        &nbsp;&nbsp;—&nbsp;&nbsp;{meta.description}
+                      </p>
+                    </div>
+                  </a>
+                </Link>
+              ) : (
+                <div>
+                  <h3 className="text-xl font-bold tracking-tight text-slate-900 dark:text-slate-200">
+                    <Link href={`/blog/${slug}`}>
+                      <a>{meta.title}</a>
+                    </Link>
+                  </h3>
+                  <div className="mt-4 mb-6 prose prose-slate dark:prose-dark">
+                    <Component />
+                  </div>
+                  <Button href={`/blog/${slug}`}>
+                    Read more<span className="sr-only">, {meta.title}</span>
+                  </Button>
+                </div>
+              )}
+              <dl
+                className={clsx(
+                  'absolute right-full mr-[calc(6.5rem+1px)]',
+                  meta.isUpdate ? 'top-4' : 'top-0'
+                )}
+              >
+                <dt className="sr-only">Date</dt>
+                <dd
+                  className={clsx(
+                    'whitespace-nowrap text-sm dark:text-slate-400',
+                    meta.isUpdate ? 'leading-8' : 'leading-7'
+                  )}
+                >
+                  <time dateTime={meta.date}>{postDateTemplate.render(new Date(meta.date))}</time>
+                </dd>
+              </dl>
+            </article>
+          ))}
+        </div>
       </div>
     </main>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -252,6 +252,7 @@ module.exports = {
     },
   },
   plugins: [
+    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
     function ({ addVariant }) {
