34d362ec33f8055fa19330c953d90774f8ecadbb
Brad Cornes <hello@bradley.dev>
2022-04-27 16:09:04 +0100

    Add blog index responsive styles

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -11,7 +11,7 @@ let postDateTemplate = tinytime('{MMMM} {DD}, {YYYY}')
 
 export default function Blog() {
   return (
-    <main className="max-w-5xl mx-auto px-4 pb-28 sm:px-6 md:px-8 xl:px-12 xl:max-w-6xl">
+    <main className="max-w-[52rem] mx-auto px-4 pb-28 sm:px-6 md:px-8 xl:px-12 lg:max-w-6xl">
       <header className="py-16 sm:text-center">
         <h1 className="mb-4 text-3xl sm:text-4xl tracking-tight text-slate-900 font-extrabold dark:text-slate-200">
           Blog
@@ -24,17 +24,17 @@ export default function Blog() {
           <NewsletterForm action="https://app.convertkit.com/forms/3181837/subscriptions" />
         </section>
       </header>
-      <div className="relative max-w-3xl ml-auto pb-12">
-        <div className="absolute top-3 bottom-0 right-full mr-[3.25rem] w-px bg-slate-200 dark:bg-slate-800" />
+      <div className="relative sm:pb-12 sm:ml-[calc(2rem+1px)] md:ml-[calc(3.5rem+1px)] lg:ml-[max(calc(14.5rem+1px),calc(100%-48rem))]">
+        <div className="hidden absolute top-3 bottom-0 right-full mr-7 md:mr-[3.25rem] w-px bg-slate-200 dark:bg-slate-800 sm:block" />
         <div className="space-y-16">
           {posts.map(({ slug, module: { default: Component, meta } }) => (
             <article key={slug} className="relative">
               <svg
                 viewBox="0 0 9 9"
                 className={clsx(
-                  'absolute right-full mr-12 w-[calc(0.5rem+1px)] h-[calc(0.5rem+1px)] overflow-visible',
+                  'hidden absolute right-full mr-6 md:mr-12 w-[calc(0.5rem+1px)] h-[calc(0.5rem+1px)] overflow-visible sm:block',
                   meta.isUpdate
-                    ? 'top-7 text-slate-300 dark:text-slate-600'
+                    ? 'top-5 md:top-7 text-slate-300 dark:text-slate-600'
                     : 'top-2.5 text-sky-400'
                 )}
               >
@@ -60,7 +60,7 @@ export default function Blog() {
               </svg>
               {meta.isUpdate ? (
                 <Link href={`/blog/${slug}`}>
-                  <a className="relative block leading-8 -mx-6 -my-4 px-6 py-4 rounded-2xl hover:bg-slate-50/70 dark:hover:bg-slate-800/50">
+                  <a className="relative block leading-8 -mx-4 md:-mx-6 -my-2 md:-my-4 px-4 md:px-6 py-2 md:py-4 sm:rounded-2xl hover:bg-slate-50/70 dark:hover:bg-slate-800/50">
                     <div className="line-clamp-2">
                       <h3 className="inline font-semibold text-slate-900 tracking-tight dark:text-slate-200">
                         {meta.title}
@@ -72,7 +72,7 @@ export default function Blog() {
                   </a>
                 </Link>
               ) : (
-                <div>
+                <>
                   <h3 className="text-xl font-bold tracking-tight text-slate-900 dark:text-slate-200">
                     <Link href={`/blog/${slug}`}>
                       <a>{meta.title}</a>
@@ -81,27 +81,39 @@ export default function Blog() {
                   <div className="mt-4 mb-6 prose prose-slate dark:prose-dark">
                     <Component />
                   </div>
+                </>
+              )}
+              <div className="flex flex-row-reverse justify-end items-center">
+                <dl
+                  className={clsx(
+                    'lg:absolute lg:right-full lg:mr-[calc(6.5rem+1px)]',
+                    meta.isUpdate ? 'top-4' : 'top-0'
+                  )}
+                >
+                  <dt className="sr-only">Date</dt>
+                  <dd
+                    className={clsx(
+                      'whitespace-nowrap text-sm dark:text-slate-400',
+                      meta.isUpdate ? 'leading-8' : 'leading-7'
+                    )}
+                  >
+                    <time dateTime={meta.date}>{postDateTemplate.render(new Date(meta.date))}</time>
+                  </dd>
+                </dl>
+                <svg
+                  width="2"
+                  height="2"
+                  fill="currentColor"
+                  className="mx-4 text-slate-700 lg:hidden"
+                >
+                  <circle cx="1" cy="1" r="1" />
+                </svg>
+                {!meta.isUpdate && (
                   <Button href={`/blog/${slug}`}>
                     Read more<span className="sr-only">, {meta.title}</span>
                   </Button>
-                </div>
-              )}
-              <dl
-                className={clsx(
-                  'absolute right-full mr-[calc(6.5rem+1px)]',
-                  meta.isUpdate ? 'top-4' : 'top-0'
                 )}
-              >
-                <dt className="sr-only">Date</dt>
-                <dd
-                  className={clsx(
-                    'whitespace-nowrap text-sm dark:text-slate-400',
-                    meta.isUpdate ? 'leading-8' : 'leading-7'
-                  )}
-                >
-                  <time dateTime={meta.date}>{postDateTemplate.render(new Date(meta.date))}</time>
-                </dd>
-              </dl>
+              </div>
             </article>
           ))}
         </div>
