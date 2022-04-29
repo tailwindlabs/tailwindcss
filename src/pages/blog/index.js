dd679b4550fe712bbb75e47f63776f537a3caefd
Brad Cornes <hello@bradley.dev>
2022-04-29 11:04:07 +0100

    Tweak blog index styles

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,4 +1,3 @@
-import { Button } from '@/components/Button'
 import { NewsletterForm } from '@/components/NewsletterForm'
 import { Widont } from '@/components/Widont'
 import { getAllPostPreviews } from '@/utils/getAllPosts'
@@ -25,137 +24,62 @@ export default function Blog() {
         </section>
       </header>
       <div className="relative sm:pb-12 sm:ml-[calc(2rem+1px)] md:ml-[calc(3.5rem+1px)] lg:ml-[max(calc(14.5rem+1px),calc(100%-48rem))]">
-        <div
-          className={clsx(
-            'hidden absolute bottom-0 right-full mr-7 md:mr-[3.25rem] w-px bg-slate-200 dark:bg-slate-800 sm:block',
-            posts[0].module.meta.type === 'update' ? 'top-3' : 'top-3'
-          )}
-        />
-        {posts.map(({ slug, module: { default: Component, meta } }, index) => {
-          // let isUpdate = meta.type === 'update'
-          let isUpdate = false
-          let prev = posts[index - 1]?.module.meta
-          // let prevIsUpdate = prev?.type === 'update'
-          let prevIsUpdate = false
-
-          return (
-            <article
-              key={slug}
-              className={clsx(
-                'relative',
-                prev && isUpdate && prevIsUpdate && 'mt-10 md:mt-8',
-                prev && isUpdate && !prevIsUpdate && 'mt-[3.25rem] md:mt-12',
-                prev && !isUpdate && prevIsUpdate && 'mt-[3.25rem] md:mt-12',
-                prev && !isUpdate && !prevIsUpdate && 'mt-16'
-              )}
-            >
+        <div className="hidden absolute top-3 bottom-0 right-full mr-7 md:mr-[3.25rem] w-px bg-slate-200 dark:bg-slate-800 sm:block" />
+        <div className="space-y-16">
+          {posts.map(({ slug, module: { default: Component, meta } }) => (
+            <article key={slug} className="relative group">
+              <div className="absolute -inset-y-2.5 -inset-x-4 md:-inset-y-4 md:-inset-x-6 sm:rounded-2xl group-hover:bg-slate-50/70 dark:group-hover:bg-slate-800/50" />
               <svg
                 viewBox="0 0 9 9"
-                className={clsx(
-                  'hidden absolute right-full mr-6 md:mr-12 w-[calc(0.5rem+1px)] h-[calc(0.5rem+1px)] overflow-visible sm:block',
-                  true
-                    ? 'top-5 lg:top-2 text-slate-200 dark:text-slate-600'
-                    : 'top-5 lg:top-2.5 text-sky-400'
-                )}
+                className="hidden absolute right-full mr-6 top-2 text-slate-200 dark:text-slate-600 md:mr-12 w-[calc(0.5rem+1px)] h-[calc(0.5rem+1px)] overflow-visible sm:block"
               >
-                {!true && (
-                  <circle
-                    cx="4.5"
-                    cy="4.5"
-                    r="9.5"
-                    className="fill-white dark:fill-slate-900"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    strokeOpacity={0.5}
-                  />
-                )}
                 <circle
                   cx="4.5"
                   cy="4.5"
                   r="4.5"
                   stroke="currentColor"
-                  className={true ? 'fill-white dark:fill-slate-900' : 'fill-current'}
+                  className="fill-white dark:fill-slate-900"
                   strokeWidth={2}
                 />
               </svg>
-              {isUpdate ? (
-                <Link href={`/blog/${slug}`}>
-                  <a className="relative block leading-8 -mx-4 md:-mx-6 px-4 md:px-6 pt-10 pb-3 md:pb-4 md:pt-12 lg:pt-4 sm:rounded-2xl hover:bg-slate-50/70 dark:hover:bg-slate-800/50">
-                    <div className="line-clamp-3">
-                      <h3 className="inline font-semibold text-slate-900 tracking-tight dark:text-slate-200">
-                        {meta.title}
-                      </h3>
-                      <p className="inline text-slate-600 dark:text-slate-400">
-                        &nbsp;&nbsp;—&nbsp;&nbsp;{meta.description}
-                      </p>
-                    </div>
-                    <div className="mt-3">
-                      <Button href={`/blog/${slug}`}>
-                        Read more<span className="sr-only">, {meta.title}</span>
-                      </Button>
-                    </div>
-                  </a>
-                </Link>
-              ) : (
-                <>
-                  <h3
-                    className={clsx(
-                      'text-base font-bold tracking-tight text-slate-900 dark:text-slate-200',
-                      isUpdate ? '' : 'pt-8 lg:pt-0'
-                    )}
+              <div className="relative">
+                <h3 className="text-base font-bold tracking-tight text-slate-900 dark:text-slate-200 pt-8 lg:pt-0">
+                  {meta.title}
+                </h3>
+                <div className="mt-2 mb-4 prose prose-slate prose-a:relative prose-a:z-10 dark:prose-dark line-clamp-2">
+                  <Component />
+                </div>
+                <dl className="absolute left-0 top-0 lg:left-auto lg:right-full lg:mr-[calc(6.5rem+1px)]">
+                  <dt className="sr-only">Date</dt>
+                  <dd className={clsx('whitespace-nowrap text-sm leading-6 dark:text-slate-400')}>
+                    <time dateTime={meta.date}>{postDateTemplate.render(new Date(meta.date))}</time>
+                  </dd>
+                </dl>
+              </div>
+              <Link href={`/blog/${slug}`}>
+                <a className="flex items-center text-sm text-sky-500 font-medium">
+                  <span className="absolute -inset-y-2.5 -inset-x-4 md:-inset-y-4 md:-inset-x-6 sm:rounded-2xl" />
+                  <span className="relative">
+                    Read more<span className="sr-only">, {meta.title}</span>
+                  </span>
+                  <svg
+                    className="relative mt-px overflow-visible ml-2.5 text-sky-300 dark:text-sky-700"
+                    width="3"
+                    height="6"
+                    viewBox="0 0 3 6"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   >
-                    <Link href={`/blog/${slug}`}>
-                      <a>{meta.title}</a>
-                    </Link>
-                  </h3>
-                  <div className="mt-2 mb-4 prose prose-slate dark:prose-dark line-clamp-2">
-                    <Component />
-                  </div>
-                </>
-              )}
-              <dl
-                className={clsx(
-                  'absolute left-0 lg:left-auto lg:right-full lg:mr-[calc(6.5rem+1px)]',
-                  isUpdate ? 'top-3 pointer-events-none md:top-4' : 'top-0'
-                )}
-              >
-                <dt className="sr-only">Date</dt>
-                <dd className={clsx('whitespace-nowrap text-sm leading-6 dark:text-slate-400')}>
-                  <time dateTime={meta.date}>{postDateTemplate.render(new Date(meta.date))}</time>
-                </dd>
-              </dl>
-              <svg
-                width="2"
-                height="2"
-                fill="currentColor"
-                className="mx-4 text-slate-700 lg:hidden"
-              >
-                <circle cx="1" cy="1" r="1" />
-              </svg>
-              <span className="flex items-center text-sm text-sky-500 font-medium">
-                Read more
-                <svg
-                  class="mt-px overflow-visible ml-2.5 text-sky-300 group-hover:text-slate-400 dark:text-slate-500 dark:group-hover:text-slate-400"
-                  width="3"
-                  height="6"
-                  viewBox="0 0 3 6"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="M0 0L3 3L0 6"></path>
-                </svg>
-              </span>
-              {!true && (
-                <Button href={`/blog/${slug}`}>
-                  Read more<span className="sr-only">, {meta.title}</span>
-                </Button>
-              )}
+                    <path d="M0 0L3 3L0 6"></path>
+                  </svg>
+                </a>
+              </Link>
             </article>
-          )
-        })}
+          ))}
+        </div>
       </div>
     </main>
   )
