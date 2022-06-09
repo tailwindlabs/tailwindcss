169b510804599fbfefa6e0f9ae3bb98acc7e2f72
Jordan Pittman <jordan@cryptica.me>
2022-06-09 14:15:45 -0400

    Fix postcss plugin type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,7 @@
-export type { Config } from './config.d'
+import { PluginCreator } from 'postcss'
+import type { Config } from './config.d'
+
+declare const plugin: PluginCreator<string | Config | { config: string | Config }>
+
+export { Config }
+export default plugin
