6e6a4e515902fc54a3c2037d3f3086eb82d9edb0
Jonathan Reinink <jonathan@reinink.ca>
2022-06-30 20:15:57 -0400

    Add Tailwind UI templates link

--- src/layouts/SidebarLayout.js
+++ src/layouts/SidebarLayout.js
@@ -319,6 +319,36 @@ function TopLevelNav({ mobile }) {
       </TopLevelLink>
       <TopLevelLink
         mobile={mobile}
+        href="https://tailwindui.com/templates"
+        className="mb-4"
+        shadow="group-hover:shadow-fuchsia-200 dark:group-hover:bg-fuchsia-600"
+        icon={
+          <>
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M8 6C6.89543 6 6 6.89543 6 8V16C6 17.1046 6.89543 18 8 18H10.5C11.0523 18 11.5 17.5523 11.5 17V12C11.5 10.6193 12.6193 9.5 14 9.5H18V8C18 6.89543 17.1046 6 16 6H8ZM7.25 8C7.25 7.58579 7.58579 7.25 8 7.25H8.01C8.42421 7.25 8.76 7.58579 8.76 8C8.76 8.41421 8.42421 8.75 8.01 8.75H8C7.58579 8.75 7.25 8.41421 7.25 8ZM10 7.25C9.58579 7.25 9.25 7.58579 9.25 8C9.25 8.41421 9.58579 8.75 10 8.75H10.01C10.4242 8.75 10.76 8.41421 10.76 8C10.76 7.58579 10.4242 7.25 10.01 7.25H10Z"
+              fill="#E879F9"
+              className={clsx(
+                'fill-fuchsia-400 group-hover:fill-fuchsia-500 dark:group-hover:fill-fuchsia-300',
+                mobile ? 'dark:fill-slate-300' : 'dark:fill-slate-400'
+              )}
+            />
+            <path
+              d="M13 12C13 11.4477 13.4477 11 14 11H17C17.5523 11 18 11.4477 18 12V17C18 17.5523 17.5523 18 17 18H14C13.4477 18 13 17.5523 13 17V12Z"
+              fill="#F0ABFC"
+              className={clsx(
+                'fill-fuchsia-300 group-hover:fill-fuchsia-400',
+                mobile ? 'dark:fill-slate-400' : 'dark:fill-slate-500'
+              )}
+            />
+          </>
+        }
+      >
+        Templates
+      </TopLevelLink>
+      <TopLevelLink
+        mobile={mobile}
         href="https://www.youtube.com/tailwindlabs"
         className="mb-4"
         shadow="group-hover:shadow-pink-200 dark:group-hover:bg-pink-500"
