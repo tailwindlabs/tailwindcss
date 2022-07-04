5c84482401f099cbab62c23ecf29bf04cf9df328
Jordan Pittman <jordan@cryptica.me>
2022-07-04 10:17:01 -0400

    Fix usage of postcss config file in standalone CLI
    The config file created with `--postcss` would fail because we didn’t stub require to load `tailwindcss` or `autoprefixer` when we should.

--- standalone-cli/tests/test.js
+++ standalone-cli/tests/test.js
@@ -1,4 +1,6 @@
 const { execSync } = require('child_process')
+const os = require('os')
+const fs = require('fs-extra')
 
 const platformMap = {
   darwin: 'macos',
@@ -23,3 +25,54 @@ it('supports first-party plugins', () => {
   expect(result).toContain('.line-clamp-2')
   expect(result).toContain('.prose')
 })
+
+it('supports postcss config files', async () => {
+  // We have to run this test outside of any place with node_modules for it to properly test this situation
+  let result = await inIsolatedContext(() => {
+    return exec(
+      '--content tests/fixtures/plugins.html --config tests/fixtures/test.config.js --postcss tests/fixtures/postcss.config.js'
+    )
+  })
+
+  expect(result).toContain('.aspect-w-1')
+  expect(result).toContain('.form-input')
+  expect(result).toContain('.line-clamp-2')
+  expect(result).toContain('.prose')
+})
+
+/**
+ * @template T
+ * @param {() => T} fn
+ * @returns {T}
+ */
+async function inIsolatedContext(fn) {
+  // Create a new directory entirely outside of the package for the test
+  let dest = `${os.tmpdir()}/tailwindcss-cli`
+
+  // Recursively copy the dist and tests folders
+  let dirs = ['dist', 'tests']
+
+  await Promise.all(
+    dirs.map((dir) =>
+      fs.copy(`${__dirname}/../${dir}`, `${dest}/${dir}`, {
+        overwrite: true,
+        recursive: true,
+      })
+    )
+  )
+
+  // Change the working directory to the new directory
+  process.chdir(dest)
+
+  console.log(dest)
+
+  try {
+    return await fn()
+  } finally {
+    // Change back to the original working directory
+    process.chdir(__dirname)
+
+    // Delete the new directory
+    await fs.rmdir(dest, { recursive: true })
+  }
+}
