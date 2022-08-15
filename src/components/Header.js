c50e6372873810ccd5f654cf15754da09bf92b7b
Adam Wathan <4323180+adamwathan@users.noreply.github.com>
2022-08-15 10:02:51 -0400

    Add "Jobs" link to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -137,6 +137,16 @@ export function NavItems() {
           <a className="hover:text-sky-500 dark:hover:text-sky-400">Blog</a>
         </Link>
       </li>
+      <li>
+        <Link href="https://jobs.tailwindcss.com/?ref=top">
+          <a className="hover:text-sky-500 dark:hover:text-sky-400">
+            Jobs
+            <span className="ml-2 font-medium text-xs leading-5 rounded-full text-sky-600 bg-sky-400/10 px-2 py-0.5  dark:text-sky-400">
+              New
+            </span>
+          </a>
+        </Link>
+      </li>
     </>
   )
 }
