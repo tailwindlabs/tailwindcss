2ede8da86d37a7c02623347d8358826af77d5fe3
Brandon McConnell <brandon@dreamthinkbuild.com>
2022-08-25 11:05:32 -0400

    Add support for `visibility: collapse` with new `collapsed` utility

--- src/corePlugins.js
+++ src/corePlugins.js
@@ -397,6 +397,7 @@ export let corePlugins = {
     addUtilities({
       '.visible': { visibility: 'visible' },
       '.invisible': { visibility: 'hidden' },
+      '.collapsed': { visibility: 'collapse' },
     })
   },
 
