36524eb40bddb732d8d7416cff14180d9a3ab853
Remco Haszing <remcohaszing@gmail.com>
2022-09-09 12:40:49 +0200

    Fix issue with Tailwind modifying global state
    When running Tailwind, it modifies the plugin defaults parameters. As a
    result Tailwind using a Tailwind plugin in the same process twice yields
    different results.

--- src/util/defaults.js
+++ src/util/defaults.js
@@ -1,17 +1,18 @@
 export function defaults(target, ...sources) {
+  let copy = { ...target }
   for (let source of sources) {
     for (let k in source) {
-      if (!target?.hasOwnProperty?.(k)) {
-        target[k] = source[k]
+      if (!copy?.hasOwnProperty?.(k)) {
+        copy[k] = source[k]
       }
     }
 
     for (let k of Object.getOwnPropertySymbols(source)) {
-      if (!target?.hasOwnProperty?.(k)) {
-        target[k] = source[k]
+      if (!copy?.hasOwnProperty?.(k)) {
+        copy[k] = source[k]
       }
     }
   }
 
-  return target
+  return copy
 }
