2033c7211da87d95080f3a5faae2710a2f872585
Robin Malfait <malfait.robin@gmail.com>
2022-09-12 15:12:58 +0200

    convert the `matchVariant` to look more like `addVariant`
    With the biggest difference that the `matchVariant` will have a callback
    function that receives the current value of the variant.

--- src/lib/setupContextUtils.js
+++ src/lib/setupContextUtils.js
@@ -500,18 +500,16 @@ function buildPluginApi(tailwindConfig, context, { variantList, variantMap, offs
   }
 
   if (flagEnabled(tailwindConfig, 'matchVariant')) {
-    api.matchVariant = function (variants, options) {
-      for (let variant in variants) {
-        for (let [k, v] of Object.entries(options?.values ?? {})) {
-          api.addVariant(`${variant}-${k}`, variants[variant](v))
-        }
-
-        api.addVariant(
-          variant,
-          Object.assign(({ args }) => variants[variant](args), { [MATCH_VARIANT]: true }),
-          options
-        )
+    api.matchVariant = function (variant, variantFn, options) {
+      for (let [k, v] of Object.entries(options?.values ?? {})) {
+        api.addVariant(`${variant}-${k}`, variantFn(v))
       }
+
+      api.addVariant(
+        variant,
+        Object.assign(({ args }) => variantFn(args), { [MATCH_VARIANT]: true }),
+        options
+      )
     }
   }
 
