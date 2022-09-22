2574a881e59fec1d2e096e6fe6ec509af40c47d9
k-utsumi <k-utsumi@users.noreply.github.com>
2022-09-23 04:17:54 +0900

    Add `break-keep` utility (#9393)
    * ✨ Add `word-break: keep-all` with `keep-all`
    
    ref. https://developer.mozilla.org/en-US/docs/Web/CSS/word-break
    
    `whitespace-nowrap` and `word-break: keep-all` behave differently in different browsers.
    
    Demo: https://jsfiddle.net/h1aj6nvy/
    
    There is a difference between Firefox and Google chrome.
    
    * Rename `keep-all` to `break-keep`
    
    Co-authored-by: Jonathan Reinink <jonathan@reinink.ca>

--- src/corePlugins.js
+++ src/corePlugins.js
@@ -1222,6 +1222,7 @@ export let corePlugins = {
       '.break-normal': { 'overflow-wrap': 'normal', 'word-break': 'normal' },
       '.break-words': { 'overflow-wrap': 'break-word' },
       '.break-all': { 'word-break': 'break-all' },
+      '.break-keep': { 'word-break': 'keep-all' },
     })
   },
 
