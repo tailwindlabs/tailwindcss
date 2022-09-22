d76e5db551a902883ea21ac8e0a666831fa7d8f8
Jordan Pittman <jordan@cryptica.me>
2022-09-22 11:31:27 -0400

    cleanup
    wip
    
    cleanup

--- src/lib/findAtConfigPath.js
+++ src/lib/findAtConfigPath.js
@@ -3,13 +3,12 @@ import path from 'path'
 /**
  * Find the @config at-rule in the given CSS AST and return the relative path to the config file
  *
- *
  * @param {import('postcss').Root} root
  * @param {import('postcss').Result} result
  */
 export function findAtConfigPath(root, result) {
   let configPath = null
-  let relativeTo = root.source.input.file ?? result.opts.from ?? null
+  let relativeTo = root.source?.input.file ?? result.opts.from ?? null
 
   root.walkAtRules('config', (rule) => {
     if (relativeTo === undefined) {
@@ -22,12 +21,19 @@ export function findAtConfigPath(root, result) {
       throw rule.error('Only `@config` at-rule is allowed per file.')
     }
 
-    configPath = rule.params.replace(/^['"]|['"]$/g, '')
-    rule.remove()
+    let matches = rule.params.match(/(['"])(.*?)\1/)
+    if (!matches) {
+      throw rule.error(
+        'The `@config` at-rule must be followed by a string containing the path to the config file.'
+      )
+    }
 
+    configPath = matches[2]
     if (path.isAbsolute(configPath)) {
       throw rule.error('The `@config` at-rule cannot be used with an absolute path.')
     }
+
+    rule.remove()
   })
 
   return configPath ? path.resolve(path.dirname(relativeTo), configPath) : null
