f0b0602c0bd0b22f640e18174e8cc825512b8d35
Jordan Pittman <jordan@cryptica.me>
2022-10-07 13:24:06 -0400

    Move class

--- src/corePlugins.js
+++ src/corePlugins.js
@@ -980,8 +980,8 @@ export let corePlugins = {
       '.place-content-between': { 'place-content': 'space-between' },
       '.place-content-around': { 'place-content': 'space-around' },
       '.place-content-evenly': { 'place-content': 'space-evenly' },
-      '.place-content-stretch': { 'place-content': 'stretch' },
       '.place-content-baseline': { 'place-content': 'baseline' },
+      '.place-content-stretch': { 'place-content': 'stretch' },
     })
   },
 
