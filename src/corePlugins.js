53502c20f93b37d8a4db813da22fc8d1aad634fc
Dawood Khan Masood <91176669+DawoodKMasood@users.noreply.github.com>
2022-10-08 14:49:20 +0500

    Update corePlugins.js

--- src/corePlugins.js
+++ src/corePlugins.js
@@ -990,6 +990,7 @@ export let corePlugins = {
       '.place-items-start': { 'place-items': 'start' },
       '.place-items-end': { 'place-items': 'end' },
       '.place-items-center': { 'place-items': 'center' },
+      '.place-items-baseline': { 'place-items': 'baseline' },
       '.place-items-stretch': { 'place-items': 'stretch' },
     })
   },
