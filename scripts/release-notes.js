cb6e45a3bc960af6d1eb8aae12c0f4b240bb4322
Robin Malfait <malfait.robin@gmail.com>
2022-10-19 15:52:39 +0200

    Prepare for release (#9605)
    * rename `build-cli.yml` to `prepare-release.yml`
    
    In other repo's we will also have a `prepare-release` so this makes it a
    bit more consistent.
    
    * use common CONSTANT_CASE for environment variables
    
    * use `strategy` for defining the node version
    
    * add script to get the release notes
    
    * add release notes to release draft
    
    * use CONSTANT_CASE for environment variables
    
    * improve consistency for relase related scripts

---
+++ scripts/release-notes.js
@@ -0,0 +1,21 @@
+// Given a version, figure out what the release notes are so that we can use this to pre-fill the
+// relase notes on a GitHub release for the current version.
+
+let path = require('path')
+let fs = require('fs')
+
+let version =
+  process.argv[2] || process.env.npm_package_version || require('../package.json').version
+
+let changelog = fs.readFileSync(path.resolve(__dirname, '..', 'CHANGELOG.md'), 'utf8')
+let match = new RegExp(
+  `## \\[${version}\\] - (.*)\\n\\n([\\s\\S]*?)\\n(?:(?:##\\s)|(?:\\[))`,
+  'g'
+).exec(changelog)
+
+if (match) {
+  let [, , notes] = match
+  console.log(notes.trim())
+} else {
+  console.log(`Placeholder release notes for version: v${version}`)
+}
