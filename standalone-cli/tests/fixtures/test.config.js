0789bb39dde2c09d82fd4473027e4dd03a25e3a8
Jonathan Reinink <jonathan@reinink.ca>
2022-11-15 20:52:42 -0500

    Add container queries plugin to standalone CLI

--- standalone-cli/tests/fixtures/test.config.js
+++ standalone-cli/tests/fixtures/test.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
     require('@tailwindcss/forms')({ strategy: 'class' }),
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/typography'),
