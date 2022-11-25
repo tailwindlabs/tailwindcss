b9bf4d740338f5eca3934bb5b37cbf54fa498b99
Robin Malfait <malfait.robin@gmail.com>
2022-11-25 15:26:14 +0100

    improve return value of `resolveConfig`, unwrap `ResolvableTo`

--- resolveConfig.d.ts
+++ resolveConfig.d.ts
@@ -1,3 +1,12 @@
-import type { Config } from './types/config'
-declare function resolveConfig(config: Config): Config
+import type { Config, ResolvableTo } from './types/config'
+
+type UnwrapResolvables<T> = {
+  [K in keyof T]: T[K] extends ResolvableTo<infer R> ? R : T[K]
+}
+
+type ResolvedConfig<T extends Config> = Omit<T, 'theme'> & {
+  theme: UnwrapResolvables<T['theme']>
+}
+
+declare function resolveConfig<T extends Config>(config: T): ResolvedConfig<T>
 export = resolveConfig
