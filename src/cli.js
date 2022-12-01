e3d27e89b94b25c59ec4fa932dbc3e0db201bb13
Adam Wathan <4323180+adamwathan@users.noreply.github.com>
2022-12-01 10:09:14 -0500

    Move TS to ./src/oxide

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 if (process.env.OXIDE) {
-  module.exports = require('../oxide/packages/tailwindcss/dist/cli.js')
+  module.exports = require('./oxide/cli.js')
 } else {
   module.exports = require('./cli/index.js')
 }
