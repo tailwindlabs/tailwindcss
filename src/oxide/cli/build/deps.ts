28e7f92a449a811d05e0be3001a3a82594fb8601
Robin Malfait <malfait.robin@gmail.com>
2022-12-01 17:50:50 +0100

    setup lightningcss

--- src/oxide/cli/build/deps.ts
+++ src/oxide/cli/build/deps.ts
@@ -1,3 +1,7 @@
+import packageJson from '../../../../package.json'
+import browserslist from 'browserslist'
+import { Result } from 'postcss'
+
 import {
   // @ts-ignore
   lazyPostcss,
@@ -11,6 +15,60 @@ import {
   // @ts-ignore
   lazyAutoprefixer,
 } from '../../../../peers/index'
+
+export function lazyLightningCss() {
+  // TODO: Make this lazy/bundled
+  return require('lightningcss')
+}
+
+let lightningCss
+
+function loadLightningCss() {
+  if (lightningCss) {
+    return lightningCss
+  }
+
+  // Try to load a local version first
+  try {
+    return (lightningCss = require('lightningcss'))
+  } catch {}
+
+  return (lightningCss = lazyLightningCss())
+}
+
+export async function lightningcss(shouldMinify: boolean, result: Result) {
+  let css = loadLightningCss()
+
+  try {
+    let transformed = css.transform({
+      filename: result.opts.from || 'input.css',
+      code: Buffer.from(result.css, 'utf-8'),
+      minify: shouldMinify,
+      sourceMap: !!result.map,
+      inputSourceMap: result.map ? result.map.toString() : undefined,
+      targets: css.browserslistToTargets(browserslist(packageJson.browserslist)),
+      drafts: {
+        nesting: true,
+      },
+    })
+
+    return Object.assign(result, {
+      css: transformed.code.toString('utf8'),
+      map: result.map
+        ? Object.assign(result.map, {
+            toString() {
+              return transformed.map.toString()
+            },
+          })
+        : result.map,
+    })
+  } catch (err) {
+    console.error('Unable to use Lightning CSS. Using raw version instead.')
+    console.error(err)
+
+    return result
+  }
+}
 
 /**
  * @returns {import('postcss')}
