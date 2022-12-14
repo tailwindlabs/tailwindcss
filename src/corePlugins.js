60b13f84a2d41121047bc9cb00cc9fc098554b3d
fratzinger <22286818+fratzinger@users.noreply.github.com>
2022-12-14 10:05:30 +0100

    feat: add hyphens

--- src/corePlugins.js
+++ src/corePlugins.js
@@ -1409,6 +1409,14 @@ export let corePlugins = {
     })
   },
 
+  hyphens: ({ addUtilities }) => {
+    addUtilities({
+      '.hyphens-none': { hyphens: 'none' },
+      '.hyphens-manual': { hyphens: 'manual' },
+      '.hyphens-auto': { hyphens: 'auto' },
+    })
+  },
+
   whitespace: ({ addUtilities }) => {
     addUtilities({
       '.whitespace-normal': { 'white-space': 'normal' },
