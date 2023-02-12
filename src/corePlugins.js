db05af90104bee7b551f747440d18b489ab4e7fa
Davy <davy@prepr.io>
2023-02-12 16:56:37 +0100

    Update corePlugins.js

--- src/corePlugins.js
+++ src/corePlugins.js
@@ -1224,7 +1224,7 @@ export let corePlugins = {
       '.justify-between': { 'justify-content': 'space-between' },
       '.justify-around': { 'justify-content': 'space-around' },
       '.justify-evenly': { 'justify-content': 'space-evenly' },
-      '.justify-normal': { 'justify-content': 'normal' },
+      '.justify-stretch': { 'justify-content': 'stretch' },
     })
   },
 
