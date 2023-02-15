c0670ccb354bd20d84a1aab63f438d1233c8da32
Davy <davy@prepr.io>
2023-02-15 17:44:23 +0100

    Add `justify-normal` and `justify-stretch` classes (#10560)
    * Added justify-normal class
    
    * Update corePlugins.js
    
    * Add `justify-normal` utility
    
    ---------
    
    Co-authored-by: Jonathan Reinink <jonathan@reinink.ca>

--- src/corePlugins.js
+++ src/corePlugins.js
@@ -1218,12 +1218,14 @@ export let corePlugins = {
 
   justifyContent: ({ addUtilities }) => {
     addUtilities({
+      '.justify-normal': { 'justify-content': 'normal' },
       '.justify-start': { 'justify-content': 'flex-start' },
       '.justify-end': { 'justify-content': 'flex-end' },
       '.justify-center': { 'justify-content': 'center' },
       '.justify-between': { 'justify-content': 'space-between' },
       '.justify-around': { 'justify-content': 'space-around' },
       '.justify-evenly': { 'justify-content': 'space-evenly' },
+      '.justify-stretch': { 'justify-content': 'stretch' },
     })
   },
 
