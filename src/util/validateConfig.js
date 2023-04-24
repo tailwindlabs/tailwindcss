1e9470a6d968df065f687b43ae8fcd9140ee0365
Robin Malfait <malfait.robin@gmail.com>
2023-04-24 18:28:05 +0200

    resolve all _existing_ content paths

--- src/util/validateConfig.js
+++ src/util/validateConfig.js
@@ -1,7 +1,7 @@
 import log from './log'
 
 export function validateConfig(config) {
-  if (config.content.files.length === 0) {
+  if (config.content !== 'auto' && config.content.files.length === 0) {
     log.warn('content-problems', [
       'The `content` option in your Tailwind CSS configuration is missing or empty.',
       'Configure your content sources or your generated CSS will be missing styles.',
