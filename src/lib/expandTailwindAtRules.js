be491e7bb886b5ddb37e2ddc8a9d6625b255f168
Jonathan Reinink <jonathan@reinink.ca>
2023-04-25 15:47:08 -0400

    Replace `quick-lru` dependency with `@alloc/quick-lru`

--- src/lib/expandTailwindAtRules.js
+++ src/lib/expandTailwindAtRules.js
@@ -1,5 +1,5 @@
 import fs from 'fs'
-import LRU from 'quick-lru'
+import LRU from '@alloc/quick-lru'
 import * as sharedState from './sharedState'
 import { generateRules } from './generateRules'
 import log from '../util/log'
