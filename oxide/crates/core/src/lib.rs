c469b00a9e648fd2b73ba9e1864163582c7560f8
Robin Malfait <malfait.robin@gmail.com>
2023-04-26 19:02:52 +0200

    improve algorithm of detecting content paths
    1. Files in the root should be listed statically instead of using globs.
    2. Files and folders in special known direct child folders should be
       listed statically instead of using globs (e.g.: `public`). This is
       because these special folders are often used to store generated AND
       source files at the same time. Using globs could trigger infinite
       loops because we are watching and acting upon dist files.
    3. All file extensions found in the project, should be used in the globs
       in addition to a known set of extensions.
    4. Direct folders seen from the root, can use the glob syntax
       `<root>/src/**/*.{...known-extensions}`

--- oxide/crates/core/src/lib.rs
+++ oxide/crates/core/src/lib.rs
@@ -1,5 +1,6 @@
 use crate::parser::Extractor;
 use fxhash::FxHashSet;
+use ignore::WalkBuilder;
 use rayon::prelude::*;
 use std::path::Path;
 use std::path::PathBuf;
@@ -29,6 +30,171 @@ pub struct ChangedContent {
     pub file: Option<PathBuf>,
     pub content: Option<String>,
     pub extension: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct ContentPathInfo {
+    pub base: String,
+}
+
+// Goals:
+//
+// 1. Recursively watch all top-level folders but not the root folders.
+// 2. Watch files in root directly.
+// 3. Watch files in the `public` folder directly (and don't watch the `public` folder itself).
+// 4. Always watch a common set of known file extensions
+//    (js,jsx,ts,tsx,html,php,vue,svelte,astro,md,mdx,...).
+// 5. Merge known list of file extensions with discovered file extensions in the project (marko).
+
+pub fn resolve_content_paths(args: ContentPathInfo) -> Vec<String> {
+    let root = Path::new(&args.base);
+
+    // A list of direct folder names where we can't use globs, but we should track each file individually instead.
+    // This is because these folders are often used for "src" and also "dist" like files.
+    let static_direct_folder_indentifiers = vec!["public"];
+
+    // A list of known extensions + a list of extensions we found in the project.
+    let mut found_extensions = FxHashSet::from_iter(
+        include_str!("fixtures/wanted-extensions.txt")
+            .trim()
+            .lines(),
+    );
+
+    // A list of direct folders, from the root, where we can use globs to watch all files.
+    let mut direct_folders = FxHashSet::default();
+
+    // A list of static files that we should track directly.
+    let mut static_files = vec![];
+
+    // Collect all valid paths from the root. This will already filter out ignored files, unknown
+    // extensions and binary files.
+    let paths: Vec<_> = WalkBuilder::new(&root)
+        .hidden(false)
+        .filter_entry(move |entry| {
+            // Skip known ignored folders
+            if entry.file_type().unwrap().is_dir() {
+                return entry
+                    .file_name()
+                    .to_str()
+                    .map(|s| s != ".git")
+                    .unwrap_or(false);
+            }
+
+            is_allowed_content_path(entry.path())
+        })
+        .build()
+        .filter_map(Result::ok)
+        .collect();
+
+    for path in &paths {
+        let path = path.path();
+
+        // If the `path` is a directory, and a direct child of the root and most importantly not a
+        // special cased direct folder (like `public`), then we can track the current folder and
+        // setup globs later.
+        if path.is_dir()
+            && path.parent().map(|parent| parent == root).unwrap_or(false)
+            && !static_direct_folder_indentifiers
+                .iter()
+                .any(|p| root.join(p) == path)
+        {
+            direct_folders.insert(path);
+        }
+
+        if let Some(parent) = path.parent() {
+            // Collect the extension for future use when building globs.
+            if let Some(extension) = path.extension() {
+                found_extensions.insert(extension.to_str().unwrap_or_default());
+            }
+
+            if path.is_file() {
+                // If the parent of the current file is the root folder, then we have to track the
+                // current file directly.
+                if parent == root {
+                    static_files.push(format!("{}", path.display()));
+                    continue;
+                }
+
+                // If the current file is located in one of the direct folders seen from the root,
+                // then we have to track the current file directly.
+                if static_direct_folder_indentifiers
+                    .iter()
+                    .any(|p| parent.starts_with(root.join(p)))
+                {
+                    static_files.push(format!("{}", path.display()));
+                }
+            }
+        }
+    }
+
+    let globs = direct_folders
+        .iter()
+        .flat_map(|path| {
+            match found_extensions.len() {
+                0 => None, // This should never happen
+                1 => Some(format!(
+                    "{}/**/*.{}",
+                    path.display(),
+                    found_extensions.iter().next().unwrap()
+                )),
+                _ => Some(format!(
+                    "{}/**/*.{{{}}}",
+                    path.display(),
+                    found_extensions
+                        .iter()
+                        .map(|x| x.to_string())
+                        .collect::<Vec<_>>()
+                        .join(",")
+                )),
+            }
+        })
+        .collect::<Vec<_>>();
+
+    static_files.extend(globs);
+    static_files
+}
+
+pub fn is_git_ignored_content_path(base: &Path, path: &Path) -> bool {
+    !WalkBuilder::new(base)
+        .hidden(false)
+        .build()
+        .filter_map(Result::ok)
+        .any(|e| e.path() == path)
+}
+
+pub fn is_allowed_content_path(path: &Path) -> bool {
+    let binary_extensions = include_str!("fixtures/binary-extensions.txt")
+        .trim()
+        .lines()
+        .collect::<Vec<_>>();
+    let ignored_extensions = include_str!("fixtures/ignored-extensions.txt")
+        .trim()
+        .lines()
+        .collect::<Vec<_>>();
+    let ignored_files = include_str!("fixtures/ignored-files.txt")
+        .trim()
+        .lines()
+        .collect::<Vec<_>>();
+
+    let path = PathBuf::from(path);
+
+    // Skip known ignored files
+    if path
+        .file_name()
+        .unwrap()
+        .to_str()
+        .map(|s| ignored_files.contains(&s))
+        .unwrap_or(false)
+    {
+        return false;
+    }
+
+    // Skip known ignored extensions
+    return path
+        .extension()
+        .map(|s| s.to_str().unwrap_or_default())
+        .map(|ext| !ignored_extensions.contains(&ext) && !binary_extensions.contains(&ext))
+        .unwrap_or(false);
 }
 
 #[derive(Debug)]
