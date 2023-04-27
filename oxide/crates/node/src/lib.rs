33741838e829e84c398b022681cd40cfadad6c87
Robin Malfait <malfait.robin@gmail.com>
2023-04-27 18:17:20 +0200

    make `sequential` and `parallel` version of a new (tmp) `parse_candidate_strings`

--- oxide/crates/node/src/lib.rs
+++ oxide/crates/node/src/lib.rs
@@ -1,3 +1,4 @@
+use napi::bindgen_prelude::ToNapiValue;
 use std::path::PathBuf;
 
 #[macro_use]
@@ -11,16 +12,88 @@ pub struct ChangedContent {
   pub extension: String,
 }
 
+impl From<ChangedContent> for tailwindcss_core::ChangedContent {
+  fn from(changed_content: ChangedContent) -> Self {
+    tailwindcss_core::ChangedContent {
+      file: changed_content.file.map(PathBuf::from),
+      content: changed_content.content,
+      extension: changed_content.extension,
+    }
+  }
+}
+
 #[napi]
 pub fn parse_candidate_strings_from_files(changed_content: Vec<ChangedContent>) -> Vec<String> {
   tailwindcss_core::parse_candidate_strings_from_files(
-    changed_content
-      .into_iter()
-      .map(|changed_content| tailwindcss_core::ChangedContent {
-        file: changed_content.file.map(PathBuf::from),
-        content: changed_content.content,
-        extension: changed_content.extension,
-      })
-      .collect(),
+    changed_content.into_iter().map(Into::into).collect(),
   )
+}
+
+#[derive(Debug, Clone)]
+#[napi(object)]
+pub struct Options {
+  pub input: Vec<ChangedContent>,
+  pub strategy: Strategy,
+}
+
+impl From<Options> for tailwindcss_core::Options {
+  fn from(options: Options) -> Self {
+    tailwindcss_core::Options {
+      input: options.input.into_iter().map(Into::into).collect(),
+      strategy: options.strategy.into(),
+    }
+  }
+}
+
+#[derive(Debug, Clone)]
+#[napi(object)]
+pub struct Strategy {
+  pub io: IOStrategy,
+  pub parsing: ParsingStrategy,
+}
+
+impl From<Strategy> for tailwindcss_core::Strategy {
+  fn from(strategy: Strategy) -> Self {
+    tailwindcss_core::Strategy {
+      io: strategy.io.into(),
+      parsing: strategy.parsing.into(),
+    }
+  }
+}
+
+#[derive(Debug)]
+#[napi]
+pub enum IOStrategy {
+  Sequential,
+  Parallel,
+}
+
+impl From<IOStrategy> for tailwindcss_core::IOStrategy {
+  fn from(io_strategy: IOStrategy) -> Self {
+    match io_strategy {
+      IOStrategy::Sequential => tailwindcss_core::IOStrategy::Sequential,
+      IOStrategy::Parallel => tailwindcss_core::IOStrategy::Parallel,
+    }
+  }
+}
+
+#[derive(Debug)]
+#[napi]
+pub enum ParsingStrategy {
+  Sequential,
+  Parallel,
+}
+
+impl From<ParsingStrategy> for tailwindcss_core::ParsingStrategy {
+  fn from(parsing_strategy: ParsingStrategy) -> Self {
+    match parsing_strategy {
+      ParsingStrategy::Sequential => tailwindcss_core::ParsingStrategy::Sequential,
+      ParsingStrategy::Parallel => tailwindcss_core::ParsingStrategy::Parallel,
+    }
+  }
+}
+
+#[napi]
+pub fn parse_candidate_strings(options: Options) -> Vec<String> {
+  tailwindcss_core::parse_candidate_strings(options.into())
 }
