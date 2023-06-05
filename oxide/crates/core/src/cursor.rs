aa0e2e91817ae95584ead8698aa5b14aeed1d5e2
Jordan Pittman <jordan@cryptica.me>
2023-06-05 11:52:33 -0400

    wip

--- oxide/crates/core/src/cursor.rs
+++ oxide/crates/core/src/cursor.rs
@@ -1,4 +1,4 @@
-use std::{ops::{Add, Sub}, io::Read};
+use std::ops::{Add, Sub};
 
 #[derive(Debug, Clone)]
 pub struct Cursor<'a> {
@@ -50,15 +50,16 @@ impl<'a> Cursor<'a> {
     }
 
     pub fn move_to(&mut self, pos: usize) {
-        let pos = pos.clamp(0, self.input.len() - 1);
+        let len = self.input.len();
+        let pos = pos.clamp(0, len);
 
         self.pos = pos;
         self.at_start = pos == 0;
-        self.at_end = pos >= self.input.len() - 1;
+        self.at_end = pos >= len;
 
-        self.prev = if !self.at_start { self.input[pos-1] } else { 0x00 };
-        self.curr = self.input[pos];
-        self.next = if !self.at_end { self.input[pos+1] } else { 0x00 };
+        self.prev = if pos > 0 { self.input[pos-1] } else { 0x00 };
+        self.curr = if pos < len { self.input[pos] } else { 0x00 };
+        self.next = if pos+1 < len { self.input[pos+1] } else { 0x00 };
     }
 }
 
@@ -104,29 +105,29 @@ mod test {
 
         // Advancing too far should stop at the end
         cursor.advance_by(10);
-        assert_eq!(cursor.pos, 10);
+        assert_eq!(cursor.pos, 11);
         assert_eq!(cursor.at_start, false);
         assert_eq!(cursor.at_end, true);
-        assert_eq!(cursor.prev, b'l');
-        assert_eq!(cursor.curr, b'd');
+        assert_eq!(cursor.prev, b'd');
+        assert_eq!(cursor.curr, 0x00);
         assert_eq!(cursor.next, 0x00);
 
         // Can't advance past the end
         cursor.advance_by(1);
-        assert_eq!(cursor.pos, 10);
+        assert_eq!(cursor.pos, 11);
         assert_eq!(cursor.at_start, false);
         assert_eq!(cursor.at_end, true);
-        assert_eq!(cursor.prev, b'l');
-        assert_eq!(cursor.curr, b'd');
+        assert_eq!(cursor.prev, b'd');
+        assert_eq!(cursor.curr, 0x00);
         assert_eq!(cursor.next, 0x00);
 
         cursor.rewind_by(1);
-        assert_eq!(cursor.pos, 9);
+        assert_eq!(cursor.pos, 10);
         assert_eq!(cursor.at_start, false);
         assert_eq!(cursor.at_end, false);
-        assert_eq!(cursor.prev, b'r');
-        assert_eq!(cursor.curr, b'l');
-        assert_eq!(cursor.next, b'd');
+        assert_eq!(cursor.prev, b'l');
+        assert_eq!(cursor.curr, b'd');
+        assert_eq!(cursor.next, 0x00);
 
         cursor.rewind_by(10);
         assert_eq!(cursor.pos, 0);
