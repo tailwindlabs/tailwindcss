b86c353a8be097dcd8749430c0995a21cf44cbb9
Jordan Pittman <jordan@cryptica.me>
2023-06-05 11:38:15 -0400

    Add cursor abstraction

---
+++ oxide/crates/core/src/cursor.rs
@@ -0,0 +1,139 @@
+use std::{ops::{Add, Sub}, io::Read};
+
+#[derive(Debug, Clone)]
+pub struct Cursor<'a> {
+    // The input we're scanning
+    pub input: &'a [u8],
+
+    // The location of the cursor in the input
+    pub pos: usize,
+
+    /// Is the cursor at the start of the input
+    pub at_start: bool,
+
+    /// Is the cursor at the end of the input
+    pub at_end: bool,
+
+    /// The previously consumed character
+    /// If `at_start` is true, this will be NUL
+    pub prev: u8,
+
+    /// The current character
+    pub curr: u8,
+
+    /// The upcoming character (if any)
+    /// If `at_end` is true, this will be NUL
+    pub next: u8,
+}
+
+impl<'a> Cursor<'a> {
+    pub fn new(input: &'a [u8]) -> Self {
+        let mut cursor = Self {
+            input,
+            pos: 0,
+            at_start: true,
+            at_end: false,
+            prev: 0x00,
+            curr: 0x00,
+            next: 0x00,
+        };
+        cursor.move_to(0);
+        cursor
+    }
+
+    pub fn rewind_by(&mut self, amount: usize) {
+        self.move_to(self.pos.saturating_sub(amount));
+    }
+
+    pub fn advance_by(&mut self, amount: usize) {
+        self.move_to(self.pos.saturating_add(amount));
+    }
+
+    pub fn move_to(&mut self, pos: usize) {
+        let pos = pos.clamp(0, self.input.len() - 1);
+
+        self.pos = pos;
+        self.at_start = pos == 0;
+        self.at_end = pos >= self.input.len() - 1;
+
+        self.prev = if !self.at_start { self.input[pos-1] } else { 0x00 };
+        self.curr = self.input[pos];
+        self.next = if !self.at_end { self.input[pos+1] } else { 0x00 };
+    }
+}
+
+impl<'a> Add<usize> for Cursor<'a> {
+    type Output = Self;
+
+    fn add(mut self, rhs: usize) -> Self::Output {
+        self.advance_by(rhs);
+        self
+    }
+}
+
+impl<'a> Sub<usize> for Cursor<'a> {
+    type Output = Self;
+
+    fn sub(mut self, rhs: usize) -> Self::Output {
+        self.rewind_by(rhs);
+        self
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_cursor() {
+        let mut cursor = Cursor::new(b"hello world");
+        assert_eq!(cursor.pos, 0);
+        assert_eq!(cursor.at_start, true);
+        assert_eq!(cursor.at_end, false);
+        assert_eq!(cursor.prev, 0x00);
+        assert_eq!(cursor.curr, b'h');
+        assert_eq!(cursor.next, b'e');
+
+        cursor.advance_by(1);
+        assert_eq!(cursor.pos, 1);
+        assert_eq!(cursor.at_start, false);
+        assert_eq!(cursor.at_end, false);
+        assert_eq!(cursor.prev, b'h');
+        assert_eq!(cursor.curr, b'e');
+        assert_eq!(cursor.next, b'l');
+
+        // Advancing too far should stop at the end
+        cursor.advance_by(10);
+        assert_eq!(cursor.pos, 10);
+        assert_eq!(cursor.at_start, false);
+        assert_eq!(cursor.at_end, true);
+        assert_eq!(cursor.prev, b'l');
+        assert_eq!(cursor.curr, b'd');
+        assert_eq!(cursor.next, 0x00);
+
+        // Can't advance past the end
+        cursor.advance_by(1);
+        assert_eq!(cursor.pos, 10);
+        assert_eq!(cursor.at_start, false);
+        assert_eq!(cursor.at_end, true);
+        assert_eq!(cursor.prev, b'l');
+        assert_eq!(cursor.curr, b'd');
+        assert_eq!(cursor.next, 0x00);
+
+        cursor.rewind_by(1);
+        assert_eq!(cursor.pos, 9);
+        assert_eq!(cursor.at_start, false);
+        assert_eq!(cursor.at_end, false);
+        assert_eq!(cursor.prev, b'r');
+        assert_eq!(cursor.curr, b'l');
+        assert_eq!(cursor.next, b'd');
+
+        cursor.rewind_by(10);
+        assert_eq!(cursor.pos, 0);
+        assert_eq!(cursor.at_start, true);
+        assert_eq!(cursor.at_end, false);
+        assert_eq!(cursor.prev, 0x00);
+        assert_eq!(cursor.curr, b'h');
+        assert_eq!(cursor.next, b'e');
+    }
+}
--- oxide/crates/core/src/lib.rs
+++ oxide/crates/core/src/lib.rs
@@ -9,6 +9,7 @@ use tracing::event;
 use walkdir::WalkDir;
 
 pub mod candidate;
+pub mod cursor;
 pub mod glob;
 pub mod location;
 pub mod modifier;
