616243975cc8eb3faf4db1c6cc197d33308094d3
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2023-09-19 03:29:04 +0900

    Add support for `text-wrap: pretty`

--- src/corePlugins.js
+++ src/corePlugins.js
@@ -1528,6 +1528,7 @@ export let corePlugins = {
       '.text-wrap': { 'text-wrap': 'wrap' },
       '.text-nowrap': { 'text-wrap': 'nowrap' },
       '.text-balance': { 'text-wrap': 'balance' },
+      '.text-pretty': { 'text-wrap': 'pretty' },
     })
   },
 
