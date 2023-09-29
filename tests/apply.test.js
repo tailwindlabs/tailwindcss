88119e2ecd32784f13c32ac3a01e6d3cb64305fb
Jordan Pittman <jordan@cryptica.me>
2023-09-29 16:04:32 -0400

    Don’t crash when important and parent selectors are equal in `@apply` (#12112)
    * Don’t crash when important and parent selectors are equal in `@apply`
    
    * Update changelog

--- tests/apply.test.js
+++ tests/apply.test.js
@@ -2470,4 +2470,56 @@ crosscheck(({ stable, oxide }) => {
   // 3. They all use invalid selector syntax that Lightning CSS does not support
   // It may be enough for Oxide to not support it at all
   oxide.test.todo('::ng-deep, ::deep, ::v-deep pseudo elements are left alone')
+
+  test('should not break replacing important selector when the same as the parent selector (pseudo)', async () => {
+    let config = {
+      important: ':root',
+      content: [],
+    }
+
+    let input = css`
+      @tailwind components;
+      @layer components {
+        :root {
+          @apply flex;
+        }
+      }
+    `
+
+    let result = await run(input, config)
+
+    expect(result.css).toMatchFormattedCss(css`
+      :root {
+        display: flex;
+      }
+    `)
+  })
+
+  test('should not break replacing important selector when the same as the parent selector (class)', async () => {
+    let config = {
+      important: '.foo',
+      content: [
+        {
+          raw: html` <div class="foo"></div> `,
+        },
+      ],
+    }
+
+    let input = css`
+      @tailwind components;
+      @layer components {
+        .foo {
+          @apply flex;
+        }
+      }
+    `
+
+    let result = await run(input, config)
+
+    expect(result.css).toMatchFormattedCss(css`
+      .foo {
+        display: flex;
+      }
+    `)
+  })
 })
