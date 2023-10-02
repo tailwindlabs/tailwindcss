0af88b1494e67488b8419dd6c36b44bb9280bf51
Robin Malfait <malfait.robin@gmail.com>
2023-10-02 16:19:23 +0200

    Improve RegEx parser, reduce possibilities as the key for arbitrary properties (#12121)
    * optimize handling of RegEx parser results
    
    Previous:
    - Copy `results`, for every subsequent result of other `patterns`
    - Loop over results to filter out `undefined` values
    - Loop over results to map to `clipAtBalancedParens`
    
    Current:
    - For each candidate, push the `clipAtBalancedParens(candidate)` into
      the `results`
    
    This way we are not copying existing results, and we are also avoiding
    additional loops over the entire array to filter out `undefined` values
    and map to `clipAtBalancedParens`.
    
    * do not allow `]` in the first part of arbitrary properties
    
    ```
    [foo:bar]
     ─┬─
      └── This part cannot contain `]`
    ```
    
    This is also a very targeted fix for when the arbitrary properties seem
    to match a large piece of text, but shouldn't
    
    * add real world tests for parsing candidate strings
    
    * sync package-lock.json
    
    * update changelog

--- src/lib/defaultExtractor.js
+++ src/lib/defaultExtractor.js
@@ -11,10 +11,12 @@ export function defaultExtractor(context) {
     let results = []
 
     for (let pattern of patterns) {
-      results = [...results, ...(content.match(pattern) ?? [])]
+      for (let result of content.match(pattern) ?? []) {
+        results.push(clipAtBalancedParens(result))
+      }
     }
 
-    return results.filter((v) => v !== undefined).map(clipAtBalancedParens)
+    return results
   }
 }
 
@@ -33,7 +35,7 @@ function* buildRegExps(context) {
     // This is a targeted fix to continue to allow theme()
     // with square brackets to work in arbitrary properties
     // while fixing a problem with the regex matching too much
-    /\[[^\s:'"`]+:[^\s]+?\[[^\s]+\][^\s]+?\]/,
+    /\[[^\s:'"`\]]+:[^\s]+?\[[^\s]+\][^\s]+?\]/,
 
     // Utilities
     regex.pattern([
