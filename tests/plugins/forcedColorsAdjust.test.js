e26825ae0b7fbf1a048f9a4e34d930d14d0c41a0
Robin Malfait <malfait.robin@gmail.com>
2023-10-09 17:29:50 +0200

    use `toMatchSnapshot` instead of `toMatchFormattedCss`
    More info: https://github.com/tailwindlabs/tailwindcss/pull/12170

--- tests/plugins/forcedColorsAdjust.test.js
+++ tests/plugins/forcedColorsAdjust.test.js
@@ -1,10 +1,3 @@
-import { css, quickPluginTest } from '../util/run'
+import { quickPluginTest } from '../util/run'
 
-quickPluginTest('forcedColorsAdjust').toMatchFormattedCss(css`
-  .forced-colors-auto {
-    forced-colors-adjust: auto;
-  }
-  .forced-colors-none {
-    forced-colors-adjust: none;
-  }
-`)
+quickPluginTest('forcedColorsAdjust').toMatchSnapshot()
