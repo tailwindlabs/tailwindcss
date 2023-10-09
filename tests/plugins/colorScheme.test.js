56ae7e6bb004c85682ad8407d113559999891c17
Robin Malfait <malfait.robin@gmail.com>
2023-10-09 17:50:58 +0200

    use `toMatchSnapshot` instead of `toMatchFormattedCss`
    More info: https://github.com/tailwindlabs/tailwindcss/pull/12170

--- tests/plugins/colorScheme.test.js
+++ tests/plugins/colorScheme.test.js
@@ -1,19 +1,3 @@
-import { css, quickPluginTest } from '../util/run'
+import { quickPluginTest } from '../util/run'
 
-quickPluginTest('colorScheme').toMatchFormattedCss(css`
-  .color-scheme-dark {
-    color-scheme: dark;
-  }
-  .color-scheme-light {
-    color-scheme: light;
-  }
-  .color-scheme-light-dark {
-    color-scheme: light dark;
-  }
-  .color-scheme-dark-only {
-    color-scheme: dark only;
-  }
-  .color-scheme-light-only {
-    color-scheme: light only;
-  }
-`)
+quickPluginTest('colorScheme').toMatchSnapshot()
