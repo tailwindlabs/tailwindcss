d9ec386fbe75ebf855085ca00189319bd47a0358
Robin Malfait <malfait.robin@gmail.com>
2023-10-09 16:58:07 +0200

    use `toMatchSnapshot` instead of `toMatchFormattedCss`
    More info: https://github.com/tailwindlabs/tailwindcss/pull/12170

--- tests/plugins/transitionBehavior.test.js
+++ tests/plugins/transitionBehavior.test.js
@@ -1,10 +1,3 @@
-import { css, quickPluginTest } from '../util/run'
+import { quickPluginTest } from '../util/run'
 
-quickPluginTest('transitionBehavior').toMatchFormattedCss(css`
-  .transition-allow-discrete {
-    transition-behavior: allow-discrete;
-  }
-  .transition-behavior-normal {
-    transition-behavior: normal;
-  }
-`)
+quickPluginTest('transitionBehavior').toMatchSnapshot()
