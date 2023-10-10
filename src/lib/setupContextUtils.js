7562353534d67b5c935dbed83d77a8242e29ec56
Jordan Pittman <jordan@cryptica.me>
2023-10-10 13:11:10 -0400

    Fix sorting of utilities that share multiple candidates

--- src/lib/setupContextUtils.js
+++ src/lib/setupContextUtils.js
@@ -938,7 +938,7 @@ function registerPlugins(plugins, context) {
 
     // Sort all classes in order
     // Non-tailwind classes won't be generated and will be left as `null`
-    let rules = generateRules(new Set(sorted), context)
+    let rules = generateRules(new Set(sorted), context, true)
     rules = context.offsets.sort(rules)
 
     let idx = BigInt(parasiteUtilities.length)
