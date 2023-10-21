0855f2ea603bf08a7b7db8b0ef87da990813d4bd
Nikita Gaidakov <ngaidakov@podfather.com>
2023-10-21 23:59:13 +0100

    Merge default theme in ResolvedConfig

--- resolveConfig.d.ts
+++ resolveConfig.d.ts
@@ -1,11 +1,23 @@
-import type { Config, ResolvableTo } from './types/config'
+import { Config, ResolvableTo } from './types/config'
+import { DefaultTheme } from './types/generated/default-theme'
+import { DefaultColors } from './types/generated/colors'
+
+type ResolvedConfig<T extends Config> = Omit<T, 'theme'> & {
+  theme: MergeThemes<UnwrapResolvables<T['theme']>>
+}
 
 type UnwrapResolvables<T> = {
   [K in keyof T]: T[K] extends ResolvableTo<infer R> ? R : T[K]
 }
 
-type ResolvedConfig<T extends Config> = Omit<T, 'theme'> & {
-  theme: UnwrapResolvables<T['theme']>
+type DefaultThemeFull = DefaultTheme & { colors: DefaultColors }
+
+type MergeThemes<T extends Config['theme']> = {
+  [K in keyof DefaultThemeFull]: K extends keyof T
+    ? T[K]
+    : K extends keyof T['extend']
+    ? DefaultThemeFull[K] & T['extend'][K]
+    : DefaultThemeFull[K]
 }
 
 declare function resolveConfig<T extends Config>(config: T): ResolvedConfig<T>
