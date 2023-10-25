c92596f549cbc13f80b7775da855aaf81adddd3f
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-10-25 20:54:28 +0300

    Fix typo in `size-*` tests (#12289)
    * Update size.test.js
    
    * Fix test
    
    ---------
    
    Co-authored-by: Jordan Pittman <jordan@cryptica.me>

--- tests/plugins/size.test.js
+++ tests/plugins/size.test.js
@@ -3,7 +3,7 @@ import { quickPluginTest } from '../util/run'
 quickPluginTest('size', {
   safelist: [
     // Arbitrary values
-    's-[12px]',
-    's-[var(--my-value)]',
+    'size-[12px]',
+    'size-[var(--my-value)]',
   ],
 }).toMatchSnapshot()
