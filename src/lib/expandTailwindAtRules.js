2aedd6422ccda174ff007bdc8aeb8c6cc6176afd
Jordan Pittman <jordan@cryptica.me>
2023-11-29 15:45:33 -0500

    Make sure the root node has a source location for the end

--- src/lib/expandTailwindAtRules.js
+++ src/lib/expandTailwindAtRules.js
@@ -291,6 +291,9 @@ export default function expandTailwindAtRules(context) {
       root.append(cloned)
     }
 
+    // TODO: Why is the root node having no source location for `end` possible?
+    root.source.end = root.source.end ?? root.source.start
+
     // If we've got a utility layer and no utilities are generated there's likely something wrong
     const hasUtilityVariants = variantNodes.some(
       (node) => node.raws.tailwind?.parentLayer === 'utilities'
