084dcbcb3b1e79452c7b0e4da533a6ae1eb26542
uttam pun <uttampun50@gmail.com>
2023-12-06 08:28:46 +0545

    position unset

--- src/corePlugins.js
+++ src/corePlugins.js
@@ -643,6 +643,7 @@ export let corePlugins = {
       '.absolute': { position: 'absolute' },
       '.relative': { position: 'relative' },
       '.sticky': { position: 'sticky' },
+      '.unset': { position: 'unset' },
     })
   },
 
