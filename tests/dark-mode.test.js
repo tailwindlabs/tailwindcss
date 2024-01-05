64985021175f8babdc2525145b39aa9c244478a5
Jordan Pittman <jordan@cryptica.me>
2024-01-05 14:02:29 -0500

    Revert "Remove `variant` dark mode strategy"
    This reverts commit 185250438ccb2f61ba876d4676823c1807891122.

--- tests/dark-mode.test.js
+++ tests/dark-mode.test.js
@@ -229,3 +229,65 @@ it('should use modern sorting otherwise', () => {
     `)
   })
 })
+
+it('should allow customization of the dark mode variant', () => {
+  let config = {
+    darkMode: ['variant', '&:not(.light *)'],
+    content: [{ raw: html`<div class="dark:font-bold"></div>` }],
+    corePlugins: { preflight: false },
+  }
+
+  let input = css`
+    @tailwind utilities;
+  `
+
+  return run(input, config).then((result) => {
+    expect(result.css).toMatchFormattedCss(css`
+      .dark\:font-bold:not(.light *) {
+        font-weight: 700;
+      }
+    `)
+  })
+})
+
+it('should support parallel selectors for the dark mode variant', () => {
+  let config = {
+    darkMode: ['variant', ['&:not(.light *)', '&:not(.extralight *)']],
+    content: [{ raw: html`<div class="dark:font-bold"></div>` }],
+    corePlugins: { preflight: false },
+  }
+
+  let input = css`
+    @tailwind utilities;
+  `
+
+  return run(input, config).then((result) => {
+    expect(result.css).toMatchFormattedCss(css`
+      .dark\:font-bold:not(.light *),
+      .dark\:font-bold:not(.extralight *) {
+        font-weight: 700;
+      }
+    `)
+  })
+})
+
+it('should support fn selectors for the dark mode variant', () => {
+  let config = {
+    darkMode: ['variant', () => ['&:not(.light *)', '&:not(.extralight *)']],
+    content: [{ raw: html`<div class="dark:font-bold"></div>` }],
+    corePlugins: { preflight: false },
+  }
+
+  let input = css`
+    @tailwind utilities;
+  `
+
+  return run(input, config).then((result) => {
+    expect(result.css).toMatchFormattedCss(css`
+      .dark\:font-bold:not(.light *),
+      .dark\:font-bold:not(.extralight *) {
+        font-weight: 700;
+      }
+    `)
+  })
+})
