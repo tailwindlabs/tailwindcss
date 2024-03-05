f43167fa948bc6d68747361032b0401c427a4855
Robin Malfait <malfait.robin@gmail.com>
2024-03-05 22:37:16 +0100

    make CLI esm only

--- packages/@tailwindcss-cli/src/utils/resolve.ts
+++ packages/@tailwindcss-cli/src/utils/resolve.ts
@@ -1,4 +1,3 @@
 import { createRequire } from 'node:module'
 
-export const resolve =
-  typeof require?.resolve !== 'undefined' ? require.resolve : createRequire(import.meta.url).resolve
+export const resolve = createRequire(import.meta.url).resolve
