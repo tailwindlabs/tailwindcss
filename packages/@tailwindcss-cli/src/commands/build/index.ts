62ee42c30c6169dd5850a3794ccb5e0ae6112871
Robin Malfait <malfait.robin@gmail.com>
2024-03-07 22:12:13 +0100

    ensure input file is an import dependency

--- packages/@tailwindcss-cli/src/commands/build/index.ts
+++ packages/@tailwindcss-cli/src/commands/build/index.ts
@@ -237,7 +237,9 @@ function handleImports(
   // Relevant specification:
   //   - CSS Import Resolve: https://csstools.github.io/css-import-resolve/
 
-  if (!input.includes('@import')) return [input, []]
+  if (!input.includes('@import')) {
+    return [input, [file]]
+  }
 
   return postcss()
     .use(atImport())
@@ -247,6 +249,8 @@ function handleImports(
 
       // Use `result.messages` to get the imported files. This also includes the
       // current file itself.
-      result.messages.filter((msg) => msg.type === 'postcss-import').map((msg) => msg.file),
+      [file].concat(
+        result.messages.filter((msg) => msg.type === 'dependency').map((msg) => msg.file),
+      ),
     ])
 }
