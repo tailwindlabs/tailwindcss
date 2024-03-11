926acd8e95c009990ea6c9adc0f6a0bc1a5b5858
Adam Wathan <4323180+adamwathan@users.noreply.github.com>
2024-03-11 13:56:08 -0400

    Use static utilities + a big loop

--- packages/tailwindcss/src/utilities.test.ts
+++ packages/tailwindcss/src/utilities.test.ts
@@ -5858,8 +5858,8 @@ test('rounded-s', () => {
     }
 
     .rounded-s-full {
-      border-start-start-radius: 9999px;
-      border-end-start-radius: 9999px;
+      border-start-start-radius: 3.40282e38px;
+      border-end-start-radius: 3.40282e38px;
     }
 
     .rounded-s-none {
@@ -5910,8 +5910,8 @@ test('rounded-e', () => {
     }
 
     .rounded-e-full {
-      border-start-end-radius: 9999px;
-      border-end-end-radius: 9999px;
+      border-start-end-radius: 3.40282e38px;
+      border-end-end-radius: 3.40282e38px;
     }
 
     .rounded-e-none {
@@ -5962,8 +5962,8 @@ test('rounded-t', () => {
     }
 
     .rounded-t-full {
-      border-top-left-radius: 9999px;
-      border-top-right-radius: 9999px;
+      border-top-left-radius: 3.40282e38px;
+      border-top-right-radius: 3.40282e38px;
     }
 
     .rounded-t-none {
@@ -6014,8 +6014,8 @@ test('rounded-r', () => {
     }
 
     .rounded-r-full {
-      border-top-right-radius: 9999px;
-      border-bottom-right-radius: 9999px;
+      border-top-right-radius: 3.40282e38px;
+      border-bottom-right-radius: 3.40282e38px;
     }
 
     .rounded-r-none {
@@ -6066,8 +6066,8 @@ test('rounded-b', () => {
     }
 
     .rounded-b-full {
-      border-bottom-right-radius: 9999px;
-      border-bottom-left-radius: 9999px;
+      border-bottom-right-radius: 3.40282e38px;
+      border-bottom-left-radius: 3.40282e38px;
     }
 
     .rounded-b-none {
@@ -6118,8 +6118,8 @@ test('rounded-l', () => {
     }
 
     .rounded-l-full {
-      border-top-left-radius: 9999px;
-      border-bottom-left-radius: 9999px;
+      border-top-left-radius: 3.40282e38px;
+      border-bottom-left-radius: 3.40282e38px;
     }
 
     .rounded-l-none {
@@ -6168,7 +6168,7 @@ test('rounded-ss', () => {
     }
 
     .rounded-ss-full {
-      border-start-start-radius: 9999px;
+      border-start-start-radius: 3.40282e38px;
     }
 
     .rounded-ss-none {
@@ -6221,7 +6221,7 @@ test('rounded-se', () => {
     }
 
     .rounded-se-full {
-      border-start-end-radius: 9999px;
+      border-start-end-radius: 3.40282e38px;
     }
 
     .rounded-se-none {
@@ -6274,7 +6274,7 @@ test('rounded-ee', () => {
     }
 
     .rounded-ee-full {
-      border-end-end-radius: 9999px;
+      border-end-end-radius: 3.40282e38px;
     }
 
     .rounded-ee-none {
@@ -6327,7 +6327,7 @@ test('rounded-es', () => {
     }
 
     .rounded-es-full {
-      border-end-start-radius: 9999px;
+      border-end-start-radius: 3.40282e38px;
     }
 
     .rounded-es-none {
@@ -6380,7 +6380,7 @@ test('rounded-tl', () => {
     }
 
     .rounded-tl-full {
-      border-top-left-radius: 9999px;
+      border-top-left-radius: 3.40282e38px;
     }
 
     .rounded-tl-none {
@@ -6433,7 +6433,7 @@ test('rounded-tr', () => {
     }
 
     .rounded-tr-full {
-      border-top-right-radius: 9999px;
+      border-top-right-radius: 3.40282e38px;
     }
 
     .rounded-tr-none {
@@ -6486,7 +6486,7 @@ test('rounded-br', () => {
     }
 
     .rounded-br-full {
-      border-bottom-right-radius: 9999px;
+      border-bottom-right-radius: 3.40282e38px;
     }
 
     .rounded-br-none {
@@ -6539,7 +6539,7 @@ test('rounded-bl', () => {
     }
 
     .rounded-bl-full {
-      border-bottom-left-radius: 9999px;
+      border-bottom-left-radius: 3.40282e38px;
     }
 
     .rounded-bl-none {
--- packages/tailwindcss/src/utilities.ts
+++ packages/tailwindcss/src/utilities.ts
@@ -1955,125 +1955,37 @@ export function createUtilities(theme: Theme) {
   staticUtility('break-keep', [['word-break', 'break-keep']])
 
   {
-    function handleBareBorderRadiusValue({ value }: NamedUtilityValue) {
-      switch (value) {
-        case 'none':
-          return '0px'
-        case 'full':
-          return 'calc(infinity * 1px)'
-        default:
-          return null
-      }
+    // border-radius
+    for (let [root, properties] of [
+      ['rounded', ['border-radius']],
+      ['rounded-s', ['border-start-start-radius', 'border-end-start-radius']],
+      ['rounded-e', ['border-start-end-radius', 'border-end-end-radius']],
+      ['rounded-t', ['border-top-left-radius', 'border-top-right-radius']],
+      ['rounded-r', ['border-top-right-radius', 'border-bottom-right-radius']],
+      ['rounded-b', ['border-bottom-right-radius', 'border-bottom-left-radius']],
+      ['rounded-l', ['border-top-left-radius', 'border-bottom-left-radius']],
+      ['rounded-ss', ['border-start-start-radius']],
+      ['rounded-se', ['border-start-end-radius']],
+      ['rounded-ee', ['border-end-end-radius']],
+      ['rounded-es', ['border-end-start-radius']],
+      ['rounded-tl', ['border-top-left-radius']],
+      ['rounded-tr', ['border-top-right-radius']],
+      ['rounded-br', ['border-bottom-right-radius']],
+      ['rounded-bl', ['border-bottom-left-radius']],
+    ] as const) {
+      staticUtility(
+        `${root}-none`,
+        properties.map((property) => [property, '0']),
+      )
+      staticUtility(
+        `${root}-full`,
+        properties.map((property) => [property, 'calc(infinity * 1px)']),
+      )
+      functionalUtility(root, {
+        themeKeys: ['--radius'],
+        handle: (value) => properties.map((property) => decl(property, value)),
+      })
     }
-
-    // rounded-*
-    functionalUtility('rounded', {
-      themeKeys: ['--radius'],
-      handleBareValue: handleBareBorderRadiusValue,
-      handle: (value) => [decl('border-radius', value)],
-    })
-
-    functionalUtility('rounded-s', {
-      themeKeys: ['--radius'],
-      handleBareValue: handleBareBorderRadiusValue,
-      handle: (value) => [
-        decl('border-start-start-radius', value),
-        decl('border-end-start-radius', value),
-      ],
-    })
-
-    functionalUtility('rounded-e', {
-      themeKeys: ['--radius'],
-      handleBareValue: handleBareBorderRadiusValue,
-      handle: (value) => [
-        decl('border-start-end-radius', value),
-        decl('border-end-end-radius', value),
-      ],
-    })
-
-    functionalUtility('rounded-t', {
-      themeKeys: ['--radius'],
-      handleBareValue: handleBareBorderRadiusValue,
-      handle: (value) => [
-        decl('border-top-left-radius', value),
-        decl('border-top-right-radius', value),
-      ],
-    })
-
-    functionalUtility('rounded-r', {
-      themeKeys: ['--radius'],
-      handleBareValue: handleBareBorderRadiusValue,
-      handle: (value) => [
-        decl('border-top-right-radius', value),
-        decl('border-bottom-right-radius', value),
-      ],
-    })
-
-    functionalUtility('rounded-b', {
-      themeKeys: ['--radius'],
-      handleBareValue: handleBareBorderRadiusValue,
-      handle: (value) => [
-        decl('border-bottom-right-radius', value),
-        decl('border-bottom-left-radius', value),
-      ],
-    })
-
-    functionalUtility('rounded-l', {
-      themeKeys: ['--radius'],
-      handleBareValue: handleBareBorderRadiusValue,
-      handle: (value) => [
-        decl('border-top-left-radius', value),
-        decl('border-bottom-left-radius', value),
-      ],
-    })
-
-    functionalUtility('rounded-ss', {
-      themeKeys: ['--radius'],
-      handleBareValue: handleBareBorderRadiusValue,
-      handle: (value) => [decl('border-start-start-radius', value)],
-    })
-
-    functionalUtility('rounded-se', {
-      themeKeys: ['--radius'],
-      handleBareValue: handleBareBorderRadiusValue,
-      handle: (value) => [decl('border-start-end-radius', value)],
-    })
-
-    functionalUtility('rounded-ee', {
-      themeKeys: ['--radius'],
-      handleBareValue: handleBareBorderRadiusValue,
-      handle: (value) => [decl('border-end-end-radius', value)],
-    })
-
-    functionalUtility('rounded-es', {
-      themeKeys: ['--radius'],
-      handleBareValue: handleBareBorderRadiusValue,
-      handle: (value) => [decl('border-end-start-radius', value)],
-    })
-
-    functionalUtility('rounded-tl', {
-      themeKeys: ['--radius'],
-      handleBareValue: handleBareBorderRadiusValue,
-      handle: (value) => [decl('border-top-left-radius', value)],
-    })
-
-    functionalUtility('rounded-tr', {
-      themeKeys: ['--radius'],
-      handleBareValue: handleBareBorderRadiusValue,
-      handle: (value) => [decl('border-top-right-radius', value)],
-    })
-
-    functionalUtility('rounded-br', {
-      themeKeys: ['--radius'],
-      handleBareValue: handleBareBorderRadiusValue,
-      handle: (value) => [decl('border-bottom-right-radius', value)],
-    })
-
-    functionalUtility('rounded-bl', {
-      themeKeys: ['--radius'],
-      handleBareValue: handleBareBorderRadiusValue,
-      handle: (value) => [decl('border-bottom-left-radius', value)],
-    })
   }
 
   staticUtility('border-solid', [
