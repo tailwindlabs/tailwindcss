13db962ebd64a67cc38c1df389348a1994ecae6b
Robin Malfait <malfait.robin@gmail.com>
2024-03-13 18:46:40 +0100

    Revert "handle `@import` ourselves"
    This reverts commit 13a46404c16ee67e4e1b7eaf58ae67321113eb07.

--- scripts/pre-publish-optimizations.mjs
+++ scripts/pre-publish-optimizations.mjs
@@ -1,5 +1,7 @@
-import fs from 'node:fs'
+import fs from 'node:fs/promises'
 import path from 'node:path'
+import postcss from 'postcss'
+import atImport from 'postcss-import'
 import prettier from 'prettier'
 
 // 1. Performance optimization: Inline the contents of the
@@ -8,29 +10,13 @@ import prettier from 'prettier'
 {
   let __dirname = path.dirname(new URL(import.meta.url).pathname)
   let file = path.resolve(__dirname, '../packages/tailwindcss/index.css')
-  let inlined = await prettier.format(inline(file), { filepath: file })
-  fs.writeFileSync(file, inlined, 'utf-8')
-
-  // Recursively inlines `@import` statements in the given file.
-  function inline(file) {
-    let contents = fs.readFileSync(file, 'utf-8')
-    if (!contents.includes('@import')) return contents
-
-    let dirname = path.dirname(file)
-
-    return contents.replace(/@import (["'])(.*?)\1(.*);/g, (_, _quote, importee, additional) => {
-      let contents = inline(path.resolve(dirname, importee)).trim()
-
-      if (additional.trim()) {
-        let layerMatch = /layer\((.*)\)/g.exec(additional)
-        if (layerMatch) {
-          return `@layer ${layerMatch[1]} {${contents}}`
-        }
-
-        return `@media (${additional.trim()}) {${contents}}`
-      }
-
-      return inline(path.resolve(dirname, importee))
-    })
-  }
+  let contents = await fs.readFile(file, 'utf-8')
+  let inlined = await prettier.format(
+    await postcss()
+      .use(atImport())
+      .process(contents, { from: file })
+      .then((result) => result.css),
+    { filepath: file },
+  )
+  await fs.writeFile(file, inlined, 'utf-8')
 }
