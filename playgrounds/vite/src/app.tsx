aaec1f7690eab0e0dff46752e9bdd71cf956f8a3
Adam Wathan <4323180+adamwathan@users.noreply.github.com>
2024-03-22 15:56:28 -0400

    Revert playground changes

--- playgrounds/vite/src/app.tsx
+++ playgrounds/vite/src/app.tsx
@@ -1,15 +1,10 @@
-import React from 'react'
 import { Foo } from './foo'
 
 export function App() {
   return (
     <div className="m-3 p-3 border">
-      <button popovertarget="my-popover" class="trigger-btn">
-        Open Popover
-      </button>
-      <div id="my-popover" popover="auto" className="open:bg-red-500">
-        <p>I am a popover with more information.</p>
-      </div>
+      <h1 className="text-blue-500">Hello World</h1>
+      <Foo />
     </div>
   )
 }
