cd0ffbd4bad5dab2da460527dcd203b3969f3db6
Adam Wathan <4323180+adamwathan@users.noreply.github.com>
2024-04-04 16:46:36 -0400

    Revert playground changes

--- playgrounds/vite/src/app.tsx
+++ playgrounds/vite/src/app.tsx
@@ -2,7 +2,7 @@ import { Foo } from './foo'
 
 export function App() {
   return (
-    <div className="m-3 p-3 border shadow-xl shadow-red-500">
+    <div className="m-3 p-3 border">
       <h1 className="text-blue-500">Hello World</h1>
       <Foo />
     </div>
