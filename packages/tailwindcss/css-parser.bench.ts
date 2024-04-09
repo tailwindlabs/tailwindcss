3c7073f397bc774aeea872d7759d3c694199e06a
Vinicius Lourenço <12551007+H4ad@users.noreply.github.com>
2024-04-09 14:25:56 -0300

    Perf: Use charCodeAt instead of string comparison (#13393)
    * Use charCodeAt instead of string comparison
    
    * Rename some things
    
    * Fix lint issues
    
    ---------
    
    Co-authored-by: Adam Wathan <4323180+adamwathan@users.noreply.github.com>

---
+++ packages/tailwindcss/css-parser.bench.ts
@@ -0,0 +1,10 @@
+import { readFileSync } from 'node:fs'
+import { bench } from 'vitest'
+import * as CSS from './src/css-parser.ts'
+
+const currentFolder = new URL('.', import.meta.url).pathname
+const cssFile = readFileSync(currentFolder + './preflight.css', 'utf-8')
+
+bench('css-parser on preflight.css', () => {
+  CSS.parse(cssFile)
+})
