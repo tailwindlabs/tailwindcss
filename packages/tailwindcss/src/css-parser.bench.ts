3de3adee4230c76b9dbb82b2e747cdea66bec73c
Robin Malfait <malfait.robin@gmail.com>
2024-04-18 12:44:38 +0200

    colocate css-parser benchmark file in `src` folder

--- packages/tailwindcss/src/css-parser.bench.ts
+++ packages/tailwindcss/src/css-parser.bench.ts
@@ -1,8 +1,8 @@
 import { readFileSync } from 'node:fs'
 import { bench } from 'vitest'
-import * as CSS from './src/css-parser.ts'
+import * as CSS from './css-parser.ts'
 
-const currentFolder = new URL('.', import.meta.url).pathname
+const currentFolder = new URL('..', import.meta.url).pathname
 const cssFile = readFileSync(currentFolder + './preflight.css', 'utf-8')
 
 bench('css-parser on preflight.css', () => {
