0e3a39dfc27fca7b4204391768b2cec30b4342b7
Jordan Pittman <jordan@cryptica.me>
2024-04-29 13:37:05 -0400

    wip

--- packages/tailwindcss/src/css-parser.bench.ts
+++ packages/tailwindcss/src/css-parser.bench.ts
@@ -1,7 +1,6 @@
 import { readFileSync } from 'node:fs'
 import { bench } from 'vitest'
 import * as CSS from './css-parser'
-import { TrackLocations } from './track-locations'
 
 const currentFolder = new URL('..', import.meta.url).pathname
 const cssFile = readFileSync(currentFolder + './preflight.css', 'utf-8')
@@ -11,5 +10,5 @@ bench('css-parser on preflight.css', () => {
 })
 
 bench('CSS with sourcemaps', () => {
-  CSS.parse(input, new TrackLocations())
+  CSS.parse(cssFile, true)
 })
