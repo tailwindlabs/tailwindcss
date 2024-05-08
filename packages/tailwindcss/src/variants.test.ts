2626a8a1551eee41ccafb750bffb5f443aac55aa
Jordan Pittman <jordan@cryptica.me>
2024-05-08 18:12:38 -0400

    Update

--- packages/tailwindcss/src/variants.test.ts
+++ packages/tailwindcss/src/variants.test.ts
@@ -1652,7 +1652,7 @@ test('nth', () => {
 
   expect(
     run(['nth-foo:flex', 'nth-of-type-foo:flex', 'nth-last-foo:flex', 'nth-last-of-type-foo:flex']),
-  ).toMatchInlineSnapshot(`""`)
+  ).toEqual(``)
 })
 
 test('container queries', () => {
