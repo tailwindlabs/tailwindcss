62de02a37946c48c5fa6cdb800173b54198ebb9b
Adam Wathan <adam.wathan@gmail.com>
2024-06-21 17:36:59 -0400

    Support combining arbitrary shadows without a color with shadow color utilities (#13876)
    * Support combining arbitrary shadows without a color with shadow color utilities
    
    * Update changelog
    
    ---------
    
    Co-authored-by: Adam Wathan <4323180+adamwathan@users.noreply.github.com>

--- packages/tailwindcss/src/utils/replace-shadow-colors.ts
+++ packages/tailwindcss/src/utils/replace-shadow-colors.ts
@@ -27,10 +27,18 @@ export function replaceShadowColors(input: string, replacement: string): string 
       }
     }
 
-    // Only replace if we found an x-offset, y-offset, and color, otherwise we
-    // may be replacing the wrong part of the box-shadow.
-    if (offsetX !== null && offsetY !== null && color !== null) {
+    // If the x and y offsets were not detected, the shadow is either invalid or
+    // using a variable to represent more than one field in the shadow value, so
+    // we can't know what to replace.
+    if (offsetX === null || offsetY === null) continue
+
+    if (color !== null) {
+      // If a color was found, replace the color.
       input = input.replace(color, replacement)
+    } else {
+      // If no color was found, assume the shadow is relying on the browser
+      // default shadow color and append the replacement color.
+      input = `${input} ${replacement}`
     }
   }
 
