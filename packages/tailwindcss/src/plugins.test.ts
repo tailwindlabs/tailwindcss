b6426b4f5771c5f4c8f81e9d0a2415ce7c0d2339
Jordan Pittman <jordan@cryptica.me>
2024-07-04 18:11:07 -0400

    wip

--- packages/tailwindcss/src/plugins.test.ts
+++ packages/tailwindcss/src/plugins.test.ts
@@ -13,6 +13,24 @@ test('Custom static utilities', () => {
           '.my-blue': {
             color: 'red',
           },
+        })
+      },
+    ],
+  }
+
+  expect(run(['my-red'], config)).toMatchInlineSnapshot(`
+    ".my-red {
+      color: red;
+    }"
+  `)
+  expect(run(['-my-red', 'my-red-[--value]'], config)).toEqual('')
+})
+
+test('Custom functional utilities', () => {
+  let config: UserConfig = {
+    plugins: [
+      ({ addUtilities }) => {
+        addUtilities({
           '.my-fn': (value, { modifier }) => ({
             color: modifier ? `${value ?? ''} / ${modifier ?? ''}` : `${value ?? ''}`,
           }),
@@ -21,18 +39,54 @@ test('Custom static utilities', () => {
     ],
   }
 
-  expect(run(['my-red', 'my-fn-[--value]', 'my-fn-[--value]/25'], config)).toMatchInlineSnapshot(`
-    ".my-fn-\\[--value\\] {
+  expect(run(['my-fn', 'my-fn-[--value]', 'my-fn-[--value]/25'], config)).toMatchInlineSnapshot(`
+    ".my-fn {
+      color: ;
+    }
+
+    .my-fn-\\[--value\\] {
       color: var(--value);
     }
 
     .my-fn-\\[--value\\]\\/25 {
       color: var(--value) / 25;
-    }
-
-    .my-red {
-      color: red;
     }"
   `)
-  expect(run(['-my-red', 'my-red-[--value]'], config)).toEqual('')
+})
+
+test('Custom static variants', () => {
+  let config: UserConfig = {
+    plugins: [
+      ({ addVariant }) => {
+        addVariant('hocus', ['&:hover', '&:focus'])
+        addVariant('hactive', ['&:hover', '&:active'])
+      },
+    ],
+  }
+
+  expect(run(['hocus:underline'], config)).toMatchInlineSnapshot(`
+    ".hocus\\:underline:hover, .hocus\\:underline:focus {
+      text-decoration-line: underline;
+    }"
+  `)
+  expect(run(['hover/foo:underline', 'hover-[123]:underline'], config)).toEqual('')
+})
+
+test('Custom functional variants', () => {
+  let config: UserConfig = {
+    plugins: [
+      ({ addVariant }) => {
+        addVariant('in', ({ value, modifier }) => {
+          return modifier ? [] : value ? `&:is(${value} *)` : []
+        })
+      },
+    ],
+  }
+
+  expect(run(['in-[:checked]:underline'], config)).toMatchInlineSnapshot(`
+    ".in-\\[\\:checked\\]\\:underline:is(:checked *) {
+      text-decoration-line: underline;
+    }"
+  `)
+  expect(run(['in-[:checked]/foo:underline'], config)).toEqual('')
 })
