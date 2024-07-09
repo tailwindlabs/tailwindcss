6b937ea9be3ec6199d7f55cdd127c002647377e4
Adam Wathan <4323180+adamwathan@users.noreply.github.com>
2024-07-09 15:27:38 -0400

    Add basic `loadPlugin` support to Vite plugin

--- playgrounds/vite/src/app.tsx
+++ playgrounds/vite/src/app.tsx
@@ -4,6 +4,7 @@ export function App() {
   return (
     <div className="m-3 p-3 border">
       <h1 className="text-blue-500">Hello World</h1>
+      <button className="hocus:underline">Click me</button>
       <Foo />
     </div>
   )
