a2f34cab215044d65a6b71d5fb0bde2f918c0125
Philipp Spiess <hello@philippspiess.com>
2024-07-29 14:56:52 +0200

    Properly convert file URLs to paths

--- packages/tailwindcss/src/css-parser.bench.ts
+++ packages/tailwindcss/src/css-parser.bench.ts
@@ -1,8 +1,10 @@
 import { readFileSync } from 'node:fs'
+import { fileURLToPath } from 'node:url'
+
 import { bench } from 'vitest'
 import * as CSS from './css-parser.ts'
 
-const currentFolder = new URL('..', import.meta.url).pathname
+const currentFolder = fileURLToPath(new URL('..', import.meta.url))
 const cssFile = readFileSync(currentFolder + './preflight.css', 'utf-8')
 
 bench('css-parser on preflight.css', () => {
