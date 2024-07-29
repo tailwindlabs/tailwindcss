b07832772ab82cf6650f7aee3eb065facd115288
Philipp Spiess <hello@philippspiess.com>
2024-07-29 17:57:50 +0200

    Add PostCSS plugin to fix relative `@content` and `@plugin` paths in `@import`ed files (#14063)
    We noticed an issue that happened when handling relative file imports in
    the `@plugin` and the upcoming `@content` APIs. The problem arises from
    relative files that are inside `@import`ed stylesheets. Take, for
    example, the following folder structure:
    
    ```css
    /* src/index.css */
    @import "./dir/index.css";
    ```
    ```css
    /* src/dir/index.css */
    @plugin "../../plugin.ts";
    ```
    
    It's expected that the path is relative to the CSS file that defined it.
    However, right now, we use
    [`postcss-import`](https://github.com/postcss/postcss-import) to flatten
    the CSS file before running the tailwind build step. This causes these
    custom-properties to be inlined in a flat file which removes the
    information of which file is being referred:
    
    ```css
    /* src/flat.css */
    @plugin "../../plugin.ts"; /* <- This is now pointing to the wrong file */
    ```
    
    There are generally two approaches that we can do to solve this:
    
    1. **Handle `@import` flattening inside tailwindcss:** While generally
    this would give us more freedom and less dependencies, this would
    require some work to get all edge cases right. We need to support
    layers/conditional imports and also handle all relative urls for
    properties like `background-image`.
    2. **Rewrite relative paths as a separate postcss visitor:** The
    approach this PR takes is instead to implement a custom postcss plugin
    that uses the AST to rewrite relative references inside `@plugin` and
    `@content`. This has the benefit of requiring little changes to our
    existing APIs. The rule is only enabled for relative references inside
    `@plugin` and `@content`, so the surface of this rule is very small.
    
    We can use this plugin inside all three current clients:
    
    - `@tailwindcss/postcss` obviously already uses postcss
    - `@tailwindcss/cli` also uses postcss to handle `@import` flattening
    - `@tailwindcss/vite` allows us to add custom postcss rules via the CSS
    pipeline. There are a few cases that we handle with care (e.g. in vite
    you can pass a string to the postcss config which is supposed to load
    the config from a file).
    
    To validate the changes, we have added both a list of unit test cases to
    the plugin itself as well as verified that all three clients are working
    as expected:
    
    - `@tailwindcss/postcss` now has an explicit test for this behavior
    - `@tailwindcss/cli` and `@tailwindcss/vite` were manually tested by
    updating the vite playground. The CLI was run with `--cwd
    playgrounds/vite/ -i ./src/app.css -o foo.css`:
    <img width="531" alt="Screenshot 2024-07-29 at 11 35 59"
    src="https://github.com/user-attachments/assets/78f0acdc-a46c-4c6c-917a-2916417b1001">

--- packages/@tailwindcss-postcss/src/index.ts
+++ packages/@tailwindcss-postcss/src/index.ts
@@ -2,9 +2,10 @@ import { scanDir } from '@tailwindcss/oxide'
 import fs from 'fs'
 import { Features, transform } from 'lightningcss'
 import path from 'path'
-import postcss, { type AcceptedPlugin, type PluginCreator } from 'postcss'
+import postcss, { AtRule, type AcceptedPlugin, type PluginCreator } from 'postcss'
 import postcssImport from 'postcss-import'
 import { compile } from 'tailwindcss'
+import fixRelativePathsPlugin from '../../internal-postcss-fix-relative-paths/src'
 
 /**
  * A Map that can generate default values for keys that don't exist.
@@ -48,114 +49,118 @@ function tailwindcss(opts: PluginOptions = {}): AcceptedPlugin {
     }
   })
 
+  let hasApply: boolean, hasTailwind: boolean
+
   return {
     postcssPlugin: '@tailwindcss/postcss',
     plugins: [
       // We need to run `postcss-import` first to handle `@import` rules.
       postcssImport(),
+      fixRelativePathsPlugin(),
 
-      (root, result) => {
-        let inputFile = result.opts.from ?? ''
-        let context = cache.get(inputFile)
-
-        let rebuildStrategy: 'full' | 'incremental' = 'incremental'
-
-        // Track file modification times to CSS files
-        {
-          let files = result.messages.flatMap((message) => {
-            if (message.type !== 'dependency') return []
-            return message.file
-          })
-          files.push(inputFile)
-          for (let file of files) {
-            let changedTime = fs.statSync(file, { throwIfNoEntry: false })?.mtimeMs ?? null
-            if (changedTime === null) {
-              if (file === inputFile) {
-                rebuildStrategy = 'full'
-              }
-              continue
-            }
-
-            let prevTime = context.mtimes.get(file)
-            if (prevTime === changedTime) continue
-
-            rebuildStrategy = 'full'
-            context.mtimes.set(file, changedTime)
-          }
-        }
-
-        let hasApply = false
-        let hasTailwind = false
-
-        root.walkAtRules((rule) => {
+      {
+        postcssPlugin: 'tailwindcss',
+        Once() {
+          // Reset some state between builds
+          hasApply = false
+          hasTailwind = false
+        },
+        AtRule(rule: AtRule) {
           if (rule.name === 'apply') {
             hasApply = true
           } else if (rule.name === 'tailwind') {
             hasApply = true
             hasTailwind = true
-            // If we've found `@tailwind` then we already
-            // know we have to run a "full" build
-            return false
           }
-        })
+        },
+        OnceExit(root, { result }) {
+          let inputFile = result.opts.from ?? ''
+          let context = cache.get(inputFile)
 
-        // Do nothing if neither `@tailwind` nor `@apply` is used
-        if (!hasTailwind && !hasApply) return
+          let rebuildStrategy: 'full' | 'incremental' = 'incremental'
 
-        let css = ''
-
-        // Look for candidates used to generate the CSS
-        let { candidates, files, globs } = scanDir({ base, globs: true })
-
-        // Add all found files as direct dependencies
-        for (let file of files) {
-          result.messages.push({
-            type: 'dependency',
-            plugin: '@tailwindcss/postcss',
-            file,
-            parent: result.opts.from,
-          })
-        }
-
-        // Register dependencies so changes in `base` cause a rebuild while
-        // giving tools like Vite or Parcel a glob that can be used to limit
-        // the files that cause a rebuild to only those that match it.
-        for (let { base, glob } of globs) {
-          result.messages.push({
-            type: 'dir-dependency',
-            plugin: '@tailwindcss/postcss',
-            dir: base,
-            glob,
-            parent: result.opts.from,
-          })
-        }
-
-        if (rebuildStrategy === 'full') {
-          let basePath = path.dirname(path.resolve(inputFile))
-          let { build } = compile(root.toString(), {
-            loadPlugin: (pluginPath) => {
-              if (pluginPath[0] === '.') {
-                return require(path.resolve(basePath, pluginPath))
+          // Track file modification times to CSS files
+          {
+            let files = result.messages.flatMap((message) => {
+              if (message.type !== 'dependency') return []
+              return message.file
+            })
+            files.push(inputFile)
+            for (let file of files) {
+              let changedTime = fs.statSync(file, { throwIfNoEntry: false })?.mtimeMs ?? null
+              if (changedTime === null) {
+                if (file === inputFile) {
+                  rebuildStrategy = 'full'
+                }
+                continue
               }
 
-              return require(pluginPath)
-            },
-          })
-          context.build = build
-          css = build(hasTailwind ? candidates : [])
-        } else if (rebuildStrategy === 'incremental') {
-          css = context.build!(candidates)
-        }
+              let prevTime = context.mtimes.get(file)
+              if (prevTime === changedTime) continue
 
-        // Replace CSS
-        if (css !== context.css && optimize) {
-          context.optimizedCss = optimizeCss(css, {
-            minify: typeof optimize === 'object' ? optimize.minify : true,
-          })
-        }
-        context.css = css
-        root.removeAll()
-        root.append(postcss.parse(optimize ? context.optimizedCss : context.css, result.opts))
+              rebuildStrategy = 'full'
+              context.mtimes.set(file, changedTime)
+            }
+          }
+
+          // Do nothing if neither `@tailwind` nor `@apply` is used
+          if (!hasTailwind && !hasApply) return
+
+          let css = ''
+
+          // Look for candidates used to generate the CSS
+          let { candidates, files, globs } = scanDir({ base, globs: true })
+
+          // Add all found files as direct dependencies
+          for (let file of files) {
+            result.messages.push({
+              type: 'dependency',
+              plugin: '@tailwindcss/postcss',
+              file,
+              parent: result.opts.from,
+            })
+          }
+
+          // Register dependencies so changes in `base` cause a rebuild while
+          // giving tools like Vite or Parcel a glob that can be used to limit
+          // the files that cause a rebuild to only those that match it.
+          for (let { base, glob } of globs) {
+            result.messages.push({
+              type: 'dir-dependency',
+              plugin: '@tailwindcss/postcss',
+              dir: base,
+              glob,
+              parent: result.opts.from,
+            })
+          }
+
+          if (rebuildStrategy === 'full') {
+            let basePath = path.dirname(path.resolve(inputFile))
+            let { build } = compile(root.toString(), {
+              loadPlugin: (pluginPath) => {
+                if (pluginPath[0] === '.') {
+                  return require(path.resolve(basePath, pluginPath))
+                }
+
+                return require(pluginPath)
+              },
+            })
+            context.build = build
+            css = build(hasTailwind ? candidates : [])
+          } else if (rebuildStrategy === 'incremental') {
+            css = context.build!(candidates)
+          }
+
+          // Replace CSS
+          if (css !== context.css && optimize) {
+            context.optimizedCss = optimizeCss(css, {
+              minify: typeof optimize === 'object' ? optimize.minify : true,
+            })
+          }
+          context.css = css
+          root.removeAll()
+          root.append(postcss.parse(optimize ? context.optimizedCss : context.css, result.opts))
+        },
       },
     ],
   }
