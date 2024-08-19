7f5ee2c084b7a18c87f16be934dc45f0cf752849
Jordan Pittman <jordan@cryptica.me>
2024-08-19 15:08:21 -0400

    Use `resolveConfig` to normalize plugins

--- packages/tailwindcss/src/compat/config/types.ts
+++ packages/tailwindcss/src/compat/config/types.ts
@@ -1,9 +1,11 @@
+import type { Plugin, PluginWithConfig } from '../../plugin-api'
 import type { PluginUtils } from './resolve-config'
 
 export type ResolvableTo<T> = T | ((utils: PluginUtils) => T)
 
 export interface UserConfig {
   theme?: ThemeConfig
+  plugins?: Plugin[]
 }
 
 export type ThemeValue = ResolvableTo<Record<string, unknown>> | null | undefined
@@ -15,4 +17,5 @@ export type ThemeConfig = Record<string, ThemeValue> & {
 
 export interface ResolvedConfig {
   theme: Record<string, Record<string, unknown>>
+  plugins: PluginWithConfig[]
 }
