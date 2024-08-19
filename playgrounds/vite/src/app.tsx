c6d5a1977d8bd7fe06a0b1b283f50e0b2cf55054
Jordan Pittman <jordan@cryptica.me>
2024-08-19 14:07:01 -0400

    Add typography plugin to playground

--- playgrounds/vite/src/app.tsx
+++ playgrounds/vite/src/app.tsx
@@ -1,11 +1,37 @@
+// prettier-ignore
 import { Foo } from './foo'
 
 export function App() {
   return (
-    <div className="m-3 p-3 border">
-      <h1 className="text-blue-500">Hello World</h1>
-      <button className="hocus:underline">Click me</button>
-      <Foo />
+    <div>
+      <div className="m-3 p-3 border">
+        <h1 className="text-blue-500">Hello World</h1>
+        <button className="hocus:underline">Click me</button>
+        <Foo />
+      </div>
+      <div class="mt-8 prose prose-slate mx-auto lg:prose-lg">
+        <h1>Headline</h1>
+        <p class="lead">
+          Until now, trying to style an article, document, or blog post with Tailwind has been a
+          tedious task that required a keen eye for typography and a lot of complex custom CSS.
+        </p>
+        <p>
+          By default, Tailwind removes all of the default browser styling from paragraphs, headings,
+          lists and more. This ends up being really useful for building application UIs because you
+          spend less time undoing user-agent styles, but when you <em>really are</em> just trying to
+          style some content that came from a rich-text editor in a CMS or a markdown file, it can
+          be surprising and unintuitive.
+        </p>
+        <p>
+          We get lots of complaints about it actually, with people regularly asking us things like:
+        </p>
+        <blockquote>
+          <p>
+            Why is Tailwind removing the default styles on my <code>h1</code> elements? How do I
+            disable this? What do you mean I lose all the other base styles too?
+          </p>
+        </blockquote>
+      </div>
     </div>
   )
 }
