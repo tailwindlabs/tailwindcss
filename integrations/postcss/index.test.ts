cdadd637264a58e665713da1609a9c758d63330e
Philipp Spiess <hello@philippspiess.com>
2024-08-23 15:25:48 +0200

    Revert change to integrations/postcss/index.test.ts

--- integrations/postcss/index.test.ts
+++ integrations/postcss/index.test.ts
@@ -257,9 +257,11 @@ test(
     },
   },
   async ({ root, fs, spawn }) => {
-    await spawn('pnpm postcss src/index.css --output dist/out.css --watch --verbose', {
-      cwd: path.join(root, 'project-a'),
-    })
+    let process = await spawn(
+      'pnpm postcss src/index.css --output dist/out.css --watch --verbose',
+      { cwd: path.join(root, 'project-a') },
+    )
+    await process.onStderr((message) => message.includes('Waiting for file changes...'))
 
     await fs.expectFileToContain('project-a/dist/out.css', [
       candidate`underline`,
