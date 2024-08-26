19948a0c2830b8034bf0342a0b5e5d0c70ad76fd
Jordan Pittman <jordan@cryptica.me>
2024-08-26 11:56:18 -0400

    Add commented out test case for embedded files bug

--- packages/@tailwindcss-standalone/src/index.ts
+++ packages/@tailwindcss-standalone/src/index.ts
@@ -1,1 +1,5 @@
+// import { embeddedFiles } from 'bun'
+// console.log({ embeddedFiles })
+// console.log(require(`@parcel/watcher-${process.platform}-${process.arch}`))
+
 await import('../../@tailwindcss-cli/src/index.ts')
