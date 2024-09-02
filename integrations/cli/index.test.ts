4843b5e648b3651f5f4ddb13865b591e631f918b
Robin Malfait <malfait.robin@gmail.com>
2024-09-02 13:54:06 +0200

    add integration test for standalone CLI

--- integrations/cli/index.test.ts
+++ integrations/cli/index.test.ts
@@ -1,142 +1,165 @@
+import os from 'node:os'
 import path from 'node:path'
+import { describe } from 'vitest'
 import { candidate, css, html, js, json, test, yaml } from '../utils'
 
-test(
-  'production build',
-  {
-    fs: {
-      'package.json': json`{}`,
-      'pnpm-workspace.yaml': yaml`
-        #
-        packages:
-          - project-a
-      `,
-      'project-a/package.json': json`
-        {
-          "dependencies": {
-            "tailwindcss": "workspace:^",
-            "@tailwindcss/cli": "workspace:^"
+const STANDALONE_BINARY = (() => {
+  switch (os.platform()) {
+    case 'win32':
+      return 'tailwindcss-windows-x64.exe'
+    case 'darwin':
+      return os.arch() === 'x64' ? 'tailwindcss-macos-x64' : 'tailwindcss-macos-arm64'
+    case 'linux':
+      return os.arch() === 'x64' ? 'tailwindcss-linux-x64' : 'tailwindcss-linux-arm64'
+    default:
+      throw new Error(`Unsupported platform: ${os.platform()} ${os.arch()}`)
+  }
+})()
+
+describe.each([
+  ['CLI', 'pnpm tailwindcss'],
+  [
+    'Standalone CLI',
+    path.resolve(__dirname, `../../packages/@tailwindcss-standalone/dist/${STANDALONE_BINARY}`),
+  ],
+])('%s', (_, command) => {
+  test(
+    'production build',
+    {
+      fs: {
+        'package.json': json`{}`,
+        'pnpm-workspace.yaml': yaml`
+          #
+          packages:
+            - project-a
+        `,
+        'project-a/package.json': json`
+          {
+            "dependencies": {
+              "tailwindcss": "workspace:^",
+              "@tailwindcss/cli": "workspace:^"
+            }
           }
-        }
-      `,
-      'project-a/index.html': html`
-        <div
-          class="underline 2xl:font-bold hocus:underline inverted:flex"
-        ></div>
-      `,
-      'project-a/plugin.js': js`
-        module.exports = function ({ addVariant }) {
-          addVariant('inverted', '@media (inverted-colors: inverted)')
-          addVariant('hocus', ['&:focus', '&:hover'])
-        }
-      `,
-      'project-a/src/index.css': css`
-        @import 'tailwindcss/utilities';
-        @source '../../project-b/src/**/*.js';
-        @plugin '../plugin.js';
-      `,
-      'project-a/src/index.js': js`
-        const className = "content-['project-a/src/index.js']"
-        module.exports = { className }
-      `,
-      'project-b/src/index.js': js`
-        const className = "content-['project-b/src/index.js']"
-        module.exports = { className }
-      `,
-    },
-  },
-  async ({ root, fs, exec }) => {
-    await exec('pnpm tailwindcss --input src/index.css --output dist/out.css', {
-      cwd: path.join(root, 'project-a'),
-    })
-
-    await fs.expectFileToContain('project-a/dist/out.css', [
-      candidate`underline`,
-      candidate`content-['project-a/src/index.js']`,
-      candidate`content-['project-b/src/index.js']`,
-      candidate`inverted:flex`,
-      candidate`hocus:underline`,
-    ])
-  },
-)
-
-test(
-  'watch mode',
-  {
-    fs: {
-      'package.json': json`{}`,
-      'pnpm-workspace.yaml': yaml`
-        #
-        packages:
-          - project-a
-      `,
-      'project-a/package.json': json`
-        {
-          "dependencies": {
-            "tailwindcss": "workspace:^",
-            "@tailwindcss/cli": "workspace:^"
+        `,
+        'project-a/index.html': html`
+          <div
+            class="underline 2xl:font-bold hocus:underline inverted:flex"
+          ></div>
+        `,
+        'project-a/plugin.js': js`
+          module.exports = function ({ addVariant }) {
+            addVariant('inverted', '@media (inverted-colors: inverted)')
+            addVariant('hocus', ['&:focus', '&:hover'])
           }
-        }
-      `,
-      'project-a/index.html': html`
-        <div
-          class="underline 2xl:font-bold hocus:underline inverted:flex"
-        ></div>
-      `,
-      'project-a/plugin.js': js`
-        module.exports = function ({ addVariant }) {
-          addVariant('inverted', '@media (inverted-colors: inverted)')
-          addVariant('hocus', ['&:focus', '&:hover'])
-        }
-      `,
-      'project-a/src/index.css': css`
-        @import 'tailwindcss/utilities';
-        @source '../../project-b/src/**/*.js';
-        @plugin '../plugin.js';
-      `,
-      'project-a/src/index.js': js`
-        const className = "content-['project-a/src/index.js']"
-        module.exports = { className }
-      `,
-      'project-b/src/index.js': js`
-        const className = "content-['project-b/src/index.js']"
-        module.exports = { className }
-      `,
+        `,
+        'project-a/src/index.css': css`
+          @import 'tailwindcss/utilities';
+          @source '../../project-b/src/**/*.js';
+          @plugin '../plugin.js';
+        `,
+        'project-a/src/index.js': js`
+          const className = "content-['project-a/src/index.js']"
+          module.exports = { className }
+        `,
+        'project-b/src/index.js': js`
+          const className = "content-['project-b/src/index.js']"
+          module.exports = { className }
+        `,
+      },
     },
-  },
-  async ({ root, fs, spawn }) => {
-    await spawn('pnpm tailwindcss --input src/index.css --output dist/out.css --watch', {
-      cwd: path.join(root, 'project-a'),
-    })
+    async ({ root, fs, exec }) => {
+      await exec(`${command} --input src/index.css --output dist/out.css`, {
+        cwd: path.join(root, 'project-a'),
+      })
 
-    await fs.expectFileToContain('project-a/dist/out.css', [
-      candidate`underline`,
-      candidate`content-['project-a/src/index.js']`,
-      candidate`content-['project-b/src/index.js']`,
-      candidate`inverted:flex`,
-      candidate`hocus:underline`,
-    ])
+      await fs.expectFileToContain('project-a/dist/out.css', [
+        candidate`underline`,
+        candidate`content-['project-a/src/index.js']`,
+        candidate`content-['project-b/src/index.js']`,
+        candidate`inverted:flex`,
+        candidate`hocus:underline`,
+      ])
+    },
+  )
 
-    await fs.write(
-      'project-a/src/index.js',
-      js`
-        const className = "[.changed_&]:content-['project-a/src/index.js']"
-        module.exports = { className }
-      `,
-    )
-    await fs.expectFileToContain('project-a/dist/out.css', [
-      candidate`[.changed_&]:content-['project-a/src/index.js']`,
-    ])
+  test(
+    'watch mode',
+    {
+      fs: {
+        'package.json': json`{}`,
+        'pnpm-workspace.yaml': yaml`
+          #
+          packages:
+            - project-a
+        `,
+        'project-a/package.json': json`
+          {
+            "dependencies": {
+              "tailwindcss": "workspace:^",
+              "@tailwindcss/cli": "workspace:^"
+            }
+          }
+        `,
+        'project-a/index.html': html`
+          <div
+            class="underline 2xl:font-bold hocus:underline inverted:flex"
+          ></div>
+        `,
+        'project-a/plugin.js': js`
+          module.exports = function ({ addVariant }) {
+            addVariant('inverted', '@media (inverted-colors: inverted)')
+            addVariant('hocus', ['&:focus', '&:hover'])
+          }
+        `,
+        'project-a/src/index.css': css`
+          @import 'tailwindcss/utilities';
+          @source '../../project-b/src/**/*.js';
+          @plugin '../plugin.js';
+        `,
+        'project-a/src/index.js': js`
+          const className = "content-['project-a/src/index.js']"
+          module.exports = { className }
+        `,
+        'project-b/src/index.js': js`
+          const className = "content-['project-b/src/index.js']"
+          module.exports = { className }
+        `,
+      },
+    },
+    async ({ root, fs, spawn }) => {
+      await spawn(`${command} --input src/index.css --output dist/out.css --watch`, {
+        cwd: path.join(root, 'project-a'),
+      })
 
-    await fs.write(
-      'project-b/src/index.js',
-      js`
-        const className = "[.changed_&]:content-['project-b/src/index.js']"
-        module.exports = { className }
-      `,
-    )
-    await fs.expectFileToContain('project-a/dist/out.css', [
-      candidate`[.changed_&]:content-['project-b/src/index.js']`,
-    ])
-  },
-)
+      await fs.expectFileToContain('project-a/dist/out.css', [
+        candidate`underline`,
+        candidate`content-['project-a/src/index.js']`,
+        candidate`content-['project-b/src/index.js']`,
+        candidate`inverted:flex`,
+        candidate`hocus:underline`,
+      ])
+
+      await fs.write(
+        'project-a/src/index.js',
+        js`
+          const className = "[.changed_&]:content-['project-a/src/index.js']"
+          module.exports = { className }
+        `,
+      )
+      await fs.expectFileToContain('project-a/dist/out.css', [
+        candidate`[.changed_&]:content-['project-a/src/index.js']`,
+      ])
+
+      await fs.write(
+        'project-b/src/index.js',
+        js`
+          const className = "[.changed_&]:content-['project-b/src/index.js']"
+          module.exports = { className }
+        `,
+      )
+      await fs.expectFileToContain('project-a/dist/out.css', [
+        candidate`[.changed_&]:content-['project-b/src/index.js']`,
+      ])
+    },
+  )
+})
