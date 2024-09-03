0ccf9cb43456f86a6ab5454b6950c14d522871ff
Adam Wathan <adam.wathan@gmail.com>
2024-09-03 10:42:04 -0400

    Update packages/@tailwindcss-cli/src/commands/build/index.ts

--- packages/@tailwindcss-cli/src/commands/build/index.ts
+++ packages/@tailwindcss-cli/src/commands/build/index.ts
@@ -145,7 +145,7 @@ export async function handle(args: Result<ReturnType<typeof options>>) {
     detectSources: { base },
     sources: compiler.globs.map(({ origin, pattern }) => ({
       // Ensure the glob is relative to the input CSS file or the config file
-      // where they are specified.
+      // where it is specified.
       base: origin ? path.dirname(path.resolve(inputBasePath, origin)) : inputBasePath,
       pattern,
     })),
