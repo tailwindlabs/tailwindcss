2dd52f5a0f46db6981567d2f7bdaedf6d8c5b050
Philipp Spiess <hello@philippspiess.com>
2024-09-04 19:14:05 +0200

    Vite: Add support for `<style>` tags in Astro files (#14340)
    This works similar to the Vue setup. The styles that Astro will receive
    might still contain Tailwind CSS APIs but since it's not picky, we can
    pass that through to the regular Vite `transform` handlers for now.
    
    This, however, will have issues like
    https://github.com/tailwindlabs/tailwindcss/issues/14205. We have to fix
    this together with Vue and other similar extensions later. For now, it
    will break when syntax is used that lightningcss rewrites (like `@apply
    text-3xl/tight;`)
    
    ---------
    
    Co-authored-by: Jordan Pittman <jordan@cryptica.me>

---
+++ integrations/vite/astro.test.ts
@@ -0,0 +1,63 @@
+import { expect } from 'vitest'
+import { candidate, fetchStyles, html, json, retryAssertion, test, ts } from '../utils'
+
+test.debug(
+  'dev mode',
+  {
+    fs: {
+      'package.json': json`
+        {
+          "type": "module",
+          "dependencies": {
+            "astro": "^4.15.2",
+            "@tailwindcss/vite": "workspace:^",
+            "tailwindcss": "workspace:^"
+          }
+        }
+      `,
+      'astro.config.mjs': ts`
+        import tailwindcss from '@tailwindcss/vite'
+        import { defineConfig } from 'astro/config'
+
+        // https://astro.build/config
+        export default defineConfig({
+          vite: {
+            plugins: [tailwindcss()],
+          },
+        })
+      `,
+      'src/pages/index.astro': html`
+       <div class="underline">Hello, world!</div>
+
+        <style is:global>
+          @import 'tailwindcss';
+        </style>
+      `,
+    },
+  },
+  async ({ fs, spawn, getFreePort }) => {
+    let port = await getFreePort()
+    await spawn(`pnpm astro dev --port ${port}`)
+
+    await retryAssertion(async () => {
+      let css = await fetchStyles(port)
+      expect(css).toContain(candidate`underline`)
+    })
+
+    await fs.write(
+      'src/pages/index.astro',
+      html`
+        <div class="underline font-bold">Hello, world!</div>
+
+        <style is:global>
+          @import 'tailwindcss';
+        </style>
+      `,
+    )
+    await retryAssertion(async () => {
+      let css = await fetchStyles(port)
+      expect(css).toContain(candidate`underline`)
+      expect(css).toContain(candidate`font-bold`)
+    })
+  },
+)
