568af8fc404389e2cb586146085426a1bb975391
Philipp Spiess <hello@philippspiess.com>
2024-09-11 16:50:53 +0200

    Make it pretty

--- packages/tailwindcss/src/index.ts
+++ packages/tailwindcss/src/index.ts
@@ -509,5 +509,7 @@ function getVersion() {
 }
 
 export default function postcssPluginWarning() {
-  throw new Error(`It looks like you're trying to use \`tailwindcss\` directly as a PostCSS plugin. The PostCSS plugin has moved to a separate package, so to continue using Tailwind CSS with PostCSS you'll need to install \`@tailwindcss/postcss\` and update your PostCSS configuration.`)
+  throw new Error(
+    `It looks like you're trying to use \`tailwindcss\` directly as a PostCSS plugin. The PostCSS plugin has moved to a separate package, so to continue using Tailwind CSS with PostCSS you'll need to install \`@tailwindcss/postcss\` and update your PostCSS configuration.`,
+  )
 }
