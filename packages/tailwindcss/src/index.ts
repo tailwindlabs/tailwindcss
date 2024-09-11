c543d1d31fb13d5d1be8ba218182aa885bb8d2ee
Philipp Spiess <hello@philippspiess.com>
2024-09-11 16:37:38 +0200

    Update packages/tailwindcss/src/index.ts
    Co-authored-by: Adam Wathan <adam.wathan@gmail.com>

--- packages/tailwindcss/src/index.ts
+++ packages/tailwindcss/src/index.ts
@@ -509,7 +509,5 @@ function getVersion() {
 }
 
 export default function postcssPluginWarning() {
-  throw new Error(`It looks like you're trying to use the \`tailwindcss\` package as a PostCSS plugin. This is no longer possible since Tailwind CSS v4.
-
-If you want to continue to use Tailwind CSS with PostCSS, please install \`@tailwindcss/postcss\` and change your PostCSS config file.`)
+  throw new Error(`It looks like you're trying to use \`tailwindcss\` directly as a PostCSS plugin. The PostCSS plugin has moved to a separate package, so to continue using Tailwind CSS with PostCSS you'll need to install \`@tailwindcss/postcss\` and update your PostCSS configuration.`)
 }
