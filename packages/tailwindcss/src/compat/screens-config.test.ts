3c01e8368e4bb2514871812324f14d949a2a1191
Philipp Spiess <hello@philippspiess.com>
2024-09-13 13:33:50 +0200

    Clean up test

--- packages/tailwindcss/src/compat/screens-config.test.ts
+++ packages/tailwindcss/src/compat/screens-config.test.ts
@@ -3,7 +3,7 @@ import { compile } from '..'
 
 const css = String.raw
 
-test('merges css breakpoints with js config screens', async () => {
+test('merges CSS `--breakpoint-*` with JS config `screens`', async () => {
   let input = css`
     @theme default {
       --breakpoint-sm: 40rem;
@@ -31,36 +31,57 @@ test('merges css breakpoints with js config screens', async () => {
     }),
   })
 
-  expect(compiler.build(['sm:flex', 'md:flex', 'lg:flex', 'min-sm:max-md:underline']))
-    .toMatchInlineSnapshot(`
-      ":root {
-        --breakpoint-md: 50rem;
-        --breakpoint-lg: 64rem;
-        --breakpoint-xl: 80rem;
-        --breakpoint-2xl: 96rem;
+  expect(
+    compiler.build([
+      'sm:flex',
+      'md:flex',
+      'lg:flex',
+      'min-sm:max-md:underline',
+      'min-md:max-lg:underline',
+      // Ensure other core variants appear at the end
+      'print:items-end',
+    ]),
+  ).toMatchInlineSnapshot(`
+    ":root {
+      --breakpoint-md: 50rem;
+      --breakpoint-lg: 64rem;
+      --breakpoint-xl: 80rem;
+      --breakpoint-2xl: 96rem;
+    }
+    .sm\\:flex {
+      @media (width >= 44rem) {
+        display: flex;
       }
-      .sm\\:flex {
-        @media (width >= 44rem) {
-          display: flex;
+    }
+    .min-sm\\:max-md\\:underline {
+      @media (width >= 44rem) {
+        @media (width < 50rem) {
+          text-decoration-line: underline;
         }
       }
-      .min-sm\\:max-md\\:underline {
-        @media (width >= 44rem) {
-          @media (width < 50rem) {
-            text-decoration-line: underline;
-          }
+    }
+    .md\\:flex {
+      @media (width >= 50rem) {
+        display: flex;
+      }
+    }
+    .min-md\\:max-lg\\:underline {
+      @media (width >= 50rem) {
+        @media (width < 64rem) {
+          text-decoration-line: underline;
         }
       }
-      .md\\:flex {
-        @media (width >= 50rem) {
-          display: flex;
-        }
+    }
+    .lg\\:flex {
+      @media (width >= 64rem) {
+        display: flex;
       }
-      .lg\\:flex {
-        @media (width >= 64rem) {
-          display: flex;
-        }
+    }
+    .print\\:items-end {
+      @media print {
+        align-items: flex-end;
       }
-      "
-    `)
+    }
+    "
+  `)
 })
