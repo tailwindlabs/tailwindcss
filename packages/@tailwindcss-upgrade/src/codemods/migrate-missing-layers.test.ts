58b8f8d79084ba3c17b02d0329c89eaf485e4b75
Robin Malfait <malfait.robin@gmail.com>
2024-09-23 14:54:48 +0200

    implement missing layer migration

---
+++ packages/@tailwindcss-upgrade/src/codemods/migrate-missing-layers.test.ts
@@ -0,0 +1,65 @@
+import dedent from 'dedent'
+import postcss from 'postcss'
+import { expect, it } from 'vitest'
+import { formatNodes } from './format-nodes'
+import { migrateMissingLayers } from './migrate-missing-layers'
+
+const css = dedent
+
+function migrate(input: string) {
+  return postcss()
+    .use(migrateMissingLayers())
+    .use(formatNodes())
+    .process(input, { from: expect.getState().testPath })
+    .then((result) => result.css)
+}
+
+it('should migrate rules between tw directives', async () => {
+  expect(
+    await migrate(css`
+      @tailwind base;
+
+      .base {
+      }
+
+      @tailwind components;
+
+      .component-a {
+      }
+      .component-b {
+      }
+
+      @tailwind utilities;
+
+      .utility-a {
+      }
+      .utility-b {
+      }
+    `),
+  ).toMatchInlineSnapshot(`
+    "@tailwind base;
+
+    @layer base {
+      .base {
+      }
+    }
+
+    @tailwind components;
+
+    @layer components {
+      .component-a {
+      }
+      .component-b {
+      }
+    }
+
+    @tailwind utilities;
+
+    @layer utilities {
+      .utility-a {
+      }
+      .utility-b {
+      }
+    }"
+  `)
+})
