4be746c15d89e757d4d130e47037175980bd925e
Robin Malfait <malfait.robin@gmail.com>
2024-09-24 15:17:22 +0200

    add generic migration test

--- packages/@tailwindcss-upgrade/src/index.test.ts
+++ packages/@tailwindcss-upgrade/src/index.test.ts
@@ -26,3 +26,86 @@ it('should print the input as-is', async () => {
     }"
   `)
 })
+
+it('should migrate a stylesheet', async () => {
+  expect(
+    await migrateContents(css`
+      @import 'tailwindcss/base';
+      @import './my-base.css';
+
+      html {
+        overflow: hidden;
+      }
+
+      @import 'tailwindcss/components';
+      @import './my-components.css';
+
+      .a {
+        z-index: 1;
+      }
+
+      @layer components {
+        .b {
+          z-index: 2;
+        }
+      }
+
+      .c {
+        z-index: 3;
+      }
+
+      @import 'tailwindcss/utilities';
+      @import './my-utilities.css';
+
+      .d {
+        z-index: 4;
+      }
+
+      @layer utilities {
+        .e {
+          z-index: 5;
+        }
+      }
+    `),
+  ).toMatchInlineSnapshot(`
+    "
+
+    @import 'tailwindcss';
+
+    @import './my-base.css' layer(base);
+    @import './my-components.css' layer(components);
+    @import './my-utilities.css' layer(utilities);
+
+    @layer base {
+      html {
+        overflow: hidden;
+      }
+    }
+
+    @layer components {
+      .a {
+        z-index: 1;
+      }
+    }
+
+    @utility b {
+      z-index: 2;
+    }
+
+    @layer components {
+      .c {
+        z-index: 3;
+      }
+    }
+
+    @layer utilities {
+      .d {
+        z-index: 4;
+      }
+    }
+
+    @utility e {
+      z-index: 5;
+    }"
+  `)
+})
