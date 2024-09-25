0d4dfe0318f97fe820aab85a4af22f61ce15eed3
Philipp Spiess <hello@philippspiess.com>
2024-09-25 12:20:32 +0200

    Move upgrade tests to dedicated directory and add one for templates

--- packages/@tailwindcss-upgrade/src/fixtures/tailwind.config.js
+++
@@ -1,4 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{html,js}'],
-}
--- packages/@tailwindcss-upgrade/src/template/parseConfig.ts
+++ packages/@tailwindcss-upgrade/src/template/parseConfig.ts
@@ -1,17 +1,12 @@
 import { __unstable__loadDesignSystem, compile } from '@tailwindcss/node'
-import dedent from 'dedent'
 import type { DesignSystem } from '../../../tailwindcss/src/design-system'
 
-let css = dedent
 export async function parseConfig(
   path: string,
   options: { base: string },
 ): Promise<{ designSystem: DesignSystem; globs: { base: string; pattern: string }[] }> {
   // TODO: base path needs to be resolved to v4
-  let input = css`
-    @import 'tailwindcss';
-    @config './${path}';
-  `
+  let input = `@import 'tailwindcss';\n@config './${path}'`
 
   let [compiler, designSystem] = await Promise.all([
     compile(input, { ...options, onDependency: () => {} }),
