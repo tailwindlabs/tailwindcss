de8015fe1d6e350218ad6cc61980ffe92677e79a
Philipp Spiess <hello@philippspiess.com>
2024-09-25 15:39:38 +0200

    Add a test case for already-migrated cases

--- packages/@tailwindcss-upgrade/src/template/codemods/migrate-important.test.ts
+++ packages/@tailwindcss-upgrade/src/template/codemods/migrate-important.test.ts
@@ -8,7 +8,7 @@ let html = dedent
 
 test('applies the migration', async () => {
   let content = html`
-    <div class="bg-blue-500 !flex">
+    <div class="bg-blue-500 !flex min-[calc(1000px+12em)]:!flex">
       <button class="md:!block">My button</button>
     </div>
   `
@@ -18,8 +18,20 @@ test('applies the migration', async () => {
   })
 
   expect(migrate(designSystem, content, [migrateImportant])).resolves.toMatchInlineSnapshot(`
-    "<div class="bg-blue-500 flex!">
+    "<div class="bg-blue-500 flex! min-[calc(1000px_+_12em)]:flex!">
       <button class="md:block!">My button</button>
     </div>"
+  `)
+})
+
+test('does not migrate if the exclamation mark is already at the end', async () => {
+  let content = html` <div class="min-[calc(1000px+12em)]:flex!"></div> `
+
+  let designSystem = await __unstable__loadDesignSystem('@import "tailwindcss";', {
+    base: __dirname,
+  })
+
+  expect(migrate(designSystem, content, [migrateImportant])).resolves.toMatchInlineSnapshot(`
+    "<div class="min-[calc(1000px+12em)]:flex!"></div>"
   `)
 })
