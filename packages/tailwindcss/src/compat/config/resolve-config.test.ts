9bf38b15713fe09a273d0d770aadfa25608d910f
Philipp Spiess <hello@philippspiess.com>
2024-10-11 17:48:47 +0200

    Apply suggestions from code review
    Co-authored-by: Adam Wathan <adam.wathan@gmail.com>

--- packages/tailwindcss/src/compat/config/resolve-config.test.ts
+++ packages/tailwindcss/src/compat/config/resolve-config.test.ts
@@ -224,11 +224,11 @@ test('theme keys can read from the CSS theme', () => {
       },
       caretColor: {
         primary: {
+          '50': '#f0fdf4',
           '100': '#dcfce7',
           '200': '#bbf7d0',
           '300': '#86efac',
           '400': '#4ade80',
-          '50': '#f0fdf4',
           '500': '#22c55e',
           '600': '#16a34a',
           '700': '#15803d',
