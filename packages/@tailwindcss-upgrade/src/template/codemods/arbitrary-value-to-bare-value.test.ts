e32e6849b351fc8055ca8cae2af1ddf0dd6cc320
Robin Malfait <malfait.robin@gmail.com>
2024-10-15 00:48:44 +0200

    convert arbitrary values to bare values

--- packages/@tailwindcss-upgrade/src/template/codemods/arbitrary-value-to-bare-value.test.ts
+++ packages/@tailwindcss-upgrade/src/template/codemods/arbitrary-value-to-bare-value.test.ts
@@ -3,6 +3,23 @@ import { expect, test } from 'vitest'
 import { arbitraryValueToBareValue } from './arbitrary-value-to-bare-value'
 
 test.each([
+  ['aspect-[12/34]', 'aspect-12/34'],
+  ['aspect-[1.2/34]', 'aspect-[1.2/34]'],
+  ['col-start-[7]', 'col-start-7'],
+  ['flex-[2]', 'flex-2'], // `flex` is implemented as static and functional utilities
+
+  // Only 50-200% (inclusive) are valid:
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/font-stretch#percentage
+  ['font-stretch-[50%]', 'font-stretch-50%'],
+  ['font-stretch-[201%]', 'font-stretch-[201%]'],
+  ['font-stretch-[49%]', 'font-stretch-[49%]'],
+  // Should stay as-is
+  ['font-stretch-[1/2]', 'font-stretch-[1/2]'],
+
+  // This test in itself is a bit flawed because `text-[1/2]` currently
+  // generates something. Converting it to `text-1/2` doesn't produce anything.
+  ['text-[1/2]', 'text-[1/2]'],
+
   ['data-[selected]:flex', 'data-selected:flex'],
   ['data-[foo=bar]:flex', 'data-[foo=bar]:flex'],
 
@@ -22,6 +39,10 @@ test.each([
   ['group-has-aria-[selected]:flex', 'group-has-aria-[selected]:flex'],
 
   ['max-lg:hover:data-[selected]:flex!', 'max-lg:hover:data-selected:flex!'],
+  [
+    'data-[selected]:aria-[selected="true"]:aspect-[12/34]',
+    'data-selected:aria-selected:aspect-12/34',
+  ],
 ])('%s => %s', async (candidate, result) => {
   let designSystem = await __unstable__loadDesignSystem('@import "tailwindcss";', {
     base: __dirname,
