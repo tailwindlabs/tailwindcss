4e219dc97d1b459df9d3c68661992bda6139185f
Philipp Spiess <hello@philippspiess.com>
2024-10-14 15:46:12 +0200

    Revert "Warn on use of plugin parameters as function" (#14662)
    Reverts tailwindlabs/tailwindcss#14661

--- packages/tailwindcss/src/compat/config/resolve-config.ts
+++ packages/tailwindcss/src/compat/config/resolve-config.ts
@@ -175,25 +175,12 @@ function extractConfigs(ctx: ResolutionContext, { config, base, path }: ConfigFi
   ctx.configs.push(config)
 }
 
-let didWarnAboutUsingObjectArgumentAsThemeFn = false
-
 function mergeTheme(ctx: ResolutionContext) {
   let themeFn = createThemeFn(ctx.design, () => ctx.theme, resolveValue)
-  let theme = Object.assign(
-    (path: string, defaultValue?: any) => {
-      if (!didWarnAboutUsingObjectArgumentAsThemeFn) {
-        didWarnAboutUsingObjectArgumentAsThemeFn = true
-        console.warn(
-          'Using the plugin object parameter as the theme function is deprecated. Please use the `theme` property instead.',
-        )
-      }
-      return themeFn(path, defaultValue)
-    },
-    {
-      theme: themeFn,
-      colors,
-    },
-  )
+  let theme = Object.assign(themeFn, {
+    theme: themeFn,
+    colors,
+  })
 
   function resolveValue(value: ThemeValue | null | undefined): ResolvedThemeValue {
     if (typeof value === 'function') {
