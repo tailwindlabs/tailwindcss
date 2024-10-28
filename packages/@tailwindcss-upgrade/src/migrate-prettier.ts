6eb77b19046046cc82afdcf4b8e325b28e5eb7a8
Robin Malfait <malfait.robin@gmail.com>
2024-10-28 14:42:44 +0100

    ensure we bump `prettier-plugin-tailwindcss` to the latest version

---
+++ packages/@tailwindcss-upgrade/src/migrate-prettier.ts
@@ -0,0 +1,15 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import { pkg } from './utils/packages'
+import { success } from './utils/renderer'
+
+export async function migratePrettierPlugin(base: string) {
+  let packageJsonPath = path.resolve(base, 'package.json')
+  try {
+    let packageJson = await fs.readFile(packageJsonPath, 'utf-8')
+    if (packageJson.includes('prettier-plugin-tailwindcss')) {
+      await pkg('add prettier-plugin-tailwindcss@latest', base)
+      success(`Prettier plugin migrated to latest version.`)
+    }
+  } catch {}
+}
