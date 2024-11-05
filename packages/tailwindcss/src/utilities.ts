4013c471ae8f6562fea58945846b97fa6acbd300
Adam Wathan <4323180+adamwathan@users.noreply.github.com>
2024-11-05 12:05:41 -0500

    Suggest original spacing scale by default

--- packages/tailwindcss/src/utilities.ts
+++ packages/tailwindcss/src/utilities.ts
@@ -396,8 +396,9 @@ export function createUtilities(theme: Theme) {
       if (!supportsNegative && candidate.negative) return
       return handle(candidate.negative ? '-1px' : '1px')
     })
+    let themeKeys = ([] as ThemeKey[]).concat(themeNamespace, '--spacing')
     functionalUtility(name, {
-      themeKeys: ([] as ThemeKey[]).concat(themeNamespace, '--spacing'),
+      themeKeys,
       ignoredThemeKeys,
       supportsFractions,
       supportsNegative,
@@ -417,6 +418,50 @@ export function createUtilities(theme: Theme) {
       },
       handle,
     })
+
+    suggest(name, () => [
+      {
+        values: [
+          '0',
+          '0.5',
+          '1',
+          '1.5',
+          '2',
+          '2.5',
+          '3',
+          '3.5',
+          '4',
+          '5',
+          '6',
+          '7',
+          '8',
+          '9',
+          '10',
+          '11',
+          '12',
+          '14',
+          '16',
+          '20',
+          '24',
+          '28',
+          '32',
+          '36',
+          '40',
+          '44',
+          '48',
+          '52',
+          '56',
+          '60',
+          '64',
+          '72',
+          '80',
+          '96',
+        ],
+        supportsNegative,
+        valueThemeKeys: themeKeys,
+        ignoredThemeKeys: ignoredThemeKeys ?? [],
+      },
+    ])
   }
 
   /**
