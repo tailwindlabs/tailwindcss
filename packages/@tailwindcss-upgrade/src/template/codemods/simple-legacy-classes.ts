bbc74221f9b711fde3c79d0df4a2bbc87fecfd95
Robin Malfait <malfait.robin@gmail.com>
2024-11-12 16:16:53 +0100

    only migrate suffix-less if it's safe to do so

--- packages/@tailwindcss-upgrade/src/template/codemods/simple-legacy-classes.ts
+++ packages/@tailwindcss-upgrade/src/template/codemods/simple-legacy-classes.ts
@@ -1,6 +1,12 @@
+import { __unstable__loadDesignSystem } from '@tailwindcss/node'
+import path from 'node:path'
+import url from 'node:url'
 import type { Config } from 'tailwindcss'
 import type { DesignSystem } from '../../../../tailwindcss/src/design-system'
 import { printCandidate } from '../candidates'
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // Classes that used to exist in Tailwind CSS v3, but do not exist in Tailwind
 // CSS v4 anymore.
@@ -35,27 +41,30 @@ const LEGACY_CLASS_MAP = {
 }
 
 const THEME_KEYS = {
-  shadow: 'boxShadow',
-  'shadow-sm': 'boxShadow',
-  'shadow-xs': 'boxShadow',
+  shadow: '--shadow',
+  'shadow-sm': '--shadow-sm',
+  'shadow-xs': '--shadow-xs',
+  'shadow-2xs': '--shadow-2xs',
 
-  'drop-shadow': 'dropShadow',
-  'drop-shadow-sm': 'dropShadow',
+  'drop-shadow': '--drop-shadow',
+  'drop-shadow-sm': '--drop-shadow-sm',
 
-  rounded: 'borderRadius',
-  'rounded-sm': 'borderRadius',
+  rounded: '--radius',
+  'rounded-sm': '--radius-sm',
+  'rounded-xs': '--radius-xs',
 
-  blur: 'blur',
-  'blur-sm': 'blur',
+  blur: '--blur',
+  'blur-sm': '--blur-sm',
+  'blur-xs': '--blur-xs',
 }
 
 const SEEDED = new WeakSet<DesignSystem>()
 
-export function simpleLegacyClasses(
+export async function simpleLegacyClasses(
   designSystem: DesignSystem,
-  userConfig: Config,
+  _userConfig: Config,
   rawCandidate: string,
-): string {
+): Promise<string> {
   // Prepare design system with the unknown legacy classes
   if (!SEEDED.has(designSystem)) {
     for (let old in LEGACY_CLASS_MAP) {
@@ -64,17 +73,42 @@ export function simpleLegacyClasses(
     SEEDED.add(designSystem)
   }
 
+  let defaultDesignSystem = await __unstable__loadDesignSystem('@import "tailwindcss";', {
+    base: __dirname,
+  })
+
   for (let candidate of designSystem.parseCandidate(rawCandidate)) {
     if (candidate.kind === 'static' && Object.hasOwn(LEGACY_CLASS_MAP, candidate.root)) {
-      let themeKey = THEME_KEYS[candidate.root as keyof typeof THEME_KEYS]
-      if (
-        themeKey &&
-        // Theme was overwritten
-        (userConfig?.theme?.[themeKey] !== undefined ||
-          // Theme extend was overwritten
-          userConfig?.theme?.extend?.[themeKey] !== undefined)
-      ) {
-        continue
+      let fromThemeKey = THEME_KEYS[candidate.root as keyof typeof THEME_KEYS]
+      let toThemeKey =
+        THEME_KEYS[
+          LEGACY_CLASS_MAP[
+            candidate.root as keyof typeof LEGACY_CLASS_MAP
+          ] as keyof typeof THEME_KEYS
+        ]
+
+      // Migrating something that resolves to a value in the theme.
+      if (fromThemeKey && toThemeKey) {
+        let customFrom = designSystem.resolveThemeValue(fromThemeKey)
+        let defaultFrom = defaultDesignSystem.resolveThemeValue(fromThemeKey)
+        let customTo = designSystem.resolveThemeValue(toThemeKey)
+        let defaultTo = defaultDesignSystem.resolveThemeValue(toThemeKey)
+
+        // The new theme value is not defined, which means we can't safely migrate
+        // the utility.
+        if (customTo === undefined) {
+          continue
+        }
+
+        // The "from" theme value changed compared to the default theme value.
+        if (customFrom !== defaultFrom) {
+          continue
+        }
+
+        // The "to" theme value changed compared to the default theme value.
+        if (customTo !== defaultTo) {
+          continue
+        }
       }
 
       return printCandidate(designSystem, {
