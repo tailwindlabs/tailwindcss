6bbb4e04febc996515fd7b0bce741bc5c8a9ed9a
Robin Malfait <malfait.robin@gmail.com>
2024-11-19 16:49:50 +0100

    try `esmResolver`, fallback to `cjsResolver`

--- packages/@tailwindcss-upgrade/src/utils/resolve.ts
+++ packages/@tailwindcss-upgrade/src/utils/resolve.ts
@@ -13,22 +13,26 @@ export function resolve(id: string) {
   return localResolve(id)
 }
 
-const jsResolver = EnhancedResolve.ResolverFactory.createResolver({
+const esmResolver = EnhancedResolve.ResolverFactory.createResolver({
   fileSystem: new EnhancedResolve.CachedInputFileSystem(fs, 4000),
   useSyncFileSystemCalls: true,
   extensions: ['.js', '.json', '.node', '.ts'],
-  conditionNames: import.meta.url ? ['node', 'import'] : ['node', 'require'],
+  conditionNames: ['node', 'import'],
+})
+
+const cjsResolver = EnhancedResolve.ResolverFactory.createResolver({
+  fileSystem: new EnhancedResolve.CachedInputFileSystem(fs, 4000),
+  useSyncFileSystemCalls: true,
+  extensions: ['.js', '.json', '.node', '.ts'],
+  conditionNames: ['node', 'require'],
 })
 
 export function resolveJsId(id: string, base: string) {
-  if (typeof globalThis.__tw_resolve === 'function') {
-    let resolved = globalThis.__tw_resolve(id, base)
-    if (resolved) {
-      return resolved
-    }
+  try {
+    return esmResolver.resolveSync({}, base, id)
+  } catch {
+    return cjsResolver.resolveSync({}, base, id)
   }
-
-  return jsResolver.resolveSync({}, base, id)
 }
 
 const resolver = EnhancedResolve.ResolverFactory.createResolver({
@@ -39,12 +43,5 @@ const resolver = EnhancedResolve.ResolverFactory.createResolver({
   conditionNames: ['style'],
 })
 export function resolveCssId(id: string, base: string) {
-  if (typeof globalThis.__tw_resolve === 'function') {
-    let resolved = globalThis.__tw_resolve(id, base)
-    if (resolved) {
-      return resolved
-    }
-  }
-
   return resolver.resolveSync({}, base, id)
 }
