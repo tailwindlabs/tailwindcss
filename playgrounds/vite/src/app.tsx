c2ff6d4000d16f33bb310b0699ad13c0caa5746d
Adam Wathan <4323180+adamwathan@users.noreply.github.com>
2024-11-22 08:33:43 -0500

    Use configured letter-spacing values for custom font-size utilities

--- playgrounds/vite/src/app.tsx
+++ playgrounds/vite/src/app.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
     <div className="m-3 p-3 border">
-      <h1 className="text-blue-500">Hello World</h1>
+      <h1 className="text-heading3xl">Hello World</h1>
       <div className="-inset-x-full -inset-y-full -space-x-full -space-y-full -inset-full"></div>
     </div>
   )
