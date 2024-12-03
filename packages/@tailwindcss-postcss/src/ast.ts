29cc49d7c10aa5086da34ddfaf2bfb1b8b02418f
Robin Malfait <malfait.robin@gmail.com>
2024-12-03 13:32:39 +0100

    implement `postCssAstToCssAst` and `cssAstToPostCssAst`
    These methods can convert PostCSS ASTs to our internal CSS ASTs and vice
    versa. This allows us to skip a step where introduce
    parsing/stringification.
    
    Instead of:
    - `PostCSS AST` -> `.toString()` -> `postcss.parse` -> `CSS AST`
    - `CSS AST` -> `toCSS(ast)` -> `CSS.parse` -> `PostCSS AST`
    
    We will now do this instead:
    - `PostCSS AST` -> `transform(…)` -> `CSS AST`
    - `CSS AST` -> `transform(…)` -> `PostCSS AST`

---
+++ packages/@tailwindcss-postcss/src/ast.ts
@@ -0,0 +1,98 @@
+import postcss, {
+  type ChildNode as PostCssChildNode,
+  type Container as PostCssContainerNode,
+  type Root as PostCssRoot,
+} from 'postcss'
+import { atRule, comment, decl, rule, type AstNode } from '../../tailwindcss/src/ast'
+
+export function cssAstToPostCssAst(ast: AstNode[]): PostCssRoot {
+  let root = postcss.root()
+
+  function transform(node: AstNode, parent: PostCssContainerNode) {
+    // Declaration
+    if (node.kind === 'declaration') {
+      parent.append(
+        postcss.decl({ prop: node.property, value: node.value ?? '', important: node.important }),
+      )
+    }
+
+    // Rule
+    else if (node.kind === 'rule') {
+      let astNode = postcss.rule({ selector: node.selector })
+      parent.append(astNode)
+      for (let child of node.nodes) {
+        transform(child, astNode)
+      }
+    }
+
+    // AtRule
+    else if (node.kind === 'at-rule') {
+      let astNode = postcss.atRule({ name: node.name.slice(1), params: node.params })
+      parent.append(astNode)
+      for (let child of node.nodes) {
+        transform(child, astNode)
+      }
+    }
+
+    // Comment
+    else if (node.kind === 'comment') {
+      parent.append(postcss.comment({ text: node.value }))
+    }
+
+    // AtRoot & Context should not happen
+    else if (node.kind === 'at-root' || node.kind === 'context') {
+    }
+
+    // Unknown
+    else {
+      node satisfies never
+    }
+  }
+
+  for (let node of ast) {
+    transform(node, root)
+  }
+
+  return root
+}
+
+export function postCssAstToCssAst(root: PostCssRoot): AstNode[] {
+  function transform(
+    node: PostCssChildNode,
+    parent: Extract<AstNode, { nodes: AstNode[] }>['nodes'],
+  ) {
+    // Declaration
+    if (node.type === 'decl') {
+      parent.push(decl(node.prop, node.value, node.important))
+    }
+
+    // Rule
+    else if (node.type === 'rule') {
+      let astNode = rule(node.selector)
+      node.each((child) => transform(child, astNode.nodes))
+      parent.push(astNode)
+    }
+
+    // AtRule
+    else if (node.type === 'atrule') {
+      let astNode = atRule(`@${node.name}`, node.params)
+      node.each((child) => transform(child, astNode.nodes))
+      parent.push(astNode)
+    }
+
+    // Comment
+    else if (node.type === 'comment') {
+      parent.push(comment(node.text))
+    }
+
+    // Unknown
+    else {
+      node satisfies never
+    }
+  }
+
+  let ast: AstNode[] = []
+  root.each((node) => transform(node, ast))
+
+  return ast
+}
