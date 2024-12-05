c63c84a380403bc9fac77a6594a7da0912e44181
Robin Malfait <malfait.robin@gmail.com>
2024-12-05 17:44:05 +0100

    remove `getFreePort`

--- integrations/vitest.config.ts
+++ integrations/vitest.config.ts
@@ -1,9 +1,8 @@
-import { defineProject } from 'vitest/config'
+import {defineProject} from 'vitest/config'
 
 export default defineProject({
   test: {
     hideSkippedTests: true,
     bail: 1,
-    globalSetup: './vitest.global-setup.ts',
   },
 })
