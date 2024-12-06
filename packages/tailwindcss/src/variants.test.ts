0bf261f3a957d3711f2250a8d9c3a7c2540f0737
Luke Warlow <luke@warlow.dev>
2024-12-06 11:26:43 +0000

    Add `details-content` variant
    This matches the new `::details-content` pseudo element.

--- packages/tailwindcss/src/variants.test.ts
+++ packages/tailwindcss/src/variants.test.ts
@@ -100,6 +100,15 @@ test('backdrop', async () => {
   expect(await run(['backdrop/foo:flex'])).toEqual('')
 })
 
+test('details-content', async () => {
+  expect(await run(['details-content:flex'])).toMatchInlineSnapshot(`
+    ".details-content\\:flex::details-content {
+      display: flex;
+    }"
+  `)
+  expect(await run(['details-content/foo:flex'])).toEqual('')
+})
+
 test('before', async () => {
   expect(await run(['before:flex'])).toMatchInlineSnapshot(`
     ".before\\:flex:before {
@@ -2099,6 +2108,7 @@ test('variant order', async () => {
         'data-custom:flex',
         'data-[custom=true]:flex',
         'default:flex',
+        'details-content:flex',
         'disabled:flex',
         'empty:flex',
         'enabled:flex',
@@ -2182,6 +2192,10 @@ test('variant order', async () => {
     }
 
     .placeholder\\:flex::placeholder, .backdrop\\:flex::backdrop {
+      display: flex;
+    }
+
+    .details-content\\:flex::details-content {
       display: flex;
     }
 
