f4d47dcaacdeafe435bf6cd502b081ee5fa2e000
Robin Malfait <malfait.robin@gmail.com>
2024-12-09 16:37:04 +0100

    inline sequential tests

--- integrations/vite/multi-root.test.ts
+++ integrations/vite/multi-root.test.ts
@@ -1,4 +1,4 @@
-import {candidate, css, fetchStyles, html, json, retryAssertion, test, ts} from '../utils'
+import { candidate, css, html, json, test, ts } from '../utils'
 
 test(
   `production build`,
@@ -64,7 +64,7 @@ test(
       `,
     },
   },
-  async ({fs, exec, expect}) => {
+  async ({ fs, exec, expect }) => {
     await exec('pnpm vite build')
 
     let files = await fs.glob('dist/**/*.css')
@@ -83,90 +83,3 @@ test(
     expect(root2![1]).toContain(candidate`two:underline`)
   },
 )
-
-export function sequentials() {
-  test.skip(
-    'dev mode',
-    {
-      fs: {
-        'package.json': json`
-          {
-            "type": "module",
-            "dependencies": {
-              "@tailwindcss/vite": "workspace:^",
-              "tailwindcss": "workspace:^"
-            },
-            "devDependencies": {
-              "vite": "^6"
-            }
-          }
-        `,
-        'vite.config.ts': ts`
-          import tailwindcss from '@tailwindcss/vite'
-          import path from 'node:path'
-          import { defineConfig } from 'vite'
-
-          export default defineConfig({
-            build: { cssMinify: false },
-            plugins: [tailwindcss()],
-          })
-        `,
-        'root1.html': html`
-          <head>
-            <link rel="stylesheet" href="./src/root1.css" />
-          </head>
-          <body>
-            <div class="one:underline two:underline">Hello, world!</div>
-          </body>
-        `,
-        'src/shared.css': css`
-          @import 'tailwindcss/theme' theme(reference);
-          @import 'tailwindcss/utilities';
-        `,
-        'src/root1.css': css`
-          @import './shared.css';
-          @variant one (&:is([data-root='1']));
-        `,
-        'root2.html': html`
-          <head>
-            <link rel="stylesheet" href="./src/root2.css" />
-          </head>
-          <body>
-            <div class="one:underline two:underline">Hello, world!</div>
-          </body>
-        `,
-        'src/root2.css': css`
-          @import './shared.css';
-          @variant two (&:is([data-root='2']));
-        `,
-      },
-    },
-    async ({spawn, expect}) => {
-      let process = await spawn('pnpm vite dev')
-      await process.onStdout((m) => m.includes('ready in'))
-
-      let url = ''
-      await process.onStdout((m) => {
-        let match = /Local:\s*(http.*)\//.exec(m)
-        if (match) url = match[1]
-        return Boolean(url)
-      })
-
-      // Candidates are resolved lazily, so the first visit of index.html
-      // will only have candidates from this file.
-      await retryAssertion(async () => {
-        let styles = await fetchStyles(url, '/root1.html')
-        expect(styles).toContain(candidate`one:underline`)
-        expect(styles).not.toContain(candidate`two:underline`)
-      })
-
-      // Going to about.html will extend the candidate list to include
-      // candidates from about.html.
-      await retryAssertion(async () => {
-        let styles = await fetchStyles(url, '/root2.html')
-        expect(styles).not.toContain(candidate`one:underline`)
-        expect(styles).toContain(candidate`two:underline`)
-      })
-    },
-  )
-}
