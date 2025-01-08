9a3d16fb502d80bc6e7794831a4ad9adfc87cfde
Robin Malfait <malfait.robin@gmail.com>
2025-01-08 21:33:46 +0100

    adjust static utility name regex

--- packages/tailwindcss/src/utilities.ts
+++ packages/tailwindcss/src/utilities.ts
@@ -24,7 +24,7 @@ import { replaceShadowColors } from './utils/replace-shadow-colors'
 import { segment } from './utils/segment'
 import * as ValueParser from './value-parser'
 
-const IS_VALID_STATIC_UTILITY_NAME = /^[a-z][a-zA-Z0-9/%._-]*$/
+const IS_VALID_STATIC_UTILITY_NAME = /^-?[a-z][a-zA-Z0-9/%._-]*$/
 const IS_VALID_FUNCTIONAL_UTILITY_NAME = /^-?[a-z][a-zA-Z0-9/%._-]*-\*$/
 
 type CompileFn<T extends Candidate['kind']> = (
