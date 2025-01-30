224294122b9743410dcb24d94ecf5d1468dde066
Robin Malfait <malfait.robin@gmail.com>
2025-01-30 16:46:29 +0100

    Refactor gradient implementation to work around prettier/prettier#17058 (#16072)
    This PR fixes an issue where tools like Prettier remove important
    trailing commas in CSS variables, making gradients invalid.
    
    We encoded the `,` in the `--tw-gradient-position` to ensure that _if_
    the `var(--tw-gradient-position)` is used, that the `,` was there. And
    if the variable was _not_ used that we didn't end up with a double `,,`
    rendering the gradient invalid.
    
    However, when running Prettier (there might be other tools that do this
    as well), the trailing comma in the `--tw-gradient-position` was removed
    which made the entire gradient invalid. E.g.:
    ```diff
      .bg-gradient-to-r {
    -   --tw-gradient-position: to right in oklab,;
    +   --tw-gradient-position: to right in oklab;
        background-image: linear-gradient(var(--tw-gradient-stops));
      }
    ```
    Notice how the `,` is removed.
    
    This PR fixes that, by moving the `,` to where the variable is being
    used. The only side effect is that we have to guarantee that the
    `--tw-gradient-position` is always present. In our testing (and using UI
    tests) this should be the case.
    
    Related Prettier issue:
    https://github.com/prettier/prettier/issues/17058
    
    Fixes: #16037

--- packages/tailwindcss/src/compat/legacy-utilities.test.ts
+++ packages/tailwindcss/src/compat/legacy-utilities.test.ts
@@ -22,42 +22,42 @@ test('bg-gradient-*', async () => {
     ),
   ).toMatchInlineSnapshot(`
     ".bg-gradient-to-b {
-      --tw-gradient-position: to bottom in oklab, ;
+      --tw-gradient-position: to bottom in oklab;
       background-image: linear-gradient(var(--tw-gradient-stops));
     }
 
     .bg-gradient-to-bl {
-      --tw-gradient-position: to bottom left in oklab, ;
+      --tw-gradient-position: to bottom left in oklab;
       background-image: linear-gradient(var(--tw-gradient-stops));
     }
 
     .bg-gradient-to-br {
-      --tw-gradient-position: to bottom right in oklab, ;
+      --tw-gradient-position: to bottom right in oklab;
       background-image: linear-gradient(var(--tw-gradient-stops));
     }
 
     .bg-gradient-to-l {
-      --tw-gradient-position: to left in oklab, ;
+      --tw-gradient-position: to left in oklab;
       background-image: linear-gradient(var(--tw-gradient-stops));
     }
 
     .bg-gradient-to-r {
-      --tw-gradient-position: to right in oklab, ;
+      --tw-gradient-position: to right in oklab;
       background-image: linear-gradient(var(--tw-gradient-stops));
     }
 
     .bg-gradient-to-t {
-      --tw-gradient-position: to top in oklab, ;
+      --tw-gradient-position: to top in oklab;
       background-image: linear-gradient(var(--tw-gradient-stops));
     }
 
     .bg-gradient-to-tl {
-      --tw-gradient-position: to top left in oklab, ;
+      --tw-gradient-position: to top left in oklab;
       background-image: linear-gradient(var(--tw-gradient-stops));
     }
 
     .bg-gradient-to-tr {
-      --tw-gradient-position: to top right in oklab, ;
+      --tw-gradient-position: to top right in oklab;
       background-image: linear-gradient(var(--tw-gradient-stops));
     }"
   `)
