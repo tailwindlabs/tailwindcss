35a5e8cb64fde0b8611ea1968ad25e5d2dafe544
Jordan Pittman <jordan@cryptica.me>
2025-01-31 09:53:02 -0500

    Discard invalid declarations when parsing CSS (#16093)
    I discovered this when triaging an error someone had on Tailwind Play.
    
    1. When we see a `;` we often assume a valid declaration precedes it but
    that may not be the case
    2. When we see the name of a custom property we assume everything that
    follows will be a valid declaration but that is not necessarily the case
    3. A bare identifier inside of a rule is treated as a declaration which
    is not the case
    
    This PR fixes all three of these by ignoring these invalid cases. Though
    some should probably be turned into errors.
    
    ---------
    
    Co-authored-by: Robin Malfait <malfait.robin@gmail.com>

--- packages/tailwindcss/src/css-parser.ts
+++ packages/tailwindcss/src/css-parser.ts
@@ -286,6 +286,8 @@ export function parse(input: string) {
       }
 
       let declaration = parseDeclaration(buffer, colonIdx)
+      if (!declaration) throw new Error(`Invalid custom property, expected a value`)
+
       if (parent) {
         parent.nodes.push(declaration)
       } else {
@@ -337,6 +339,11 @@ export function parse(input: string) {
       closingBracketStack[closingBracketStack.length - 1] !== ')'
     ) {
       let declaration = parseDeclaration(buffer)
+      if (!declaration) {
+        if (buffer.length === 0) throw new Error('Unexpected semicolon')
+        throw new Error(`Invalid declaration: \`${buffer.trim()}\``)
+      }
+
       if (parent) {
         parent.nodes.push(declaration)
       } else {
@@ -435,7 +442,10 @@ export function parse(input: string) {
 
           // Attach the declaration to the parent.
           if (parent) {
-            parent.nodes.push(parseDeclaration(buffer, colonIdx))
+            let node = parseDeclaration(buffer, colonIdx)
+            if (!node) throw new Error(`Invalid declaration: \`${buffer.trim()}\``)
+
+            parent.nodes.push(node)
           }
         }
       }
@@ -543,7 +553,11 @@ export function parseAtRule(buffer: string, nodes: AstNode[] = []): AtRule {
   return atRule(buffer.trim(), '', nodes)
 }
 
-function parseDeclaration(buffer: string, colonIdx: number = buffer.indexOf(':')): Declaration {
+function parseDeclaration(
+  buffer: string,
+  colonIdx: number = buffer.indexOf(':'),
+): Declaration | null {
+  if (colonIdx === -1) return null
   let importantIdx = buffer.indexOf('!important', colonIdx + 1)
   return decl(
     buffer.slice(0, colonIdx).trim(),
