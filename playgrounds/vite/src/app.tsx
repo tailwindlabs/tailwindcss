736439aa53e547933b8a6d4273a21236fd8fc652
Philipp Spiess <hello@philippspiess.com>
2025-02-03 14:09:50 +0100

    Revert playground changes

--- playgrounds/vite/src/app.tsx
+++ playgrounds/vite/src/app.tsx
@@ -2,7 +2,6 @@ export function App() {
   return (
     <div className="m-3 p-3 border">
       <h1 className="text-blue-500">Hello World</h1>
-      <div className="h-[var(--\_root-size)] text-[calc(0.2*var(--\_root-size))] [--Chip-decoratorChildHeight:calc(var(--\_Chip-minHeight)-0.25rem-2*var(--variant-borderWidth))] [--LinearProgress-progressRadius:max(var(--LinearProgress-radius)-var(--\_LinearProgress-padding),min(var(--\_LinearProgress-padding)/2,var(--LinearProgress-radius)/2))]"></div>
     </div>
   )
 }
