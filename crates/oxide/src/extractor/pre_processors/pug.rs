269cd62333bf09ef5eaf6d7911b0e3f2cea41f21
Robin Malfait <malfait.robin@gmail.com>
2025-03-06 11:30:52 +0100

    allow `.` in arbitrary values for Pug

--- crates/oxide/src/extractor/pre_processors/pug.rs
+++ crates/oxide/src/extractor/pre_processors/pug.rs
@@ -1,4 +1,5 @@
 use crate::cursor;
+use crate::extractor::bracket_stack::BracketStack;
 use crate::extractor::machine::Machine;
 use crate::extractor::pre_processors::pre_processor::PreProcessor;
 use crate::StringMachine;
@@ -12,6 +13,7 @@ impl PreProcessor for Pug {
         let mut result = content.to_vec();
         let mut cursor = cursor::Cursor::new(content);
         let mut string_machine = StringMachine;
+        let mut bracket_stack = BracketStack::default();
 
         while cursor.pos < len {
             match cursor.curr {
@@ -21,8 +23,16 @@ impl PreProcessor for Pug {
                 }
 
                 // Replace dots with spaces
-                b'.' => {
+                b'.' if bracket_stack.is_empty() => {
                     result[cursor.pos] = b' ';
+                }
+
+                b'(' | b'[' | b'{' => {
+                    bracket_stack.push(cursor.curr);
+                }
+
+                b')' | b']' | b'}' if !bracket_stack.is_empty() => {
+                    bracket_stack.pop(cursor.curr);
                 }
 
                 // Consume everything else
@@ -49,6 +59,11 @@ mod tests {
             (".flex.bg-red-500", " flex bg-red-500"),
             // Keep dots in strings
             (r#"div(class="px-2.5")"#, r#"div(class="px-2.5")"#),
+            // Nested brackets
+            (
+                "bg-[url(https://example.com/?q=[1,2])]",
+                "bg-[url(https://example.com/?q=[1,2])]",
+            ),
         ] {
             Pug::test(input, expected);
         }
