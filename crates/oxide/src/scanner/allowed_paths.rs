a5bc6cbdc2e890870a69261c37d1b643e61d618c
Robin Malfait <malfait.robin@gmail.com>
2025-03-15 00:07:03 +0100

    WIP

--- crates/oxide/src/scanner/allowed_paths.rs
+++ crates/oxide/src/scanner/allowed_paths.rs
@@ -1,5 +1,4 @@
 use ignore::gitignore::{Gitignore, GitignoreBuilder};
-use ignore::WalkBuilder;
 use std::{path::Path, sync};
 
 static BINARY_EXTENSIONS: sync::LazyLock<Vec<&'static str>> = sync::LazyLock::new(|| {
@@ -44,64 +43,6 @@ pub static AUTO_SOURCE_DETECTION_RULES: sync::LazyLock<Gitignore> = sync::LazyLo
 
     builder.build().unwrap()
 });
-
-pub fn create_walk_builder(root: &Path) -> WalkBuilder {
-    let mut builder = WalkBuilder::new(root);
-
-    // Scan hidden files / directories
-    builder.hidden(false);
-
-    // Don't respect global gitignore files
-    builder.git_global(false);
-
-    // By default, allow .gitignore files to be used regardless of whether or not
-    // a .git directory is present. This is an optimization for when projects
-    // are first created and may not be in a git repo yet.
-    builder.require_git(false);
-
-    // Don't descend into .git directories inside the root folder
-    // This is necessary when `root` contains the `.git` dir.
-    builder.filter_entry(|entry| entry.file_name() != ".git");
-
-    // If we are in a git repo then require it to ensure that only rules within
-    // the repo are used. For example, we don't want to consider a .gitignore file
-    // in the user's home folder if we're in a git repo.
-    //
-    // The alternative is using a call like `.parents(false)` but that will
-    // prevent looking at parent directories for .gitignore files from within
-    // the repo and that's not what we want.
-    //
-    // For example, in a project with this structure:
-    //
-    // home
-    // .gitignore
-    //  my-project
-    //   .gitignore
-    //   apps
-    //     .gitignore
-    //     web
-    //       {root}
-    //
-    // We do want to consider all .gitignore files listed:
-    // - home/.gitignore
-    // - my-project/.gitignore
-    // - my-project/apps/.gitignore
-    //
-    // However, if a repo is initialized inside my-project then only the following
-    // make sense for consideration:
-    // - my-project/.gitignore
-    // - my-project/apps/.gitignore
-    //
-    // Setting the require_git(true) flag conditionally allows us to do this.
-    for parent in root.ancestors() {
-        if parent.join(".git").exists() {
-            builder.require_git(true);
-            break;
-        }
-    }
-
-    builder
-}
 
 pub fn is_allowed_content_path(path: &Path) -> bool {
     // Skip known ignored files
