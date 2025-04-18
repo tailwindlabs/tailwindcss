ca467d399867373953c2740f081d50cfddbc3ad9
Jordan Pittman <jordan@cryptica.me>
2025-04-18 13:47:28 -0400

    wip

--- packages/tailwindcss/src/test-utils/run.ts
+++ packages/tailwindcss/src/test-utils/run.ts
@@ -7,12 +7,14 @@ export async function compileCss(
   options: Parameters<typeof compile>[1] = {},
 ) {
   let { build } = await compile(css, options)
-  return optimize(build(candidates)).trim()
+  return optimize(build(candidates)).code.trim()
 }
 
 export async function run(candidates: string[]) {
   let { build } = await compile('@tailwind utilities;')
-  return optimize(build(candidates)).trim()
+  return optimize(build(candidates)).code.trim()
 }
 
-export const optimizeCss = optimize
+export function optimizeCss(input: string) {
+  return optimize(input).code
+}
