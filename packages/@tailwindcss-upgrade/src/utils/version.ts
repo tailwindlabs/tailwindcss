2879f4ef6317476d7f8d5744a69a524b13166ee1
Robin Malfait <malfait.robin@gmail.com>
2025-04-18 11:32:12 +0200

    add `version` related helpers

---
+++ packages/@tailwindcss-upgrade/src/utils/version.ts
@@ -0,0 +1,22 @@
+import semver from 'semver'
+import { DefaultMap } from '../../../tailwindcss/src/utils/default-map'
+import { getPackageVersionSync } from './package-version'
+
+/**
+ * Must be of major version.
+ *
+ * E.g.: `isMajor(3)`
+ */
+export function isMajor(version: number) {
+  return semver.satisfies(installedTailwindVersion(), `>=${version}.0.0 <${version + 1}.0.0`)
+}
+
+let cache = new DefaultMap((base) => {
+  let tailwindVersion = getPackageVersionSync('tailwindcss', base)
+  if (!tailwindVersion) throw new Error('Tailwind CSS is not installed')
+  return tailwindVersion
+})
+
+function installedTailwindVersion(base = process.cwd()): string {
+  return cache.get(base)
+}
