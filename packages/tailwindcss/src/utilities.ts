2ae171f03066ff6780cf8289b3f343d6cfcf4bba
Philipp Spiess <hello@philippspiess.com>
2025-04-28 18:02:33 +0200

    Add discrete properties to the default list of `transition` properties

--- packages/tailwindcss/src/utilities.ts
+++ packages/tailwindcss/src/utilities.ts
@@ -4496,7 +4496,7 @@ export function createUtilities(theme: Theme) {
 
     functionalUtility('transition', {
       defaultValue:
-        'color, background-color, border-color, outline-color, text-decoration-color, fill, stroke, --tw-gradient-from, --tw-gradient-via, --tw-gradient-to, opacity, box-shadow, transform, translate, scale, rotate, filter, -webkit-backdrop-filter, backdrop-filter',
+        'color, background-color, border-color, outline-color, text-decoration-color, fill, stroke, --tw-gradient-from, --tw-gradient-via, --tw-gradient-to, opacity, box-shadow, transform, translate, scale, rotate, filter, -webkit-backdrop-filter, backdrop-filter, display, visibility, content-visibility, overlay, pointer-events',
       themeKeys: ['--transition-property'],
       handle: (value) => [
         decl('transition-property', value),
