5d8b626f111604caa1c596f4a05b85e6eb56d2fb
Robin Malfait <malfait.robin@gmail.com>
2025-05-01 20:50:34 +0200

    Update packages/@tailwindcss-upgrade/src/utils/dimension.ts
    Co-authored-by: Jordan Pittman <jordan@cryptica.me>

--- packages/@tailwindcss-upgrade/src/utils/dimension.ts
+++ packages/@tailwindcss-upgrade/src/utils/dimension.ts
@@ -2,7 +2,7 @@ import { DefaultMap } from '../../../tailwindcss/src/utils/default-map'
 
 // Parse a dimension such as `64rem` into `[64, 'rem']`.
 export const dimensions = new DefaultMap((input) => {
-  let match = /^(?<value>-?(\d*)?\.?\d+)(?<unit>.*)$/.exec(input)
+  let match = /^(?<value>-?(?:\d*\.)?\d+)(?<unit>[a-z]+|%)$/i.exec(input)
   if (!match) return null
 
   let value = match.groups?.value
