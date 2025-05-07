c9cdbc1f35443e49d88d011108ce2f526a5c5b8c
Robin Malfait <malfait.robin@gmail.com>
2025-05-07 16:09:08 +0200

    output where we are writing the log file

--- crates/oxide/src/scanner/mod.rs
+++ crates/oxide/src/scanner/mod.rs
@@ -38,17 +38,38 @@ static SHOULD_TRACE: sync::LazyLock<bool> = sync::LazyLock::new(
     || matches!(std::env::var("DEBUG"), Ok(value) if value.eq("*") || (value.contains("tailwindcss:oxide") && !value.contains("-tailwindcss:oxide"))),
 );
 
+fn dim(input: &str) -> String {
+    format!("\u{001b}[2m{input}\u{001b}[22m")
+}
+
+fn blue(input: &str) -> String {
+    format!("\u{001b}[34m{input}\u{001b}[39m")
+}
+
+fn highlight(input: &str) -> String {
+    format!("{}{}{}", dim(&blue("`")), blue(input), dim(&blue("`")))
+}
+
 fn init_tracing() {
     if !*SHOULD_TRACE {
         return;
     }
 
-    let file = format!("tailwindcss-{}.log", std::process::id());
+    let file_path = format!("tailwindcss-{}.log", std::process::id());
     let file = OpenOptions::new()
         .create(true)
         .append(true)
-        .open(&file)
-        .unwrap_or_else(|_| panic!("Failed to open {file}"));
+        .open(&file_path)
+        .unwrap_or_else(|_| panic!("Failed to open {file_path}"));
+
+    let file_path = Path::new(&file_path);
+    let absolute_file_path = dunce::canonicalize(file_path)
+        .unwrap_or_else(|_| panic!("Failed to canonicalize {file_path:?}"));
+    eprintln!(
+        "{} Writing debug info to: {}\n",
+        dim("[DEBUG]"),
+        highlight(absolute_file_path.as_path().to_str().unwrap())
+    );
 
     let file = Arc::new(Mutex::new(file));
 
