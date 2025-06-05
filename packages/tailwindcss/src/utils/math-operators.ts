1cd177f534a3140e446112a6708b6c824897d573
Rózsa Zoltán <67325669+rozsazoltan@users.noreply.github.com>
2025-06-05 11:52:21 +0200

    Update math-operators.ts

--- packages/tailwindcss/src/utils/math-operators.ts
+++ packages/tailwindcss/src/utils/math-operators.ts
@@ -125,7 +125,7 @@ export function addWhitespaceAroundMathOperators(input: string) {
         continue
       }
 
-      // If there's no number before or after it and it's not followed by a math function, don't add spaces.
+      // If there's no number/operator before or after it and it's not followed by a math function, don't add spaces
       else if (!/\d/.test(prev + next) && !/^[a-zA-Z_$][a-zA-Z0-9_$]*\(/.test(input.slice(i + 1)) && next !== '(' && next !== '+' && next !== '-') {
         result += char
         continue
