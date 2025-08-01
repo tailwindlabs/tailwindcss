cabd29f384b498eb3f04c76a9a1469e8f55b862a
Nikita <lisqxd@proton.me>
2025-08-01 15:51:33 +0200

    Update utilities.ts

--- packages/tailwindcss/src/utilities.ts
+++ packages/tailwindcss/src/utilities.ts
@@ -1006,11 +1006,11 @@ export function createUtilities(theme: Theme) {
 
   for (let [name, namespaces, property] of [
     ['w', ['--width', '--size', '--spacing', '--container'], 'width'],
-    ['min-w', ['--min-width', '--size', '--spacing', '--container'], 'min-width'],
-    ['max-w', ['--max-width', '--size', '--spacing', '--container'], 'max-width'],
+    ['min-w', ['--min-width', '--width', '--size', '--spacing', '--container'], 'min-width'],
+    ['max-w', ['--max-width', '--width', '--size', '--spacing', '--container'], 'max-width'],
     ['h', ['--height', '--size', '--spacing'], 'height'],
-    ['min-h', ['--min-height', '--size', '--height', '--spacing'], 'min-height'],
-    ['max-h', ['--max-height', '--size', '--height', '--spacing'], 'max-height'],
+    ['min-h', ['--min-height', '--height', '--size', '--spacing'], 'min-height'],
+    ['max-h', ['--max-height', '--height', '--size', '--spacing'], 'max-height'],
   ] as [string, ThemeKey[], string][]) {
     spacingUtility(name, namespaces, (value) => [decl(property, value)], {
       supportsFractions: true,
