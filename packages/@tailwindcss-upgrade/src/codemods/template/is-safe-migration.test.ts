5d9949030362390a8bd2fc8a73a5d5a050239202
Robin Malfait <malfait.robin@gmail.com>
2025-08-11 23:30:43 +0200

    refactor is-safe-migration test
    Use test table such that if a single test fails, all tests after still
    execute.

--- packages/@tailwindcss-upgrade/src/codemods/template/is-safe-migration.test.ts
+++ packages/@tailwindcss-upgrade/src/codemods/template/is-safe-migration.test.ts
@@ -1,15 +1,59 @@
 import { __unstable__loadDesignSystem } from '@tailwindcss/node'
-import { expect, test, vi } from 'vitest'
+import { describe, expect, test, vi } from 'vitest'
 import * as versions from '../../utils/version'
 import { migrateCandidate } from './migrate'
 vi.spyOn(versions, 'isMajor').mockReturnValue(true)
 
-test('does not replace classes in invalid positions', async () => {
+describe('is-safe-migration', async () => {
   let designSystem = await __unstable__loadDesignSystem('@import "tailwindcss";', {
     base: __dirname,
   })
 
-  async function shouldNotReplace(example: string, candidate = '!border') {
+  test.each([
+    [`let notBorder = !border    \n`, '!border'],
+    [`{ "foo": !border.something + ""}\n`, '!border'],
+    [`<div v-if="something && !border"></div>\n`, '!border'],
+    [`<div v-else-if="something && !border"></div>\n`, '!border'],
+    [`<div v-show="something && !border"></div>\n`, '!border'],
+    [`<div v-if="!border || !border"></div>\n`, '!border'],
+    [`<div v-else-if="!border || !border"></div>\n`, '!border'],
+    [`<div v-show="!border || !border"></div>\n`, '!border'],
+    [`<div v-if="!border"></div>\n`, '!border'],
+    [`<div v-else-if="!border"></div>\n`, '!border'],
+    [`<div v-show="!border"></div>\n`, '!border'],
+    [`<div x-if="!border"></div>\n`, '!border'],
+
+    [`let notShadow = shadow    \n`, 'shadow'],
+    [`{ "foo": shadow.something + ""}\n`, 'shadow'],
+    [`<div v-if="something && shadow"></div>\n`, 'shadow'],
+    [`<div v-else-if="something && shadow"></div>\n`, 'shadow'],
+    [`<div v-show="something && shadow"></div>\n`, 'shadow'],
+    [`<div v-if="shadow || shadow"></div>\n`, 'shadow'],
+    [`<div v-else-if="shadow || shadow"></div>\n`, 'shadow'],
+    [`<div v-show="shadow || shadow"></div>\n`, 'shadow'],
+    [`<div v-if="shadow"></div>\n`, 'shadow'],
+    [`<div v-else-if="shadow"></div>\n`, 'shadow'],
+    [`<div v-show="shadow"></div>\n`, 'shadow'],
+    [`<div x-if="shadow"></div>\n`, 'shadow'],
+    [`<div style={{filter: 'drop-shadow(30px 10px 4px #4444dd)'}}/>\n`, 'shadow'],
+
+    // Next.js Image placeholder cases
+    [`<Image placeholder="blur" src="/image.jpg" />`, 'blur'],
+    [`<Image placeholder={'blur'} src="/image.jpg" />`, 'blur'],
+    [`<Image placeholder={blur} src="/image.jpg" />`, 'blur'],
+
+    // https://github.com/tailwindlabs/tailwindcss/issues/17974
+    ['<div v-if="!duration">', '!duration'],
+    ['<div :active="!duration">', '!duration'],
+    ['<div :active="!visible">', '!visible'],
+
+    // Alpine/Livewire wire:…
+    ['<x-input.number required="foo" wire:model.blur="coins" />', 'blur'],
+
+    // Vue 3 events
+    [`emit('blur', props.modelValue)\n`, 'blur'],
+    [`$emit('blur', props.modelValue)\n`, 'blur'],
+  ])('does not replace classes in invalid positions #%#', async (example, candidate) => {
     expect(
       await migrateCandidate(designSystem, {}, candidate, {
         contents: example,
@@ -17,52 +61,5 @@ test('does not replace classes in invalid positions', async () => {
         end: example.indexOf(candidate) + candidate.length,
       }),
     ).toEqual(candidate)
-  }
-
-  await shouldNotReplace(`let notBorder = !border    \n`)
-  await shouldNotReplace(`{ "foo": !border.something + ""}\n`)
-  await shouldNotReplace(`<div v-if="something && !border"></div>\n`)
-  await shouldNotReplace(`<div v-else-if="something && !border"></div>\n`)
-  await shouldNotReplace(`<div v-show="something && !border"></div>\n`)
-  await shouldNotReplace(`<div v-if="!border || !border"></div>\n`)
-  await shouldNotReplace(`<div v-else-if="!border || !border"></div>\n`)
-  await shouldNotReplace(`<div v-show="!border || !border"></div>\n`)
-  await shouldNotReplace(`<div v-if="!border"></div>\n`)
-  await shouldNotReplace(`<div v-else-if="!border"></div>\n`)
-  await shouldNotReplace(`<div v-show="!border"></div>\n`)
-  await shouldNotReplace(`<div x-if="!border"></div>\n`)
-
-  await shouldNotReplace(`let notShadow = shadow    \n`, 'shadow')
-  await shouldNotReplace(`{ "foo": shadow.something + ""}\n`, 'shadow')
-  await shouldNotReplace(`<div v-if="something && shadow"></div>\n`, 'shadow')
-  await shouldNotReplace(`<div v-else-if="something && shadow"></div>\n`, 'shadow')
-  await shouldNotReplace(`<div v-show="something && shadow"></div>\n`, 'shadow')
-  await shouldNotReplace(`<div v-if="shadow || shadow"></div>\n`, 'shadow')
-  await shouldNotReplace(`<div v-else-if="shadow || shadow"></div>\n`, 'shadow')
-  await shouldNotReplace(`<div v-show="shadow || shadow"></div>\n`, 'shadow')
-  await shouldNotReplace(`<div v-if="shadow"></div>\n`, 'shadow')
-  await shouldNotReplace(`<div v-else-if="shadow"></div>\n`, 'shadow')
-  await shouldNotReplace(`<div v-show="shadow"></div>\n`, 'shadow')
-  await shouldNotReplace(`<div x-if="shadow"></div>\n`, 'shadow')
-  await shouldNotReplace(
-    `<div style={{filter: 'drop-shadow(30px 10px 4px #4444dd)'}}/>\n`,
-    'shadow',
-  )
-
-  // Next.js Image placeholder cases
-  await shouldNotReplace(`<Image placeholder="blur" src="/image.jpg" />`, 'blur')
-  await shouldNotReplace(`<Image placeholder={'blur'} src="/image.jpg" />`, 'blur')
-  await shouldNotReplace(`<Image placeholder={blur} src="/image.jpg" />`, 'blur')
-
-  // https://github.com/tailwindlabs/tailwindcss/issues/17974
-  await shouldNotReplace('<div v-if="!duration">', '!duration')
-  await shouldNotReplace('<div :active="!duration">', '!duration')
-  await shouldNotReplace('<div :active="!visible">', '!visible')
-
-  // Alpine/Livewire wire:…
-  await shouldNotReplace('<x-input.number required="foo" wire:model.blur="coins" />', 'blur')
-
-  // Vue 3 events
-  await shouldNotReplace(`emit('blur', props.modelValue)\n`, 'blur')
-  await shouldNotReplace(`$emit('blur', props.modelValue)\n`, 'blur')
+  })
 })
