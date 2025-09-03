a9ba89be656483e0ac2bd8dfcd45a9a8479e52d5
Robin Malfait <malfait.robin@gmail.com>
2025-09-03 12:52:26 +0200

    adjust variant name validation regex
    This will disallow variants starting with `-` (or existing on its own)

--- packages/tailwindcss/src/variants.ts
+++ packages/tailwindcss/src/variants.ts
@@ -18,7 +18,7 @@ import { DefaultMap } from './utils/default-map'
 import { isPositiveInteger } from './utils/infer-data-type'
 import { segment } from './utils/segment'
 
-export const IS_VALID_VARIANT_NAME = /^@?[a-zA-Z0-9_-]*$/
+export const IS_VALID_VARIANT_NAME = /^@?[a-z0-9][a-zA-Z0-9_-]*$/
 
 type VariantFn<T extends Variant['kind']> = (
   rule: Rule,
