b7c7e48c5d897d1b3d20c936e2baccc89b75f43e
Robin Malfait <malfait.robin@gmail.com>
2025-09-09 14:20:04 +0200

    Add `@container-size` utility (#18901)
    This PR adds a new `@container-size` utility instead of
    `@container-[size]`.
    
    The main reason we didn't do this before is because we only have
    container width related container queries, and not block based ones so
    we never needed `size` and `inline-size` was enough.
    
    However, `@container-size` is still useful if you are using container
    query related units such as `cqb` which are using the block size of the
    container not the inline size.
    
    I also added a little helper such that `@container-size` is only
    available in `insiders` and `4.2.0` (and later) so `4.1.x` releases
    won't have this utility yet. This will require some CHANGELOG changes
    such that we don't include this when releasing the next minor release.

--- packages/tailwindcss/src/utilities.ts
+++ packages/tailwindcss/src/utilities.ts
@@ -12,6 +12,7 @@ import {
 } from './ast'
 import type { Candidate, CandidateModifier, NamedUtilityValue } from './candidate'
 import type { DesignSystem } from './design-system'
+import { enableContainerSizeUtility } from './feature-flags'
 import type { Theme, ThemeKey } from './theme'
 import { compareBreakpoints } from './utils/compare-breakpoints'
 import { DefaultMap } from './utils/default-map'
@@ -5712,6 +5713,12 @@ export function createUtilities(theme: Theme) {
       value = candidate.value.value
     } else if (candidate.value.kind === 'named' && candidate.value.value === 'normal') {
       value = 'normal'
+    } else if (
+      enableContainerSizeUtility &&
+      candidate.value.kind === 'named' &&
+      candidate.value.value === 'size'
+    ) {
+      value = 'size'
     }
 
     if (value === null) return
