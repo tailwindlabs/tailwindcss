1456235fc654fd65475701d118c0c037a9258b86
Philipp Spiess <hello@philippspiess.com>
2025-09-16 18:03:19 +0200

    chore(node): fix napi artifacts flow for cli 3

--- crates/node/scripts/move-artifacts.mjs
+++ crates/node/scripts/move-artifacts.mjs
@@ -3,27 +3,15 @@ import path from 'node:path'
 import url from 'node:url'
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
-const root = path.resolve(__dirname, '..')
-const tailwindcssOxideRoot = root
-const artifactsRoot = path.join(tailwindcssOxideRoot, 'artifacts')
-
-let hasArtifactsDir = false
-try {
-  await fs.access(artifactsRoot)
-  hasArtifactsDir = true
-} catch {}
-
-if (!hasArtifactsDir) {
-  console.warn('No artifacts directory found, skipping artifact move step.')
-  process.exit(0)
-}
+let root = path.resolve(__dirname, '..')
+const tailwindcssOxideRoot = path.join(root)
 
 // Move napi artifacts into sub packages
-for (let file of await fs.readdir(artifactsRoot)) {
+for (let file of await fs.readdir(tailwindcssOxideRoot)) {
   if (file.startsWith('tailwindcss-oxide.') && file.endsWith('.node')) {
     let target = file.split('.')[1]
     await fs.cp(
-      path.join(artifactsRoot, file),
+      path.join(tailwindcssOxideRoot, file),
       path.join(tailwindcssOxideRoot, 'npm', target, file),
     )
     console.log(`Moved ${file} to npm/${target}`)
@@ -39,12 +27,12 @@ let wasmArtifacts = {
   'wasi-worker-browser.mjs': 'wasi-worker-browser.mjs',
   'wasi-worker.mjs': 'wasi-worker.mjs',
 }
-
-for (let file of await fs.readdir(artifactsRoot)) {
+for (let file of await fs.readdir(tailwindcssOxideRoot)) {
   if (!wasmArtifacts[file]) continue
-  const source = path.join(artifactsRoot, file)
-  await fs.cp(source, path.join(tailwindcssOxideRoot, 'npm', 'wasm32-wasi', wasmArtifacts[file]))
+  await fs.cp(
+    path.join(tailwindcssOxideRoot, file),
+    path.join(tailwindcssOxideRoot, 'npm', 'wasm32-wasi', wasmArtifacts[file]),
+  )
   console.log(`Moved ${file} to npm/wasm32-wasi`)
-  await fs.rm(source, { force: true })
 }
 
