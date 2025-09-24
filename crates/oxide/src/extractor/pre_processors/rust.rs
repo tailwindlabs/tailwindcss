cc40fbd0ce9aabaebc9e79c97f744eec96e0c26b
Robin Malfait <malfait.robin@gmail.com>
2025-09-24 11:28:38 +0200

    add support for Maud templates in Rust

--- crates/oxide/src/extractor/pre_processors/rust.rs
+++ crates/oxide/src/extractor/pre_processors/rust.rs
@@ -1,4 +1,9 @@
+use crate::extractor::bracket_stack;
+use crate::extractor::cursor;
+use crate::extractor::machine::Machine;
 use crate::extractor::pre_processors::pre_processor::PreProcessor;
+use crate::extractor::variant_machine::VariantMachine;
+use crate::extractor::MachineState;
 use bstr::ByteSlice;
 
 #[derive(Debug, Default)]
@@ -7,10 +12,84 @@ pub struct Rust;
 impl PreProcessor for Rust {
     fn process(&self, content: &[u8]) -> Vec<u8> {
         // Leptos support: https://github.com/tailwindlabs/tailwindcss/pull/18093
-        content
+        let replaced_content = content
             .replace(" class:", " class ")
             .replace("\tclass:", " class ")
-            .replace("\nclass:", " class ")
+            .replace("\nclass:", " class ");
+
+        if replaced_content.contains_str(b"html!") {
+            self.process_maud_templates(&replaced_content)
+        } else {
+            replaced_content
+        }
+    }
+}
+
+impl Rust {
+    fn process_maud_templates(&self, replaced_content: &[u8]) -> Vec<u8> {
+        let len = replaced_content.len();
+        let mut result = replaced_content.to_vec();
+        let mut cursor = cursor::Cursor::new(replaced_content);
+        let mut bracket_stack = bracket_stack::BracketStack::default();
+
+        while cursor.pos < len {
+            match cursor.curr {
+                // Only replace `.` with a space if it's not surrounded by numbers. E.g.:
+                //
+                // ```diff
+                // - .flex.items-center
+                // +  flex items-center
+                // ```
+                //
+                // But with numbers, it's allowed:
+                //
+                // ```diff
+                // - px-2.5
+                // + px-2.5
+                // ```
+                b'.' => {
+                    // Don't replace dots with spaces when inside of any type of brackets, because
+                    // this could be part of arbitrary values. E.g.: `bg-[url(https://example.com)]`
+                    //                                                                       ^
+                    if !bracket_stack.is_empty() {
+                        cursor.advance();
+                        continue;
+                    }
+
+                    // If the dot is surrounded by digits, we want to keep it. E.g.: `px-2.5`
+                    // EXCEPT if it's followed by a valid variant that happens to start with a
+                    // digit.
+                    // E.g.: `bg-red-500.2xl:flex`
+                    //                 ^^^
+                    if cursor.prev.is_ascii_digit() && cursor.next.is_ascii_digit() {
+                        let mut next_cursor = cursor.clone();
+                        next_cursor.advance();
+
+                        let mut variant_machine = VariantMachine::default();
+                        if let MachineState::Done(_) = variant_machine.next(&mut next_cursor) {
+                            result[cursor.pos] = b' ';
+                        }
+                    } else {
+                        result[cursor.pos] = b' ';
+                    }
+                }
+
+                b'[' => {
+                    bracket_stack.push(cursor.curr);
+                }
+
+                b']' if !bracket_stack.is_empty() => {
+                    bracket_stack.pop(cursor.curr);
+                }
+
+                // Consume everything else
+                _ => {}
+            };
+
+            cursor.advance();
+        }
+
+        result
     }
 }
 
@@ -40,5 +119,55 @@ mod tests {
         ] {
             Rust::test(input, expected);
         }
+    }
+
+    // https://github.com/tailwindlabs/tailwindcss/issues/18984
+    #[test]
+    fn test_maud_template_extraction() {
+        let input = r#"
+            use maud::{html, Markup};
+
+            pub fn main() -> Markup {
+                html! {
+                    header.px-8.py-4.text-black {
+                        "Hello, world!"
+                    }
+                }
+            }
+        "#;
+
+        Rust::test_extract_contains(input, vec!["px-8", "py-4", "text-black"]);
+
+        // https://maud.lambda.xyz/elements-attributes.html#classes-and-ids-foo-bar
+        let input = r#"
+            html! {
+                input #cannon .big.scary.bright-red type="button" value="Launch Party Cannon";
+            }
+        "#;
+        Rust::test_extract_contains(input, vec!["big", "scary", "bright-red"]);
+
+        let input = r#"
+            html! {
+                div."bg-[#0088cc]" { "Quotes for backticks" }
+            }
+        "#;
+        Rust::test_extract_contains(input, vec!["bg-[#0088cc]"]);
+
+        let input = r#"
+            html! {
+                #main {
+                    "Main content!"
+                    .tip { "Storing food in a refrigerator can make it 20% cooler." }
+                }
+            }
+        "#;
+        Rust::test_extract_contains(input, vec!["tip"]);
+
+        let input = r#"
+            html! {
+                div."bg-[url(https://example.com)]" { "Arbitrary values" }
+            }
+        "#;
+        Rust::test_extract_contains(input, vec!["bg-[url(https://example.com)]"]);
     }
 }
