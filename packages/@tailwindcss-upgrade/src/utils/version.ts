ab7dda11c8a30dc4ebc7c65f71a6d08efeb9579a
Robin Malfait <malfait.robin@gmail.com>
2025-09-29 12:37:25 +0200

    add expected Tailwind CSS version

--- packages/@tailwindcss-upgrade/src/utils/version.ts
+++ packages/@tailwindcss-upgrade/src/utils/version.ts
@@ -1,3 +1,4 @@
+import { execSync } from 'node:child_process'
 import semver from 'semver'
 import { DefaultMap } from '../../../tailwindcss/src/utils/default-map'
 import { getPackageVersionSync } from './package-version'
@@ -28,4 +29,35 @@ let cache = new DefaultMap((base) => {
 
 export function installedTailwindVersion(base = process.cwd()): string {
   return cache.get(base)
+}
+
+let expectedCache = new DefaultMap((base) => {
+  try {
+    // This will report a problem if the package.json/package-lock.json
+    // mismatches with the installed version in node_modules.
+    //
+    // Also tested this with Bun and PNPM, both seem to work fine.
+    execSync('npm ls tailwindcss --json', { cwd: base, stdio: 'pipe' })
+    return installedTailwindVersion(base)
+  } catch (_e) {
+    try {
+      let e = _e as { stdout: Buffer }
+      let data = JSON.parse(e.stdout.toString())
+
+      return (
+        // Could be a sub-dependency issue, but we are only interested in
+        // the top-level version mismatch.
+        /"(.*?)" from the root project/.exec(data.dependencies.tailwindcss.invalid)?.[1] ??
+        // Fallback to the installed version
+        installedTailwindVersion(base)
+      )
+    } catch {
+      // We don't know how to verify, so let's just return the installed
+      // version to not block the user.
+      return installedTailwindVersion(base)
+    }
+  }
+})
+export function expectedTailwindVersion(base = process.cwd()): string {
+  return expectedCache.get(base)
 }
