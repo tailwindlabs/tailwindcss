01d1e9825982894a293ccbf4d2c5e6a702b3868c
Robin Malfait <malfait.robin@gmail.com>
2025-10-09 20:09:29 +0200

    Canonicalization constant folding and handling zeros (#19095)
    The main goal of this PR was to support canonicalization of zero like
    values. We essentially want to canonicalize `-mt-0` as `mt-0`, but also
    `mt-[0px]`, `mt-[0rem]`, and other length-like units to just `mt-0`.
    
    To do this, we had to handle 2 things:
    
    1. We introduced some more constant folding, including making `0px` and
    `0rem` fold to `0`. We only do this for length units. We also normalize
    `-0`, `+0`, `-0.0` and so on to `0`.
    2. While pre-computing utilities in our lookup table, we make sure that
    we prefer `mt-0` over `-mt-0` if both result in the same signature.
    
    Moved some of the constant folding logic into its own function and added
    a bunch of separate tests for it.
    
    ## Test plan
    
    Added more unit tests where we normalize different zero-like values to
    `0`.
    
    Running the canonicalization logic:
    ```js
    designSystem.canonicalizeCandidates([
      '-m-0',
      '-m-[-0px]',
      '-m-[-0rem]',
      '-m-[0px]',
      '-m-[0rem]',
      'm-0',
      'm-[-0px]',
      'm-[-0rem]',
      'm-[0px]',
      'm-[0rem]',
      'm-[calc(var(--spacing)*0)]',
      'm-[--spacing(0)]',
      'm-[--spacing(0.0)]',
      'm-[+0]',
      'm-[-0]',
      '-m-[-0]',
      '-m-[+0]',
    ]) // → ['m-0']
    ```
    
    ---------
    
    Co-authored-by: Jordan Pittman <jordan@cryptica.me>

--- packages/tailwindcss/src/canonicalize-candidates.test.ts
+++ packages/tailwindcss/src/canonicalize-candidates.test.ts
@@ -675,6 +675,30 @@ describe.each([['default'], ['with-variant'], ['important'], ['prefix']])('%s', 
       ['grid-cols-[subgrid]', 'grid-cols-subgrid'],
       ['grid-rows-[subgrid]', 'grid-rows-subgrid'],
 
+      // Handle zeroes
+      ['m-[0]', 'm-0'],
+      ['m-[0px]', 'm-0'],
+      ['m-[0rem]', 'm-0'],
+
+      ['-m-[0]', 'm-0'],
+      ['-m-[0px]', 'm-0'],
+      ['-m-[0rem]', 'm-0'],
+
+      ['m-[-0]', 'm-0'],
+      ['m-[-0px]', 'm-0'],
+      ['m-[-0rem]', 'm-0'],
+
+      ['-m-[-0]', 'm-0'],
+      ['-m-[-0px]', 'm-0'],
+      ['-m-[-0rem]', 'm-0'],
+
+      ['[margin:0]', 'm-0'],
+      ['[margin:-0]', 'm-0'],
+      ['[margin:0px]', 'm-0'],
+
+      // Not a length-unit, can't safely constant fold
+      ['[margin:0%]', 'm-[0%]'],
+
       // Only 50-200% (inclusive) are valid:
       // https://developer.mozilla.org/en-US/docs/Web/CSS/font-stretch#percentage
       ['font-stretch-[50%]', 'font-stretch-50%'],
