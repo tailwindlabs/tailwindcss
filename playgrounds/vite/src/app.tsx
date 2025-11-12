5232c869898117f690466c490cfbf4286c74c576
Baptist <baptist@battlekart.com>
2025-11-12 14:58:28 +0100

    reset playground indent

--- playgrounds/vite/src/app.tsx
+++ playgrounds/vite/src/app.tsx
@@ -1,7 +1,7 @@
 export function App() {
-	return (
-		<div className="m-3 p-3 border">
-			<h1 className="text-blue-500">Hello World</h1>
-		</div>
-	);
+  return (
+    <div className="m-3 p-3 border">
+      <h1 className="text-blue-500">Hello World</h1>
+    </div>
+  )
 }
