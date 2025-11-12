c080436022cf635806d34e94f0ee09d4d21fa82a
Baptist <baptist@battlekart.com>
2025-11-12 14:57:59 +0100

    reset playground

--- playgrounds/vite/src/app.tsx
+++ playgrounds/vite/src/app.tsx
@@ -2,12 +2,6 @@ export function App() {
 	return (
 		<div className="m-3 p-3 border">
 			<h1 className="text-blue-500">Hello World</h1>
-			<button
-				className="bg-blue-500 text-white p-2 rounded-2xl corner-squircle"
-				type="button"
-			>
-				Click me
-			</button>
 		</div>
 	);
 }
