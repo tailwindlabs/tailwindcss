f598f1ee9c8acdfad69269679a20d7b18e2cde3e
Jordan Pittman <jordan@cryptica.me>
2025-11-26 14:12:13 -0500

    Update test

--- integrations/upgrade/index.test.ts
+++ integrations/upgrade/index.test.ts
@@ -2347,8 +2347,7 @@ test(
         --shadow-*: initial;
         --shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
 
-        --ring-width-*: initial;
-        --ring-width: 4px;
+        --default-ring-width: 4px;
 
         --blur: var(--custom-default-blur);
 
@@ -2381,7 +2380,7 @@ test(
         <div class="blur blur-xs"></div>
         <div class="backdrop-blur backdrop-blur-xs"></div>
         <div class="rounded rounded-sm"></div>
-        <div class="ring"></div>
+        <div class="ring-3"></div>
       </div>
       "
     `)
