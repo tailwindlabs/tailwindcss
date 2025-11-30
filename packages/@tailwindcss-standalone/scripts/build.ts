082a16e6762cea36af209ab9d0f0867b2055df87
Jordan Pittman <jordan@cryptica.me>
2025-11-30 08:29:26 -0500

    Rework standalone CLI build setup
    This is equivalent to the old build setup but doesn’t need to use CLI commands to build the binaries

--- packages/@tailwindcss-standalone/scripts/build.ts
+++ packages/@tailwindcss-standalone/scripts/build.ts
@@ -1,4 +1,3 @@
-import { $ } from 'bun'
 import { createHash } from 'node:crypto'
 import { mkdir, readFile, writeFile } from 'node:fs/promises'
 import * as path from 'node:path'
@@ -6,80 +5,74 @@ import { fileURLToPath } from 'node:url'
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
-async function buildForPlatform(triple: string, outfile: string) {
-  // We wrap this in a retry because occasionally the atomic rename fails for some reason
-  for (let i = 0; i < 5; ++i) {
-    try {
-      let cmd = $`bun build --compile --target=${triple} ./src/index.ts --outfile=${outfile} --env inline`
+// Workaround for Bun binary downloads failing on Windows CI when
+// USERPROFILE is passed through by Turborepo.
+//
+// Unfortunately, setting this at runtime doesn't appear to work so we have to
+// spawn a new process without the env var.
+if (process.env.NESTED_BUILD !== '1' && process.env.USERPROFILE && process.env.USERPROFILE !== '') {
+  let result = await Bun.$`bun ${fileURLToPath(import.meta.url)}`.env({
+    USERPROFILE: '',
+    NESTED_BUILD: '1',
+  })
 
-      // This env var is used by our patched versions of Lightning CSS and Parcel Watcher to
-      // statically bundle the proper binaries for musl vs glibc
-      cmd = cmd.env({
-        PLATFORM_LIBC: triple.includes('-musl') ? 'musl' : 'glibc',
-
-        // Workaround for Bun binary downloads failing on Windows CI when
-        // USERPROFILE is passed through by Turborepo.
-        USERPROFILE: '',
-      })
-
-      return await cmd
-    } catch (err) {
-      if (i < 5) continue
-
-      throw new Error(`Failed to build for platform ${triple}`, { cause: err })
-    }
-  }
+  process.exit(result.exitCode)
 }
 
-async function build(triple: string, file: string) {
-  let start = process.hrtime.bigint()
+// We use baseline builds for all x64 platforms to ensure compatibility with
+// older hardware.
+let builds: { target: Bun.Build.Target; name: string }[] = [
+  { name: 'tailwindcss-linux-arm64', target: 'bun-linux-arm64' },
+  { name: 'tailwindcss-linux-arm64-musl', target: 'bun-linux-arm64-musl' },
+  // @ts-expect-error: Either the types are wrong or the runtime needs to be updated
+  // to accept a `-glibc` at the end like the types suggest.
+  { name: 'tailwindcss-linux-x64', target: 'bun-linux-x64-baseline' },
+  { name: 'tailwindcss-linux-x64-musl', target: 'bun-linux-x64-baseline-musl' },
+  { name: 'tailwindcss-macos-arm64', target: 'bun-darwin-arm64' },
+  { name: 'tailwindcss-macos-x64', target: 'bun-darwin-x64-baseline' },
+  { name: 'tailwindcss-windows-x64.exe', target: 'bun-windows-x64-baseline' },
+]
 
-  let outfile = path.resolve(__dirname, `../dist/${file}`)
+let summary: { target: Bun.Build.Target; name: string; sum: string }[] = []
 
-  await buildForPlatform(triple, outfile)
+// Build platform binaries and checksum them.
+let start = process.hrtime.bigint()
+for (let { target, name } of builds) {
+  let outfile = path.resolve(__dirname, `../dist/${name}`)
 
-  await new Promise((resolve) => setTimeout(resolve, 100))
+  process.env.PLATFORM_LIBC = target.includes('-musl') ? 'musl' : 'glibc'
+
+  let result = await Bun.build({
+    entrypoints: ['./src/index.ts'],
+    target: 'node',
+    env: 'inline',
+    compile: {
+      target,
+      outfile,
+    },
+  })
+
+  let entry = result.outputs.find((output) => output.kind === 'entry-point')
+  if (!entry) throw new Error(`Build failed for ${target}`)
 
   let content = await readFile(outfile)
-  let sum = createHash('sha256').update(content).digest('hex')
 
-  let elapsed = process.hrtime.bigint() - start
-
-  return {
-    triple,
-    file,
-    sum,
-    elapsed,
-  }
+  summary.push({
+    target,
+    name,
+    sum: createHash('sha256').update(content).digest('hex'),
+  })
 }
 
 await mkdir(path.resolve(__dirname, '../dist'), { recursive: true })
 
-// Build platform binaries and checksum them. We use baseline builds for all x64 platforms to ensure
-// compatibility with older hardware.
-let results = await Promise.all([
-  build('bun-linux-arm64', './tailwindcss-linux-arm64'),
-  build('bun-linux-arm64-musl', './tailwindcss-linux-arm64-musl'),
-
-  build('bun-linux-x64-baseline', './tailwindcss-linux-x64'),
-  build('bun-linux-x64-musl-baseline', './tailwindcss-linux-x64-musl'),
-
-  build('bun-darwin-arm64', './tailwindcss-macos-arm64'),
-  build('bun-darwin-x64-baseline', './tailwindcss-macos-x64'),
-
-  build('bun-windows-x64-baseline', './tailwindcss-windows-x64.exe'),
-])
-
 // Write the checksums to a file
 let sumsFile = path.resolve(__dirname, '../dist/sha256sums.txt')
-let sums = results.map(({ file, sum }) => `${sum}  ${file}`)
-
-console.table(
-  results.map(({ triple, sum, elapsed }) => ({
-    triple,
-    sum,
-    elapsed: `${(Number(elapsed) / 1e6).toFixed(0)}ms`,
-  })),
-)
+let sums = summary.map(({ name, sum }) => `${sum}  ./${name}`)
 
 await writeFile(sumsFile, sums.join('\n') + '\n')
+
+console.table(summary.map(({ target, sum }) => ({ target, sum })))
+
+let elapsed = process.hrtime.bigint() - start
+console.log(`Build completed in ${(Number(elapsed) / 1e6).toFixed(0)}ms`)
