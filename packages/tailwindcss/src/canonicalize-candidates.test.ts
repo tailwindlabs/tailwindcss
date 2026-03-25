94ba203759d606be049e1b0dc6238f01d4a523c9
Robin Malfait <malfait.robin@gmail.com>
2026-03-25 14:58:42 +0100

    split tests into an `arbitrary properties` and `arbitrary values` section

--- packages/tailwindcss/src/canonicalize-candidates.test.ts
+++ packages/tailwindcss/src/canonicalize-candidates.test.ts
@@ -118,162 +118,100 @@ describe.each([['default'], ['with-variant'], ['important'], ['prefix']])('%s', 
 
   /// ----------------------------------
 
-  test.each([
-    /// theme(…) to `var(…)`
-    // Keep candidates that don't contain `theme(…)` or `theme(…, …)`
-    ['[color:red]', 'text-[red]'],
+  describe('arbitrary properties', () => {
+    test.each([
+      /// theme(…) to `var(…)`
+      // Keep candidates that don't contain `theme(…)` or `theme(…, …)`
+      ['[color:red]', 'text-[red]'],
 
-    // Handle special cases around `.1` in the `theme(…)`
-    ['[--value:theme(spacing.1)]', '[--value:--spacing(1)]'],
-    ['[--value:theme(fontSize.xs.1.lineHeight)]', '[--value:var(--text-xs--line-height)]'],
-    ['[--value:theme(spacing[1.25])]', '[--value:--spacing(1.25)]'],
+      // Handle special cases around `.1` in the `theme(…)`
+      ['[--value:theme(spacing.1)]', '[--value:--spacing(1)]'],
+      ['[--value:theme(fontSize.xs.1.lineHeight)]', '[--value:var(--text-xs--line-height)]'],
+      ['[--value:theme(spacing[1.25])]', '[--value:--spacing(1.25)]'],
 
-    // Should not convert invalid spacing values to calc
-    ['[--value:theme(spacing[1.1])]', '[--value:theme(spacing[1.1])]'],
+      // Should not convert invalid spacing values to calc
+      ['[--value:theme(spacing[1.1])]', '[--value:theme(spacing[1.1])]'],
 
-    // Convert to `var(…)` if we can resolve the path
-    ['[color:theme(colors.red.500)]', 'text-red-500'], // Arbitrary property
-    ['[color:theme(colors.red.500)]/50', 'text-red-500/50'], // Arbitrary property + modifier
-    ['bg-[theme(colors.red.500)]', 'bg-red-500'], // Arbitrary value
-    ['bg-[size:theme(spacing.4)]', 'bg-size-[--spacing(4)]'], // Arbitrary value + data type hint
+      // Convert to `var(…)` if we can resolve the path
+      ['[color:theme(colors.red.500)]', 'text-red-500'], // Arbitrary property
+      ['[color:theme(colors.red.500)]/50', 'text-red-500/50'], // Arbitrary property + modifier
 
-    // Pretty print CSS functions preceded by an operator to prevent consecutive
-    // operator characters.
-    ['w-[calc(100dvh-theme(spacing.2))]', 'w-[calc(100dvh-(--spacing(2)))]'],
-    ['w-[calc(100dvh+theme(spacing.2))]', 'w-[calc(100dvh+(--spacing(2)))]'],
-    ['w-[calc(100dvh/theme(spacing.2))]', 'w-[calc(100dvh/(--spacing(2)))]'],
-    ['w-[calc(100dvh*theme(spacing.2))]', 'w-[calc(100dvh*(--spacing(2)))]'],
+      // Multiple `theme(… / …)` calls should result in modern syntax of `theme(…)`
+      // - Can't convert to `var(…)` because that would lose the modifier.
+      // - Can't convert to a candidate modifier because there are multiple
+      //   `theme(…)` calls.
+      //
+      //   If we really want to, we can make a fancy migration that tries to move it
+      //   to a candidate modifier _if_ all `theme(…)` calls use the same modifier.
+      [
+        '[color:theme(colors.red.500/50,theme(colors.blue.500/50))]',
+        'text-[--theme(--color-red-500/50,--theme(--color-blue-500/50))]',
+      ],
+      [
+        '[color:theme(colors.red.500/50,theme(colors.blue.500/50))]/50',
+        'text-[--theme(--color-red-500/50,--theme(--color-blue-500/50))]/50',
+      ],
 
-    // Convert to `var(…)` if we can resolve the path, but keep fallback values
-    ['bg-[theme(colors.red.500,red)]', 'bg-(--color-red-500,red)'],
+      // Convert the `theme(…)`, but try to move the inline modifier (e.g. `50%`),
+      // to a candidate modifier.
+      // Arbitrary property, with simple percentage modifier
+      ['[color:theme(colors.red.500/75%)]', 'text-red-500/75'],
 
-    // Keep `theme(…)` if we can't resolve the path
-    ['bg-[theme(colors.foo.1000)]', 'bg-[theme(colors.foo.1000)]'],
+      // Arbitrary property, with numbers (0-1) without a unit
+      ['[color:theme(colors.red.500/.12)]', 'text-red-500/12'],
+      ['[color:theme(colors.red.500/0.12)]', 'text-red-500/12'],
 
-    // Keep `theme(…)` if we can't resolve the path, but still try to convert the
-    // fallback value.
-    ['bg-[theme(colors.foo.1000,theme(colors.red.500))]', 'bg-red-500'],
+      // Arbitrary property, with more complex modifier (we only allow whole numbers
+      // as bare modifiers). Convert the complex numbers to arbitrary values instead.
+      ['[color:theme(colors.red.500/12.34%)]', 'text-red-500/[12.34%]'],
+      ['[color:theme(colors.red.500/var(--opacity))]', 'text-red-500/(--opacity)'],
+      ['[color:theme(colors.red.500/.12345)]', 'text-red-500/1234.5'],
+      ['[color:theme(colors.red.500/50.25%)]', 'text-red-500/50.25'],
 
-    // Use `theme(…)` (deeply nested) inside of a `calc(…)` function
-    ['text-[calc(theme(fontSize.xs)*2)]', 'text-[calc(var(--text-xs)*2)]'],
+      // Arbitrary property that already contains a modifier
+      ['[color:theme(colors.red.500/50%)]/50', 'text-[--theme(--color-red-500/50%)]/50'],
 
-    // Multiple `theme(… / …)` calls should result in modern syntax of `theme(…)`
-    // - Can't convert to `var(…)` because that would lose the modifier.
-    // - Can't convert to a candidate modifier because there are multiple
-    //   `theme(…)` calls.
-    //
-    //   If we really want to, we can make a fancy migration that tries to move it
-    //   to a candidate modifier _if_ all `theme(…)` calls use the same modifier.
-    [
-      '[color:theme(colors.red.500/50,theme(colors.blue.500/50))]',
-      'text-[--theme(--color-red-500/50,--theme(--color-blue-500/50))]',
-    ],
-    [
-      '[color:theme(colors.red.500/50,theme(colors.blue.500/50))]/50',
-      'text-[--theme(--color-red-500/50,--theme(--color-blue-500/50))]/50',
-    ],
+      // `calc(var(--spacing)*…)` to `--spacing(…)`
+      ['[padding-top:min(20%,calc(var(--spacing)*8))]', 'pt-[min(20%,--spacing(8))]'],
+      [
+        '[padding-top:min(20%,calc(var(--spacing)*var(--other)))]',
+        'pt-[min(20%,--spacing(var(--other)))]',
+      ],
+      ['[padding-top:calc(var(--spacing)*8)]', 'pt-8'],
+      ['[padding-top:calc(var(--spacing)*var(--other))]', 'pt-[--spacing(var(--other))]'],
 
-    // Convert the `theme(…)`, but try to move the inline modifier (e.g. `50%`),
-    // to a candidate modifier.
-    // Arbitrary property, with simple percentage modifier
-    ['[color:theme(colors.red.500/75%)]', 'text-red-500/75'],
+      // `theme(…)` calls valid in v3, but not in v4 should still be converted.
+      ['[--foo:theme(transitionDuration.500)]', '[--foo:theme(transitionDuration.500)]'],
 
-    // Arbitrary property, with numbers (0-1) without a unit
-    ['[color:theme(colors.red.500/.12)]', 'text-red-500/12'],
-    ['[color:theme(colors.red.500/0.12)]', 'text-red-500/12'],
+      // Invalid cases
+      ['[--foo:theme(colors.red.500/50/50)]', '[--foo:theme(colors.red.500/50/50)]'],
+      ['[--foo:theme(colors.red.500/50/50)]/50', '[--foo:theme(colors.red.500/50/50)]/50'],
 
-    // Arbitrary property, with more complex modifier (we only allow whole numbers
-    // as bare modifiers). Convert the complex numbers to arbitrary values instead.
-    ['[color:theme(colors.red.500/12.34%)]', 'text-red-500/[12.34%]'],
-    ['[color:theme(colors.red.500/var(--opacity))]', 'text-red-500/(--opacity)'],
-    ['[color:theme(colors.red.500/.12345)]', 'text-red-500/1234.5'],
-    ['[color:theme(colors.red.500/50.25%)]', 'text-red-500/50.25'],
+      // Partially invalid cases
+      [
+        '[--foo:theme(colors.red.500/50/50)_theme(colors.blue.200)]',
+        '[--foo:theme(colors.red.500/50/50)_var(--color-blue-200)]',
+      ],
+      [
+        '[--foo:theme(colors.red.500/50/50)_theme(colors.blue.200)]/50',
+        '[--foo:theme(colors.red.500/50/50)_var(--color-blue-200)]/50',
+      ],
 
-    // Arbitrary value
-    ['bg-[theme(colors.red.500/75%)]', 'bg-red-500/75'],
-    ['bg-[theme(colors.red.500/12.34%)]', 'bg-red-500/[12.34%]'],
-
-    // Arbitrary property that already contains a modifier
-    ['[color:theme(colors.red.500/50%)]/50', 'text-[--theme(--color-red-500/50%)]/50'],
-
-    // Values that don't contain only `theme(…)` calls should not be converted to
-    // use a modifier since the color is not the whole value.
-    [
-      'shadow-[shadow:inset_0px_1px_theme(colors.white/15%)]',
-      'shadow-[inset_0px_1px_--theme(--color-white/15%)]',
-    ],
-
-    // Arbitrary value, where the candidate already contains a modifier
-    // This should still migrate the `theme(…)` syntax to the modern syntax.
-    ['bg-[theme(colors.red.500/50%)]/50', 'bg-[--theme(--color-red-500/50%)]/50'],
-
-    // Variants, we can't use `var(…)` especially inside of `@media(…)`. We can
-    // still upgrade the `theme(…)` to the modern syntax.
-    ['max-[theme(screens.lg)]:flex', 'max-[--theme(--breakpoint-lg)]:flex'],
-    // There are no variables for `--spacing` multiples, so we can't convert this
-    ['max-[theme(spacing.4)]:flex', 'max-[theme(spacing.4)]:flex'],
-
-    // This test in itself doesn't make much sense. But we need to make sure
-    // that this doesn't end up as the modifier in the candidate itself.
-    ['max-[theme(spacing.4/50)]:flex', 'max-[theme(spacing.4/50)]:flex'],
-
-    // `theme(…)` calls in another CSS function is replaced correctly.
-    // Additionally we remove unnecessary whitespace.
-    ['grid-cols-[min(50%_,_theme(spacing.80))_auto]', 'grid-cols-[min(50%,--spacing(80))_auto]'],
-
-    // `calc(var(--spacing)*…)` to `--spacing(…)`
-    ['pt-[min(20%,calc(var(--spacing)*8))]', 'pt-[min(20%,--spacing(8))]'],
-    ['pt-[min(20%,calc(var(--spacing)*var(--other)))]', 'pt-[min(20%,--spacing(var(--other)))]'],
-    ['pt-[calc(var(--spacing)*8)]', 'pt-8'],
-    ['pt-[calc(var(--spacing)*var(--other))]', 'pt-[--spacing(var(--other))]'],
-
-    ['[padding-top:min(20%,calc(var(--spacing)*8))]', 'pt-[min(20%,--spacing(8))]'],
-    [
-      '[padding-top:min(20%,calc(var(--spacing)*var(--other)))]',
-      'pt-[min(20%,--spacing(var(--other)))]',
-    ],
-    ['[padding-top:calc(var(--spacing)*8)]', 'pt-8'],
-    ['[padding-top:calc(var(--spacing)*var(--other))]', 'pt-[--spacing(var(--other))]'],
-
-    // `theme(…)` calls valid in v3, but not in v4 should still be converted.
-    ['[--foo:theme(transitionDuration.500)]', '[--foo:theme(transitionDuration.500)]'],
-
-    // Renamed theme keys
-    ['max-w-[theme(screens.md)]', 'max-w-(--breakpoint-md)'],
-    ['w-[theme(maxWidth.md)]', 'w-md'],
-
-    // Invalid cases
-    ['[--foo:theme(colors.red.500/50/50)]', '[--foo:theme(colors.red.500/50/50)]'],
-    ['[--foo:theme(colors.red.500/50/50)]/50', '[--foo:theme(colors.red.500/50/50)]/50'],
-
-    // Partially invalid cases
-    [
-      '[--foo:theme(colors.red.500/50/50)_theme(colors.blue.200)]',
-      '[--foo:theme(colors.red.500/50/50)_var(--color-blue-200)]',
-    ],
-    [
-      '[--foo:theme(colors.red.500/50/50)_theme(colors.blue.200)]/50',
-      '[--foo:theme(colors.red.500/50/50)_var(--color-blue-200)]/50',
-    ],
-
-    // If a utility sets `property` and `--tw-{property}` with the same value,
-    // we can ignore the `--tw-{property}`. This is just here for composition.
-    // This means that we should be able to upgrade the one _without_ to the one
-    // _with_ the variable
-    ['[font-weight:400]', 'font-normal'],
-    ['[line-height:0]', 'leading-0'],
-    ['[border-style:solid]', 'border-solid'],
-  ])(testName, { timeout }, async (candidate, expected) => {
-    await expectCanonicalization(
-      css`
+      // If a utility sets `property` and `--tw-{property}` with the same value,
+      // we can ignore the `--tw-{property}`. This is just here for composition.
+      // This means that we should be able to upgrade the one _without_ to the one
+      // _with_ the variable
+      ['[font-weight:400]', 'font-normal'],
+      ['[line-height:0]', 'leading-0'],
+      ['[border-style:solid]', 'border-solid'],
+    ])(testName, { timeout }, async (candidate, expected) => {
+      let input = css`
         @import 'tailwindcss';
-      `,
-      candidate,
-      expected,
-    )
-  })
+      `
 
-  describe('arbitrary utilities', () => {
+      await expectCanonicalization(input, candidate, expected)
+    })
+
     test.each([
       // Arbitrary property to static utility
       ['[text-wrap:balance]', 'text-balance'],
@@ -283,10 +221,6 @@ describe.each([['default'], ['with-variant'], ['important'], ['prefix']])('%s', 
       ['[display:_flex_]', 'flex'],
       ['[display:_flex]', 'flex'],
       ['[display:flex_]', 'flex'],
-
-      // Arbitrary property to static utility
-      // Map number to keyword-like value
-      ['leading-[1]', 'leading-none'],
 
       // Arbitrary property to named functional utility
       ['[color:var(--color-red-500)]', 'text-red-500'],
@@ -315,20 +249,6 @@ describe.each([['default'], ['with-variant'], ['important'], ['prefix']])('%s', 
       ['[grid-column:2]', 'col-2'],
       ['[grid-column:1234]', 'col-1234'],
 
-      // Arbitrary value to bare value
-      ['border-[2px]', 'border-2'],
-      ['border-[1234px]', 'border-1234'],
-
-      // Arbitrary value with data type, to more specific arbitrary value
-      ['bg-[position:123px]', 'bg-position-[123px]'],
-      ['bg-[size:123px]', 'bg-size-[123px]'],
-
-      // Arbitrary value with inferred data type, to more specific arbitrary value
-      ['bg-[123px]', 'bg-position-[123px]'],
-
-      // Arbitrary value with spacing mul
-      ['w-[64rem]', 'w-256'],
-
       // Complex arbitrary property to arbitrary value
       [
         '[grid-template-columns:repeat(2,minmax(100px,1fr))]',
@@ -336,13 +256,6 @@ describe.each([['default'], ['with-variant'], ['important'], ['prefix']])('%s', 
       ],
       // Complex arbitrary property to bare value
       ['[grid-template-columns:repeat(2,minmax(0,1fr))]', 'grid-cols-2'],
-
-      // Arbitrary value to bare value with percentage
-      ['from-[25%]', 'from-25%'],
-
-      // Arbitrary percentage value must be a whole number. Should not migrate to
-      // a bare value.
-      ['from-[2.5%]', 'from-[2.5%]'],
     ])(testName, { timeout }, async (candidate, expected) => {
       let input = css`
         @import 'tailwindcss';
@@ -359,7 +272,107 @@ describe.each([['default'], ['with-variant'], ['important'], ['prefix']])('%s', 
 
       await expectCanonicalization(input, candidate, expected)
     })
+  })
 
+  describe('arbitrary values', () => {
+    test.each([
+      // Convert to `var(…)` if we can resolve the path
+      ['bg-[theme(colors.red.500)]', 'bg-red-500'], // Arbitrary value
+      ['bg-[size:theme(spacing.4)]', 'bg-size-[--spacing(4)]'], // Arbitrary value + data type hint
+
+      // Pretty print CSS functions preceded by an operator to prevent consecutive
+      // operator characters.
+      ['w-[calc(100dvh-theme(spacing.2))]', 'w-[calc(100dvh-(--spacing(2)))]'],
+      ['w-[calc(100dvh+theme(spacing.2))]', 'w-[calc(100dvh+(--spacing(2)))]'],
+      ['w-[calc(100dvh/theme(spacing.2))]', 'w-[calc(100dvh/(--spacing(2)))]'],
+      ['w-[calc(100dvh*theme(spacing.2))]', 'w-[calc(100dvh*(--spacing(2)))]'],
+
+      // Convert to `var(…)` if we can resolve the path, but keep fallback values
+      ['bg-[theme(colors.red.500,red)]', 'bg-(--color-red-500,red)'],
+
+      // Keep `theme(…)` if we can't resolve the path
+      ['bg-[theme(colors.foo.1000)]', 'bg-[theme(colors.foo.1000)]'],
+
+      // Keep `theme(…)` if we can't resolve the path, but still try to convert the
+      // fallback value.
+      ['bg-[theme(colors.foo.1000,theme(colors.red.500))]', 'bg-red-500'],
+
+      // Use `theme(…)` (deeply nested) inside of a `calc(…)` function
+      ['text-[calc(theme(fontSize.xs)*2)]', 'text-[calc(var(--text-xs)*2)]'],
+
+      // Arbitrary value
+      ['bg-[theme(colors.red.500/75%)]', 'bg-red-500/75'],
+      ['bg-[theme(colors.red.500/12.34%)]', 'bg-red-500/[12.34%]'],
+
+      // Values that don't contain only `theme(…)` calls should not be converted to
+      // use a modifier since the color is not the whole value.
+      [
+        'shadow-[shadow:inset_0px_1px_theme(colors.white/15%)]',
+        'shadow-[inset_0px_1px_--theme(--color-white/15%)]',
+      ],
+
+      // Arbitrary value, where the candidate already contains a modifier
+      // This should still migrate the `theme(…)` syntax to the modern syntax.
+      ['bg-[theme(colors.red.500/50%)]/50', 'bg-[--theme(--color-red-500/50%)]/50'],
+
+      // Variants, we can't use `var(…)` especially inside of `@media(…)`. We can
+      // still upgrade the `theme(…)` to the modern syntax.
+      ['max-[theme(screens.lg)]:flex', 'max-[--theme(--breakpoint-lg)]:flex'],
+      // There are no variables for `--spacing` multiples, so we can't convert this
+      ['max-[theme(spacing.4)]:flex', 'max-[theme(spacing.4)]:flex'],
+
+      // This test in itself doesn't make much sense. But we need to make sure
+      // that this doesn't end up as the modifier in the candidate itself.
+      ['max-[theme(spacing.4/50)]:flex', 'max-[theme(spacing.4/50)]:flex'],
+
+      // `theme(…)` calls in another CSS function is replaced correctly.
+      // Additionally we remove unnecessary whitespace.
+      ['grid-cols-[min(50%_,_theme(spacing.80))_auto]', 'grid-cols-[min(50%,--spacing(80))_auto]'],
+
+      // `calc(var(--spacing)*…)` to `--spacing(…)`
+      ['pt-[min(20%,calc(var(--spacing)*8))]', 'pt-[min(20%,--spacing(8))]'],
+      ['pt-[min(20%,calc(var(--spacing)*var(--other)))]', 'pt-[min(20%,--spacing(var(--other)))]'],
+      ['pt-[calc(var(--spacing)*8)]', 'pt-8'],
+      ['pt-[calc(var(--spacing)*var(--other))]', 'pt-[--spacing(var(--other))]'],
+
+      // Renamed theme keys
+      ['max-w-[theme(screens.md)]', 'max-w-(--breakpoint-md)'],
+      ['w-[theme(maxWidth.md)]', 'w-md'],
+
+      // Arbitrary property to static utility
+      // Map number to keyword-like value
+      ['leading-[1]', 'leading-none'],
+
+      // Arbitrary value to bare value
+      ['border-[2px]', 'border-2'],
+      ['border-[1234px]', 'border-1234'],
+
+      // Arbitrary value with data type, to more specific arbitrary value
+      ['bg-[position:123px]', 'bg-position-[123px]'],
+      ['bg-[size:123px]', 'bg-size-[123px]'],
+
+      // Arbitrary value with inferred data type, to more specific arbitrary value
+      ['bg-[123px]', 'bg-position-[123px]'],
+
+      // Arbitrary value with spacing mul
+      ['w-[64rem]', 'w-256'],
+
+      // Arbitrary value to bare value with percentage
+      ['from-[25%]', 'from-25%'],
+
+      // Arbitrary percentage value must be a whole number. Should not migrate to
+      // a bare value.
+      ['from-[2.5%]', 'from-[2.5%]'],
+    ])(testName, { timeout }, async (candidate, expected) => {
+      let input = css`
+        @import 'tailwindcss';
+      `
+
+      await expectCanonicalization(input, candidate, expected)
+    })
+  })
+
+  describe('arbitrary utilities', () => {
     test('migrate with custom static utility `@utility custom {…}`', { timeout }, async () => {
       let candidate = '[--key:value]'
       let expected = 'custom'
