a7d30ab3cbb333c5f6f71eada4e378dd98847d37
Robin Malfait <malfait.robin@gmail.com>
2026-04-30 17:04:51 +0200

    update test to also use Lightning CSS

--- packages/tailwindcss/src/index.test.ts
+++ packages/tailwindcss/src/index.test.ts
@@ -858,7 +858,7 @@ describe('@apply', () => {
   })
 
   it('should be usable with CSS mixins', async () => {
-    let compiler = await compile(css`
+    let input = css`
       .foo {
         /* Utility usage */
         @apply underline;
@@ -872,8 +872,9 @@ describe('@apply', () => {
           color: red;
         }
       }
-    `)
+    `
 
+    let compiler = await compile(input)
     expect(compiler.build([])).toMatchInlineSnapshot(`
       ".foo {
         text-decoration-line: underline;
@@ -886,6 +887,26 @@ describe('@apply', () => {
         }
       }
       "
+    `)
+
+    // TODO: This output is currently broken because Lightning CSS doesn't
+    // handle this case correctly yet
+    expect(await compileCss(input)).toMatchInlineSnapshot(`
+      ".foo {
+        text-decoration-line: underline;
+      }
+
+      @apply --my-mixin-1;
+
+      @apply --my-mixin-1();
+
+      @apply --my-mixin-1 --my-mixin-2;
+
+      @apply --my-mixin-1() --my-mixin-2();
+
+      @apply --my-mixin-3 {
+        color: red;
+      }"
     `)
   })
 
