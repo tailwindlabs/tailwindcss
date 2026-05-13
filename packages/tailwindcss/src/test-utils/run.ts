be450bc299fb94bfba55889f6b2e9dc05fbb97fb
Robin Malfait <malfait.robin@gmail.com>
2026-05-14 00:49:34 +0200

    re-use `run` in `optimizeCss`

--- packages/tailwindcss/src/test-utils/run.ts
+++ packages/tailwindcss/src/test-utils/run.ts
@@ -3,11 +3,6 @@ import { optimize } from '../../../@tailwindcss-node/src/optimize'
 
 const css = String.raw
 
-export async function compileCss(css: string, options: Parameters<typeof compile>[1] = {}) {
-  let { build } = await compile(css, options)
-  return pretty(optimize(build([])).code)
-}
-
 export async function run(
   candidates: string[],
   input = css`
@@ -17,6 +12,10 @@ export async function run(
 ) {
   let { build } = await compile(input, options)
   return pretty(optimize(build(candidates)).code)
+}
+
+export async function compileCss(css: string, options: Parameters<typeof compile>[1] = {}) {
+  return run([], css, options)
 }
 
 export function pretty(input: string) {
