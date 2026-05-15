bd5d2855c440d576c4ebe67183259d379345657b
Robin Malfait <malfait.robin@gmail.com>
2026-05-15 17:31:42 +0200

    improve instrumentation
    While working on another feature, I used this instrumentation and made
    some changes. So why not commit it?
    
    Added a `let value = I.span(label, () => 123)` callback version such
    that you don't need to use `I.start(label)` and `I.end(label)`. It also
    returns the value, but that's optional. This also handles
    async/promises.
    
    Additionally added `using _ = I.track(label)` which uses the new `using`
    keyword and it will internally call `I.end(label)` whenever the variable
    `_` goes out of scope. This is useful in situations where you don't want
    an additionaly wrapping function, or where it's easy to forget to call
    `I.end(label)` if there are mutliple exit points (return) of a function.

--- packages/@tailwindcss-node/src/instrumentation.test.ts
+++ packages/@tailwindcss-node/src/instrumentation.test.ts
@@ -1,8 +1,9 @@
 import { stripVTControlCharacters } from 'util'
 import { expect, it } from 'vitest'
+import { dimensions } from '../../tailwindcss/src/utils/dimensions'
 import { Instrumentation } from './instrumentation'
 
-it('should add instrumentation', () => {
+it('should add instrumentation using start/end markers', () => {
   let I = new Instrumentation()
 
   I.start('Foo')
@@ -35,6 +36,80 @@ it('should add instrumentation', () => {
   })
 })
 
+it('should measure callbacks via the `span` api', () => {
+  let I = new Instrumentation()
+
+  I.span('Foo', () => {
+    let x = 1
+    for (let i = 0; i < 100; i++) {
+      I.span('Bar', () => {
+        x **= 2
+      })
+    }
+  })
+
+  expect.assertions(1)
+
+  I.report((output) => {
+    expect(stripVTControlCharacters(output).replace(/\[.*\]/g, '[0.xxms]')).toMatchInlineSnapshot(`
+      "
+      [0.xxms] Foo
+      [0.xxms]   ↳ Bar × 100
+      "
+    `)
+  })
+})
+
+it('should measure async callbacks via the `span` api', async () => {
+  let I = new Instrumentation()
+
+  await I.span('Foo', async () => {
+    let x = 1
+    for (let i = 0; i < 100; i++) {
+      I.span('Bar', () => {
+        x **= 2
+      })
+    }
+    await new Promise((r) => setTimeout(r, 500))
+  })
+
+  expect.assertions(1)
+
+  I.report((output) => {
+    expect(stabilize(output)).toMatchInlineSnapshot(`
+      "
+      [510.00ms] Foo
+      [  0.05ms]   ↳ Bar × 100
+      "
+    `)
+  })
+})
+
+it('should measure blocks until they go out of scope via `using`', () => {
+  let I = new Instrumentation()
+
+  {
+    using _ = I.track('Foo')
+
+    let x = 1
+    for (let i = 0; i < 100; i++) {
+      using _ = I.track('Bar')
+      x **= 2
+    }
+  }
+
+  expect.assertions(1)
+
+  I.report((output) => {
+    expect(stripVTControlCharacters(output).replace(/\[.*\]/g, '[0.xxms]')).toMatchInlineSnapshot(`
+      "
+      [0.xxms] Foo
+      [0.xxms]   ↳ Bar × 100
+      "
+    `)
+  })
+})
+
 it('should auto end pending timers when reporting', () => {
   let I = new Instrumentation()
 
@@ -59,3 +134,29 @@ it('should auto end pending timers when reporting', () => {
     `)
   })
 })
+
+let nf = new Intl.NumberFormat(undefined, {
+  style: 'decimal',
+  minimumFractionDigits: 2,
+})
+
+function stabilize(output: string) {
+  return stripVTControlCharacters(output).replace(/\[(\s*)(.*)\]/g, (_, whitespace, duration) => {
+    let [value, unit] = dimensions.get(duration.trim())!
+    return `[${whitespace}${nf.format(stableRound(value))}${unit}]`
+  })
+}
+
+function stableRound(value: number) {
+  if (value === 0) return 0
+
+  let sign = Math.sign(value)
+  let abs = Math.abs(value)
+
+  let magnitude = 10 ** Math.floor(Math.log10(abs))
+
+  let step = magnitude / 10
+  if (abs < 0.1) step = 0.05
+
+  return sign * Math.ceil(abs / step) * step
+}
